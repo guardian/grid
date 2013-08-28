d7cbf6fadb9217050d37c20042af9b288ee1ada0
Ben James <ben.james@guardian.co.uk>
2013-08-28 12:10:20 +0100

    dev-image-loader sub-project

--- project/Build.scala
+++ project/Build.scala
@@ -4,12 +4,19 @@ import play.Project._
 
 object Build extends Build {
 
+  val commonSettings = Seq(
+    scalaVersion := "2.10.2"
+  )
+
   lazy val root = sbt.Project("root", file("."))
-    .settings(scalaVersion in ThisBuild := "2.10.2")
+    .settings(commonSettings: _*)
     .aggregate(mediaApi)
 
   val mediaApi = play.Project("media-api", path = file("media-api"))
-    .settings(scalaVersion := "2.10.2")
+    .settings(commonSettings: _*)
+
+  val devImageLoader = sbt.Project("dev-image-loader", file("dev-image-loader"))
+    .settings(commonSettings: _*)
+
 
 }
-
