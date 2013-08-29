f013d376b201e94ad9803cb62513112835d55550
Ben James <ben.james@guardian.co.uk>
2013-08-29 14:45:44 +0100

    Elasticsearch install & start scripts

---
+++ elasticsearch/install_elasticsearch.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+ELASTICSEARCH_VERSION=0.90.3
+ELASTICSEARCH_DIR=$(dirname $0)
+
+if [ -d "$ELASTICSEARCH_DIR/elasticsearch" ]; then
+  echo "It looks like you've already downloaded and installed elastic search"
+  echo "Start it with $ELASTICSEARCH_DIR/start_elasticsearch.sh"
+  exit 0
+fi
+
+DOWNLOAD_URI="https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ELASTICSEARCH_VERSION.tar.gz"
+
+cd $ELASTICSEARCH_DIR
+
+if wget $DOWNLOAD_URI -O elasticsearch.tar.gz
+then
+    tar -zxf elasticsearch.tar.gz
+    mv elasticsearch-$ELASTICSEARCH_VERSION elasticsearch
+    rm elasticsearch/config/elasticsearch.yml
+    ln -s $(pwd)/elasticsearch.yml elasticsearch/config/
+    ./elasticsearch/bin/plugin install mobz/elasticsearch-head
+    ./elasticsearch/bin/plugin install elasticsearch/elasticsearch-cloud-aws/1.11.0
+    ./elasticsearch/bin/plugin -install lukas-vlcek/bigdesk
+    ./elasticsearch/bin/plugin -install karmi/elasticsearch-paramedic
+    echo "Done"
+    echo "Start it with $ELASTICSEARCH_DIR/start_elasticsearch.sh"
+else
+    "Failed to download Elastic Search"
+    exit 1
+fi
---
+++ elasticsearch/start_elasticsearch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ELASTICSEARCH_DIR=$(dirname $0)
+
+ES_OPTIONS="-Des.discovery.type=zen
+            -Des.index.number_of_replicas=0
+            -Des.gateway.recover_after_nodes=1
+            -Des.gateway.recover_after_time=5s
+            -Des.gateway.expected_nodes=1
+            -Des.discovery.zen.minimum_master_nodes=1"
+
+$ELASTICSEARCH_DIR/elasticsearch/bin/elasticsearch $ES_OPTIONS $*
+
+echo "Started. Try: http://localhost:9200/_plugin/head/ (may take a few seconds to become available)"
