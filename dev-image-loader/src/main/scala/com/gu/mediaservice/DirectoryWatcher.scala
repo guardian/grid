ce71f84e679436790af491ed53e600bdc23a002b
Ben James <ben.james@guardian.co.uk>
2013-09-02 16:14:24 +0100

    Interleave two streams of events

--- dev-image-loader/src/main/scala/com/gu/mediaservice/DirectoryWatcher.scala
+++ dev-image-loader/src/main/scala/com/gu/mediaservice/DirectoryWatcher.scala
@@ -17,6 +17,17 @@ case class FStream[A](run: Future[(A, FStream[A])])(implicit ex: ExecutionContex
 
   def unfold[B](f: A => Future[B]): FStream[B] =
     FStream(for ((a, as) <- run; b <- f(a)) yield (b, as.unfold(f)))
+
+  /** Interleave two streams, taking values from either as soon as they are available.
+    */
+  def nonDeterministicInterleave(that: FStream[A]): FStream[A] = {
+    def f(lefts: FStream[Either[A, A]], rights: FStream[Either[A, A]]): FStream[Either[A, A]] =
+      FStream(Future.firstCompletedOf(Seq(lefts.run, rights.run)) map {
+        case (left @ Left(_), ls)  => (left, ls.nonDeterministicInterleave(rights))
+        case (right @ Right(_), rs) => (right, rs.nonDeterministicInterleave(lefts))
+      })
+    f(this map Left.apply, that map Right.apply) map (_.merge)
+  }
 }
 
 object DirectoryWatcher {
