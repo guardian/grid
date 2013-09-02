36f8b4b09bdf0181a4ec38bb0973bf385f2df867
Ben James <ben.james@guardian.co.uk>
2013-09-02 13:47:15 +0100

    Prototype NIO-based directory watcher

--- dev-image-loader/src/main/scala/com/gu/mediaservice/DevImageLoader.scala
+++ dev-image-loader/src/main/scala/com/gu/mediaservice/DevImageLoader.scala
@@ -22,3 +22,47 @@ object DevImageLoader extends App {
   }
 
 }
+
+
+import scala.concurrent.Future
+import scala.collection.JavaConverters._
+import java.nio.file._
+import java.util.concurrent.Executors
+import StandardWatchEventKinds._
+
+object DirectoryWatcher {
+
+  case class FStream[A](run: Future[(A, FStream[A])]) {
+
+    def map[B](f: A => B): FStream[B] =
+      FStream(for ((a, as) <- run) yield (f(a), as.map(f)))
+
+    def foreach(f: A => Unit): Unit =
+      for ((a, as) <- run) { f(a); as.foreach(f) }
+  }
+
+  val threadPool = Executors.newFixedThreadPool(4)
+
+  implicit val ctx: ExecutionContext = ExecutionContext.fromExecutorService(threadPool)
+
+  def watch(path: String): FStream[List[WatchEvent[Path]]] = {
+    val watcher = FileSystems.getDefault.newWatchService
+    val watchedDir = Paths.get(path)
+    watchedDir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY)
+    var key: WatchKey = null
+    FStream {
+      for {
+        head <- Future {
+          key = watcher.take()
+          key.pollEvents().asScala.toList map (_.asInstanceOf[WatchEvent[Path]])
+        }
+        _ = key.reset()
+        tail = watch(path)
+      } yield (head, tail)
+    }
+  }
+
+  def shutdown() {
+    threadPool.shutdown()
+  }
+}
