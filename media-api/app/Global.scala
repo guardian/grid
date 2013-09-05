532957308aa62d7aa74757770438e2de020ebe3c
Ben James <ben.james@guardian.co.uk>
2013-09-05 17:41:52 +0100

    Media API controllers tidy-up

--- media-api/app/Global.scala
+++ media-api/app/Global.scala
@@ -1,5 +1,11 @@
+import lib.elasticsearch.ElasticSearch
 import play.api.{Application, GlobalSettings}
 
+
 object Global extends GlobalSettings {
+
+  override def beforeStart(app: Application) {
+    ElasticSearch.ensureIndexExists()
+  }
 
 }
---
+++ media-api/app/controllers/IndexManagement.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+import lib.elasticsearch.ElasticSearch
+
+
+object IndexManagement extends Controller {
+
+  def deleteIndex = Action {
+    ElasticSearch.deleteIndex()
+    ElasticSearch.ensureIndexExists()
+    Ok("Deleted and recreated index.\r\n")
+  }
+
+}
