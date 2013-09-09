c2b85068ce2451b62d91ab757fffc7379b990605
Ben James <ben.james@guardian.co.uk>
2013-09-09 16:28:53 +0100

    Throw in a chunk of dubious CloudFormation config

---
+++ elasticsearch/cloud-formation/create.sh
@@ -0,0 +1,6 @@
+cfn-create-stack media-service-elasticsearch-$1 \
+	--capabilities CAPABILITY_IAM \
+	--template-file cf.json \
+  --region eu-west-1 \
+	--parameters "Stage=$1" \
+	--disable-rollback
