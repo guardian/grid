3c61a13c3ea36ccd98abba0aae9726c37e493af0
Ben James <ben.james@guardian.co.uk>
2013-09-10 14:39:13 +0100

    Use 64-bit Ubuntu 12.04 image

--- elasticsearch/cloud-formation/create.sh
+++ elasticsearch/cloud-formation/create.sh
@@ -1,6 +1,6 @@
 cfn-create-stack media-service-elasticsearch-$1 \
-	--capabilities CAPABILITY_IAM \
-	--template-file cf.json \
-  --region eu-west-1 \
-	--parameters "Stage=$1" \
-	--disable-rollback
+    --capabilities CAPABILITY_IAM \
+    --template-file cf.json \
+    --region eu-west-1 \
+    --parameters "Stage=$1" \
+    --disable-rollback
