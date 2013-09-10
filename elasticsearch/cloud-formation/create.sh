c90070bdf7dc6cc5c62ed3a7283add03f72fe494
Ben James <ben.james@guardian.co.uk>
2013-09-10 16:21:20 +0100

    Usage hints for CloudFormation scripts

--- elasticsearch/cloud-formation/create.sh
+++ elasticsearch/cloud-formation/create.sh
@@ -1,4 +1,11 @@
-cfn-create-stack media-service-elasticsearch-$1 \
+STAGE=$1
+
+if ([ -z $STAGE ]); then
+  echo "usage: $0 <STAGE>"
+  exit 1
+fi
+
+cfn-create-stack media-service-elasticsearch-$STAGE \
     --capabilities CAPABILITY_IAM \
     --template-file cf.json \
     --region eu-west-1 \
