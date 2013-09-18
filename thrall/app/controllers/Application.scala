7b2e746923a66b0c2aad4bca14dfde62b2a81927
Ben James <ben.james@guardian.co.uk>
2013-09-18 15:05:55 +0100

    thrall: dummy health-check endpoint

--- thrall/app/controllers/Application.scala
+++ thrall/app/controllers/Application.scala
@@ -6,6 +6,10 @@ import play.api.mvc.{Action, Controller}
 object Application extends Controller {
 
   def index = Action {
+    Ok("This is a thrall.")
+  }
+
+  def healthCheck = Action {
     Ok("OK")
   }
 
