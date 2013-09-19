cf9ae2146eb2e2ba90d7bb7198901e786f2d770d
Ben James <ben.james@guardian.co.uk>
2013-09-19 10:56:00 +0100

    Remove unused code

--- dev-image-loader/src/main/scala/com/gu/mediaservice/DirectoryWatcher.scala
+++ dev-image-loader/src/main/scala/com/gu/mediaservice/DirectoryWatcher.scala
@@ -5,28 +5,12 @@ import scala.collection.JavaConverters._
 import java.nio.file._
 import java.util.concurrent.Executors
 
-import com.gu.mediaservice.util.FStream
-
 
 object DirectoryWatcher {
 
   val threadPool = Executors.newFixedThreadPool(4)
 
   implicit val ctx: ExecutionContext = ExecutionContext.fromExecutorService(threadPool)
-
-  def watchF(path: Path, kinds: WatchEvent.Kind[Path]*): FStream[List[WatchEvent[Path]]] = {
-
-    val watcher = FileSystems.getDefault.newWatchService
-    val key = path.register(watcher, kinds: _*)
-
-    FStream.continually(Future {
-      assert(key.isValid)
-      watcher.take()
-      val events = key.pollEvents.asScala.toList map (_.asInstanceOf[WatchEvent[Path]])
-      key.reset()
-      events
-    })
-  }
 
   def watch(path: Path, kinds: WatchEvent.Kind[Path]*)(f: WatchEvent[Path] => Unit) {
     val watcher = FileSystems.getDefault.newWatchService
