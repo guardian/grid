a323b9ef5e2386be8f339c47bb67be08e86be47e
Ben James <ben.james@guardian.co.uk>
2013-10-01 17:39:53 +0100

    Remove redundant code

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -1,12 +1,8 @@
 package controllers
 
-import scala.concurrent.Future
-
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits._
-import play.api.Logger
 
-import lib.imaging.ImageMetadata
 import lib.storage.{NullStorage, StorageBackend}
 import lib.elasticsearch.ElasticSearch
 import play.api.libs.json.{JsArray, JsObject}
@@ -36,20 +32,6 @@ abstract class MediaApiController extends Controller {
     ElasticSearch.search(term) map { hits =>
       Ok(JsObject(Seq("hits" -> JsArray(hits))))
     }
-  }
-
-  def putImage(id: String) = Action.async(parse.temporaryFile) { request =>
-    val tempFile = request.body
-
-    val response = for {
-      meta <- Future { ImageMetadata.iptc(tempFile.file) }
-      uri  <- storage.store(tempFile.file)
-      image = model.Image(id, uri, meta)
-      _    <- ElasticSearch.indexImage(image)
-    } yield NoContent
-
-    response.onComplete(_ => tempFile.clean())
-    response
   }
 
 }
