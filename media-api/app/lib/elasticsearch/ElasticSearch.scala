c9c709839faa29dd70e349ff8778b4483d734624
Ben James <ben.james@guardian.co.uk>
2013-10-01 17:39:53 +0100

    Remove redundant code

--- media-api/app/lib/elasticsearch/ElasticSearch.scala
+++ media-api/app/lib/elasticsearch/ElasticSearch.scala
@@ -3,7 +3,6 @@ package lib.elasticsearch
 import scala.concurrent.{ExecutionContext, Future}
 
 import play.api.libs.json.JsValue
-import org.elasticsearch.action.index.IndexResponse
 import org.elasticsearch.action.search.SearchRequestBuilder
 import org.elasticsearch.index.query.QueryBuilders._
 import scalaz.syntax.id._
@@ -11,18 +10,13 @@ import scalaz.syntax.id._
 import com.gu.mediaservice.syntax._
 import com.gu.mediaservice.lib.elasticsearch.ElasticSearchClient
 import lib.Config
-import model.Image
+
 
 object ElasticSearch extends ElasticSearchClient {
 
   val host = Config.elasticsearchHost
   val port = Config.int("es.port")
   val cluster = Config("es.cluster")
-
-  def indexImage(image: Image): Future[IndexResponse] =
-    client.prepareIndex(imagesIndex, imageType, image.id)
-      .setSource(image.asJson)
-      .execute.asScala
 
   def getImageById(id: String)(implicit ex: ExecutionContext): Future[Option[JsValue]] =
     client.prepareGet(imagesIndex, imageType, id).execute.asScala map (_.sourceOpt)
