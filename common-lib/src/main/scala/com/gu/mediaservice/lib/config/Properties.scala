6395aa0dec232a81ced7bcb0abd3c36ad7acc9dc
Ben James <ben.james@guardian.co.uk>
2013-10-04 17:30:38 +0100

    Loader -> API integration test

--- common-lib/src/main/scala/com/gu/mediaservice/lib/config/Properties.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/config/Properties.scala
@@ -1,17 +1,21 @@
 package com.gu.mediaservice.lib.config
 
-import java.io.FileInputStream
+import java.io.{InputStream, FileInputStream}
+import java.net.URL
 import scala.collection.JavaConverters._
 
 
 object Properties {
 
-  def fromFile(file: String): Map[String, String] = {
+  def fromFile(file: String): Map[String, String] =
+    fromStream(new FileInputStream(file))
+
+  def fromURL(url: URL): Map[String, String] =
+    fromStream(url.openStream)
+
+  def fromStream(stream: InputStream): Map[String, String] = {
     val props = new java.util.Properties
-    val is = new FileInputStream(file)
-    props.load(is)
-    is.close()
+    try props.load(stream) finally stream.close()
     props.asScala.toMap
   }
-
 }
---
+++ integration/src/test/scala/com/gu/mediaservice/integration/Config.scala
@@ -0,0 +1,19 @@
+package com.gu.mediaservice.integration
+
+import com.gu.mediaservice.lib.config.Properties
+import java.net.{URLEncoder, URL}, URLEncoder.encode
+
+
+object Config {
+
+  val properties = Properties.fromURL(getClass.getResource("/integration.properties"))
+
+  val loaderApi: URL = new URL(properties("loader.api.url"))
+
+  val mediaApi: URL = new URL(properties("media.api.url"))
+
+  def imageLoadEndpoint(id: String): URL = new URL(loaderApi, "image/" + encode(id, "UTF-8"))
+
+  def imageEndpoint(id: String): URL = new URL(mediaApi, "image/" + encode(id, "UTF-8"))
+
+}
