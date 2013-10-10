28dbe9e3d61d0e2ea33539e3042f4d3cfc1941f7
Ben James <ben.james@guardian.co.uk>
2013-10-10 16:44:36 +0100

    Fix ES dev start script by overriding server config

--- elasticsearch/dev-start.sh
+++ elasticsearch/dev-start.sh
@@ -7,7 +7,10 @@ ES_OPTIONS="-Des.discovery.type=zen
             -Des.gateway.recover_after_nodes=1
             -Des.gateway.recover_after_time=5s
             -Des.gateway.expected_nodes=1
-            -Des.discovery.zen.minimum_master_nodes=1"
+            -Des.discovery.zen.minimum_master_nodes=1
+            -Des.path.data=data
+            -Des.path.logs=logs
+            -Des.path.conf=config"
 
 $ELASTICSEARCH_DIR/elasticsearch/bin/elasticsearch $ES_OPTIONS $*
 
