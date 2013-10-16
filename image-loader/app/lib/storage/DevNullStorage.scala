0a66f870b34613d70206c656f65155f092fd0b11
Ben James <ben.james@guardian.co.uk>
2013-10-16 16:52:43 +0100

    Offload S3 uploads from Play's thread pool

--- image-loader/app/lib/storage/DevNullStorage.scala
+++ image-loader/app/lib/storage/DevNullStorage.scala
@@ -1,8 +1,9 @@
 package lib.storage
 
 import java.io.File
+import scala.concurrent.Future
 
 
 object DevNullStorage extends StorageBackend {
-  def store(id: String, file: File) = new File("/dev/null").toURI
+  def store(id: String, file: File) = Future.successful(new File("/dev/null").toURI)
 }
