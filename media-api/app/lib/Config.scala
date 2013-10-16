890cf45aae9fff8b2229a1700522eb66908cbaeb
Ben James <ben.james@guardian.co.uk>
2013-10-16 10:46:06 +0100

    Undo inheritance for media-api properties-based config

--- media-api/app/lib/Config.scala
+++ media-api/app/lib/Config.scala
@@ -5,10 +5,18 @@ import com.amazonaws.auth.{BasicAWSCredentials, AWSCredentials}
 import scalaz.syntax.id._
 
 import com.gu.mediaservice.lib.elasticsearch.EC2._
-import com.gu.mediaservice.lib.config.{PropertiesConfig, CommonPlayAppConfig}
+import com.gu.mediaservice.lib.config.{Properties, CommonPlayAppConfig}
 
 
-object Config extends PropertiesConfig("media-api") with CommonPlayAppConfig {
+object Config extends CommonPlayAppConfig {
+
+  val properties = Properties.fromFile("/etc/gu/media-api.properties")
+
+  val awsCredentials: AWSCredentials =
+    new BasicAWSCredentials(properties("aws.id"), properties("aws.secret"))
+
+  val ec2Client: AmazonEC2Client =
+    new AmazonEC2Client(awsCredentials) <| (_ setEndpoint awsEndpoint)
 
   val elasticsearchHost: String =
     if (stage == "DEV")
@@ -16,14 +24,8 @@ object Config extends PropertiesConfig("media-api") with CommonPlayAppConfig {
     else
       findElasticsearchHost(ec2Client, Map("Stage" -> Seq(stage), "Role" -> Seq(elasticsearchRole)))
 
-  lazy val awsCredentials: AWSCredentials =
-    new BasicAWSCredentials(properties("aws.id"), properties("aws.secret"))
-
   val s3Bucket: String = properties("s3.bucket")
 
   val topicArn: String = properties("sns.topic.arn")
-
-  private lazy val ec2Client: AmazonEC2Client =
-    new AmazonEC2Client(awsCredentials) <| (_ setEndpoint awsEndpoint)
 
 }
