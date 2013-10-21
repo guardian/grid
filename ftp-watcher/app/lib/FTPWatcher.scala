82516391f242c38c72bc1e07eaf4ffee12e1e618
Ben James <ben.james@guardian.co.uk>
2013-10-21 16:54:02 +0100

    Refactor

--- ftp-watcher/app/lib/FTPWatcher.scala
+++ ftp-watcher/app/lib/FTPWatcher.scala
@@ -5,35 +5,48 @@ import java.io.InputStream
 
 import scalaz.syntax.bind._
 import scalaz.concurrent.Task
-import scalaz.stream.Process
+import scalaz.stream.{process1, Process}
 import scalaz.stream.Process._
 import scalaz.stream.processes.resource
 
 
 object FTPWatcher {
 
-  def watchDir(host: String, user: String, password: String, dir: FilePath, emptySleep: Long): Process[Task, File] =
-    resource(initClient(host, user, password, dir))(_.quit)(client =>
-      listNextFile(client, emptySleep) map (client -> _))
-      .flatMap { case (client, path) => retrieveFile(client, path).map(File(path, _)) }
+  import Processes._
 
-  private def initClient(host: String, user: String, password: String, dir: FilePath): Task[Client] = {
+  /** Produces a stream of `File`s, each exposing an `InputStream`.
+    *
+    * This stream is safe to use within the context of safe stream combinators, e.g. by sending it
+    * to a `Sink`.
+    *
+    * The stream will be closed, and the file deleted from the server, once the `File` element
+    * has been consumed.
+    */
+  def watchDir(host: String, user: String, password: String, dir: FilePath): Process[Task, File] = {
     val client = new Client
+    val filePaths = resource(
+      acquire = initClient(client, host, user, password, dir))(
+      release = _ => client.quit)(
+      step = _ => listFiles(client, emptySleep = 1000))
+    filePaths |> unChunk >>= (path => retrieveFile(client, path).map(File(path, _)))
+  }
+
+  private def initClient(client: Client, host: String, user: String, password: String, dir: FilePath): Task[Unit] =
     client.connect(host, 21) >>
       client.login(user, password) >>
       client.cwd(dir) >>
       client.enterLocalPassiveMode >>
-      client.setBinaryFileType >|
-      client
-  }
+      client.setBinaryFileType
 
-  private def listNextFile(client: Client, emptySleep: Long): Task[FilePath] =
-    client.listFiles.flatMap(_.headOption.map(file => Task.now(file.getName))
-      .getOrElse(sleep(emptySleep) >> listNextFile(client, emptySleep)))
+  private def listFiles(client: Client, emptySleep: Long, batchSize: Int = 10): Task[Seq[FilePath]] =
+    client.listFiles.flatMap {
+      case Nil   => sleep(emptySleep) >> listFiles(client, emptySleep)
+      case files => Task.now(files.take(batchSize).map(_.getName))
+    }
 
   private def retrieveFile(client: Client, file: FilePath): Process[Task, InputStream] =
-    Processes.resource1(client.retrieveFile(file))(
-      stream => Task.delay(stream.close()) >> client.completePendingCommand >> client.delete(file))
+    resource1(client.retrieveFile(file))(stream =>
+      Task.delay(stream.close()) >> client.completePendingCommand >> client.delete(file))
 
   private def sleep(millis: Long): Task[Unit] = Task(Thread.sleep(millis))
 
@@ -45,6 +58,9 @@ object Processes {
 
   def resource1[O](acquire: Task[O])(release: O => Task[Unit]): Process[Task, O] =
     await(acquire)(o => emit(o) ++ suspend(eval(release(o))).drain)
+
+  def unChunk[O]: Process1[Seq[O], O] =
+    process1.id[Seq[O]].flatMap(emitAll)
 
 }
 
