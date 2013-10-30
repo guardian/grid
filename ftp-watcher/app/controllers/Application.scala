b880c30c62e9219282c4d8fee57722f96d650f2d
Ben James <ben.james@guardian.co.uk>
2013-10-30 18:14:31 +0000

    FTP watcher periodically reconnects
    This is to adapt to DNS failovers.

--- ftp-watcher/app/controllers/Application.scala
+++ ftp-watcher/app/controllers/Application.scala
@@ -7,7 +7,7 @@ import scalaz.syntax.bind._
 
 import play.api.Logger
 import play.api.mvc.{Action, Controller}
-import lib.{Sinks, File, Config, FTPWatcher}
+import lib.{Sinks, Config, FTPWatcher}
 
 
 object Application extends Controller {
@@ -15,7 +15,7 @@ object Application extends Controller {
   import play.api.libs.concurrent.Execution.Implicits._
 
   def index = Action {
-    Ok("This is an FTP Watcher.\r\n")
+    Ok("This is an FTP Watcher.\n")
   }
 
   import FTPWatchers.future
@@ -36,7 +36,7 @@ object Application extends Controller {
     val active = request.getQueryString("active") map (_.toBoolean)
     active match {
       case Some(b) =>
-        Config.passive.set(! b)
+        Config.active.set(b)
         Logger.info("Mode set to " + Config.status)
         NoContent
       case None =>
@@ -48,45 +48,49 @@ object Application extends Controller {
 
 object FTPWatchers {
 
-  import scalaz.stream.{Process, wye}
+  import scalaz.stream.wye
   import scalaz.concurrent.Task
 
   private implicit val ctx: ExecutionContext =
     ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor)
 
-  def watchers =
+  val watchers =
     for (path <- Config.ftpPaths)
     yield FTPWatcher(Config.ftpHost, Config.ftpUser, Config.ftpPassword, path)
 
-  def stream = watchers.map(_.watchDir)
-    .foldLeft[Process[Task, File]](Process.halt)((p1, p2) => p1.wye(p2)(wye.merge))
-
-  def watcherTask(batchSize: Int): Task[Unit] = {
+  def watcherTask(batchSize: Int = 10): Task[Unit] = {
+    val stream = watchers.map(_.watchDir(batchSize)).reduceLeft((p1, p2) => p1.wye(p2)(wye.merge))
     val sink = Sinks.httpPost(Config.imageLoaderUri)
-    stream.take(batchSize).to(sink).run
+    stream.to(sink).run
   }
 
   def waitForActive(sleep: Long): Task[Unit] =
-    Task(Config.isActive).ifM(Task.now(()),
-      Task(Thread.sleep(sleep)) >> waitForActive(sleep))
+    Task(Config.isActive).ifM(Task.now(()), Task(Thread.sleep(sleep)) >> waitForActive(sleep))
 
   /** When running, this starts the watcher process immediately if the
     * `active` atomic variable is set to `true`.
-    * 
+    *
     * Otherwise, it sleeps, waking periodically to check `active` again.
-    * 
-    * Once `active` is `true`, the watcher process runs, checking at intervals
-    * whether `active` is still `true`, and stopping if it becomes `false`.
+    *
+    * Once `active` is `true`, the watcher process runs, checking at
+    * intervals whether `active` is still `true`, and stopping if it becomes
+    * `false`.
     */
   lazy val future: Future[Unit] = _future
 
+  private val cancel: AtomicBoolean = new AtomicBoolean(false)
+
+  def shutdown() {
+    cancel.set(true)
+  }
+
   private def _future: Future[Unit] =
-    retryFuture("FTP watcher", 3000) {
-      val task = waitForActive(sleep = 250) >> watcherTask(batchSize = 10)
+    retryFuture("FTP watcher", 10000) {
+      val task = waitForActive(sleep = 250) >> watcherTask()
       val promise = Promise[Unit]()
-      task.runAsync(_.fold(promise.failure, promise.success))
-      promise.future
-    }.flatMap(_ => _future)
+      task.runAsyncInterruptibly(_.fold(promise.failure, promise.success), cancel)
+      promise.future.flatMap(_ => _future) // promise.future >> _future
+    }
 
   def retryFuture[A](desc: String, wait: Long)(future: => Future[A]): Future[A] =
     future.recoverWith {
