9391cba6ef78788e28fcea6344501377801e3bbd
Ben James <ben.james@guardian.co.uk>
2013-11-06 11:07:24 +0000

    Move stage name in namespace

--- media-api/app/lib/MediaApiMetrics.scala
+++ media-api/app/lib/MediaApiMetrics.scala
@@ -3,7 +3,7 @@ package lib
 import com.gu.mediaservice.lib.metrics.CloudWatchMetrics
 import Config.{awsCredentials, stage}
 
-object MediaApiMetrics extends CloudWatchMetrics(s"$stage/MediaService/MediaApi", awsCredentials) {
+object MediaApiMetrics extends CloudWatchMetrics(s"MediaService/$stage/MediaApi", awsCredentials) {
 
   val searchQueries = new TimeMetric("ElasticSearch", Seq("QueryType" -> "Search"))
 
