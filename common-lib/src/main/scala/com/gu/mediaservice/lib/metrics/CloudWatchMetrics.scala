b36b3e658b9c4fdaadd2332faf770af78997b14d
Ben James <ben.james@guardian.co.uk>
2013-11-06 10:54:20 +0000

    Report search query times to CloudWatch

--- common-lib/src/main/scala/com/gu/mediaservice/lib/metrics/CloudWatchMetrics.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/metrics/CloudWatchMetrics.scala
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory
 
 import com.amazonaws.auth.AWSCredentials
 import com.amazonaws.services.cloudwatch.{AmazonCloudWatch, AmazonCloudWatchClient}
-import com.amazonaws.services.cloudwatch.model.{StandardUnit, MetricDatum, PutMetricDataRequest}
+import com.amazonaws.services.cloudwatch.model.{Dimension, StandardUnit, MetricDatum, PutMetricDataRequest}
 
 import scalaz.concurrent.Task
 import scalaz.stream.async, async.mutable.Topic
@@ -13,11 +13,19 @@ import scalaz.stream.Process.{Sink, constant, emitAll}
 import scalaz.syntax.id._
 
 
-trait Metrics {
+trait Metric[A] {
 
-  def namespace: String
-  def credentials: AWSCredentials
+  def recordOne(a: A): Unit
 
+  def recordMany(as: Seq[A]): Unit
+
+}
+
+abstract class CloudWatchMetrics(namespace: String, credentials: AWSCredentials) {
+
+  /** The number of data points to report in one batch
+    * (Each batch costs 1 HTTP request to CloudWatch)
+    */
   val chunkSize: Int = 10
 
   final val topic: Topic[MetricDatum] = async.topic[MetricDatum]
@@ -34,15 +42,21 @@ trait Metrics {
   private def putData(data: Seq[MetricDatum]): Task[Unit] = Task {
     client.putMetricData(new PutMetricDataRequest()
       .withNamespace(namespace)
-      .withMetricData(data.asJavaCollection))
+      .withMetricData(data.asJava))
   }
 
-  class CountMetric(name: String) extends Metric[Long](name) {
+  class CountMetric(name: String, dimensions: Seq[(String, String)] = Nil)
+    extends CloudWatchMetric[Long](name, dimensions) {
 
-    protected def toDatum(a: Long) =
-      new MetricDatum().withMetricName(name).withUnit(StandardUnit.Count).withValue(a.toDouble)
+    protected def toDatum(a: Long) = datum(StandardUnit.Count, a)
 
     def increment(): Unit = recordOne(1)
+  }
+
+  class TimeMetric(name: String, dimensions: Seq[(String, String)] = Nil)
+    extends CloudWatchMetric[Long](name, dimensions) {
+
+    protected def toDatum(a: Long) = datum(StandardUnit.Milliseconds, a)
   }
 
   import scalaz.{\/, -\/, \/-}
@@ -51,7 +65,8 @@ trait Metrics {
     case \/-(_) =>
   }
 
-  abstract class Metric[A](val name: String) {
+  abstract class CloudWatchMetric[A](val name: String, val dimensions: Seq[(String, String)] = Nil)
+    extends Metric[A] {
 
     final def recordOne(a: A): Unit =
       topic.publishOne(toDatum(a).withTimestamp(new java.util.Date)).runAsync(loggingErrors)
@@ -62,6 +77,17 @@ trait Metrics {
 
     /** Must be implemented to provide a way to turn an `A` into a `MetricDatum` */
     protected def toDatum(a: A): MetricDatum
+
+    protected val _dimensions: java.util.Collection[Dimension] =
+      dimensions.map { case (k, v) => new Dimension().withName(k).withValue(v) }.asJava
+
+    /** Convenience method for instantiating a `MetricDatum` with this metric's `name` and `dimension` */
+    protected def datum(unit: StandardUnit, value: Double): MetricDatum =
+      new MetricDatum()
+        .withMetricName(name)
+        .withUnit(unit)
+        .withValue(value)
+        .withDimensions(_dimensions)
 
   }
 
