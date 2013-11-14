d91ebd94fbcf1e52a6987ab239420c704b0d1320
Sébastien Cevey <seb.cevey@guardian.co.uk>
2013-11-14 12:27:09 +0000

    Drop lazy val, just inline the config lookup

--- media-api/app/lib/CorsFilter.scala
+++ media-api/app/lib/CorsFilter.scala
@@ -4,11 +4,10 @@ import lib.Config
 object CorsFilter extends Filter {
   import scala.concurrent._
   import ExecutionContext.Implicits.global
-  lazy val allowedDomain = Config.corsAllowedDomain
 
   def apply(f: (RequestHeader) => Future[SimpleResult])(request: RequestHeader): Future[SimpleResult] = {
     f(request).map { _.withHeaders(
-      "Access-Control-Allow-Origin" -> allowedDomain
+      "Access-Control-Allow-Origin" -> Config.corsAllowedDomain
     ) }
   }
 
