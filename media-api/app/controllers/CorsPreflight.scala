45c1c0762df28c88d2698423a32a0cd8575c9d4b
Sébastien Cevey <seb.cevey@guardian.co.uk>
2013-11-14 12:34:07 +0000

    Tidy up

--- media-api/app/controllers/CorsPreflight.scala
+++ media-api/app/controllers/CorsPreflight.scala
@@ -1,16 +1,19 @@
 package controllers
 
 import play.api.mvc._
+import scala.concurrent.duration._
 
 object CorsPreflight extends Controller {
+
+  // cache preflight access control response for one day
+  val maxAge = 1.day.toSeconds.toString
 
   // Handle CORS preflight request
   def options(url: String) = Action {
     Ok("").withHeaders(
       "Access-Control-Allow-Methods" -> "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers" -> "Content-Type, X-Requested-With, Accept",
-      // cache access control response for one day
-      "Access-Control-Max-Age" -> (60 * 60 * 24).toString
+      "Access-Control-Max-Age" -> maxAge
     )
   }
 
