82a9ad4ce2c8f4d1fec071736e10bdbf3d2ed56c
Sébastien Cevey <seb.cevey@guardian.co.uk>
2013-11-15 14:49:44 +0000

    Add routing to page with full image

--- kahuna/public/js/app.js
+++ kahuna/public/js/app.js
@@ -1,30 +1,60 @@
 // TODO: Grunt: compile almond + app
 // TODO: Grunt: hash dependencies? or ETag?
+// TODO: Load templates using AMD so they can be compiled in
 
 require.config({
     baseUrl: '/assets',
     paths: {
-        angular: 'bower_components/angular/angular.min'
-        // angular: 'dist/angular.1234.min'
+        'angular':       'bower_components/angular/angular.min',
+        'angular-route': 'bower_components/angular-route/angular-route.min'
     },
     shim: {
-        angular: {exports: 'angular'}
+        'angular': {exports: 'angular'},
+        'angular-route': ['angular']
     }
 });
 
-require(['angular'], function(angular) {
+require([
+    'angular',
+    'angular-route'
+], function(
+    angular,
+    angularRoute
+) {
     var mainScript = document.getElementById('main-script');
     var config = {
         mediaApiUri: mainScript.getAttribute('data-media-api-uri')
     };
 
-    var kahuna = angular.module('kahuna', []);
+    var kahuna = angular.module('kahuna', [
+        'ngRoute'
+    ]);
 
 
     // Inject configuration values into the app
     angular.forEach(config, function(value, key) {
         kahuna.constant(key, value);
     });
+
+    kahuna.config(['$locationProvider', '$routeProvider',
+                   function($locationProvider, $routeProvider) {
+
+        // Use real URLs (with History API) instead of hashbangs
+        $locationProvider.html5Mode(true).hashPrefix('!');
+
+
+        var templatesDirectory = '/assets/templates';
+        $routeProvider.when('/', {
+            templateUrl: templatesDirectory + '/search.html',
+            controller: 'SearchCtrl'
+        });
+
+        $routeProvider.when('/images/:imageId', {
+            templateUrl: templatesDirectory + '/image.html',
+            controller: 'ImageCtrl'
+        });
+    }]);
+
 
     kahuna.factory('mediaApi',
                    ['$http', 'mediaApiUri',
@@ -44,6 +74,12 @@ require(['angular'], function(angular) {
             });
         }
 
+        function find(id) {
+            return $http.get(mediaApiUri + '/images/' + id).then(function(response) {
+                return response.data;
+            });
+        }
+
         function listBuckets() {
             return $http.get(mediaApiUri + '/buckets').then(function(response) {
                 return response.data.buckets;
@@ -52,11 +88,12 @@ require(['angular'], function(angular) {
 
         return {
             search: search,
+            find: find,
             listBuckets: listBuckets
         };
     }]);
 
-    kahuna.controller('TestCtrl',
+    kahuna.controller('SearchCtrl',
                       ['$scope', 'mediaApi',
                        function($scope, mediaApi) {
 
@@ -80,8 +117,17 @@ require(['angular'], function(angular) {
 
     }]);
 
-// TODO: router to catch navigation to image
-// TODO: show full image + metadata
+    kahuna.controller('ImageCtrl',
+                      ['$scope', '$routeParams', 'mediaApi',
+                       function($scope, $routeParams, mediaApi) {
+
+        var imageId = $routeParams.imageId;
+
+        mediaApi.find(imageId).then(function(image) {
+            $scope.image = image;
+        });
+
+    }]);
 
     angular.bootstrap(document, ['kahuna']);
 });
