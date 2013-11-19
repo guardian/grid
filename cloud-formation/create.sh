115dd7d14f8800138a8ee1a40c676b4c66388983
Ben James <ben.james@guardian.co.uk>
2013-11-19 16:56:37 +0000

    Enable SSL on Media API load balancer

--- cloud-formation/create.sh
+++ cloud-formation/create.sh
@@ -1,7 +1,8 @@
 STAGE=$1
+MEDIA_API_CERT=$2
 
 if ([ -z $STAGE ]); then
-  echo "usage: $0 <STAGE>"
+  echo "usage: $0 <STAGE> <Media API SSL certificate ARN>"
   exit 1
 fi
 
@@ -11,5 +12,5 @@ cfn-create-stack media-service-$STAGE \
     --capabilities CAPABILITY_IAM \
     --template-file $TEMPLATE \
     --region eu-west-1 \
-    --parameters "Stage=$STAGE" \
+    --parameters "Stage=$STAGE;MediaApiSSLCertificateId=$MEDIA_API_CERT" \
     --disable-rollback
