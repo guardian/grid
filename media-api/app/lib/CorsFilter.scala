9fe391b354fa99fff5cb09ecf46ce213eefe22c7
Ben James <ben.james@guardian.co.uk>
2013-11-20 13:50:56 +0000

    Fix Access-Control-Allow-Origin header for new DNS

--- media-api/app/lib/CorsFilter.scala
+++ media-api/app/lib/CorsFilter.scala
@@ -6,8 +6,14 @@ object CorsFilter extends Filter {
   import ExecutionContext.Implicits.global
 
   def apply(f: (RequestHeader) => Future[SimpleResult])(request: RequestHeader): Future[SimpleResult] = {
+
+    val requestProtocol =
+      request.headers.get("X-Forwarded-Proto").map(_.toLowerCase).getOrElse("http")
+
+    val corsAllowOrigin = s"$requestProtocol://${Config.corsAllowedDomain}"
+
     f(request).map { _.withHeaders(
-      "Access-Control-Allow-Origin" -> Config.corsAllowedDomain
+      "Access-Control-Allow-Origin" -> corsAllowOrigin
     ) }
   }
 
