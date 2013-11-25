029e8708ca0f4e38f7ed9f291d227ba8e1735180
Ben James <ben.james@guardian.co.uk>
2013-11-25 13:54:49 +0000

    Generalise User to Principal
    So that we can start to distinguish between "users" and other service
    clients.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/auth/Authentication.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/auth/Authentication.scala
@@ -1,20 +1,24 @@
 package com.gu.mediaservice.lib.auth
 
 import play.api.mvc.Security.AuthenticatedBuilder
-
 import play.api.mvc._
 import play.api.libs.json.Json
 
-
 object Authenticated {
-  def apply(onUnauthorized: RequestHeader => SimpleResult): AuthenticatedBuilder[User] =
-    AuthenticatedBuilder(req => User.fromRequest(req), onUnauthorized)
+  def apply(onUnauthorized: RequestHeader => SimpleResult): AuthenticatedBuilder[Principal] =
+    AuthenticatedBuilder(Principal.fromRequest, onUnauthorized)
 }
 
-case class User(openid: String, email: String, firstName: String, lastName: String) {
-  def fullName = firstName + " " + lastName
+sealed trait Principal {
+  def name: String
+}
+
+case class User(openid: String, email: String, firstName: String, lastName: String) extends Principal {
+  def name = s"$firstName $lastName"
   def emailDomain = email.split("@").last
 }
+
+case class ServicePeer(name: String) extends Principal
 
 object User {
   val KEY = "identity"
@@ -23,4 +27,17 @@ object User {
   def writeJson(id: User) = Json.stringify(Json.toJson(id))
   def fromRequest(request: RequestHeader): Option[User] =
     request.session.get(KEY).map(User.readJson)
+}
+
+object Principal {
+
+  /** As a workaround until services have API keys, assumes that any
+    * non-HTTPS or non-load-balanced traffic is from internal trusted clients.
+    */
+  def fromRequest(request: RequestHeader): Option[Principal] =
+    request.headers.get("X-Forwarded-Proto") match {
+      case Some("https") | None => User.fromRequest(request)
+      case Some("http")         => Some(ServicePeer("Trusted internal service"))
+      case Some(_)              => None
+    }
 }
