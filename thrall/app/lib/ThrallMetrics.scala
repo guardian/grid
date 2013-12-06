7d0122ef11f0322251b43f25ceb01cdcb0773d44
Ben James <ben.james@guardian.co.uk>
2013-12-06 14:17:50 +0000

    Shorten metric namespaces
    "MediaService" seems redundant, anyway.

--- thrall/app/lib/ThrallMetrics.scala
+++ thrall/app/lib/ThrallMetrics.scala
@@ -3,7 +3,7 @@ package lib
 import com.gu.mediaservice.lib.metrics.CloudWatchMetrics
 import Config.{awsCredentials, stage}
 
-object ThrallMetrics extends CloudWatchMetrics(s"MediaService/$stage/Thrall", awsCredentials) {
+object ThrallMetrics extends CloudWatchMetrics(s"$stage/Thrall", awsCredentials) {
 
   val indexedImages = new CountMetric("IndexedImages")
 
