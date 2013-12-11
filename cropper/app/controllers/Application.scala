f8c539292cf7d5b231c4b80b833fea5d8853667d
Ben James <ben.james@guardian.co.uk>
2013-12-11 11:06:27 +0000

    Store cropped image in S3

--- cropper/app/controllers/Application.scala
+++ cropper/app/controllers/Application.scala
@@ -3,14 +3,13 @@ package controllers
 import java.net.URI
 import scala.concurrent.Future
 
+import play.api.mvc.{Action, Controller}
+import play.api.libs.concurrent.Execution.Implicits._
 import scalaz.syntax.all._
 import scalaz.std.list._
 import scalaz.{Failure, Success}
 import com.gu.mediaservice.syntax._
-import lib.{Bounds, Crops}
-
-import play.api.mvc.{Action, Controller}
-import play.api.libs.concurrent.Execution.Implicits._
+import lib.{Storage, Bounds, Crops}
 
 object Application extends Controller {
 
@@ -21,17 +20,20 @@ object Application extends Controller {
   def crop = Action.async { req =>
 
     val cropFile =
-      (req.queryParam[URI]("source") |@|
-       req.queryParam[Int]("x") |@|
-       req.queryParam[Int]("y") |@|
-       req.queryParam[Int]("w") |@|
-       req.queryParam[Int]("h")) {
-        (source, x, y, width, height) => Crops.crop(source.toURL, Bounds(x, y, width, height))
+      ( req.queryParam[URI]("source")
+    |@| req.queryParam[Int]("x")
+    |@| req.queryParam[Int]("y")
+    |@| req.queryParam[Int]("w")
+    |@| req.queryParam[Int]("h")
+      ) {
+        (source, x, y, width, height) =>
+          Crops.crop(source, Bounds(x, y, width, height))
+            .flatMap(file => Storage.store("foo", file) <| (_.onComplete { case _ => file.delete() }))
       }
 
     cropFile match {
-      case Success(file) =>
-        file map (f => Ok(s"Saved crop to file: ${f.getAbsolutePath}"))
+      case Success(cropUri) =>
+        cropUri map (f => Ok(s"Saved crop to file: $f"))
       case Failure(errors) =>
         Future.successful(BadRequest("Bad request: " + errors.toList.mkString(", ")))
     }
