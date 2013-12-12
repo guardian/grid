3180471b07bd212cc4e0399941720915497cf1da
Ben James <ben.james@guardian.co.uk>
2013-12-12 13:00:45 +0000

    Produce crops from Media API URIs

--- cropper/app/controllers/Application.scala
+++ cropper/app/controllers/Application.scala
@@ -1,15 +1,16 @@
 package controllers
 
-import java.net.URI
+import java.net.{URL, URI}
 import scala.concurrent.Future
 
 import play.api.data._, Forms._
 import play.api.mvc.{Action, Controller}
-import play.api.libs.json.{Json, JsValue}
+import play.api.libs.json._
 import play.api.libs.concurrent.Execution.Implicits._
 import scalaz.syntax.id._
 
 import lib.{Storage, Bounds, Crops}
+import play.api.libs.ws.WS
 
 object Application extends Controller {
 
@@ -24,18 +25,34 @@ object Application extends Controller {
 
     boundsForm.bindFromRequest()(req).fold(
       errors => Future.successful(BadRequest(errors.errorsAsJson)),
-      { case (source, x, y, width, height) =>
-        for {
-          file <- Crops.crop(new URI(source), Bounds(x, y, width, height))
-          uri  <- Storage.store("foo", file) <| (_.onComplete { case _ => file.delete })
-        } yield Ok(cropUriResponse(uri))
+      {
+        case (source, x, y, w, h) =>
+          val actualSource = nonHttpsUri(new URI(source)).toURL.toExternalForm
+          for {
+            apiResp <- WS.url(actualSource).get
+            SourceImage(id, file) = apiResp.json.as[SourceImage]
+            cropFilename = s"$id/${x}_${y}_${w}_$h.jpg"
+            file <- Crops.crop(new URI(file), Bounds(x, y, w, h))
+            uri  <- Storage.store(cropFilename, file) <| (_.onComplete { case _ => file.delete })
+          } yield Ok(cropUriResponse(uri))
       }
     )
 
   }
 
+  def nonHttpsUri(uri: URI): URI =
+    new URI("http", uri.getUserInfo, uri.getHost, uri.getPort, uri.getPath, uri.getQuery, uri.getFragment)
 
   def cropUriResponse(uri: URI): JsValue =
     Json.obj("uri" -> uri.toString)
 
+}
+
+case class SourceImage(id: String, file: String)
+
+object SourceImage {
+  import play.api.libs.functional.syntax._
+
+  implicit val readsSourceImage: Reads[SourceImage] =
+    ((__ \ "id").read[String] ~ (__ \ "secureUrl").read[String])(SourceImage.apply _)
 }
