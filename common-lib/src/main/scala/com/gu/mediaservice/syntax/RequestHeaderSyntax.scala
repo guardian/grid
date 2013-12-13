31970aaef988b8b560708d1233d9cfbd69ff84cd
Ben James <ben.james@guardian.co.uk>
2013-12-13 13:28:32 +0000

    Revert inadvertent changes to RequestHeaderSyntax

--- common-lib/src/main/scala/com/gu/mediaservice/syntax/RequestHeaderSyntax.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/syntax/RequestHeaderSyntax.scala
@@ -5,10 +5,10 @@ import play.api.mvc.RequestHeader
 trait RequestHeaderSyntax {
 
   implicit class RequestHeaderOps(self: RequestHeader) {
-
     def forwardedProtocol: Option[String] =
       self.headers.get("X-Forwarded-Proto")
-
   }
 
 }
+
+object RequestHeaderSyntax extends RequestHeaderSyntax
