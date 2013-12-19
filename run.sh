afa005ce6c965339ccf2a88a6fe455e779145b4f
Sébastien Cevey <seb.cevey@guardian.co.uk>
2013-12-19 12:04:33 +0000

    Add Cropper to run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ x-terminal-emulator -e "sbt 'project image-loader' 'run 9003'"
 x-terminal-emulator -e "sbt -Dactive=$FTP_ACTIVE
                             'project ftp-watcher'  'run 9004'"
 x-terminal-emulator -e "sbt 'project kahuna'       'run 9005'"
+x-terminal-emulator -e "sbt 'project cropper'      'run 9006'"
 
 # TODO: wake them all up using curl?
 # TODO: optionally open in tabs, set title?
