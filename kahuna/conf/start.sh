ebc69cab4d11a38281af967266f742590fa8d7fb
Ben James <ben.james@guardian.co.uk>
2014-01-07 16:50:57 +0000

    Separate start script from upstart config

---
+++ kahuna/conf/start.sh
@@ -0,0 +1,13 @@
+STAGE=`cat /etc/gu/stage | tr '[:upper:]' '[:lower:]'`
+
+if [ "$STAGE" == "prod" ]
+then
+  DOMAIN_ROOT="media.***REMOVED***"
+else
+  DOMAIN_ROOT="media.$STAGE.dev-***REMOVED***"
+fi
+
+APP_OPTIONS="-Ddomain.root=$DOMAIN_ROOT"
+CMD="java $JVM_OPTIONS $APP_OPTIONS -jar $JAR"
+echo "$CMD" > /home/media-service/logs/cmd.txt
+$CMD > $LOGFILE 2>&1
