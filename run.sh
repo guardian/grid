ee2994b8c516a762bfd6bf4c064a52b8b5756087
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-01-08 15:49:31 +0000

    Run commands in the background in run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 # Change this to true to ingest images
 FTP_ACTIVE=false
 
-x-terminal-emulator -e "sbt 'project media-api'    'run 9001'"
-x-terminal-emulator -e "sbt 'project thrall'       'run 9002'"
-x-terminal-emulator -e "sbt 'project image-loader' 'run 9003'"
+x-terminal-emulator -e "sbt 'project media-api'    'run 9001'" &
+x-terminal-emulator -e "sbt 'project thrall'       'run 9002'" &
+x-terminal-emulator -e "sbt 'project image-loader' 'run 9003'" &
 x-terminal-emulator -e "sbt -Dactive=$FTP_ACTIVE
-                            'project ftp-watcher'  'run 9004'"
-x-terminal-emulator -e "sbt 'project kahuna'       'run 9005'"
-x-terminal-emulator -e "sbt 'project cropper'      'run 9006'"
+                            'project ftp-watcher'  'run 9004'" &
+x-terminal-emulator -e "sbt 'project kahuna'       'run 9005'" &
+x-terminal-emulator -e "sbt 'project cropper'      'run 9006'" &
 
 # TODO: wake them all up using curl?
 # TODO: optionally open in tabs, set title?
