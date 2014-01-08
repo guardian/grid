784e5e4310abe2c9a9e9f67f031dc6ff49a6d11f
Ben James <ben.james@guardian.co.uk>
2014-01-08 15:10:28 +0000

    Oops, session.domain is not unused.

--- kahuna/conf/start.sh
+++ kahuna/conf/start.sh
@@ -7,7 +7,9 @@ else
   DOMAIN_ROOT="media.$STAGE.dev-***REMOVED***"
 fi
 
-APP_OPTIONS="-Ddomain.root=$DOMAIN_ROOT"
+# session.domain is read by Play
+APP_OPTIONS="-Dsession.domain=.$DOMAIN_ROOT -Ddomain.root=$DOMAIN_ROOT"
+
 CMD="java $JVM_OPTIONS $APP_OPTIONS -jar $JAR"
 echo "$CMD" > /home/media-service/logs/cmd.txt
 $CMD > $LOGFILE 2>&1
