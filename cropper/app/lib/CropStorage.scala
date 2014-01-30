a128ef2a081eb28d95b727ba5b49e60ba8e6f2a3
Ben James <ben.james@guardian.co.uk>
2014-01-30 15:23:26 +0000

    Changed more things, don't really remember why

--- cropper/app/lib/CropStorage.scala
+++ cropper/app/lib/CropStorage.scala
@@ -5,21 +5,23 @@ import java.net.URL
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
 import com.gu.mediaservice.lib.aws.S3
-import model.{Bounds, Crop}
+import model.{Dimensions, Bounds, Crop}
 
 object CropStorage extends S3(Config.awsCredentials) {
 
   protected final implicit val ctx: ExecutionContext =
     ExecutionContext.fromExecutor(Executors.newFixedThreadPool(8))
 
-  def storeCropSizing(file: File, filename: String, crop: Crop): Future[URL] = {
+  def storeCropSizing(file: File, filename: String, crop: Crop, dimensions: Dimensions): Future[URL] = {
     val Crop(source, Bounds(x, y, w, h)) = crop
     val metadata = Map("source" -> source,
-                       "x" -> x,
-                       "y" -> y,
-                       "width" -> w,
-                       "height" -> h)
+                       "bounds_x" -> x,
+                       "bounds_y" -> y,
+                       "bounds_w" -> w,
+                       "bounds_h" -> h,
+                       "width" -> dimensions.width,
+                       "height" -> dimensions.height)
     store(Config.cropBucket, filename, file, metadata.mapValues(_.toString))
   }
-  
+
 }
