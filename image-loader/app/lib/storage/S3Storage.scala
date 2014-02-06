5f4eb641887e0e3836534eb5ad2d1cf54b5a19fd
Ben James <ben.james@guardian.co.uk>
2014-02-06 15:01:35 +0000

    Fix reaper

--- image-loader/app/lib/storage/S3Storage.scala
+++ image-loader/app/lib/storage/S3Storage.scala
@@ -1,11 +1,8 @@
 package lib.storage
 
 import java.io.File
-import java.net.{URI, URL}
-import scala.concurrent.Future
 import lib.Config
 import com.gu.mediaservice.lib.aws.S3
-import com.amazonaws.services.s3.model.{ObjectMetadata, PutObjectRequest}
 
 
 object S3Storage extends S3(Config.awsCredentials) with StorageBackend {
