c56114015b67230eaf879397d05eeaa56c54f6a7
Ben James <ben.james@guardian.co.uk>
2014-02-11 10:01:57 +0000

    Log S3 image deletions

--- common-lib/src/main/scala/com/gu/mediaservice/lib/S3ImageStorage.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/S3ImageStorage.scala
@@ -4,10 +4,13 @@ import java.io.File
 import scala.concurrent.Future
 import com.amazonaws.auth.AWSCredentials
 import com.gu.mediaservice.lib.aws.S3
+import org.slf4j.LoggerFactory
 
 
 class S3ImageStorage(imageBucket: String, thumbnailBucket: String, credentials: AWSCredentials)
   extends S3(credentials) with ImageStorage {
+
+  private val log = LoggerFactory.getLogger(getClass)
 
   def storeImage(id: String, file: File, meta: Map[String, String] = Map.empty) =
     store(imageBucket, id, file, meta)
@@ -16,6 +19,7 @@ class S3ImageStorage(imageBucket: String, thumbnailBucket: String, credentials: 
 
   def deleteImage(id: String) = Future {
     client.deleteObject(imageBucket, id)
+    log.info(s"Deleted image $id from bucket $imageBucket")
   }
 
   def deleteThumbnail(id: String) = Future {
