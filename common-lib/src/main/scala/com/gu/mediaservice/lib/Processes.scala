a0248523670afae482bc3d7666c7515794eb3861
Ben James <ben.james@guardian.co.uk>
2014-02-19 14:13:28 +0000

    ftp-watcher: Sleep less between batches

--- common-lib/src/main/scala/com/gu/mediaservice/lib/Processes.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/Processes.scala
@@ -20,6 +20,7 @@ object Processes {
   def sleepIfEmpty[A](duration: Duration)(input: Seq[A]): Process[Task, Seq[A]] =
     if (input.nonEmpty) emit(input) else sleep(duration)
 
+  /* Repeat the process continually, even if it terminates with an error. */
   def retryContinually[A](duration: Duration)(process: => Process[Task, A]): Process[Task, A] =
     process.orElse(sleep(duration) fby process)
 
