8cfe268855d4c867424fc12441d49909a005c3ea
Ben James <ben.james@guardian.co.uk>
2014-05-12 12:14:46 +0100

    Raw query string needs to be decoded

--- kahuna/app/lib/ForceHTTPSFilter.scala
+++ kahuna/app/lib/ForceHTTPSFilter.scala
@@ -1,6 +1,6 @@
 package lib
 
-import java.net.URI
+import java.net.{URLDecoder, URI}
 import scala.concurrent.Future
 import _root_.play.api.mvc.{Results, SimpleResult, RequestHeader, Filter}
 import com.gu.mediaservice.syntax._
@@ -14,7 +14,7 @@ object ForceHTTPSFilter extends Filter {
     */
   def apply(f: (RequestHeader) => Future[SimpleResult])(request: RequestHeader): Future[SimpleResult] =
     if (request.forwardedProtocol.exists(_ != "https")) {
-      val queryString = Some(request.rawQueryString).filter(_.nonEmpty)
+      val queryString = Some(URLDecoder.decode(rawQueryString, "UTF-8")).filter(_.nonEmpty)
       val uri = new URI("https", request.host, request.path, queryString.orNull, null)
       Future.successful(Results.MovedPermanently(uri.toString))
     }
