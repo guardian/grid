ff02e47e8683d93692240d0739e9c81efec019d4
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-08-18 10:46:25 +0100

    Remove obsolete crop bucket creation

--- scripts/src/main/scala/com/gu/mediaservice/scripts/Main.scala
+++ scripts/src/main/scala/com/gu/mediaservice/scripts/Main.scala
@@ -6,6 +6,7 @@ object Main extends App {
   args.toList match {
     case "UpdateStack" :: as      => UpdateStack(as)
     case "CreateStack" :: as      => CreateStack(as)
+    case "DeleteStack" :: as      => DeleteStack(as)
     case "LoadFromS3Bucket" :: as => LoadFromS3Bucket(as)
     case a :: _ => sys.error(s"Unrecognised command: $a")
     case Nil    => sys.error("Usage: <Command> <args ...>")
