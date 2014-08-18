bdf9659bc5e8b3c14ef422e5bec3b242ddcbf720
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-08-18 10:57:38 +0100

    Actually start servers in run script

--- run.sh
+++ run.sh
@@ -3,13 +3,10 @@
 # Change this to true to ingest images
 FTP_ACTIVE=false
 
-x-terminal-emulator -e "sbt 'project media-api'    'run 9001'" &
-x-terminal-emulator -e "sbt 'project thrall'       'run 9002'" &
-x-terminal-emulator -e "sbt 'project image-loader' 'run 9003'" &
-x-terminal-emulator -e "sbt -Dactive=$FTP_ACTIVE
-                            'project ftp-watcher'  'run 9004'" &
-x-terminal-emulator -e "sbt 'project kahuna'       'run 9005'" &
-x-terminal-emulator -e "sbt 'project cropper'      'run 9006'" &
-
-# TODO: wake them all up using curl?
-# TODO: optionally open in tabs, set title?
+x-terminal-emulator -T media-api    -e "sbt 'project media-api'    'start 9001'" &
+x-terminal-emulator -T thrall       -e "sbt 'project thrall'       'start 9002'" &
+x-terminal-emulator -T image-loader -e "sbt 'project image-loader' 'start 9003'" &
+x-terminal-emulator -T ftp-loader   -e "sbt -Dactive=$FTP_ACTIVE
+                            'project ftp-watcher'  'start 9004'" &
+x-terminal-emulator -T kahuna       -e "sbt 'project kahuna'       'start 9005'" &
+x-terminal-emulator -T cropper      -e "sbt 'project cropper'      'start 9006'" &
