c09aba1a96adcfc879c5178ba0c4591eb0f07d61
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-09-03 15:18:12 +0100

    Add helper script to ingest into DEV stack

---
+++ dev-utils/dev-ingest.sh
@@ -0,0 +1,22 @@
+if [ $# != 1 ]
+then
+    echo "usage: dev-ingest.sh <PROD_API_KEY>"
+    echo
+    echo "Pro tip: you can get an ingestion API key by running"
+    echo
+    echo "   $ aws s3 ls ***REMOVED*** | grep dev-ingest"
+    echo
+    exit 1
+fi
+
+DEV_INGEST_KEY=$1
+
+curl -H "X-Gu-Media-Key: $DEV_INGEST_KEY" https://api.media.***REMOVED***/images?length=40 \
+    | jq '.data[].data.secureUrl' \
+    | cut -d '"' -f2 \
+    | while read url
+do
+    echo Ingest...
+    curl -s -o /tmp/img.jpg "$url"
+    curl --data-binary @/tmp/img.jpg http://localhost:9003/images
+done
