01910d0414a88647fd34b89ea3a724df23e7173c
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-09-10 14:25:12 +0100

    New ui:crop-box directive using jCrop (and jquery...)

---
+++ kahuna/public/js/directives.js
@@ -0,0 +1,6 @@
+import angular from 'angular';
+
+// TODO: in the future we may want to group them separately
+var controlsDirectives = angular.module('kahuna.directives', []);
+
+export default controlsDirectives;
