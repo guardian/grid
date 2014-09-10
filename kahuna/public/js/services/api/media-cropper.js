17f9547c5f2dbac6c6fb803e0d44a088131599e2
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-09-10 14:33:00 +0100

    Simple cropper service

---
+++ kahuna/public/js/services/api/media-cropper.js
@@ -0,0 +1,38 @@
+import angular from 'angular';
+import apiServices from 'services/api';
+
+apiServices.factory('mediaCropper',
+                    ['$http', 'mediaApi',
+                     function($http, mediaApi) {
+
+    var cropperRoot;
+
+    function getCropperRoot() {
+        if (! cropperRoot) {
+            cropperRoot = mediaApi.getRoot().then(function(response) {
+                var resource = response.data;
+                var cropperLink = resource.links.filter(l => l.rel === 'cropper')[0];
+                return $http.get(cropperLink.href);
+            });
+        }
+        return cropperRoot;
+    }
+
+    function createCrop(image, coords) {
+        return getCropperRoot().then(function(response) {
+            var cropperResource = response.data;
+            var cropLink = cropperResource.links.filter(l => l.rel === 'crop')[0];
+            return $http.post(cropLink.href, {
+                source: image.uri,
+                x: coords.x,
+                y: coords.y,
+                width: coords.width,
+                height: coords.height
+            });
+        });
+    }
+
+    return {
+        createCrop: createCrop
+    };
+}]);
