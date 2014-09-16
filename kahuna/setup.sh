da82b40381dec8724ac76126ccf477ca960975ec
James A Gorrie <james.gorrie@theguardian.com>
2014-09-16 10:16:02 +0100

    Exit on npm or jspm install fail

--- kahuna/setup.sh
+++ kahuna/setup.sh
@@ -2,6 +2,16 @@
 
 echo Installing NPM dependencies
 npm install
+NPM_RC=$?
+if [ $NPM_RC != "0" ]; then
+    echo "##teamcity[message text='npm install error' status='ERROR']"
+    exit $NPM_RC
+fi
 
 echo Installing JSPM packages
 node_modules/.bin/jspm install
+JSPM_RC=$?
+if [ $JSPM_RC != "0" ]; then
+    echo "##teamcity[message text='jspm install error' status='ERROR']"
+    exit $JSPM_RC
+fi
