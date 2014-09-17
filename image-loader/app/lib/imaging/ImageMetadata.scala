7f7d65a0193f7e24f0b4c5ee04125eb5f25835ef
James A Gorrie <james.gorrie@theguardian.com>
2014-09-17 16:12:11 +0100

    Add copyright and suppliersReference

--- image-loader/app/lib/imaging/ImageMetadata.scala
+++ image-loader/app/lib/imaging/ImageMetadata.scala
@@ -1,7 +1,7 @@
 package lib.imaging
 
 import java.io.File
-
+import com.drew.metadata.icc.{IccDescriptor, IccDirectory}
 import scala.concurrent.{ExecutionContext, Future}
 import com.drew.imaging.ImageMetadataReader
 import com.drew.metadata.iptc.{IptcDescriptor, IptcDirectory}
@@ -23,20 +23,25 @@ object ImageMetadata {
     yield {
       for {
         iptcDir <- Option(metadata.getDirectory(classOf[IptcDirectory]))
-        descriptor = new IptcDescriptor(iptcDir)
+        iptcDescriptor = new IptcDescriptor(iptcDir)
       }
-      yield ImageMetadata(
-        nonEmptyTrimmed(descriptor.getCaptionDescription),
-        nonEmptyTrimmed(descriptor.getByLineDescription),
-        nonEmptyTrimmed(descriptor.getHeadlineDescription),
-        nonEmptyTrimmed(descriptor.getCreditDescription),
-        nonEmptyTrimmed(descriptor.getCopyrightNoticeDescription),
-        nonEmptyTrimmed(descriptor.getSourceDescription),
-        nonEmptyTrimmed(descriptor.getSpecialInstructionsDescription),
-        nonEmptyTrimmed(descriptor.getKeywordsDescription) map (_.split(Array(';', ',')).toList) getOrElse Nil,
-        nonEmptyTrimmed(descriptor.getCityDescription),
-        nonEmptyTrimmed(descriptor.getCountryOrPrimaryLocationDescription)
-      )
+      yield {
+        ImageMetadata(
+          nonEmptyTrimmed(iptcDescriptor.getCaptionDescription),
+          nonEmptyTrimmed(iptcDescriptor.getByLineDescription),
+          nonEmptyTrimmed(iptcDescriptor.getHeadlineDescription),
+          nonEmptyTrimmed(iptcDescriptor.getCreditDescription),
+          nonEmptyTrimmed(iptcDescriptor.getCopyrightNoticeDescription),
+          nonEmptyTrimmed(getIccAlternatve(metadata, IccDirectory.TAG_ICC_TAG_cprt, iptcDescriptor.getCopyrightNoticeDescription)),
+          nonEmptyTrimmed(Option(iptcDescriptor.getOriginalTransmissionReferenceDescription)
+            .getOrElse(iptcDescriptor.getObjectNameDescription)),
+          nonEmptyTrimmed(iptcDescriptor.getSourceDescription),
+          nonEmptyTrimmed(iptcDescriptor.getSpecialInstructionsDescription),
+          nonEmptyTrimmed(iptcDescriptor.getKeywordsDescription) map (_.split(Array(';', ',')).toList) getOrElse Nil,
+          nonEmptyTrimmed(iptcDescriptor.getCityDescription),
+          nonEmptyTrimmed(iptcDescriptor.getCountryOrPrimaryLocationDescription)
+        )
+      }
     }
 
   def dimensions(image: File): Future[Option[Dimensions]] =
@@ -54,9 +59,12 @@ object ImageMetadata {
     Option(nullableStr) map (_.trim) filter (_.nonEmpty)
 
   private def readMetadata(file: File): Future[Metadata] =
-    Future {
-      ImageMetadataReader.readMetadata(file)
-    }
+    Future(ImageMetadataReader.readMetadata(file))
+
+  private def getIccAlternatve(metadata: Metadata, iccTag: Int, alternative: String) =
+    Option(metadata.getDirectory(classOf[IccDirectory])).map { iccDirectory =>
+      new IccDescriptor(iccDirectory).getDescription(iccTag)
+    }.getOrElse(alternative)
 
 }
 
@@ -66,6 +74,8 @@ case class ImageMetadata(
   title: Option[String],
   credit: Option[String],
   copyrightNotice: Option[String],
+  copyright: Option[String],
+  suppliersReference: Option[String],
   source: Option[String],
   specialInstructions: Option[String],
   keywords: List[String],
