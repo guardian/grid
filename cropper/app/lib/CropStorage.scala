abc20170701b6993d8973c6208fee9eb3930b493
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-09-30 11:25:57 +0100

    Tidier code to extract crops

--- cropper/app/lib/CropStorage.scala
+++ cropper/app/lib/CropStorage.scala
@@ -5,7 +5,7 @@ import java.net.URI
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
 import com.gu.mediaservice.lib.aws.S3
-import model.{Dimensions, Bounds, CropSource}
+import model.{Dimensions, Bounds, CropSource, CropSizing}
 
 object CropStorage extends S3(Config.imgPublishingCredentials) {
 
@@ -31,20 +31,23 @@ object CropStorage extends S3(Config.imgPublishingCredentials) {
   def listCrops(id: String): Future[Map[CropSource, List[CropSizing]]] = {
     list(Config.imgPublishingBucket, id) map { crops =>
       crops.foldLeft(Map[CropSource, List[CropSizing]]()) { case (map, (uri, metadata)) =>
-        val cropSource = CropSource(
-          metadata("source"),
-          Bounds(
-            metadata("bounds_x").toInt,
-            metadata("bounds_y").toInt,
-            metadata("bounds_w").toInt,
-            metadata("bounds_h").toInt
-          ),
-          metadata.get("aspect_ratio")
-        )
-        val dimensions = Dimensions(metadata("width").toInt, metadata("height").toInt)
-        val cropSizing = CropSizing(translateImgHost(uri).toString, dimensions)
-        val currentSizings = map.getOrElse(cropSource, Nil)
-        map + (cropSource -> (currentSizings :+ cropSizing))
+        val updatedCrop = for {
+          // Note: if any is missing, the entry won't be registered
+          source <- metadata.get("source")
+          x      <- metadata.get("bounds_x").map(_.toInt)
+          y      <- metadata.get("bounds_y").map(_.toInt)
+          w      <- metadata.get("bounds_w").map(_.toInt)
+          h      <- metadata.get("bounds_h").map(_.toInt)
+          ratio   = metadata.get("aspect_ratio")
+          width  <- metadata.get("width").map(_.toInt)
+          height <- metadata.get("height").map(_.toInt)
+          cropSource = CropSource(source, Bounds(x, y, w, h), ratio)
+          dimensions = Dimensions(width, height)
+          cropSizing = CropSizing(translateImgHost(uri).toString, dimensions)
+          currentSizings = map.getOrElse(cropSource, Nil)
+        } yield (cropSource -> (currentSizings :+ cropSizing))
+
+        map ++ updatedCrop
       }
     }
   }
