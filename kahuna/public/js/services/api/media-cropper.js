2bd7a6146c7acdf399f6cf72521f0e6881b9fe1e
James A Gorrie <james.gorrie@theguardian.com>
2014-10-01 13:56:28 +0100

    New flow for cropping
    Conflicts:
    	kahuna/public/js/app.js

--- kahuna/public/js/services/api/media-cropper.js
+++ kahuna/public/js/services/api/media-cropper.js
@@ -18,22 +18,37 @@ apiServices.factory('mediaCropper',
         return cropperRoot;
     }
 
-    function createCrop(image, coords, ratio) {
+    function getLink(link) {
         return getCropperRoot().then(function(response) {
-            var cropperResource = response.data;
-            var cropLink = cropperResource.links.filter(l => l.rel === 'crop')[0];
-            return $http.post(cropLink.href, {
+            return response.data.links.filter(l => l.rel === link)[0];
+        });
+    }
+
+    function createCrop(image, coords, ratio) {
+        return getLink('crop').then(function(cropsLink) {
+            return $http.post(cropsLink.href, {
                 source: image.uri,
                 x: coords.x,
                 y: coords.y,
                 width: coords.width,
                 height: coords.height,
                 aspectRatio: ratio
-            }, { withCredentials: true });
+            }, { withCredentials: true }).then(function(response) {
+                return response.data;
+            });
+        });
+    }
+
+    function getCropsFor(imageKey) {
+        return getLink('crop').then(function(cropsLink) {
+            return $http.get(cropsLink.href + '/' + imageKey, { withCredentials: true }).then(function(response) {
+                return response.data.data;
+            });
         });
     }
 
     return {
-        createCrop: createCrop
+        createCrop: createCrop,
+        getCropsFor: getCropsFor
     };
 }]);
