19a30f52101f9951dae94f7f623205e5a36539c8
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-10-07 18:07:07 +0100

    Use simple AuthAction for Kahuna

--- kahuna/app/controllers/Application.scala
+++ kahuna/app/controllers/Application.scala
@@ -2,17 +2,14 @@ package controllers
 
 import play.api.mvc.Controller
 import lib.Config
-import com.gu.mediaservice.lib.auth._
-
+import com.gu.mediaservice.lib.auth.PanDomainAuthActions
 
 // TODO: retire Panda entirely from kahuna, let the JS app and the APIs manage auth
 
 object Application extends Controller with PanDomainAuthActions {
 
-  val keyStore = new KeyStore(Config.keyStoreBucket, Config.awsCredentials)
-
-  def index(ignored: String) = Authenticated.async(keyStore)(sendForAuth(_)) { req =>
-    Ok(views.html.main(mediaApiUri = Config.mediaApiUri, principal = req.user))
+  def index(ignored: String) = AuthAction { req =>
+    Ok(views.html.main(mediaApiUri = Config.mediaApiUri))
   }
 
 }
