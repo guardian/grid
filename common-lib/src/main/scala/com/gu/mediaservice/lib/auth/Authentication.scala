642fbe7443b753d664bb330d87612663a57dbab5
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-10-07 14:38:04 +0100

    Move KeyStore to its own file

--- common-lib/src/main/scala/com/gu/mediaservice/lib/auth/Authentication.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/auth/Authentication.scala
@@ -1,20 +1,12 @@
 package com.gu.mediaservice.lib.auth
 
-import scala.collection.JavaConverters._
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
-import akka.actor.Scheduler
-import akka.agent.Agent
 import play.api.mvc._
 import play.api.libs.json.Json
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc.Security.AuthenticatedRequest
 
-import com.amazonaws.auth.AWSCredentials
-import com.gu.mediaservice.lib.aws.S3
-import org.apache.commons.io.IOUtils
-import com.amazonaws.AmazonServiceException
 import com.gu.mediaservice.syntax._
 import com.gu.pandomainauth.model.{AuthenticatedUser, User}
 
@@ -82,42 +74,6 @@ object Principal {
     }
 }
 
-class KeyStore(bucket: String, credentials: AWSCredentials) {
-
-  val s3 = new S3(credentials)
-
-  def lookupIdentity(key: String): Future[Option[String]] =
-    store.future.map(_.get(key))
-
-  def findKey(prefix: String): Option[String] = s3.syncFindKey(bucket, prefix)
-
-  private val store: Agent[Map[String, String]] = Agent(Map.empty)
-
-  def scheduleUpdates(scheduler: Scheduler) {
-    scheduler.schedule(0.seconds, 10.minutes)(update())
-  }
-
-  def update() {
-    store.sendOff(_ => fetchAll)
-  }
-
-  private def fetchAll: Map[String, String] = {
-    val keys = s3.client.listObjects(bucket).getObjectSummaries.asScala.map(_.getKey)
-    keys.flatMap(k => getIdentity(k).map(k -> _)).toMap
-  }
-
-  private def getIdentity(key: String): Option[String] = {
-    val content = s3.client.getObject(bucket, key)
-    val stream = content.getObjectContent
-    try
-      Some(IOUtils.toString(stream, "utf-8"))
-    catch {
-      case e: AmazonServiceException if e.getErrorCode == "NoSuchKey" => None
-    }
-    finally
-      stream.close()
-  }
-}
 
 /** A variant of Play's AuthenticatedBuilder which permits the user info to be retrieved in a Future,
   * rather than immediately (/blocking)
