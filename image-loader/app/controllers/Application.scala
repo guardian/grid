7ba83a1e1b0edce2950da30579a86ad70fb7b4c0
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-10-20 10:49:07 +0100

    Extract FileMetadata from common directories

--- image-loader/app/controllers/Application.scala
+++ image-loader/app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.json.Json
 import play.api.Logger
 
-import lib.imaging.{MimeTypeDetection, Thumbnailer, ImageMetadata}
+import lib.imaging.{FileMetadata, MimeTypeDetection, Thumbnailer, ImageMetadata}
 import lib.play.BodyParsers.digestedFile
 import lib.play.DigestedFile
 
@@ -33,21 +33,26 @@ class ImageLoader(storage: ImageStorage) extends Controller {
     // These futures are started outside the for-comprehension, otherwise they will not run in parallel
     val uriFuture = storage.storeImage(id, tempFile, uploadedBy.map(s => ("uploaded_by", s)).toMap)
     val thumbFuture = Thumbnailer.createThumbnail(Config.thumbWidth, tempFile.toString)
-    val dimensionsFuture = ImageMetadata.dimensions(tempFile)
+    val dimensionsFuture = FileMetadata.dimensions(tempFile)
     val metadataFuture = ImageMetadata.fromIPTCHeaders(tempFile)
+    val fileMetadataFuture = FileMetadata.fromIPTCHeaders(tempFile)
+    // TODO: derive ImageMetadata from FileMetadata
 
     val future = bracket(thumbFuture)(_.delete) { thumb =>
       for {
         uri        <- uriFuture
         dimensions <- dimensionsFuture
+        // TODO: fail with error if missing metadata
         metadata   <- metadataFuture
+        fileMetadata <- fileMetadataFuture
+        // TODO: validate mime-type against white-list
         mimeType    = MimeTypeDetection.guessMimeType(tempFile)
         sourceAsset = Asset(uri, tempFile.length, mimeType, dimensions)
         thumbUri   <- storage.storeThumbnail(id, thumb)
         thumbSize   = thumb.length
-        thumbDimensions <- ImageMetadata.dimensions(thumb)
+        thumbDimensions <- FileMetadata.dimensions(thumb)
         thumbAsset  = Asset(thumbUri, thumbSize, mimeType, thumbDimensions)
-        image       = Image.uploadedNow(id, uploadedBy, sourceAsset, thumbAsset, metadata)
+        image       = Image.uploadedNow(id, uploadedBy, sourceAsset, thumbAsset, fileMetadata, metadata)
       } yield {
         Notifications.publish(Json.toJson(image), "image")
         Accepted(Json.obj("id" -> id))
--- image-loader/app/model/Image.scala
+++ image-loader/app/model/Image.scala
@@ -6,7 +6,7 @@ import play.api.libs.functional.syntax._
 import org.joda.time.DateTime
 
 import com.gu.mediaservice.lib.formatting._
-import lib.imaging.ImageMetadata
+import lib.imaging.{FileMetadata, ImageMetadata}
 
 
 case class Image(id: String,
@@ -14,6 +14,7 @@ case class Image(id: String,
                  uploadedBy: Option[String],
                  source: Asset,
                  thumbnail: Option[Asset],
+                 fileMetadata: FileMetadata,
                  metadata: Option[ImageMetadata]
 ) {
 
@@ -25,8 +26,9 @@ case class Image(id: String,
 object Image {
 
   def uploadedNow(id: String, uploadedBy: Option[String],
-                  source: Asset, thumbnail: Asset, metadata: Option[ImageMetadata]): Image =
-    Image(id, DateTime.now, uploadedBy, source, Some(thumbnail), metadata)
+                  source: Asset, thumbnail: Asset,
+                  fileMetadata: FileMetadata, metadata: Option[ImageMetadata]): Image =
+    Image(id, DateTime.now, uploadedBy, source, Some(thumbnail), fileMetadata, metadata)
 
   implicit val IptcMetadataWrites: Writes[ImageMetadata] =
     ((__ \ "description").writeNullable[String] ~
@@ -43,12 +45,15 @@ object Image {
       (__ \ "country").writeNullable[String]
       )(unlift(ImageMetadata.unapply))
 
+  implicit val FileMetadataWrites: Writes[FileMetadata] = Json.writes[FileMetadata]
+
   implicit val ImageWrites: Writes[Image] = (
     (__ \ "id").write[String] ~
       (__ \ "uploadTime").write[String].contramap(printDateTime) ~
       (__ \ "uploadedBy").writeNullable[String] ~
       (__ \ "source").write[Asset] ~
       (__ \ "thumbnail").writeNullable[Asset] ~
+      (__ \ "fileMetadata").write[FileMetadata] ~
       (__ \ "metadata").writeNullable[ImageMetadata]
     )(unlift(Image.unapply))
 
