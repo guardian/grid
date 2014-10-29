57c84abf75f32ac83c63183da9fc04a0cd55c033
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-10-29 17:02:33 +0000

    Link to new metadata API

--- common-lib/src/main/scala/com/gu/mediaservice/lib/config/Services.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/config/Services.scala
@@ -1,16 +1,24 @@
 package com.gu.mediaservice.lib.config
 
 class Services(domainRoot: String, ssl: Boolean) {
+  // FIXME: do this via new config properties!
+  val DomainExtractor = """^([-a-z]+)\.(.*)""".r
+  val (appName, parentDomain) = domainRoot match {
+    case DomainExtractor(a, b) => (a, b)
+  }
 
   val kahunaHost: String   = domainRoot
   val apiHost: String      = s"api.$domainRoot"
   val loaderHost: String   = s"loader.$domainRoot"
   val cropperHost: String  = s"cropper.$domainRoot"
+  val metadataHost: String = s"$appName-metadata.$parentDomain"
+
 
   val kahunaBaseUri   = baseUri(kahunaHost)
   val apiBaseUri      = baseUri(apiHost)
   val loaderBaseUri   = baseUri(loaderHost)
   val cropperBaseUri  = baseUri(cropperHost)
+  val metadataBaseUri = baseUri(metadataHost)
 
   def baseUri(host: String) = {
     val protocol = if (ssl) "https" else "http"
