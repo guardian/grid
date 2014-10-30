a80fbd06b1f272106d348f5ea6610e44eb10fbce
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-10-30 16:49:21 +0000

    Debug statements removed

--- thrall/app/lib/MessageConsumer.scala
+++ thrall/app/lib/MessageConsumer.scala
@@ -69,10 +69,8 @@ object MessageConsumer {
   def updateImageExports(exports: JsValue): Future[UpdateResponse] =
     withImageId(exports)(id => ElasticSearch.updateImageExports(id, exports \ "data"))
 
-  def updateImageUserMetadata(metadata: JsValue): Future[UpdateResponse] = {
-    println("metadata", metadata)
+  def updateImageUserMetadata(metadata: JsValue): Future[UpdateResponse] =
     withImageId(metadata)(id => ElasticSearch.updateImageUserMetadata(id, metadata \ "data"))
-  }
 
   def deleteImage(image: JsValue): Future[DeleteByQueryResponse] =
     withImageId(image) { id =>
