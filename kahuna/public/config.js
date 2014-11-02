c6d8679c6014a6a80a724d68096330244af6e6ac
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-11-02 13:20:46 +0000

    jspm deps now ordered alphabetically

--- kahuna/public/config.js
+++ kahuna/public/config.js
@@ -9,8 +9,18 @@ System.config({
 System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.2.23",
+    "angular-ui-router": "github:angular-ui/ui-router@0.2.11",
+    "css": "github:systemjs/plugin-css@^0.1.0",
+    "github:tapmodo/Jcrop": "github:tapmodo/Jcrop@0.9.12",
+    "jcrop": "github:tapmodo/Jcrop@0.9.12",
+    "jquery": "github:components/jquery@2.1.1",
     "npm:angular-ui-router": "npm:angular-ui-router@0.2.10",
-    "npm:angular-ui-router@0.2.10": {},
+    "theseus": "github:argo-rest/theseus@master",
+    "github:argo-rest/theseus@master": {
+      "reqwest": "github:ded/reqwest@^1.1.2",
+      "npm:uri-templates": "npm:uri-templates@^0.1.5",
+      "jquery": "github:components/jquery@2.1.1"
+    },
     "github:jspm/nodelibs@0.0.2": {
       "inherits": "npm:inherits@^2.0.1",
       "base64-js": "npm:base64-js@^0.0.4",
@@ -18,51 +28,54 @@ System.config({
       "json": "github:systemjs/plugin-json@master",
       "ieee754": "npm:ieee754@^1.1.1"
     },
-    "npm:inherits@2.0.1": {},
-    "npm:base64-js@0.0.4": {},
-    "npm:Base64@0.2.1": {},
-    "npm:ieee754@1.1.3": {},
-    "jquery": "github:components/jquery@2.1.1",
-    "github:tapmodo/Jcrop": "github:tapmodo/Jcrop@0.9.12",
-    "css": "github:systemjs/plugin-css@^0.1.0",
-    "jcrop": "github:tapmodo/Jcrop@0.9.12",
-    "angular-ui-router": "github:angular-ui/ui-router@0.2.11",
-    "github:jspm/nodelibs@0.0.3": {
+    "github:jspm/nodelibs@0.0.5": {
+      "Base64": "npm:Base64@^0.2.0",
+      "base64-js": "npm:base64-js@^0.0.4",
       "ieee754": "npm:ieee754@^1.1.1",
-      "Base64": "npm:Base64@0.2",
-      "base64-js": "npm:base64-js@0.0",
       "inherits": "npm:inherits@^2.0.1",
-      "json": "github:systemjs/plugin-json@master"
+      "json": "github:systemjs/plugin-json@master",
+      "pbkdf2-compat": "npm:pbkdf2-compat@^2.0.1",
+      "ripemd160": "npm:ripemd160@^0.2.0",
+      "sha.js": "npm:sha.js@^2.2.6"
+    },
+    "npm:Base64@0.2.1": {},
+    "npm:angular-ui-router@0.2.10": {},
+    "npm:base64-js@0.0.4": {},
+    "npm:ieee754@1.1.3": {},
+    "npm:inherits@2.0.1": {},
+    "npm:json@9.0.2": {},
+    "npm:pbkdf2-compat@2.0.1": {},
+    "npm:ripemd160@0.2.0": {},
+    "npm:sha.js@2.2.6": {
+      "json": "npm:json@^9.0.2"
     },
     "npm:uri-templates@0.1.5": {
       "json": "github:systemjs/plugin-json@master"
-    },
-    "theseus": "github:argo-rest/theseus@master",
-    "github:argo-rest/theseus@master": {
-      "reqwest": "github:ded/reqwest@^1.1.2",
-      "npm:uri-templates": "npm:uri-templates@^0.1.5",
-      "jquery": "github:components/jquery@^2.1.1"
     }
   }
 });
 
 System.config({
   "versions": {
-    "github:angular/bower-angular": "1.2.23",
-    "npm:angular-ui-router": "0.2.10",
-    "github:jspm/nodelibs": "0.0.3",
-    "npm:inherits": "2.0.1",
-    "npm:base64-js": "0.0.4",
-    "npm:Base64": "0.2.1",
-    "github:systemjs/plugin-json": "master",
-    "npm:ieee754": "1.1.3",
-    "github:components/jquery": "2.1.1",
-    "github:tapmodo/Jcrop": "0.9.12",
-    "github:systemjs/plugin-css": "0.1.0",
     "github:angular-ui/ui-router": "0.2.11",
-    "npm:uri-templates": "0.1.5",
+    "github:angular/bower-angular": "1.2.23",
+    "github:argo-rest/theseus": "master",
+    "github:components/jquery": "2.1.1",
     "github:ded/reqwest": "1.1.2",
-    "github:argo-rest/theseus": "master"
+    "github:jspm/nodelibs": "0.0.5",
+    "github:systemjs/plugin-css": "0.1.0",
+    "github:systemjs/plugin-json": "master",
+    "github:tapmodo/Jcrop": "0.9.12",
+    "npm:Base64": "0.2.1",
+    "npm:angular-ui-router": "0.2.10",
+    "npm:base64-js": "0.0.4",
+    "npm:ieee754": "1.1.3",
+    "npm:inherits": "2.0.1",
+    "npm:json": "9.0.2",
+    "npm:pbkdf2-compat": "2.0.1",
+    "npm:ripemd160": "0.2.0",
+    "npm:sha.js": "2.2.6",
+    "npm:uri-templates": "0.1.5"
   }
 });
 
