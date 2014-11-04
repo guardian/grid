34e6830fd6ea2d4fc8487995de1e7992c529aad1
James A Gorrie <james.gorrie@theguardian.com>
2014-11-04 11:36:45 +0000

    add session information

--- kahuna/app/controllers/Panda.scala
+++ kahuna/app/controllers/Panda.scala
@@ -1,13 +1,22 @@
 package controllers
 
 import lib.Config
+import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
-import play.api.libs.concurrent.Execution.Implicits._
 import com.gu.mediaservice.lib.auth.PanDomainAuthActions
 
 object Panda extends Controller with PanDomainAuthActions {
 
   override lazy val authCallbackBaseUri = Config.rootUri
+
+  def session = Action { implicit request =>
+    readAuthenticatedUser(request).map { authedUser =>
+      Ok(Json.obj("data" -> Json.obj("user" ->
+        Json.obj(
+          "email" -> authedUser.user.email
+        ))))
+    }.getOrElse(Unauthorized)
+  }
 
   def doLogin = Action.async { implicit request =>
     sendForAuth
