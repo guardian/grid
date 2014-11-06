7d60b817b74f93b561e682c4e96861bcb7aa7324
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-11-06 18:42:03 +0000

    Set cache-control header on image files, rounding the expiry to limit cache invalidation by query string

--- common-lib/src/main/scala/com/gu/mediaservice/lib/S3ImageStorage.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/S3ImageStorage.scala
@@ -6,17 +6,23 @@ import com.amazonaws.auth.AWSCredentials
 import com.gu.mediaservice.lib.aws.S3
 import org.slf4j.LoggerFactory
 
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class S3ImageStorage(imageBucket: String, thumbnailBucket: String, credentials: AWSCredentials)
   extends S3(credentials) with ImageStorage {
 
   private val log = LoggerFactory.getLogger(getClass)
 
+  // Images can be cached "forever" as they never should change
+  val cacheDuration = 365 days
+  val cacheForever = s"max-age=${cacheDuration.toSeconds}"
+
   def storeImage(id: String, file: File, mimeType: Option[String], meta: Map[String, String] = Map.empty) =
-    store(imageBucket, id, file, mimeType, meta)
+    store(imageBucket, id, file, mimeType, meta, Some(cacheForever))
 
   def storeThumbnail(id: String, file: File, mimeType: Option[String]) =
-    store(thumbnailBucket, id, file, mimeType)
+    store(thumbnailBucket, id, file, mimeType, cacheControl = Some(cacheForever))
 
   def deleteImage(id: String) = Future {
     client.deleteObject(imageBucket, id)
