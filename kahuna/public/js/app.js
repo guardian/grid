f3aa2b6bad68441c7dd4de1a95270db961018380
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-11-13 15:47:17 +0000

    Show result count

--- kahuna/public/js/app.js
+++ kahuna/public/js/app.js
@@ -119,7 +119,8 @@ kahuna.controller('SearchResultsCtrl',
     // See:   https://github.com/guardian/media-service/pull/64#discussion-diff-17351746L116
     $scope.loading = true;
     $scope.searched = search().then(function(images) {
-        $scope.images = images;
+        $scope.totalResults = images.total;
+        $scope.images = images.data;
         // yield so images render before we check if there's more space
         $timeout(function() {
             if ($scope.hasSpace) {
@@ -170,7 +171,7 @@ kahuna.controller('SearchResultsCtrl',
             var until = lastImage.data.uploadTime;
             return search(until).then(function(moreImages) {
                 // Filter out duplicates (esp. on exact same 'until' date)
-                var newImages = moreImages.filter(function(im) {
+                var newImages = moreImages.data.filter(function(im) {
                     return $scope.images.filter(function(existing) {
                         // TODO: revert back to using uri
                         return existing.data.id === im.data.id;
@@ -682,7 +683,7 @@ kahuna.controller('FileUploaderCtrl',
 
         (function searchForUploads() {
             $timeout.cancel(timeout);
-            mediaApi.search('', { ids: ids }).then(images => {
+            mediaApi.search('', { ids: ids }).then(resp => resp.data).then(images => {
                 if(images.length === ids.length) {
                     def.resolve(images);
                 } else {
