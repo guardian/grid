22874aced993d928a61ceed66753f67d61f35686
James A Gorrie <james.gorrie@theguardian.com>
2014-11-13 10:33:13 +0000

    Allow the updating of ES mappings from a script

--- scripts/src/main/scala/com/gu/mediaservice/scripts/Main.scala
+++ scripts/src/main/scala/com/gu/mediaservice/scripts/Main.scala
@@ -4,11 +4,12 @@ package com.gu.mediaservice.scripts
 object Main extends App {
 
   args.toList match {
-    case "UpdateStack" :: as      => UpdateStack(as)
-    case "CreateStack" :: as      => CreateStack(as)
-    case "DeleteStack" :: as      => DeleteStack(as)
+    case "UpdateStack"      :: as => UpdateStack(as)
+    case "CreateStack"      :: as => CreateStack(as)
+    case "DeleteStack"      :: as => DeleteStack(as)
     case "LoadFromS3Bucket" :: as => LoadFromS3Bucket(as)
-    case "Reindex" :: as          => Reindex(as)
+    case "Reindex"          :: as => Reindex(as)
+    case "UpdateMapping"    :: as => UpdateMapping(as)
     case a :: _ => sys.error(s"Unrecognised command: $a")
     case Nil    => sys.error("Usage: <Command> <args ...>")
   }
