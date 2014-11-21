86e55a5eefdc86f40b8e4bda8e6d41b8e800a217
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-11-21 14:36:56 -0500

    Add upload views and controllers

---
+++ kahuna/public/js/upload/jobs/required-metadata-editor.js
@@ -0,0 +1,56 @@
+import angular from 'angular';
+
+export var jobs = angular.module('kahuna.upload.jobs.requiredMetadataEditor', []);
+
+
+jobs.controller('RequiredMetadataEditorCtrl',
+                ['$scope', '$window',
+                 function($scope, $window) {
+
+    var ctrl = this;
+
+    ctrl.description = $scope.initial.description;
+    ctrl.credit      = $scope.initial.credit;
+
+    ctrl.saving = false;
+
+    ctrl.save = function() {
+        ctrl.saving = true;
+        var metadata = {
+            description: ctrl.description,
+            credit:      ctrl.credit
+        };
+        var updatedResource = $scope.overrideResource.put({
+            // TODO: dispose of boilerplate, just send data as entity
+            data: metadata
+        });
+        // FIXME: this is really a hack; should put() return a Promise?
+        updatedResource.response.
+            then(() => {
+                $scope.jobEditor.$setPristine();
+                $scope.onUpdate({metadata: metadata});
+            }).
+            catch(() => {
+                $window.alert('Failed to save the changes, please try again.');
+            }).
+            finally(() => {
+                ctrl.saving = false;
+            });
+    };
+}]);
+
+
+jobs.directive('uiRequiredMetadataEditor', ['jsDirectory', function(jsDirectory) {
+    return {
+        restrict: 'E',
+        scope: {
+            // Annoying that we can't make a uni-directional binding
+            // as we don't really want to modify the original
+            initial: '=',
+            overrideResource: '=',
+            onUpdate: '&'
+        },
+        controller: 'RequiredMetadataEditorCtrl as editorCtrl',
+        templateUrl: jsDirectory + '/upload/jobs/required-metadata-editor.html'
+    };
+}]);
