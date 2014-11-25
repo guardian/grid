e60355ae51d9ff94fe73647e9ef9c4928dee61f2
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-11-25 15:35:48 +0000

    Add AAP to list of free providers

--- media-api/app/lib/Config.scala
+++ media-api/app/lib/Config.scala
@@ -52,6 +52,6 @@ object Config extends CommonPlayAppConfig with CommonPlayAppProperties {
     "Press Association Images", "Action Images", "Keystone", "AFP", "Getty Images", "Alamy", "FilmMagic", "WireImage",
     "Pool", "Rex Features", "Allsport", "BFI", "ANSA", "The Art Archive", "Hulton Archive", "Hulton Getty", "RTRPIX",
     "Community Newswire", "THE RONALD GRANT ARCHIVE", "NPA ROTA", "Ronald Grant Archive", "PA WIRE", "AP POOL",
-    "REUTER", "dpa", "BBC", "Allstar Picture Library", "AFP/Getty Images")
+    "REUTER", "dpa", "BBC", "Allstar Picture Library", "AFP/Getty Images", "AAPIMAGE")
 
 }
