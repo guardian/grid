16b43ac1d627f970c1a69051ba305ec7c39baef9
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-11-27 16:57:29 +0000

    Generate funny random description placeholders for upload pane

--- kahuna/public/js/upload/jobs/required-metadata-editor.js
+++ kahuna/public/js/upload/jobs/required-metadata-editor.js
@@ -41,6 +41,44 @@ jobs.controller('RequiredMetadataEditorCtrl',
     };
 }]);
 
+jobs.controller('DescriptionPlaceholderCtrl',
+                ['$scope',
+                 function($scope) {
+
+    var people = [
+        'George Osborne',
+        'A teary Nick Clegg',
+        'Pop singer Rihanna',
+        'US actress and director Angelina Jolie',
+        'George W. Bush'
+    ];
+
+    var actions = [
+        'eating',
+        'caught with',
+        'wrestling',
+        'chants while marching for a third night of protests about',
+        'making a toast to'
+    ];
+
+    var things = [
+        'a large pheasant burger',
+        'two human-sized rubber ducks',
+        'a proposal for a new Union Jack',
+        'the recently passed Owning The Internet bill',
+        'the first crewed spaceship to reach Mars',
+        'the largest ever koala recorded in history'
+    ];
+
+    function random(array) {
+        var index = Math.floor(Math.random() * array.length);
+        return array[index];
+    }
+
+    $scope.funnyDescription = [people, actions, things].map(random).join(' ');
+
+}]);
+
 
 jobs.directive('uiRequiredMetadataEditor', ['jsDirectory', function(jsDirectory) {
     return {
