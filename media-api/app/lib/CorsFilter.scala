719eeabcb607652121a018519ac610028631a7b9
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-12-01 14:52:48 +0000

    Allow configuring more origins that can do CORS to the media-api

--- media-api/app/lib/CorsFilter.scala
+++ media-api/app/lib/CorsFilter.scala
@@ -8,14 +8,20 @@ object CorsFilter extends Filter {
   import scala.concurrent._
   import ExecutionContext.Implicits.global
 
+  def isAllowed(origin: String) = Config.corsAllAllowedOrigins.contains(origin)
+
   def apply(f: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
 
-    val corsAllowOrigin = Config.corsAllowedOrigin
-
-    f(request).map { _.withHeaders(
-      "Access-Control-Allow-Credentials" -> "true",
-      "Access-Control-Allow-Origin" -> corsAllowOrigin
-    ) }
+    // Add CORS headers iff allowed origin
+    val origin = request.headers.get("Origin") getOrElse ""
+    if (isAllowed(origin)) {
+      f(request).map { _.withHeaders(
+        "Access-Control-Allow-Credentials" -> "true",
+        "Access-Control-Allow-Origin" -> origin
+      ) }
+    } else {
+      f(request)
+    }
   }
 
 }
