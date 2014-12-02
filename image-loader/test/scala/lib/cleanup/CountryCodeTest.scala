0983c0b04cd8b5aabfbcc5bc9cef13fef3bf0df0
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-12-02 15:23:56 +0000

    Map both UK and GB to United Kingdom, and US/USA to United States, as they should

--- image-loader/test/scala/lib/cleanup/CountryCodeTest.scala
+++ image-loader/test/scala/lib/cleanup/CountryCodeTest.scala
@@ -30,6 +30,27 @@ class CountryCodeTest extends FunSpec with Matchers with MetadataHelper {
     clean("XXX") should be (Some("XXX"))
   }
 
+  // Exception: United Kingdom
+
+  it("should map the UK country code to United Kingdom") {
+    clean("UK") should be (Some("United Kingdom"))
+  }
+
+  it("should map the GB country code to United Kingdom") {
+    clean("GB") should be (Some("United Kingdom"))
+  }
+
+  // Exception: United States
+
+  it("should map the US country code to United States") {
+    clean("US") should be (Some("United States"))
+  }
+
+  it("should map the USA country code to United States") {
+    clean("USA") should be (Some("United States"))
+  }
+
+
   def clean(country: String): Option[String] = {
     CountryCode.clean(createImageMetadata("country" -> country)).country
   }
