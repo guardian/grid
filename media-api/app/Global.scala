bd110516a1995ca584a6e5bd918c099dfbd02661
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-12-02 16:09:36 +0000

    Add GzipFilter to all relevant servers to compress our responses

--- media-api/app/Global.scala
+++ media-api/app/Global.scala
@@ -4,9 +4,10 @@ import lib.elasticsearch.ElasticSearch
 import play.api.libs.concurrent.Akka
 import play.api.{Application, GlobalSettings}
 import play.api.mvc.WithFilters
+import play.filters.gzip.GzipFilter
 
 
-object Global extends WithFilters(CorsFilter) with GlobalSettings {
+object Global extends WithFilters(CorsFilter, new GzipFilter) with GlobalSettings {
 
   override def beforeStart(app: Application) {
     ElasticSearch.ensureAliasAssigned()
