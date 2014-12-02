e4ebe6e45ea267b6938102ba9bd28a5ee4d358cc
James A Gorrie <james.gorrie@theguardian.com>
2014-12-02 17:08:52 +0000

    refactor labeler to the right place

--- kahuna/public/js/app.js
+++ kahuna/public/js/app.js
@@ -21,7 +21,6 @@ import searchResultsTemplate from './search/results.html!text';
 import imageTemplate         from './image/view.html!text';
 import cropTemplate          from './crop/view.html!text';
 import uploadTemplate        from './upload/view.html!text';
-import imageLabelsTemplate   from './image/labels.html!text';
 
 
 var apiLink = document.querySelector('link[rel="media-api-uri"]');
@@ -180,63 +179,6 @@ kahuna.controller('ImageCtrl',
     };
 
     $scope.priorityMetadata = ['byline', 'credit'];
-}]);
-
-
-kahuna.controller('ImageLabelsCtrl',
-                  ['$scope', '$window',
-                   function($scope, $window) {
-
-    function saveFailed() {
-        $window.alert('Something went wrong when saving, please try again!');
-    }
-
-    this.addLabel = () => {
-        // Prompt for a label and add if not empty
-        var label = ($window.prompt("Enter a label:") || '').trim();
-        if (label) {
-            this.adding = true;
-            $scope.labels.post({data: label}).
-                then(newLabel => {
-                    // FIXME: don't mutate original, replace the whole resource with the new state
-                    $scope.labels.data.push(newLabel);
-                }).
-                catch(saveFailed).
-                finally(() => {
-                    this.adding = false;
-                });
-        }
-    };
-
-    this.labelsBeingRemoved = new Set;
-    this.removeLabel = (label) => {
-        this.labelsBeingRemoved.add(label);
-
-        label.delete().
-            then(() => {
-                // FIXME: don't mutate original, replace the whole resource with the new state
-                var labelIndex = $scope.labels.data.findIndex(l => l.data === label.data);
-                $scope.labels.data.splice(labelIndex, 1);
-            }).
-            catch(saveFailed).
-            finally(() => {
-                this.labelsBeingRemoved.remove(label);
-            });
-    };
-
-}]);
-
-kahuna.directive('uiImageLabels', [function() {
-    return {
-        restrict: 'E',
-        scope: {
-            // Annoying that we can't make a uni-directional binding
-            // as we don't really want to modify the original
-            labels: '='
-        },
-        controller: 'ImageLabelsCtrl as labelsCtrl',
-        template: imageLabelsTemplate
-    };
 }]);
 
 
