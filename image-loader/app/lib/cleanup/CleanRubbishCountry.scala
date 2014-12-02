911d15b8ee307de7d04b474727177643a2419a73
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-12-02 15:30:33 +0000

    Small cleaner to clean rubbish country names

---
+++ image-loader/app/lib/cleanup/CleanRubbishCountry.scala
@@ -0,0 +1,16 @@
+package lib.cleanup
+
+import lib.imaging.ImageMetadata
+
+object CleanRubbishCountry extends MetadataCleaner {
+
+  val Rubbish = """(\s*[.-]*\s*)""".r
+
+  override def clean(metadata: ImageMetadata): ImageMetadata = metadata.country match {
+    // if rubbish country name, strip it
+    case Some(Rubbish(_)) => metadata.copy(country = None)
+    // otherwise, just pass through
+    case Some(country)    => metadata
+    case None             => metadata
+  }
+}
