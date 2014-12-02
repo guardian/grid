b18b04c30d5356cf1e984fa48280a5d76272e77c
James A Gorrie <james.gorrie@theguardian.com>
2014-12-02 17:08:52 +0000

    refactor labeler to the right place

--- kahuna/public/js/edits/index.js
+++ kahuna/public/js/edits/index.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
 
 import 'edits/archiver';
+import 'edits/labeler';
 
 export var upload = angular.module('kahuna.edits', [
-    'kahuna.edits.archiver'
+    'kahuna.edits.archiver',
+    'kahuna.edits.labeler'
 ]);
