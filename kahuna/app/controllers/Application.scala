f5c1cfba889f9e2f3a8056e4128f9a2b8f2fb508
James A Gorrie <james.gorrie@theguardian.com>
2014-12-12 12:25:23 +0000

    optionlise mixpanel token in template

--- kahuna/app/controllers/Application.scala
+++ kahuna/app/controllers/Application.scala
@@ -14,7 +14,6 @@ object Application extends Controller with PanDomainAuthActions {
 
   def index(ignored: String) = AuthAction { req =>
     logUnsupportedBrowsers(req.headers.get("User-Agent"), req.user)
-
     Ok(views.html.main(Config.mediaApiUri, Config.mixpanelToken))
   }
 
