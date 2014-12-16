f2eee6cf4bf69758320758979fb05bca5748b7ee
James A Gorrie <james.gorrie@theguardian.com>
2014-12-16 10:56:17 +0000

    more verbose screen tracking

--- kahuna/public/js/analytics/track.js
+++ kahuna/public/js/analytics/track.js
@@ -6,12 +6,22 @@ export var track = angular.module('analytics.track', ['mixpanel']);
 // TODO: look into tidying the async flow. There might be a small chance of
 // `track` being called before the session is got.
 
-track.factory('track', ['$location', '$window', 'mixpanel', function($location, $window, mixpanel) {
+track.factory('track', ['$location', '$window', '$document', 'mixpanel',
+                        function($location, $window, $document, mixpanel) {
     return function track(event, opts) {
+        var doc = $document[0];
+        var winX = $window.screen.width;
+        var winY = $window.screen.height;
+        var docX = doc.documentElement.clientWidth;
+        var docY = doc.documentElement.clientHeight;
         var finalOpts = angular.extend({}, opts, {
             'Url': $location.url(),
-            'Screen resolution': window.screen.width + ' x ' + window.screen.height,
-            'Screen viewport': document.documentElement.clientWidth + ' x ' + document.documentElement.clientHeight
+            'Screen resolution': winX + ' x ' + winY,
+            'Screen resolution X': winX,
+            'Screen resolution Y': winY,
+            'Screen viewport': docX + ' x ' + docY,
+            'Screen viewport X': docX,
+            'Screen viewport Y': docY
         });
 
         mixpanel.track(event, finalOpts);
