01fdd3e201ed88813e1523637885d77bcf6ea686
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-12-27 19:12:26 +0000

    Move image and crop controller and routing to its sub-module

--- kahuna/public/js/app.js
+++ kahuna/public/js/app.js
@@ -8,6 +8,8 @@ import 'services/api/media-api';
 import 'services/api/media-cropper';
 import 'services/api/loader';
 import 'directives/ui-crop-box';
+import 'crop/index';
+import 'image/index';
 import 'upload/index';
 import 'search/index';
 import 'edits/index';
@@ -15,10 +17,6 @@ import 'util/async';
 import 'util/digest';
 import 'pandular/heal';
 import 'analytics/track';
-
-// TODO: refactor all into their respective directories
-import imageTemplate         from './image/view.html!text';
-import cropTemplate          from './crop/view.html!text';
 
 // TODO: move to an async config to remove deps on play
 var apiLink = document.querySelector('link[rel="media-api-uri"]');
@@ -38,6 +36,8 @@ var kahuna = angular.module('kahuna', [
     'util.async',
     'util.digest',
     'analytics.track',
+    'kahuna.crop',
+    'kahuna.image',
     'kahuna.upload',
     'kahuna.search',
     'kahuna.edits',
@@ -58,21 +58,8 @@ kahuna.config(['$locationProvider',
     $locationProvider.html5Mode(true).hashPrefix('!');
 }]);
 
-
-kahuna.config(['$stateProvider', '$urlRouterProvider',
-               function($stateProvider, $urlRouterProvider) {
-
-    $stateProvider.state('image', {
-        url: '/images/:imageId?crop',
-        template: imageTemplate,
-        controller: 'ImageCtrl'
-    });
-
-    $stateProvider.state('crop', {
-        url: '/images/:imageId/crop',
-        template: cropTemplate,
-        controller: 'ImageCropCtrl as imageCropCtrl'
-    });
+kahuna.config(['$urlRouterProvider',
+               function($urlRouterProvider) {
 
     $urlRouterProvider.otherwise("/search");
 }]);
@@ -127,98 +114,6 @@ kahuna.controller('SessionCtrl',
     });
 }]);
 
-
-kahuna.controller('ImageCtrl',
-                  ['$scope', '$stateParams', 'mediaApi', 'mediaCropper',
-                   function($scope, $stateParams, mediaApi, mediaCropper) {
-
-    var imageId = $stateParams.imageId;
-    $scope.cropKey = $stateParams.crop;
-
-    mediaApi.find(imageId).then(function(image) {
-        $scope.image = image;
-
-        // FIXME: we need not to use imageSync but find a way to use the promised URI
-        image.uri.then(uri => $scope.imageSync = {uri: uri, data: image.data});
-
-        mediaCropper.getCropsFor(image).then(function(crops) {
-           $scope.crops = crops;
-           $scope.crop = crops.find(crop => crop.id === $scope.cropKey);
-        });
-    });
-
-    var ignoredMetadata = ['description', 'source', 'copyright', 'keywords'];
-    $scope.isUsefulMetadata = function(metadataKey) {
-        return ignoredMetadata.indexOf(metadataKey) === -1;
-    };
-
-    $scope.priorityMetadata = ['byline', 'credit'];
-}]);
-
-
-kahuna.controller('ImageCropCtrl',
-                  ['$scope', '$stateParams', '$state', '$filter', 'mediaApi', 'mediaCropper',
-                   function($scope, $stateParams, $state, $filter, mediaApi, mediaCropper) {
-
-    var imageId = $stateParams.imageId;
-
-    mediaApi.find(imageId).then(function(image) {
-        $scope.image = image;
-    });
-
-    $scope.cropping = false;
-
-    // Standard ratios
-    $scope.landscapeRatio = 5 / 3;
-    $scope.portraitRatio = 2 / 3;
-    $scope.freeRatio = null;
-
-    // TODO: migrate the other properties to be on the ctrl (this) instead of $scope
-    this.aspect = $scope.landscapeRatio;
-    $scope.coords = {
-        x1: 0,
-        y1: 0,
-        // max out to fill the image with the selection
-        x2: 10000,
-        y2: 10000
-    };
-
-    $scope.crop = function() {
-        // TODO: show crop
-        var coords = {
-            x: $scope.coords.x1,
-            y: $scope.coords.y1,
-            width:  $scope.coords.x2 - $scope.coords.x1,
-            height: $scope.coords.y2 - $scope.coords.y1
-        };
-
-        var ratio;
-        if (Number(this.aspect) === $scope.landscapeRatio) {
-            ratio = '5:3';
-        } else if (Number(this.aspect) === $scope.portraitRatio) {
-            ratio = '3:2';
-        }
-
-        $scope.cropping = true;
-
-        mediaCropper.createCrop($scope.image, coords, ratio).then(function(crop) {
-            // Global notification of action
-            $scope.$emit('events:crop-created', {
-                image: $scope.image,
-                crop: crop
-            });
-
-            $state.go('image', {
-                imageId: imageId,
-                crop: crop.data.id
-            });
-        // FIXME: traceur promises don't have finally?
-        }).finally(function() {
-            $scope.cropping = false;
-        });
-    }.bind(this);
-
-}]);
 
 kahuna.filter('getExtremeAssets', function() {
     return function(image) {
@@ -314,7 +209,7 @@ kahuna.directive('uiHasSpace', ['$window', function($window) {
                 scope[attrs.uiHasSpace] = el.clientHeight + el.offsetTop <= $window.innerHeight;
             });
         }
-    }
+    };
 }]);
 
 kahuna.directive('uiNearBottom', ['$window', function($window) {
