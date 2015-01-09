e31d0d69f89f189f3a707cf2f7e0e1b29b1335ca
James A Gorrie <james.gorrie@theguardian.com>
2015-01-09 17:17:11 +0000

    remove debugging

--- kahuna/public/js/edits/archiver.js
+++ kahuna/public/js/edits/archiver.js
@@ -19,7 +19,6 @@ archiver.controller('ArchiverCtrl', ['$scope', '$window',
 
         // FIXME: theseus should return a `Resource` on `put` that we can
         // update `ctrl.archived` with.
-        console.log(ctrl.archived)
         ctrl.archived
             .put({ data: setVal })
             .response.then(
