d4dc190007a5f19edf191135a39411bbf6d8127f
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-01-18 18:41:11 +0000

    Run tests as part of dist script

--- kahuna/dist.sh
+++ kahuna/dist.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo Running tests
+npm test || { echo "##teamcity[message text='npm test error' status='ERROR']"; exit 1; }
+
 echo Building JSPM bundle
 node_modules/.bin/jspm bundle js/main public/js/dist/build.js -m --inject
 
