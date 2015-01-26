6c90205436441ed6f645b4c238d09e7100d5a749
James A Gorrie <james.gorrie@theguardian.com>
2015-01-26 16:40:14 +0000

    Remove preview controller

--- kahuna/public/js/image/preview.js
+++ kahuna/public/js/image/preview.js
@@ -4,17 +4,13 @@ import template from './preview.html!text';
 
 export var preview = angular.module('kahuna.image.preview', []);
 
-preview.controller('PreviewCtrl', function() {});
-
 preview.directive('uiPreview', function() {
     return {
         restrict: 'E',
-        controller: 'PreviewCtrl as ctrl',
         scope: {
             image: '=',
             hideInfo: '='
         },
-        bindToController: true,
         template: template
     };
 });
