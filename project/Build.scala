c55c7f164e0c98b6f9594b491d0c12727c53c668
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-01-27 22:20:14 +0000

    Bump Scala version to 0.11.5, minor syntax fix

--- project/Build.scala
+++ project/Build.scala
@@ -11,8 +11,8 @@ object Build extends Build {
 
   val commonSettings =
     Seq(
-      scalaVersion := "2.11.4",
-      scalaVersion in ThisBuild := "2.11.4",
+      scalaVersion := "2.11.5",
+      scalaVersion in ThisBuild := "2.11.5",
       organization := "com.gu",
       version      := "0.1",
       resolvers ++= Seq(
