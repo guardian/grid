68b1bea3f992445ccf4476ec910b1568493ce855
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-02-03 15:11:30 +0000

    :ingest command

--- picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/lib/db/ExportDynamoDB.scala
+++ picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/lib/db/ExportDynamoDB.scala
@@ -18,6 +18,12 @@ import play.api.libs.concurrent.Execution.Implicits._
 import scala.collection.JavaConversions._
 import scala.concurrent.Future
 
+case class AssetRow(
+  picdarUrn: String,
+  picdarCreated: DateTime,
+  picdarAssetUrl: URI
+)
+
 class ExportDynamoDB(credentials: AWSCredentials, region: Region, tableName: String)
   extends DynamoDB(credentials, region, tableName) {
 
@@ -35,6 +41,9 @@ class ExportDynamoDB(credentials: AWSCredentials, region: Region, tableName: Str
   val rangeDateFormat = ISODateTimeFormat.date
   def asRangeString(dateTime: DateTime) = rangeDateFormat print dateTime
 
+  val timestampDateFormat = ISODateTimeFormat.dateTimeNoMillis
+  def asTimestampString(dateTime: DateTime) = timestampDateFormat print dateTime
+
   def scanUnfetched(dateRange: DateRange): Future[Seq[AssetRef]] = Future {
     // FIXME: query by range only?
     val queryConds = List("attribute_not_exists(picdarAssetUrl)") ++
@@ -48,7 +57,43 @@ class ExportDynamoDB(credentials: AWSCredentials, region: Region, tableName: Str
     val query = queryConds.mkString(" AND ")
     val items = table.scan(query, "picdarUrn, picdarCreated", null, values)
     items.iterator.map { item =>
-      AssetRef(item.getString("picdarUrn"), DateTime.parse(item.getString("picdarCreated")))
+      AssetRef(item.getString("picdarUrn"), rangeDateFormat.parseDateTime(item.getString("picdarCreated")))
+    }.toSeq
+  }
+
+  def scanFetchedNotIngested(dateRange: DateRange): Future[Seq[AssetRow]] = Future {
+    // FIXME: query by range only?
+    val queryConds = List("attribute_exists(picdarAssetUrl)", "attribute_not_exists(mediaUri)") ++
+      dateRange.start.map(date => s"picdarCreated >= :startDate") ++
+      dateRange.end.map(date => s"picdarCreated <= :endDate")
+
+    val values = Map() ++
+      dateRange.start.map(asRangeString).map(":startDate" -> _) ++
+      dateRange.end.map(asRangeString).map(":endDate" -> _)
+
+    val query = queryConds.mkString(" AND ")
+    val items = table.scan(query, "picdarUrn, picdarCreated, picdarAssetUrl", null, values)
+    items.iterator.map { item =>
+      val picdarCreated = rangeDateFormat.parseDateTime(item.getString("picdarCreated"))
+      AssetRow(item.getString("picdarUrn"), picdarCreated, URI.create(item.getString("picdarAssetUrl")))
+    }.toSeq
+  }
+
+  def scanIngested(dateRange: DateRange): Future[Seq[AssetRow]] = Future {
+    // FIXME: query by range only?
+    val queryConds = List("attribute_exists(picdarAssetUrl)", "attribute_exists(mediaUri)") ++
+      dateRange.start.map(date => s"picdarCreated >= :startDate") ++
+      dateRange.end.map(date => s"picdarCreated <= :endDate")
+
+    val values = Map() ++
+      dateRange.start.map(asRangeString).map(":startDate" -> _) ++
+      dateRange.end.map(asRangeString).map(":endDate" -> _)
+
+    val query = queryConds.mkString(" AND ")
+    val items = table.scan(query, "picdarUrn, picdarCreated, picdarAssetUrl", null, values)
+    items.iterator.map { item =>
+      val picdarCreated = rangeDateFormat.parseDateTime(item.getString("picdarCreated"))
+      AssetRow(item.getString("picdarUrn"), picdarCreated, URI.create(item.getString("picdarAssetUrl")))
     }.toSeq
   }
 
@@ -64,6 +109,18 @@ class ExportDynamoDB(credentials: AWSCredentials, region: Region, tableName: Str
     // TODO: store all
     // TODO: bump assetUrlModifiedAt
     // TODO: bump metadataModifiedAt
+  }
+
+  def recordIngested(urn: String, range: DateTime, mediaUri: URI) = Future {
+    val baseUpdateSpec = new UpdateItemSpec().
+      withPrimaryKey(IdKey, urn, RangeKey, asRangeString(range)).
+      withUpdateExpression("SET mediaUri = :mediaUri, mediaUriModified = :mediaUriModified").
+      withValueMap(new ValueMap().
+        withString(":mediaUri", mediaUri.toString).
+        withString(":mediaUriModified", asTimestampString(new DateTime))).
+      withReturnValues(ReturnValue.ALL_NEW)
+
+    table.updateItem(baseUpdateSpec)
   }
 
 }
