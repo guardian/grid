181cee2760929d4f368bd6c60a998b8e8a1162f8
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-02-03 15:10:25 +0000

    record now takes a URI

--- picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/lib/db/ExportDynamoDB.scala
+++ picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/lib/db/ExportDynamoDB.scala
@@ -1,5 +1,7 @@
 package com.gu.mediaservice.picdarexport.lib.db
 
+import java.net.URI
+
 import com.amazonaws.auth.AWSCredentials
 import com.amazonaws.regions.Region
 import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec
@@ -50,11 +52,11 @@ class ExportDynamoDB(credentials: AWSCredentials, region: Region, tableName: Str
     }.toSeq
   }
 
-  def record(urn: String, range: DateTime, assetUrl: String, picdarCreated: DateTime, picdarModified: Option[DateTime], metadata: Map[String, String]) = Future {
+  def record(urn: String, range: DateTime, assetUrl: URI, picdarCreated: DateTime, picdarModified: Option[DateTime], metadata: Map[String, String]) = Future {
     val baseUpdateSpec = new UpdateItemSpec().
       withPrimaryKey(IdKey, urn, RangeKey, asRangeString(range)).
       withUpdateExpression("SET picdarAssetUrl = :picdarAssetUrl").
-      withValueMap(new ValueMap().withString(":picdarAssetUrl", assetUrl)).
+      withValueMap(new ValueMap().withString(":picdarAssetUrl", assetUrl.toString)).
       withReturnValues(ReturnValue.ALL_NEW)
 
     table.updateItem(baseUpdateSpec)
