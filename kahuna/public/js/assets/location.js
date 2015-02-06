5491202e3f18c0f81c679703eed06c48087ddca2
James A Gorrie <james.gorrie@theguardian.com>
2015-02-06 10:30:05 +0000

    remove asset location from main -> image preview

--- kahuna/public/js/assets/location.js
+++ kahuna/public/js/assets/location.js
@@ -2,6 +2,6 @@ import angular from 'angular';
 
 export var assetLocation = angular.module('kahuna.assets.location', []);
 
-assetLocation.filter('assetLocation', ['assetRoot', function(assetRoot) {
+assetLocation.filter('assetLocation', ['assetsRoot', function(assetsRoot) {
     return loc => assetsRoot + loc;
 }]);
