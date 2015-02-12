e9a16a68442a9cd59a99ab502164f5377b407ca6
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-02-12 15:12:50 +0000

    More precise errors to distinguish download vs upload failed

--- picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/PicdarExport.scala
+++ picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/PicdarExport.scala
@@ -279,7 +279,7 @@ object ExportApp extends App with ExportManagerProvider with ArgumentHelpers wit
 
         val updates = assets.drop(rangeStart).take(rangeLen).map { asset =>
           getExportManager("library", env).ingest(asset.picdarAssetUrl, asset.picdarUrn, asset.picdarCreated) flatMap { mediaUri =>
-            println(s"Ingested ${asset.picdarUrn} to $mediaUri")
+            Logger.info(s"Ingested ${asset.picdarUrn} to $mediaUri")
             dynamo.recordIngested(asset.picdarUrn, asset.picdarCreated, mediaUri)
           } recover { case e: Throwable =>
             Logger.warn(s"Upload error for ${asset.picdarUrn}: $e")
