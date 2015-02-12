d8f03de4fba294035702cda20ed6f271dbc27008
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-02-12 15:12:50 +0000

    More precise errors to distinguish download vs upload failed

--- picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/lib/picdar/PicdarClient.scala
+++ picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/lib/picdar/PicdarClient.scala
@@ -1,6 +1,6 @@
 package com.gu.mediaservice.picdarexport.lib.picdar
 
-import java.net.URI
+import java.net.{SocketTimeoutException, URI}
 
 import com.gu.mediaservice.picdarexport.lib.HttpClient
 import com.gu.mediaservice.picdarexport.model.{AssetRef, Asset, DateRange}
@@ -8,12 +8,17 @@ import com.gu.mediaservice.picdarexport.model.{AssetRef, Asset, DateRange}
 import scala.concurrent.Future
 import com.gu.mediaservice.picdarexport.lib.ExecutionContexts
 
+case class PicdarAssetReadError(message: String) extends Exception(message)
+
 trait PicdarClient extends PicdarApi with HttpClient {
 
   // Use picdar pool for all operations by default
   import ExecutionContexts.picdar
 
-  def getAssetData(assetUri: URI): Future[Array[Byte]] = readBytes(assetUri)(ExecutionContexts.picdarAsset)
+  def getAssetData(assetUri: URI): Future[Array[Byte]] =
+    readBytes(assetUri)(ExecutionContexts.picdarAsset) recoverWith {
+      case ex: SocketTimeoutException => Future.failed(PicdarAssetReadError(ex.getMessage))
+    }
 
   def get(urn: String): Future[Asset] =
     for {
