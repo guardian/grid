e6b2ebb572b85e72eff760deaf204f72d48106c5
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-02-15 21:56:57 +0000

    Clean use of scope and use ctrl instead

--- kahuna/public/js/image/index.js
+++ kahuna/public/js/image/index.js
@@ -18,6 +18,39 @@ image.config(['$stateProvider',
     $stateProvider.state('image', {
         url: '/images/:imageId?crop',
         template: imageTemplate,
-        controller: 'ImageCtrl'
+        controller: 'ImageCtrl',
+        controllerAs: 'ctrl',
+        resolve: {
+            imageId: ['$stateParams', $stateParams => $stateParams.imageId],
+            cropKey: ['$stateParams', $stateParams => $stateParams.crop],
+            image: ['mediaApi', 'imageId',
+                    (mediaApi, imageId) => {
+                return mediaApi.find(imageId);
+            }]
+        }
     });
+
+    // FIXME: elsewhere:
+    $stateProvider.state('error', {
+        params: {message: {}},
+        template: 'Error: {{message}}',
+        controller: ['$scope', 'message', ($scope, message) => {
+            $scope.message = message;
+        }],
+        resolve: {
+            message: ['$stateParams', $stateParams => $stateParams.message]
+        }
+    });
+}]);
+
+image.run(['$rootScope', '$state',
+           ($rootScope, $state) => {
+
+    $rootScope.$on('$stateChangeError',
+                   function(event, toState, toParams, fromState, fromParams, error) {
+        if (toState.name === 'image' && error && error.status === 404) {
+            $state.go('error', {message: "Image not found"});
+        }
+    });
+
 }]);
