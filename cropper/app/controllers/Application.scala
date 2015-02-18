390a215107e8d0310af30371c62f5191c54174d9
James A Gorrie <james.gorrie@theguardian.com>
2015-02-17 21:19:10 -0500

    remove the portrait sizings

--- cropper/app/controllers/Application.scala
+++ cropper/app/controllers/Application.scala
@@ -86,10 +86,7 @@ object Application extends Controller with ArgoHelpers {
       Bounds(_, _, masterW, masterH) = source.bounds
       aspect     = masterW.toFloat / masterH
       portrait   = masterW < masterH
-      outputDims = if (portrait)
-        Config.portraitCropSizingHeights.filter(_ <= masterH).map(h => Dimensions(math.round(h * aspect), h))
-      else
-        Config.landscapeCropSizingWidths.filter(_ <= masterW).map(w => Dimensions(w, math.round(w / aspect)))
+      outputDims = Config.landscapeCropSizingWidths.filter(_ <= masterW).map(w => Dimensions(w, math.round(w / aspect)))
 
       sizings <- Future.traverse(outputDims) { dim =>
         val filename = outputFilename(apiImage, source.bounds, dim.width)
