1f88b2bd123543f9fc2fb8ae720f1c5de7d09923
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-02-22 00:38:30 +0000

    Delete UpdateStack scripts (moved to grid-infra)

--- scripts/src/main/scala/com/gu/mediaservice/scripts/Main.scala
+++ scripts/src/main/scala/com/gu/mediaservice/scripts/Main.scala
@@ -4,9 +4,6 @@ package com.gu.mediaservice.scripts
 object Main extends App {
 
   args.toList match {
-    case "UpdateStack"      :: as => UpdateStack(as)
-    case "CreateStack"      :: as => CreateStack(as)
-    case "DeleteStack"      :: as => DeleteStack(as)
     case "LoadFromS3Bucket" :: as => LoadFromS3Bucket(as)
     case "Reindex"          :: as => Reindex(as)
     case "UpdateMapping"    :: as => UpdateMapping(as)
