fdbf14535ca51f06866664b8cc73d667e660bfd3
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-02-23 17:47:08 +0000

    Rename execution context for clarity

--- picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/lib/ExecutionContexts.scala
+++ picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/lib/ExecutionContexts.scala
@@ -8,5 +8,5 @@ object ExecutionContexts {
   // Rely on global pool for now, seems to hold
   implicit val picdar       = ExecutionContext.Implicits.global
   implicit val picdarAsset  = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Config.concurrencyPicdarAsset))
-  implicit val mediaService = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Config.concurrencyMediaLoader))
+  implicit val mediaLoader  = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Config.concurrencyMediaLoader))
 }
