b245281ce891e2890c11cd1473714a37147835a7
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-03-16 16:01:42 +0000

    Less ambitious cleaners, just strip single-token byline from credit

--- common-lib/src/test/scala/com/gu/mediaservice/lib/cleanup/StripBylineFromCreditTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/cleanup/StripBylineFromCreditTest.scala
@@ -11,27 +11,6 @@ class StripBylineFromCreditTest extends FunSpec with Matchers with MetadataHelpe
     cleanedMetadata.credit should be (None)
   }
 
-  it("should leave byline and credit as they are if each made of a single token") {
-    val metadata = bylineAndCredit("Huw Evans", "REX")
-    val cleanedMetadata = StripBylineFromCredit.clean(metadata)
-    cleanedMetadata.byline should be (Some("Huw Evans"))
-    cleanedMetadata.credit should be (Some("REX"))
-  }
-
-  it("should leave byline and credit as they are if equal and made of two tokens (agency name)") {
-    val metadata = bylineAndCredit("Startraks Photo/REX", "Startraks Photo/REX")
-    val cleanedMetadata = StripBylineFromCredit.clean(metadata)
-    cleanedMetadata.byline should be (Some("Startraks Photo/REX"))
-    cleanedMetadata.credit should be (Some("Startraks Photo/REX"))
-  }
-
-  it("should leave byline and credit as they are if equal and made of two tokens (actual photographer name)") {
-    val metadata = bylineAndCredit("Huw Evans/REX", "Huw Evans/REX")
-    val cleanedMetadata = StripBylineFromCredit.clean(metadata)
-    cleanedMetadata.byline should be (Some("Huw Evans/REX"))
-    cleanedMetadata.credit should be (Some("Huw Evans/REX"))
-  }
-
   it("should strip the byline from the credit if a single name") {
     val metadata = bylineAndCredit("Guillermo Arias", "Guillermo Arias/Xinhua Press/Corbis")
     val cleanedMetadata = StripBylineFromCredit.clean(metadata)
@@ -53,18 +32,49 @@ class StripBylineFromCreditTest extends FunSpec with Matchers with MetadataHelpe
     cleanedMetadata.credit should be (Some("Corbis"))
   }
 
-  it("should extract the first token as byline and the rest as credit when both are the same and made of more than two tokens") {
-    val metadata = bylineAndCredit("Kieran McManus/BPI/REX", "Kieran McManus/BPI/REX")
+  // Note: leaving quite a lot of these no-op transformation tests for reference
+  // and if we find a clever way to cleanup more by distinguishing agency from photographer prefixes
+
+  it("should leave byline and credit as they are if each made of a single token") {
+    val metadata = bylineAndCredit("Huw Evans", "REX")
     val cleanedMetadata = StripBylineFromCredit.clean(metadata)
-    cleanedMetadata.byline should be (Some("Kieran McManus"))
-    cleanedMetadata.credit should be (Some("BPI/REX"))
+    cleanedMetadata.byline should be (Some("Huw Evans"))
+    cleanedMetadata.credit should be (Some("REX"))
   }
 
-  it("should extract the first token as byline and the rest as credit when the byline is a prefix of the credit") {
+  it("should leave byline and credit as they are if equal and made of two tokens (agency name)") {
+    val metadata = bylineAndCredit("Startraks Photo/REX", "Startraks Photo/REX")
+    val cleanedMetadata = StripBylineFromCredit.clean(metadata)
+    cleanedMetadata.byline should be (Some("Startraks Photo/REX"))
+    cleanedMetadata.credit should be (Some("Startraks Photo/REX"))
+  }
+
+  it("should leave byline and credit as they are if equal and made of two tokens (actual photographer name)") {
+    val metadata = bylineAndCredit("Huw Evans/REX", "Huw Evans/REX")
+    val cleanedMetadata = StripBylineFromCredit.clean(metadata)
+    cleanedMetadata.byline should be (Some("Huw Evans/REX"))
+    cleanedMetadata.credit should be (Some("Huw Evans/REX"))
+  }
+
+  it("should leave byline and credit as they are if equal and made of more than two tokens (agency name)") {
+    val metadata = bylineAndCredit("Warner Bros/Courtesy Everet/REX", "Warner Bros/Courtesy Everet/REX")
+    val cleanedMetadata = StripBylineFromCredit.clean(metadata)
+    cleanedMetadata.byline should be (Some("Warner Bros/Courtesy Everet/REX"))
+    cleanedMetadata.credit should be (Some("Warner Bros/Courtesy Everet/REX"))
+  }
+
+  it("should leave byline and credit as they are if equal and made of more than two tokens (actual photographer name)") {
+    val metadata = bylineAndCredit("Kieran McManus/BPI/REX", "Kieran McManus/BPI/REX")
+    val cleanedMetadata = StripBylineFromCredit.clean(metadata)
+    cleanedMetadata.byline should be (Some("Kieran McManus/BPI/REX"))
+    cleanedMetadata.credit should be (Some("Kieran McManus/BPI/REX"))
+  }
+
+  it("should leave byline and credit as they are if the byline is a prefix of the credit") {
     val metadata = bylineAndCredit("Eduardo Valente/Frame/AGENCIA", "Eduardo Valente/Frame/AGENCIA/Xinhua Press/Corbis")
     val cleanedMetadata = StripBylineFromCredit.clean(metadata)
-    cleanedMetadata.byline should be (Some("Eduardo Valente"))
-    cleanedMetadata.credit should be (Some("Frame/AGENCIA/Xinhua Press/Corbis"))
+    cleanedMetadata.byline should be (Some("Eduardo Valente/Frame/AGENCIA"))
+    cleanedMetadata.credit should be (Some("Eduardo Valente/Frame/AGENCIA/Xinhua Press/Corbis"))
   }
 
 
