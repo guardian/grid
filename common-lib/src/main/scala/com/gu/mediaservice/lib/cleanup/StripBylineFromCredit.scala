113ac04ef3c07127bc0aa9feb32cf1ee27f5968e
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-03-16 16:01:42 +0000

    Less ambitious cleaners, just strip single-token byline from credit

--- common-lib/src/main/scala/com/gu/mediaservice/lib/cleanup/StripBylineFromCredit.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/cleanup/StripBylineFromCredit.scala
@@ -5,27 +5,17 @@ import com.gu.mediaservice.model.ImageMetadata
 object StripBylineFromCredit extends MetadataCleaner {
 
   override def clean(metadata: ImageMetadata): ImageMetadata = (metadata.credit, metadata.byline) match {
-    case (Some(credit), Some(byline)) => {
-      val (c, b) = stripBylinePrefix(credit, byline)
-      metadata.copy(credit = Some(c), byline = Some(b))
-    }
+    case (Some(credit), Some(byline)) => metadata.copy(credit = Some(stripBylinePrefix(credit, byline)))
     case _ => metadata
   }
 
-  def stripBylinePrefix(credit: String, byline: String): (String, String) = (tokenise(credit), tokenise(byline)) match {
-    // Fields equal, but too few tokens to guarantee first one is byline (e.g. both "Startraks Photo/REX")
-    case (c, b) if c == b && c.size < 3 => (credit, byline)
-    // Fields equal or byline prefix of credit, with enough tokens to assume
-    // the first token is the byline (e.g. "Kieran McManus/BPI/REX").
-    // Recursively strip byline prefix in case it appears multiple times (I know, right?).
-    case (c, b) if normalise(c).startsWith(normalise(b)) => stripBylinePrefix(c.tail.mkString("/"), b.head)
-    // Else leave fields as they are
-    case _ => (credit, byline)
+  def stripBylinePrefix(credit: String, byline: String): String = (tokenise(credit), tokenise(byline)) match {
+    // If byline is a single token and a prefix of credit, strip from credit.
+    // Strip recursively in case byline is present in credit multiple times.
+    case (headC :: tailC, b :: Nil) if headC.toLowerCase == b.toLowerCase => stripBylinePrefix(tailC.mkString("/"), b)
+    // Else leave credit as-is
+    case _ => credit
   }
 
-  // Note: use lists so Lists can be compared
   def tokenise(str: String): List[String] = str.split("/").toList
-
-  def normalise(list: List[String]): List[String] = list.map(normalise)
-  def normalise(str: String): String = str.toLowerCase
 }
