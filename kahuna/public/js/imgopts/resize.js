bb0be73337dad10b9145c51e53a74b8a37db2bb0
James A Gorrie <james.gorrie@theguardian.com>
2015-03-16 17:31:12 +0000

    imgopts resize directive

--- kahuna/public/js/imgopts/resize.js
+++ kahuna/public/js/imgopts/resize.js
@@ -2,26 +2,42 @@ import angular from 'angular';
 
 export var resize = angular.module('imgopts.resize', []);
 
-resize.filter('imgResize', ['imgoptsUri', 'imgGet', (imgoptsUri, imgGet) => {
-    return filepath => {
-        var u = new URL(filepath);
-        var resizeValues = imgGet.getResizeValues();
+resize.directive('imgResize', ['imgOpts', imgOpts => {
+    return {
+        restrict: 'A',
+        scope: {
+            src: '@imgSrc',
+            dimensions: '=imgDimensions'
+        },
+        link: (scope, element) => {
+            element.attr('src', imgOpts.getSrc(scope.src));
+        }
+    };
+}]);
+
+
+resize.factory('imgOpts', ['imgoptsUri', imgoptsUri => {
+    var getResizeValues = () => {
+        return {
+            w: 500,
+            h: 500,
+            q: 500
+        }
+    };
+
+    var getSrc = uri => {
+        var u = new URL(uri);
+        var resizeValues = getResizeValues();
         var resizeParams = Object.keys(resizeValues).map(key => [key, resizeValues[key]].join('='));
 
         var qs = u.search.split('&').concat(resizeParams).join('&');
 
         return imgoptsUri + u.pathname + qs;
-    }
-}]);
+    };
 
-resize.factory('imgGet', [() => {
+
     return {
-        getResizeValues: () => {
-            return {
-                w: 500,
-                h: 500,
-                q: 500
-            }
-        }
+        getSrc,
+        getResizeValues
     }
 }]);
