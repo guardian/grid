2dbd37d34b449667d60326f479cec5e1543e3312
James A Gorrie <james.gorrie@theguardian.com>
2015-03-17 17:47:06 +0000

    move resolvers to crop route

--- kahuna/public/js/crop/index.js
+++ kahuna/public/js/crop/index.js
@@ -16,6 +16,24 @@ crop.config(['$stateProvider',
     $stateProvider.state('crop', {
         url: '/images/:imageId/crop',
         template: cropTemplate,
-        controller: 'ImageCropCtrl as imageCropCtrl'
+        controller: 'ImageCropCtrl as imageCropCtrl',
+        resolve: {
+            // TODO: abstract these resolvers out as we use them on the image
+            // view too
+            imageId: ['$stateParams', $stateParams => $stateParams.imageId],
+            image: ['$state', '$q', 'mediaApi', 'imageId',
+                    ($state, $q, mediaApi, imageId) => {
+
+                return mediaApi.find(imageId).catch(error => {
+                    if (error && error.status === 404) {
+                        $state.go('image-not-found', {message: 'Image not found'});
+                    } else {
+                        $q.reject(error);
+                    }
+                });
+            }],
+            optimisedImageUri: ['image', 'imgopts', (image, imgopts) => imgopts.getUri(image)]
+        }
     });
+
 }]);
