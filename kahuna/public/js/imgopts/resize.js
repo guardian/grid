84f129d2a3726adb90e11e7e685b349bef70428e
James A Gorrie <james.gorrie@theguardian.com>
2015-03-17 09:43:17 +0000

    image resizer on main image

--- kahuna/public/js/imgopts/resize.js
+++ kahuna/public/js/imgopts/resize.js
@@ -5,39 +5,32 @@ export var resize = angular.module('imgopts.resize', []);
 resize.directive('imgResize', ['imgOpts', imgOpts => {
     return {
         restrict: 'A',
-        scope: {
-            src: '@imgSrc',
-            dimensions: '=imgDimensions'
-        },
-        link: (scope, element) => {
-            element.attr('src', imgOpts.getSrc(scope.src));
+        link: (scope, element, attrs) => {
+            // we're using the attrs here as if we use scope we land up into
+            // trouble trying to acces the same scope values as other directives.
+            // Damn lack of one way data binding:
+            // https://docs.angularjs.org/error/$compile/multidir
+            element.attr('src', imgOpts.getSrc(attrs.imgSrc));
         }
     };
 }]);
 
 
-resize.factory('imgOpts', ['imgoptsUri', imgoptsUri => {
+resize.factory('imgOpts', ['$window', 'imgoptsUri', ($window, imgoptsUri) => {
+
     var getResizeValues = () => {
-        return {
-            w: 500,
-            h: 500,
-            q: 500
-        }
+        var { width: w, height: h } = $window.screen;
+        return { w, h, q: 95 };
     };
 
     var getSrc = uri => {
         var u = new URL(uri);
         var resizeValues = getResizeValues();
         var resizeParams = Object.keys(resizeValues).map(key => [key, resizeValues[key]].join('='));
-
         var qs = u.search.split('&').concat(resizeParams).join('&');
 
         return imgoptsUri + u.pathname + qs;
     };
 
-
-    return {
-        getSrc,
-        getResizeValues
-    }
+    return { getSrc };
 }]);
