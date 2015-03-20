0c14beb68961f7c823b0612dd577c21cb51361ef
James A Gorrie <james.gorrie@theguardian.com>
2015-03-20 11:32:53 +0000

    decrease imops quality

--- kahuna/public/js/imgops/service.js
+++ kahuna/public/js/imgops/service.js
@@ -3,11 +3,12 @@ import angular from 'angular';
 export var imgops = angular.module('kahuna.imgops', []);
 
 imgops.factory('imgops', ['$window', function($window) {
+    var quality = 85;
 
     var getUri = image => {
         var { width: w, height: h } = $window.screen;
 
-        return image.follow('optimised', { w, h, q: 95 }).getUri().catch(() => {
+        return image.follow('optimised', { w, h, q: quality }).getUri().catch(() => {
             return image.source.secureUrl || image.source.file;
         });
     };
