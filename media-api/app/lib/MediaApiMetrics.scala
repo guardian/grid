1a8c2cd157398f72361a00e1a6f7e824cb17ad91
James A Gorrie <james.gorrie@theguardian.com>
2015-03-25 10:58:59 +0000

    dimensionalise ES metrics

--- media-api/app/lib/MediaApiMetrics.scala
+++ media-api/app/lib/MediaApiMetrics.scala
@@ -1,11 +1,14 @@
 package lib
 
+import com.amazonaws.services.cloudwatch.model.Dimension
 import com.gu.mediaservice.lib.metrics.CloudWatchMetrics
 import Config.{awsCredentials, stage}
 
 object MediaApiMetrics extends CloudWatchMetrics(s"$stage/MediaApi", awsCredentials) {
 
   val searchQueries = new TimeMetric("ElasticSearch")
-  val aggregateSearchQueries = new TimeMetric("ElasticSearchAggregate")
+
+  def searchTypeDimension(value: String): Dimension =
+    new Dimension().withName("SearchType").withValue(value)
 
 }
