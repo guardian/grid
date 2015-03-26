5967adfb02917e56a6a020c7fb0ef824a44dc20e
James A Gorrie <james.gorrie@theguardian.com>
2015-03-26 15:13:39 +0000

    tidy up some lose ends in metadata editor

--- metadata-editor/app/model/edits.scala
+++ metadata-editor/app/model/edits.scala
@@ -3,13 +3,12 @@ package model
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-case class Edits(archived: Boolean, labels: List[String], rightsNotices: List[String], metadata: Metadata)
+case class Edits(archived: Boolean, labels: List[String], metadata: Metadata)
 
 object Edits {
   implicit val EditsReads: Reads[Edits] = (
     (__ \ "archived").readNullable[Boolean].map(_ getOrElse false) ~
     (__ \ "labels").readNullable[List[String]].map(_ getOrElse Nil) ~
-    (__ \ "rightsNotices").readNullable[List[String]].map(_ getOrElse Nil) ~
     (__ \ "metadata").readNullable[Metadata].map(_ getOrElse Metadata())
   )(Edits.apply _)
 
@@ -17,17 +16,13 @@ object Edits {
   implicit val EditsWrites: Writes[Edits] = (
       (__ \ "archived").write[Boolean] ~
       (__ \ "labels").write[List[String]] ~
-      (__ \ "rightsNotices").write[List[String]] ~
       (__ \ "metadata").writeNullable[Metadata].contramap(noneIfEmptyMetadata)
     )(unlift(Edits.unapply))
 
   def noneIfEmptyMetadata(m: Metadata) = if(m.isEmpty) None else Some(m)
 }
 
-
-
-
-// work with Picdar extra fields eg copyright
+// TODO: work with Picdar extra fields eg copyright
 case class Metadata(
   description: Option[String] = None,
   byline: Option[String] = None,
