2d0e97bdfd6cb5bb69ca9c8699ed60417ddf0e9d
Robert Kenny <kenoir@gmail.com>
2015-03-30 14:22:33 +0100

    kestrel

--- cropper/app/lib/imaging/Conversion.scala
+++ cropper/app/lib/imaging/Conversion.scala
@@ -6,6 +6,7 @@ import scala.concurrent.{Future, ExecutionContext}
 import org.im4java.core.{IMOperation, Operation, ConvertCmd}
 import lib.Config
 import model.{Bounds, Dimensions}
+import com.gu.mediaservice.syntax.{KestrelSyntax}
 
 
 object Conversion {
@@ -13,35 +14,19 @@ object Conversion {
   private implicit val ctx: ExecutionContext =
     ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Config.imagingThreadPoolSize))
 
-  def stripMeta(op: IMOperation) = {
-    op.strip()
+  def stripMeta(op: IMOperation) = op |< (_.strip())
 
-    op
-  }
+  def imageSource(source: File): IMOperation = (new IMOperation()) |< (_.addImage(source.getAbsolutePath))
 
-  def imageSource(source: File): IMOperation = {
-    val op = new IMOperation()
-    op.addImage(source.getAbsolutePath)
+  def addDestImage(op: IMOperation, dest: File) = op |< (_.addImage(dest.getAbsolutePath))
 
-    op
-  }
-
-  def addDestImage(op: IMOperation, dest: File) = {
-    op.addImage(dest.getAbsolutePath)
-
-    op
-  }
-
-  def cropResize(op: IMOperation, bounds: Bounds, dimensions: Dimensions): IMOperation = {
+  def cropResize(op: IMOperation, bounds: Bounds, dimensions: Dimensions): IMOperation = op |< { o =>
     val Bounds(x, y, w, h) = bounds
 
-    op.crop(w, h, x, y)
-    op.scale(dimensions.width, dimensions.height)
-    op.colorspace("RGB")
-
-    op
+    o.crop(w, h, x, y)
+    o.scale(dimensions.width, dimensions.height)
+    o.colorspace("RGB")
   }
 
   def runOp(op: IMOperation): Future[Unit] = Future((new ConvertCmd).run(op))
-
 }
