ccf61f1f104f1b37c415d7f0259abeb8b000c6da
Robert Kenny <kenoir@gmail.com>
2015-03-31 14:17:41 +0100

    start to extract metadata from file

--- cropper/app/lib/Crops.scala
+++ cropper/app/lib/Crops.scala
@@ -9,13 +9,22 @@ import model.{Dimensions, CropSource}
 
 
 object Crops {
-  import lib.imaging.Conversion._
+  import lib.imaging.Convert._
+  import lib.imaging.ExifTool
 
   /** Crops the source image and saves the output to a JPEG file on disk.
     *
     * It is the responsibility of the caller to clean up the file when it is no longer needed.
     */
-  def create(sourceFile: File, spec: CropSource, dimensions: Dimensions): Future[File] =
+  def create(sourceFile: File, spec: CropSource, dimensions: Dimensions): Future[File] = {
+    val s0 = ExifTool.tagSource(sourceFile)
+    val s1 = ExifTool.getTags(s0)("AuthorsPosition")
+    val s2 = ExifTool.getTags(s1)("Creator")
+
+    ExifTool.runOp(s2) onSuccess {
+      case tag => println(s"ohnoes: $tag whoop")
+    }
+
     for {
       outputFile <- createTempFile("cropOutput", ".jpg")
       source      = imageSource(sourceFile)
@@ -25,5 +34,6 @@ object Crops {
       _          <- runOp(addOutput)
     }
     yield outputFile
+  }
 
 }
--- cropper/app/lib/imaging/Conversion.scala
+++ cropper/app/lib/imaging/Conversion.scala
@@ -3,14 +3,58 @@ package lib.imaging
 import java.util.concurrent.Executors
 import java.io.File
 import scala.concurrent.{Future, ExecutionContext}
-import org.im4java.core.{IMOperation, Operation, ConvertCmd}
+import org.im4java.core.{ETOperation, IMOperation, Operation, ConvertCmd, ExiftoolCmd}
+import org.im4java.process.OutputConsumer
 import lib.Config
 import model.{Bounds, Dimensions}
 import com.gu.mediaservice.syntax.{KestrelSyntax}
+import java.io.InputStream
+import java.util.Scanner
 
+object ExifTool {
+  class ExifToolOutputConsumer extends OutputConsumer{
+    import scala.concurrent.ExecutionContext.Implicits.global
+    import akka.agent.Agent
 
-object Conversion {
+    val stringAgent: Agent[String] = Agent("")
+    def consumeOutput(is: InputStream) = {
+      stringAgent send convertStreamToString(is)
+    }
 
+    def get(): String = {
+      stringAgent.get()
+    }
+
+    private def convertStreamToString(is: InputStream): String = {
+      val s = new Scanner(is).useDelimiter("\\A");
+      if(s.hasNext()) s.next() else ""
+    }
+  }
+  case class StreamableExifToolCmd(cmd: ExiftoolCmd, consumer: ExifToolOutputConsumer)
+  object StreamableExifToolCmd {
+    def create(): StreamableExifToolCmd = {
+      val consumer = new ExifToolOutputConsumer()
+      val cmd = (new ExiftoolCmd()) |< (_.setOutputConsumer(consumer))
+
+      StreamableExifToolCmd(cmd, consumer)
+    }
+  }
+
+  private implicit val ctx: ExecutionContext =
+    ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Config.imagingThreadPoolSize))
+
+  def tagSource(source: File) = (new ETOperation()) |< (_.addImage(source.getAbsolutePath))
+  def getTags(op: ETOperation)(tags: String) = op |< (_.getTags(tags))
+
+  def runOp(op: ETOperation): Future[String] = {
+    val exifTool = StreamableExifToolCmd.create
+    exifTool.cmd.run(op)
+
+    exifTool.consumer.stringAgent.future
+  }
+}
+
+object Convert {
   private implicit val ctx: ExecutionContext =
     ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Config.imagingThreadPoolSize))
 
