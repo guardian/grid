c0a3a32a5f5e4549f08ce45708052e6cdd9df019
James A Gorrie <james.gorrie@theguardian.com>
2015-03-31 14:16:51 +0100

    add rights form with validation

--- metadata-editor/app/lib/Config.scala
+++ metadata-editor/app/lib/Config.scala
@@ -25,5 +25,5 @@ object Config extends CommonPlayAppProperties {
   val loginUri = services.loginUri
 
   val corsAllAllowedOrigins = List(services.kahunaBaseUri)
-
+  val freeRights: List[String] = List("handout", "PR image")
 }
