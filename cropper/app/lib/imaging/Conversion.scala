02796505d56acc5cfd2043d6eaf21392a02a1d00
Robert Kenny <kenoir@gmail.com>
2015-04-01 09:29:26 +0100

    cull the kestrel

--- cropper/app/lib/imaging/Conversion.scala
+++ cropper/app/lib/imaging/Conversion.scala
@@ -7,19 +7,19 @@ import org.im4java.core.{ETOperation, IMOperation, Operation, ConvertCmd, Exifto
 import org.im4java.process.OutputConsumer
 import lib.Config
 import model.{Bounds, Dimensions}
-import com.gu.mediaservice.syntax.{KestrelSyntax}
 import java.io.InputStream
 import java.util.Scanner
+import scalaz.syntax.id._
 
 
 object ExifTool {
   private implicit val ctx: ExecutionContext =
     ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Config.imagingThreadPoolSize))
 
-  def tagSource(source: File) = (new ETOperation()) |< (_.addImage(source.getAbsolutePath))
+  def tagSource(source: File) = (new ETOperation()) <| (_.addImage(source.getAbsolutePath))
 
   def setTags(ops: ETOperation)(tags: Map[String, String]): ETOperation =  {
-    tags.foldLeft(ops) { case (ops, (key, value)) => ops |< (_.setTags(s"$key=$value")) }
+    tags.foldLeft(ops) { case (ops, (key, value)) => ops <| (_.setTags(s"$key=$value")) }
   }
 
   def runExiftoolCmd(op: ETOperation): Future[Unit] = Future((new ExiftoolCmd).run(op))
@@ -29,13 +29,13 @@ object Convert {
   private implicit val ctx: ExecutionContext =
     ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Config.imagingThreadPoolSize))
 
-  def imageSource(source: File) = (new IMOperation()) |< (_.addImage(source.getAbsolutePath))
+  def imageSource(source: File) = (new IMOperation()) <| (_.addImage(source.getAbsolutePath))
 
-  def stripMeta(op: IMOperation) = op |< (_.strip())
+  def stripMeta(op: IMOperation) = op <| (_.strip())
 
-  def addDestImage(op: IMOperation)(dest: File) = op |< (_.addImage(dest.getAbsolutePath))
+  def addDestImage(op: IMOperation)(dest: File) = op <| (_.addImage(dest.getAbsolutePath))
 
-  def cropResize(op: IMOperation)(bounds: Bounds, dimensions: Dimensions): IMOperation = op |< { o =>
+  def cropResize(op: IMOperation)(bounds: Bounds, dimensions: Dimensions): IMOperation = op <| { o =>
     val Bounds(x, y, w, h) = bounds
 
     o.crop(w, h, x, y)
