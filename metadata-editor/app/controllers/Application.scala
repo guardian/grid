a66428edfac12aaca2227d6405c73a2f07167dd4
James A Gorrie <james.gorrie@theguardian.com>
2015-04-01 14:56:17 +0100

    remove superflous metadata links

--- metadata-editor/app/controllers/Application.scala
+++ metadata-editor/app/controllers/Application.scala
@@ -39,13 +39,11 @@ object Application extends Controller with ArgoHelpers {
   val indexResponse = {
     val indexData = Map("description" -> "This is the Metadata Editor Service")
     val indexLinks = List(
-      // TODO: replace "image-metadata" -> "metadata"
-      Link("metadata",       s"$rootUri/metadata/{id}"),
-      Link("edits",          s"$rootUri/metadata/{id}"),
-      Link("archived",       s"$rootUri/metadata/{id}/archived"),
-      Link("labels",         s"$rootUri/metadata/{id}/labels"),
-      Link("rights",         s"$rootUri/metadata/{id}/rights"),
-      Link("image-metadata", s"$rootUri/metadata/{id}/rights")
+      Link("edits",    s"$rootUri/metadata/{id}"),
+      Link("archived", s"$rootUri/metadata/{id}/archived"),
+      Link("labels",   s"$rootUri/metadata/{id}/labels"),
+      Link("rights",   s"$rootUri/metadata/{id}/rights"),
+      Link("metadata", s"$rootUri/metadata/{id}/rights")
     )
     respond(indexData, indexLinks)
   }
