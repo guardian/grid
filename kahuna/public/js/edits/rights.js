3020130b3128651264a53449a00d649e794b9982
James A Gorrie <james.gorrie@theguardian.com>
2015-04-02 10:40:47 +0100

    checkboxes for rights

--- kahuna/public/js/edits/rights.js
+++ kahuna/public/js/edits/rights.js
@@ -1,19 +1,40 @@
 import angular from 'angular';
 import template from './rights.html!text';
 
-export var rights = angular.module('kahuna.rights', []);
+export var rights = angular.module('kahuna.edits.rights', []);
 
 rights.controller('RightsCtrl', [function() {
-    
+
+    var ctrl = this;
+
+    // FIXME: quite stateful, quite crap
+    // TODO: perhaps look into abstracting a collection of radios into a directive
+    var rightsSet = new Set();
+    ctrl.rightExists = right => rightsSet.has(right);
+    ctrl.toggleRight = right => {
+        if (rightsSet.has(right)) {
+            rightsSet.delete(right);
+        } else {
+            rightsSet.add(right);
+        }
+    };
+
+    ctrl.rights = [
+        "PR image",
+        "handout"
+    ];
 }]);
 
 
-rights.directive('uiRights', [function() {
+rights.directive('gridRights', [function() {
     return {
         restrict: 'E',
         controller: 'RightsCtrl',
         controllerAs: 'ctrl',
         bindToController: true,
+        scope: {
+            //rights: '='
+        },
         template: template
     };
 }]);
