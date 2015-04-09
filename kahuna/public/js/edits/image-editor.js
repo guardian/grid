d5850db109a9aa9981394eb142ed95c13c26d903
James A Gorrie <james.gorrie@theguardian.com>
2015-04-09 19:05:48 +0100

    tidy up the image editor to just deal with saving states

--- kahuna/public/js/edits/image-editor.js
+++ kahuna/public/js/edits/image-editor.js
@@ -13,66 +13,26 @@ imageEditor.controller('ImageEditorCtrl',
                        ['$scope', '$q', 'poll', 'editsService',
                         function($scope, $q, poll, editsService) {
 
-    var pollFrequency = 500; // ms
-    var pollTimeout   = 20 * 1000; // ms
+    var ctrl = this;
 
-    this.status = this.image.data.valid ? 'ready' : 'invalid';
-    this.saving = false;
+    ctrl.status = ctrl.image.data.valid ? 'ready' : 'invalid';
 
-    editsService.on(
-        this.image.data.userMetadata.data.rights,
-        'update-start',
-        () => {
-            this.status = 'saving';
-            this.saving = true;
-        }
-    );
+    const rights = ctrl.image.data.userMetadata.data.rights;
+    const metadata = ctrl.image.data.userMetadata.data.metadata;
 
-    editsService.on(
-        this.image.data.userMetadata.data.rights,
-        'update-end',
-        () => {
-            this.image.get().then(newImage => {
-                this.image = newImage;
-                this.status = this.image.data.valid ? 'ready' : 'invalid';
-            });
-        }
-    );
+    editsService.on(rights, 'update-start', () => ctrl.status = 'saving');
+    editsService.on(rights, 'update-end', refreshImage);
 
-    // Watch the metadata for an update from `required-metadata-editor`.
-    // When the metadata is overridden, we don't know if the resulting
-    // image is valid or not. This code checks when the update has
-    // been processed and updates the status accordingly.
-    // TODO: Move this over to use the new `edits-service`
-    //$scope.$watch(() => this.image.data.userMetadata.data.metadata, (newMetadata, oldMetadata) => {
-    //    if (newMetadata !== oldMetadata) {
-    //
-    //        this.status = 'saving';
-    //        this.saving = true;
-    //
-    //        var metadataMatches = (image) => {
-    //            // FIXME: Hack for a modelling problem in edits. Keywords shouldn't
-    //            // be sent across, but they are as the `ImageMetadata.Writes` converts nothing
-    //            // into `Nil`.
-    //            var matches = Object.keys(newMetadata.data).every(key =>
-    //                angular.equals(newMetadata.data[key], image.data.metadata[key])
-    //            );
-    //
-    //            return matches ? image : $q.reject('no match');
-    //        };
-    //        var apiSynced = () => this.image.get().then(metadataMatches);
-    //
-    //        var whenIndexed = poll(apiSynced, pollFrequency, pollTimeout);
-    //        whenIndexed.then(image => {
-    //            this.status = image.data.valid ? 'ready' : 'invalid';
-    //
-    //            // FIXME: this is a bit of a hack to not trigger the watch again.
-    //            image.data.userMetadata.data.metadata = this.image.data.userMetadata.data.metadata;
-    //            this.image = image;
-    //        }).finally(() => this.saving = false);
-    //
-    //    }
-    //});
+    editsService.on(metadata, 'update-start', () => ctrl.status = 'saving')
+    editsService.on(metadata, 'update-end', refreshImage);
+
+    function refreshImage() {
+        return ctrl.image.get().then(newImage => {
+            ctrl.image = newImage;
+            ctrl.status = ctrl.image.data.valid ? 'ready' : 'invalid';
+        });
+    }
+
 }]);
 
 
