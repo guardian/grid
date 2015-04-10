4f5be8bdc15447487511859e86a2e910b8a89d59
Robert Kenny <kenoir@gmail.com>
2015-04-10 13:43:39 +0100

    replace cropsize with asset

--- common-lib/src/main/scala/com/gu/mediaservice/model/Asset.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/model/Asset.scala
@@ -1,25 +1,27 @@
 package com.gu.mediaservice.model
 
-import java.net.URI
+import java.net.{URI, URL}
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 
-case class Asset(file: URI, size: Long, mimeType: Option[String], dimensions: Option[Dimensions])
+case class Asset(file: URI, size: Long, mimeType: Option[String], dimensions: Option[Dimensions], secureUrl: Option[URL] = None)
 object Asset {
 
   implicit val assetReads: Reads[Asset] =
     ((__ \ "file").read[String].map(URI.create(_)) ~
       (__ \ "size").read[Long] ~
       (__ \ "mimeType").readNullable[String] ~
-      (__ \ "dimensions").readNullable[Dimensions]
+      (__ \ "dimensions").readNullable[Dimensions] ~
+      (__ \ "secureUrl").readNullable[String].map(_.map(new URL(_)))
       )(Asset.apply _)
 
   implicit val assetWrites: Writes[Asset] =
     ((__ \ "file").write[String].contramap((_: URI).toString) ~
       (__ \ "size").write[Long] ~
       (__ \ "mimeType").writeNullable[String] ~
-      (__ \ "dimensions").writeNullable[Dimensions]
+      (__ \ "dimensions").writeNullable[Dimensions] ~
+      (__ \ "secureUrl").writeNullable[String].contramap((_: Option[URL]).map(_.toString))
       )(unlift(Asset.unapply))
 
 }
