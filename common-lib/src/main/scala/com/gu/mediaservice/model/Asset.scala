8d42738071dca4f52ed3c172dec9db02ed30ab64
Robert Kenny <kenoir@gmail.com>
2015-04-10 09:49:16 +0100

    move SourceImage to common-lib

--- common-lib/src/main/scala/com/gu/mediaservice/model/Asset.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/model/Asset.scala
@@ -7,18 +7,29 @@ import play.api.libs.functional.syntax._
 
 case class Asset(file: URI, size: Long, mimeType: Option[String], dimensions: Option[Dimensions])
 object Asset {
-  implicit val AssetWrites: Writes[Asset] =
+
+  implicit val assetReads: Reads[Asset] =
+    ((__ \ "file").read[String].map(URI.create(_)) ~
+      (__ \ "size").read[Long] ~
+      (__ \ "mimeType").readNullable[String] ~
+      (__ \ "dimensions").readNullable[Dimensions]
+      )(Asset.apply _)
+
+  implicit val assetWrites: Writes[Asset] =
     ((__ \ "file").write[String].contramap((_: URI).toString) ~
       (__ \ "size").write[Long] ~
       (__ \ "mimeType").writeNullable[String] ~
       (__ \ "dimensions").writeNullable[Dimensions]
       )(unlift(Asset.unapply))
+
 }
 
 case class Dimensions(width: Int, height: Int)
 object Dimensions {
   implicit val dimensionsReads: Reads[Dimensions] = Json.reads[Dimensions]
   implicit val dimensionsWrites: Writes[Dimensions] =
-    ((__ \ "width").write[Int] ~ (__ \ "height").write[Int])(unlift(Dimensions.unapply))
+    ((__ \ "width").write[Int] ~
+      (__ \ "height").write[Int]
+      )(unlift(Dimensions.unapply))
 }
 
