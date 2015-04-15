b74608952ad7944b28e34bdd64caa1b50fb2d4e1
James A Gorrie <james.gorrie@theguardian.com>
2015-04-15 18:00:58 +0100

    remove usageRights description

--- common-lib/src/main/scala/com/gu/mediaservice/lib/elasticsearch/Mappings.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/elasticsearch/Mappings.scala
@@ -68,7 +68,6 @@ object Mappings {
   val usageRightsMapping = nonDynamicObj(
     "cost" -> nonAnalyzedString,
     "category" -> nonAnalyzedString,
-    "description" -> standardAnalysedString,
     "restrictions" -> standardAnalysedString
   )
 
