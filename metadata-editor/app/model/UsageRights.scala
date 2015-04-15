f62793b20be12836b25a57ce537b16dc0daf97d7
James A Gorrie <james.gorrie@theguardian.com>
2015-04-15 22:42:32 +0100

    use case objects for UsageRightsCategories

--- metadata-editor/app/model/UsageRights.scala
+++ metadata-editor/app/model/UsageRights.scala
@@ -5,9 +5,7 @@ import play.api.libs.functional.syntax._
 
 case class UsageRights(
   cost: Cost,
-  // TODO: Not sure if we should have `case object`s representing these as they
-  // might change more than, say, `Cost`
-  category: String,
+  category: UsageRightsCategory,
   restrictions: String
 )
 
@@ -18,11 +16,12 @@ object UsageRights {
   // Json writers, so we have to do this manually
   implicit val UsageRightsWrites: Writes[UsageRights] = (
     (__ \ "cost").write[String].contramap(costToString) ~
-    (__ \ "category").write[String] ~
+    (__ \ "category").write[String].contramap(categoryToString) ~
     (__ \ "restrictions").write[String]
   )(unlift(UsageRights.unapply))
 
   def costToString(c: Cost): String = c.toString
+  def categoryToString(c: UsageRightsCategory): String = c.toString
 }
 
 sealed trait Cost
@@ -44,3 +43,21 @@ object Cost {
   implicit val CostReads: Reads[Cost] = __.read[String].map(fromString)
 
 }
+
+
+sealed trait UsageRightsCategory
+
+object UsageRightsCategory {
+  def fromString(string: String): UsageRightsCategory =
+    // I think as we move forward we can find out what the more intelligent and
+    // correct default here. This feels better that reverting to `None` though as
+    // it's required by `UsageRights`.
+    // TODO: Perhaps we should validate on this?
+    Vector(PrImage).find(_.toString == string).getOrElse(PrImage)
+
+    implicit val UsageRightsCategoryReads: Reads[UsageRightsCategory] =
+      __.read[String].map(fromString)
+}
+
+case object PrImage
+  extends UsageRightsCategory { override def toString = "PR Image" }
