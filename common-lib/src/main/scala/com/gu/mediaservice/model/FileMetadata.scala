99ed18d6ba67966855601edfb5323174fcaed6f8
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-04-21 11:38:15 +0100

    Default FileMetadata maps to be empty for ease of use

--- common-lib/src/main/scala/com/gu/mediaservice/model/FileMetadata.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/model/FileMetadata.scala
@@ -3,11 +3,12 @@ package com.gu.mediaservice.model
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-case class FileMetadata(iptc: Map[String, String],
-                        exif: Map[String, String],
-                        exifSub: Map[String, String],
-                        xmp: Map[String, String],
-                        getty: Map[String, String]
+case class FileMetadata(
+  iptc: Map[String, String]    = Map(),
+  exif: Map[String, String]    = Map(),
+  exifSub: Map[String, String] = Map(),
+  xmp: Map[String, String]     = Map(),
+  getty: Map[String, String]   = Map()
 )
 
 object FileMetadata {
