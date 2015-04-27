f32d37389ee289183d782bc844402b6646c6737f
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-04-27 13:01:07 +0100

    Use RequestLoggingFilter in all apps

--- kahuna/app/Global.scala
+++ kahuna/app/Global.scala
@@ -3,13 +3,14 @@ import com.typesafe.config.ConfigValue
 
 import play.api.{Logger, Application, GlobalSettings}
 import play.api.mvc.WithFilters
-import play.api.libs.concurrent.Akka
 import play.filters.gzip.GzipFilter
 
-import controllers.Application
 import lib.{Config, ForceHTTPSFilter}
 
-object Global extends WithFilters(ForceHTTPSFilter, new GzipFilter) with GlobalSettings {
+import com.gu.mediaservice.lib.play.RequestLoggingFilter
+
+
+object Global extends WithFilters(ForceHTTPSFilter, RequestLoggingFilter, new GzipFilter) with GlobalSettings {
 
   override def beforeStart(app: Application) {
 
