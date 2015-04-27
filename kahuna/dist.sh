325ac93fa42e262373ca5d3eecb9a103bf0a24b5
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-04-27 14:05:27 +0100

    Fix error message

--- kahuna/dist.sh
+++ kahuna/dist.sh
@@ -7,4 +7,4 @@ echo Running tests
 npm test || { echo "##teamcity[message text='npm test error' status='ERROR']"; exit 1; }
 
 echo Building JSPM bundle
-npm run dist || { echo "##teamcity[message text='jspm bundle error' status='ERROR']"; exit 1; }
+npm run dist || { echo "##teamcity[message text='npm run dist error' status='ERROR']"; exit 1; }
