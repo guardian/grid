180f656927788e3b668418aed80147f831046124
Akash A <akash1810@gmail.com>
2015-04-28 14:09:19 +0100

    Metadata editing for all!
    - Using angular-xeditable.
    - Currently, title, description, byline and credit are editable.

--- kahuna/public/js/image/controller.js
+++ kahuna/public/js/image/controller.js
@@ -1,57 +1,132 @@
 import angular from 'angular';
 
-var image = angular.module('kahuna.image.controller', []);
+import '../edits/service';
 
-image.controller('ImageCtrl',
-                 ['$rootScope', 'image', 'optimisedImageUri', 'cropKey', 'mediaCropper',
-                  function($rootScope, image, optimisedImageUri, cropKey, mediaCropper) {
+var image = angular.module('kahuna.image.controller', ['kahuna.edits.service']);
 
-    var ctrl = this;
+image.controller('ImageCtrl', [
+    '$rootScope',
+    '$scope',
+    'onValChange',
+    'image',
+    'optimisedImageUri',
+    'cropKey',
+    'mediaCropper',
+    'editsService',
 
-    ctrl.image = image;
-    ctrl.optimisedImageUri = optimisedImageUri;
-    // TODO: we should be able to rely on ctrl.crop.id instead once
-    // all existing crops are migrated to have an id (they didn't
-    // initially)
-    ctrl.cropKey = cropKey;
+    function ($rootScope,
+              $scope,
+              onValChange,
+              image,
+              optimisedImageUri,
+              cropKey,
+              mediaCropper,
+              editsService) {
 
-    // Alias for convenience in view
-    ctrl.metadata = image.data.metadata;
+        var ctrl = this;
 
-    // Map of metadata location field to query filter name
-    ctrl.locationFieldMap = {
-        'subLocation': 'location',
-        'city':        'city',
-        'state':       'state',
-        'country':     'country'
-    };
+        ctrl.image = image;
+        ctrl.optimisedImageUri = optimisedImageUri;
+        // TODO: we should be able to rely on ctrl.crop.id instead once
+        // all existing crops are migrated to have an id (they didn't
+        // initially)
+        ctrl.cropKey = cropKey;
 
-    ctrl.isUsefulMetadata = isUsefulMetadata;
-    ctrl.cropSelected = cropSelected;
+        // Alias for convenience in view
+        ctrl.metadata = image.data.metadata;
 
-    mediaCropper.getCropsFor(image).then(crops => {
-        ctrl.crops = crops;
-        ctrl.crop = crops.find(crop => crop.id === cropKey);
-    });
+        ctrl.originalMetadata = image.data.originalMetadata;
 
-    mediaCropper.canBeCropped(image).then(croppable => {
-        ctrl.canBeCropped = croppable;
-    });
+        ctrl.resource = image.data.userMetadata.data.metadata;
 
+        // Map of metadata location field to query filter name
+        ctrl.locationFieldMap = {
+            'subLocation': 'location',
+            'city': 'city',
+            'state': 'state',
+            'country': 'country'
+        };
 
-    var ignoredMetadata = [
-        'title', 'description', 'copyright', 'keywords', 'byline',
-        'credit', 'subLocation', 'city', 'state', 'country',
-        'dateTaken'
-    ];
-    function isUsefulMetadata(metadataKey) {
-        return ignoredMetadata.indexOf(metadataKey) === -1;
-    }
+        ctrl.isUsefulMetadata = isUsefulMetadata;
+        ctrl.cropSelected = cropSelected;
 
-    function cropSelected(crop) {
-        $rootScope.$emit('events:crop-selected', {
-            image: ctrl.image,
-            crop: crop
+        mediaCropper.getCropsFor(image).then(crops => {
+            ctrl.crops = crops;
+            ctrl.crop = crops.find(crop => crop.id === cropKey);
         });
-    }
-}]);
+
+        mediaCropper.canBeCropped(image).then(croppable => {
+            ctrl.canBeCropped = croppable;
+        });
+
+
+        var ignoredMetadata = [
+            'title', 'description', 'copyright', 'keywords', 'byline',
+            'credit', 'subLocation', 'city', 'state', 'country',
+            'dateTaken'
+        ];
+
+        function isUsefulMetadata(metadataKey) {
+            return ignoredMetadata.indexOf(metadataKey) === -1;
+        }
+
+        function cropSelected(crop) {
+            $rootScope.$emit('events:crop-selected', {
+                image: ctrl.image,
+                crop: crop
+            });
+        }
+
+        function getMetadataDiff (updated) {
+            var toUpdate = {};
+
+            Object.keys(ctrl.originalMetadata).forEach(function (key) {
+                if (ctrl.originalMetadata[key] !== updated[key]) {
+                    toUpdate[key] = updated[key];
+                }
+            });
+
+            return toUpdate;
+        }
+
+        ctrl.save = function (metadata) {
+            return editsService
+                .update(ctrl.resource, metadata, ctrl.image)
+                .then(resource => {
+                    ctrl.resource = resource;
+                });
+        };
+
+        function _updateMetadata (field, value) {
+            if (ctrl.metadata[field] === value) {
+                /*
+                 Nothing has changed.
+
+                 Per the angular-xeditable docs, returning false indicates success but model
+                 will not be updated.
+
+                 http://vitalets.github.io/angular-xeditable/#onbeforesave
+                 */
+                return false;
+            }
+
+            var proposedMetadata = angular.copy(ctrl.metadata);
+            proposedMetadata[field] = value;
+
+            var changed = getMetadataDiff(proposedMetadata);
+
+            return ctrl
+                .save(changed)
+                .catch(() => {
+                    return 'failed to save (press esc to cancel)';
+                });
+        }
+
+        ctrl.updateTitle = (value) => { return _updateMetadata('title', value); };
+
+        ctrl.updateDescription = (value) => { return _updateMetadata('description', value); };
+
+        ctrl.updateByline = (value) => { return _updateMetadata('byline', value); };
+
+        ctrl.updateCredit = (value) => { return _updateMetadata('credit', value); };
+    }]);
