eb68f4dbde6aa778189475c0a363fe3d139b2e29
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-04-29 15:27:27 +0100

    Move Image and ImageUsageRights to common model, adapt supplier processors

---
+++ common-lib/src/main/scala/com/gu/mediaservice/model/Image.scala
@@ -0,0 +1,42 @@
+package com.gu.mediaservice.model
+
+import org.joda.time.DateTime
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+
+import com.gu.mediaservice.lib.formatting._
+
+case class Image(
+  id:               String,
+  uploadTime:       DateTime,
+  uploadedBy:       String,
+  lastModified:     Option[DateTime],
+  identifiers:      Map[String, String],
+  source:           Asset,
+  thumbnail:        Option[Asset],
+  fileMetadata:     FileMetadata,
+  metadata:         ImageMetadata,
+  originalMetadata: ImageMetadata,
+  usageRights:      ImageUsageRights
+)
+
+object Image {
+
+  implicit val FileMetadataWrites: Writes[FileMetadata] = Json.writes[FileMetadata]
+
+  implicit val ImageWrites: Writes[Image] = (
+    (__ \ "id").write[String] ~
+      (__ \ "uploadTime").write[String].contramap(printDateTime) ~
+      (__ \ "uploadedBy").write[String] ~
+      (__ \ "lastModified").writeNullable[String].contramap(printOptDateTime) ~
+      (__ \ "identifiers").write[Map[String, String]] ~
+      (__ \ "source").write[Asset] ~
+      (__ \ "thumbnail").writeNullable[Asset] ~
+      (__ \ "fileMetadata").write[FileMetadata] ~
+      (__ \ "metadata").write[ImageMetadata] ~
+      (__ \ "originalMetadata").write[ImageMetadata] ~
+      (__ \ "usageRights").write[ImageUsageRights]
+    )(unlift(Image.unapply))
+
+}
+
