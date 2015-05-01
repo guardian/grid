2ab72480c127ea495ca8530543f8edc98781e930
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-05-01 14:31:14 +0100

    Allow loading images based on a query

--- picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/lib/picdar/PicdarClient.scala
+++ picdar-export/src/main/scala/com.gu.mediaservice.picdarexport/lib/picdar/PicdarClient.scala
@@ -33,19 +33,19 @@ trait PicdarClient extends PicdarApi with HttpClient {
       _              <- closeSearch(mak, searchInstance)
     } yield searchInstance.count
 
-  def query(dateField: String, dateRange: DateRange, queryRange: Option[Range]): Future[Seq[AssetRef]] = {
+  def query(dateField: String, dateRange: DateRange, queryRange: Option[Range], query: Option[String] = None): Future[Seq[AssetRef]] = {
     for {
       mak            <- currentMak
-      searchInstance <- search(mak, dateField, dateRange)
+      searchInstance <- search(mak, dateField, dateRange, query = query)
       results        <- fetchResults(mak, searchInstance, queryRange)
       _              <- closeSearch(mak, searchInstance)
     } yield results
   }
 
-  def queryAssets(dateField: String, dateRange: DateRange, queryRange: Option[Range]): Future[Seq[Asset]] = {
+  def queryAssets(dateField: String, dateRange: DateRange, queryRange: Option[Range], query: Option[String] = None): Future[Seq[Asset]] = {
     for {
       mak            <- currentMak
-      searchInstance <- search(mak, dateField, dateRange)
+      searchInstance <- search(mak, dateField, dateRange, query = query)
       results        <- fetchResults(mak, searchInstance, queryRange)
       assets         <- Future.sequence(results.map(_.urn).map(urn => fetchAsset(mak, urn)))
       _              <- closeSearch(mak, searchInstance)
