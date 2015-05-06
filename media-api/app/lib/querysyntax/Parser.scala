a5c2e4aa2fa91f9c27e023aa99b375c913b396b9
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-05-06 17:08:30 +0100

    Redo query terms aggregation using post-parse normalisation to fix parsing errors

--- media-api/app/lib/querysyntax/Parser.scala
+++ media-api/app/lib/querysyntax/Parser.scala
@@ -3,6 +3,20 @@ package lib.querysyntax
 object Parser {
 
   def run(input: String): List[Condition] =
+    normalise(parse(input))
+
+  def parse(input: String): List[Condition] =
     new QuerySyntax(input.trim).Query.run().map(_.toList) getOrElse List()
+
+  // Post-hoc normalisation that are harder to do via the PEG grammar
+  def normalise(conditions: List[Condition]): List[Condition] = conditions match {
+    // Merge consecutive terms into a single match (e.g. "cats and dogs")
+    case Match(AnyField, Words(words1)) :: Match(AnyField, Words(words2)) :: xs =>
+      normalise(Match(AnyField, Words(s"$words1 $words2")) :: xs)
+    // Else, recursively match the next list tail
+    case x :: xs => x :: normalise(xs)
+    // Until we reach the end of the list
+    case Nil => Nil
+  }
 
 }
