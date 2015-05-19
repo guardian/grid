12dbb2838f498b312eeb2571be0213ebd7cc7290
Akash A <akash1810@gmail.com>
2015-05-19 12:41:42 +0100

    First pass at multi-select.

---
+++ kahuna/public/js/components/gr-panel/gr-panel.js
@@ -0,0 +1,27 @@
+import angular from 'angular';
+
+import './gr-panel.css!';
+import '../../services/preview-selection';
+
+export var grPanel = angular.module('grPanel', ['kahuna.services.selection']);
+
+grPanel.controller('GrPanel', [
+    '$scope',
+    'selectionService',
+    function (
+        $scope,
+        selection) {
+
+        var ctrl = this;
+
+        ctrl.selectedImages = selection.selectedImages;
+        ctrl.multipleValues = (val) => Array.isArray(val);
+        ctrl.clear = selection.clear;
+
+        $scope.$watchCollection(() => selection.getMetadata(), (newMetadata, oldMetadata) => {
+            if(!angular.equals(newMetadata, oldMetadata)) {
+                ctrl.metadata = newMetadata;
+            }
+        });
+    }
+]);
