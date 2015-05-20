e9cce9a5528b73e6d03bb193bdca131fd8c8c16c
James A Gorrie <james.gorrie@theguardian.com>
2015-05-20 17:00:20 +0100

    catch errors from panda lib and respond to client

--- kahuna/app/controllers/Panda.scala
+++ kahuna/app/controllers/Panda.scala
@@ -3,12 +3,19 @@ package controllers
 import lib.Config
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
-import com.gu.mediaservice.lib.auth.PanDomainAuthActions
+import com.gu.mediaservice.lib.auth.{ArgoErrorResponses, PanDomainAuthActions}
 import com.gu.pandomainauth.model.AuthenticatedUser
+import com.gu.pandomainauth.service.GoogleAuthException
 
-object Panda extends Controller with PanDomainAuthActions {
+import scala.concurrent.Future
+import scala.util.{Success, Try, Failure}
+
+object Panda extends Controller
+  with PanDomainAuthActions
+  with ArgoErrorResponses {
 
   override lazy val authCallbackBaseUri = Config.rootUri
+  def loginUri: String = Config.loginUri
 
   def session = Action { implicit request =>
     readAuthenticatedUser(request).map { case AuthenticatedUser(user, _, _, _, _) =>
@@ -31,7 +38,16 @@ object Panda extends Controller with PanDomainAuthActions {
   }
 
   def oauthCallback = Action.async { implicit request =>
-    processGoogleCallback()
+    // We use the `Try` here as the `GoogleAuthException` are thrown before we
+    // get to the asynchronicity of the `Future` it returns.
+    Try(processGoogleCallback) match {
+      case Success(result) => result
+      case Failure(error) => Future.successful(error match {
+        // This is when session session args are missing
+        case e: GoogleAuthException =>
+          respondError(BadRequest, "missing-session-parameters", e.getMessage, loginLinks)
+      })
+    }
   }
 
   def logout = Action { implicit request =>
