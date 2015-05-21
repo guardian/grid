0698148689273f70c73b2ef1f43ee16f186df555
James A Gorrie <james.gorrie@theguardian.com>
2015-05-21 16:23:38 +0100

    add bogus unknown image right and agency

---
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/model/ImageUsageRights.scala
@@ -0,0 +1,14 @@
+package com.gu.mediaservice.model
+
+import org.scalatest.{FunSpec, Matchers}
+
+
+class ImageUsageRightsTest extends FunSpec with Matchers {
+
+  it("work") {
+    val usageRights = ImageUsageRights()
+  }
+
+}
+
+
