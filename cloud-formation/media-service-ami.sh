7b6a54e3cbb4a6beee16f3c3c599d3d116d738e6
Akash A <akash1810@gmail.com>
2015-05-27 12:19:08 +0100

    Building off a machine-images AMI.
    Baking in:
      - graphicsmagick
      - cloud watch monitoring scripts
      - a cronjob to report disk space available on /dev/xvda1
    
    The AMI build has id ami-bb83f0cc

---
+++ cloud-formation/media-service-ami.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function new_section {
+  echo
+  echo $(date +"%F %T") $1
+  echo "----------------------------------------------------------------------------------------"
+}
+
+set -e
+
+new_section "Updating package lists"
+apt-get -y update
+
+new_section "Installing graphicsmagick"
+apt-get -y install graphicsmagick graphicsmagick-imagemagick-compat
+
+new_section "Installing cloud watch monitoring scripts"
+apt-get -y install unzip libwww-perl libdatetime-perl
+wget "http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip" -O /tmp/CloudWatchMonitoringScripts.zip
+unzip /tmp/CloudWatchMonitoringScripts.zip -d /tmp/
+rm /tmp/CloudWatchMonitoringScripts.zip
+mv /tmp/aws-scripts-mon /usr/local/
+
+new_section "Adding disk available cronjob"
+disk_space_available="*/5 * * * * /usr/local/aws-scripts-mon/mon-put-instance-data.pl --disk-space-avail --mem-avail --disk-path /dev/xvda1 --auto-scaling --from-cron"
+echo "$disk_space_available" | crontab -u root -
