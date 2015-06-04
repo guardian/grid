4cda219ce013112f50346fc5a7437664e9b242aa
James A Gorrie <james.gorrie@theguardian.com>
2015-06-04 10:28:33 +0100

    Update to the datalist to make it way less buggy

--- kahuna/public/js/forms/datalist.js
+++ kahuna/public/js/forms/datalist.js
@@ -1,9 +1,122 @@
 import angular from 'angular';
 
 import template from './datalist.html!text';
+import '../util/eq';
+
+export var datalist = angular.module('kahuna.forms.datalist', ['util.eq']);
 
 
-export var datalist = angular.module('kahuna.forms.datalist', []);
+
+datalist.directive('grDatalist', ['onValChange', function(onValChange) {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            search: '&grSearch'
+        },
+        template: `
+            <div class="datalist">
+                <ng:transclude></ng:transclude>
+                <div class="datalist__options" ng:if="ctrl.active">
+                    <div class="datalist__option"
+                         ng:repeat="(key, result) in ctrl.results"
+                         ng:click="ctrl.setValueTo(result)"
+                         ng:class="{ 'datalist__option--selected': ctrl.isSelected(key) }">
+                         {{result}}
+                    </div>
+                </div>
+            </div>
+        `,
+        controllerAs: 'ctrl',
+        controller: [function() {
+            var ctrl = this;
+            var selectedIndex = 0;
+
+            ctrl.results = [];
+
+            ctrl.moveIndex = movement =>
+                selectedIndex = (selectedIndex + movement + ctrl.results.length) % ctrl.results.length;
+
+            ctrl.isSelected = key => key === selectedIndex;
+
+            ctrl.searchFor = q =>
+                ctrl.search({ q }).then(results => ctrl.results = results);
+
+            ctrl.setValueTo = value => ctrl.value = value;
+            ctrl.setValueFromIndex = index => ctrl.value = ctrl.results[index];
+        }],
+        bindToController: true
+    }
+}]);
+
+
+datalist.directive('grDatalistInput', ['onValChange', function(onValChange) {
+    return {
+        restrict: 'A',
+        require:['^grDatalist', '?ngModel'],
+
+        link: function(scope, element, _/*attrs*/, [parentCtrl, ngModel]) {
+            parentCtrl.active = false;
+
+            const input = angular.element(element[0]);
+            const keys = { 38: 'up', 40: 'down', 13: 'enter', 27: 'esc', 9: 'tab' };
+            const keyFuncs = {
+                up:    () => parentCtrl.moveIndex(-1),
+                down:  () => parentCtrl.moveIndex(+1),
+                esc:   () => parentCtrl.active = false,
+                enter: () => parentCtrl.setValueFromIndex(parentCtrl.selectedIndex)
+            };
+
+            input.on('keyup', event => {
+                const func = keyFuncs[keys[event.which]];
+
+                if (func && parentCtrl.active) {
+                    event.preventDefault();
+                    scope.$apply(func);
+                } else {
+                    parentCtrl.searchFor(input.val()).then(activate);
+                }
+            });
+
+            input.on('click', () => parentCtrl.searchFor(input.val()).then(activate));
+
+            scope.$watch(() => parentCtrl.value, onValChange(newVal => {
+                ngModel.$setViewValue(newVal, 'gr:datalist:update');
+                ngModel.$commitViewValue();
+                ngModel.$render();
+                deactivate();
+            }));
+
+            function activate(results) {
+                const isOnlyResult = results.length === 1 && input.val() === results[0];
+                const noResults = results.length === 0 || isOnlyResult;
+
+                parentCtrl.active = !noResults;
+            }
+
+            function deactivate() {
+                parentCtrl.selectedIndex = 0;
+                parentCtrl.active = false;
+            }
+        }
+    }
+}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 datalist.controller('DatalistController',
                     ['$scope', '$timeout', 'onValChange',
