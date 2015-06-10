8a3044c1e7b234281c8991ba257949cf9fdca956
James A Gorrie <james.gorrie@theguardian.com>
2015-06-10 16:38:12 +0100

    TMP

--- kahuna/public/js/services/api/edits-api.js
+++ kahuna/public/js/services/api/edits-api.js
@@ -3,44 +3,16 @@ import apiServices from '../api';
 apiServices.factory('editsApi', ['$q', 'mediaApi', function($q, mediaApi) {
 
     var root;
-    var updatedMetadataDefs = new Map();
 
     function getRoot() {
         return root || (root = mediaApi.root.follow('edits'));
     }
 
-    function getMetadata(id) {
-        return getRoot(id).follow('metadata');
-    }
-
-    function updateMetadata(id, metadata) {
-        // FIXME: this shouldn't be returning the `Resource` and `id`, but we
-        // need some updated theseus juice here to be able to return the
-        // `Resource` with useful information
-        return getMetadata(id)
-            .then(resource => resource.put({ data: metadata }))
-            .then(resource => {
-                updatedMetadataDefs.forEach(def => def.notify({ resource, metadata, id }));
-                return resource;
-            })
-            .catch(e => updatedMetadataDefs.forEach(def => def.reject(e)));
-    }
-
-    function onMetadataUpdate(onupdate, failure) {
-        var def = $q.defer();
-        def.promise.then(() => {}, failure, onupdate);
-        updatedMetadataDefs.set(onupdate, def);
-
-        return () => offMetadataUpdate(onupdate);
-    }
-
-    function offMetadataUpdate(onupdate) {
-        return updatedMetadataDefs.delete(onupdate);
+    function getUsageRights() {
+        return getRoot().follow('usageRightsList').getData();
     }
 
     return {
-        updateMetadata: updateMetadata,
-        onMetadataUpdate: onMetadataUpdate,
-        offMetadataUpdate: offMetadataUpdate
+        getUsageRights
     };
 }]);
