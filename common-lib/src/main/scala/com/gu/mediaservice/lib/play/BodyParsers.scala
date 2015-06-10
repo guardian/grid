e608565b6c1a516381f2bf1511d6ef437ed6f73d
Robert Kenny <kenoir@gmail.com>
2015-06-10 17:05:37 +0100

    Return a Left from the BodyParser if content length is wrong

--- common-lib/src/main/scala/com/gu/mediaservice/lib/play/BodyParsers.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/play/BodyParsers.scala
@@ -2,10 +2,13 @@ package com.gu.mediaservice.lib.play
 
 import java.io.{FileOutputStream, File}
 import java.security.MessageDigest
+
 import scala.concurrent.ExecutionContext
+import scala.util.{Left, Right, Either}
 
 import play.api.libs.iteratee.Iteratee
 import play.api.mvc.BodyParser
+import play.api.mvc.Results.Status
 
 
 case class DigestedFile(file: File, digest: String)
@@ -18,7 +21,7 @@ object DigestedFile {
 object BodyParsers {
 
   def digestedFile(to: File)(implicit ex: ExecutionContext): BodyParser[DigestedFile] =
-    BodyParser("digested file, to=" + to) { request =>
+    BodyParser("digested file, to=" + to) { request => {
       Iteratee.fold[Array[Byte], (MessageDigest, FileOutputStream)]((MessageDigest.getInstance("SHA-1"), new FileOutputStream(to))) {
         case ((md, os), data) =>
           md.update(data)
@@ -26,8 +29,12 @@ object BodyParsers {
           (md, os)
       }.map { case (md, os) =>
         os.close()
-        Right(DigestedFile(to, md.digest))
+
+        request.headers.get("Content-Length").foldLeft[Either[Status, DigestedFile]](Left(Status(411))) { (_,expectedContentLength) =>
+          if(to.length==expectedContentLength.toInt) Right(DigestedFile(to, md.digest))
+          else Left(Status(400))
+        }
       }
     }
-
+  }
 }
