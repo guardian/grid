5538bb3fa47651cda3916153b997e1d880590df7
James A Gorrie <james.gorrie@theguardian.com>
2015-06-10 16:55:52 +0100

    category response case class

--- metadata-editor/app/controllers/EditsApi.scala
+++ metadata-editor/app/controllers/EditsApi.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import com.gu.mediaservice.lib.config.UsageRightsConfig
+import play.api.libs.json.{Json, Writes}
 import play.api.mvc.Controller
 
 import com.gu.mediaservice.lib.argo.ArgoHelpers
@@ -10,6 +11,7 @@ import com.gu.mediaservice.lib.auth.KeyStore
 import com.gu.mediaservice.model._
 
 import lib.Config
+
 
 
 object EditsApi extends Controller with ArgoHelpers {
@@ -39,20 +41,30 @@ object EditsApi extends Controller with ArgoHelpers {
   val usageRightsResponse = {
     val usageRightsData =
       List(PrImage, Handout, Screengrab, GuardianWitness, SocialMedia, Obituary)
-        .map(categoryMap)
+        .map(CategoryResponse.fromCat)
 
     respond(usageRightsData)
   }
 
   def getUsageRights = Authenticated { usageRightsResponse }
+}
+
+case class CategoryResponse(value: String, name: String, cost: String)
+object CategoryResponse {
+  // I'd like to have an override of the `apply`, but who nows how you do that
+  // with the JSON parsing stuff
+  def fromCat(cat: UsageRightsCategory): CategoryResponse =
+    CategoryResponse(
+      value = cat.toString,
+      name  = makeCategoryName(cat.toString),
+      cost  = UsageRightsConfig.categoryCosts.getOrElse(cat, Pay).toString
+    )
 
   // Not sure about this, but I don't want to add it to the case classes.
   private def makeCategoryName(s: String) =
     s.replace("-", " ").split(" ").map(_.capitalize).mkString(" ")
 
-  private def categoryMap(cat: UsageRightsCategory) = Map(
-    "value" -> cat.toString,
-    "name"  -> makeCategoryName(cat.toString),
-    "cost"  -> UsageRightsConfig.categoryCosts.getOrElse(cat, Pay).toString
-  )
+
+  implicit val categoryResponseWrites: Writes[CategoryResponse] = Json.writes[CategoryResponse]
+
 }
