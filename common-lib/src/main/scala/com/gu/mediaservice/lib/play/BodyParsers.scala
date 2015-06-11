337c8c07e88c69b3dd67883f5b40cf88a845c1af
Robert Kenny <kenoir@gmail.com>
2015-06-11 10:24:34 +0100

    log if content length does not match

--- common-lib/src/main/scala/com/gu/mediaservice/lib/play/BodyParsers.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/play/BodyParsers.scala
@@ -9,7 +9,7 @@ import scala.util.{Left, Right, Either}
 import play.api.libs.iteratee.Iteratee
 import play.api.mvc.BodyParser
 import play.api.mvc.Results.Status
-
+import play.api.Logger
 
 case class DigestedFile(file: File, digest: String)
 
@@ -22,17 +22,26 @@ object BodyParsers {
 
   def digestedFile(to: File)(implicit ex: ExecutionContext): BodyParser[DigestedFile] =
     BodyParser("digested file, to=" + to) { request => {
-      Iteratee.fold[Array[Byte], (MessageDigest, FileOutputStream)]((MessageDigest.getInstance("SHA-1"), new FileOutputStream(to))) {
-        case ((md, os), data) =>
+      Iteratee.fold[Array[Byte], (MessageDigest, FileOutputStream)](
+        (MessageDigest.getInstance("SHA-1"), new FileOutputStream(to))) {
+
+        case ((md, os), data) => {
           md.update(data)
           os.write(data)
+
           (md, os)
+        }
+
       }.map { case (md, os) =>
         os.close()
-
-        request.headers.get("Content-Length").foldLeft[Either[Status, DigestedFile]](Left(Status(411))) { (_,expectedContentLength) =>
-          if(to.length==expectedContentLength.toInt) Right(DigestedFile(to, md.digest))
-          else Left(Status(400))
+        request.headers.get("Content-Length").foldLeft[Either[Status, DigestedFile]](Left(Status(411))) {
+          (_,expectedContentLength) => {
+            if(to.length==expectedContentLength.toInt) Right(DigestedFile(to, md.digest))
+            else {
+              Logger.info("Received file does not match specified 'Content-Length'")
+              Left(Status(400))
+            }
+          }
         }
       }
     }
