12387bccc3fc0401cbf6e3a0d30101eca69d664c
Robert Kenny <kenoir@gmail.com>
2015-06-11 12:16:28 +0100

    provide argo style json errors

--- common-lib/src/main/scala/com/gu/mediaservice/lib/play/BodyParsers.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/play/BodyParsers.scala
@@ -7,9 +7,13 @@ import scala.concurrent.ExecutionContext
 import scala.util.{Left, Right, Either}
 
 import play.api.libs.iteratee.Iteratee
-import play.api.mvc.BodyParser
+
 import play.api.mvc.Results.Status
+import play.api.mvc._
+
 import play.api.Logger
+
+import com.gu.mediaservice.lib.argo.ArgoHelpers
 
 case class DigestedFile(file: File, digest: String)
 
@@ -18,10 +22,9 @@ object DigestedFile {
     DigestedFile(file, digest.map("%02x".format(_)).mkString)
 }
 
-object BodyParsers {
+object DigestBodyParser extends ArgoHelpers {
 
-  def digestedFile(to: File)(implicit ex: ExecutionContext): BodyParser[DigestedFile] =
-    BodyParser("digested file, to=" + to) { request => {
+  def slurp(to: File)(implicit ec: ExecutionContext): Iteratee[Array[Byte],(MessageDigest, FileOutputStream)]= {
       Iteratee.fold[Array[Byte], (MessageDigest, FileOutputStream)](
         (MessageDigest.getInstance("SHA-1"), new FileOutputStream(to))) {
 
@@ -31,18 +34,38 @@ object BodyParsers {
 
           (md, os)
         }
+     }
+  }
 
-      }.map { case (md, os) =>
-        os.close()
-        request.headers.get("Content-Length").foldLeft[Either[Status, DigestedFile]](Left(Status(411))) {
-          (_,expectedContentLength) => {
-            if(to.length==expectedContentLength.toInt) Right(DigestedFile(to, md.digest))
-            else {
-              Logger.info("Received file does not match specified 'Content-Length'")
-              Left(Status(400))
-            }
-          }
+  val missingContenLengthError = respondError(
+    Status(411),
+    "missing-content-length",
+    s"Missing content-length. Please specify a correct 'Content-Length' header"
+  )
+
+  val incorrectContentLengthError = respondError(
+    Status(400),
+    "incorrect-content-length",
+    s"Incorrect content-length. The specified content-length does match that of the received file."
+  )
+
+  def validate(request: RequestHeader, to: File, md: MessageDigest): Either[Result, DigestedFile] = {
+    request.headers.get("Content-Length").foldLeft[Either[Result, DigestedFile]](Left(missingContenLengthError)) {
+      (_,expectedContentLength) => {
+        if(to.length==expectedContentLength.toInt) Right(DigestedFile(to, md.digest))
+        else {
+          Logger.info("Received file does not match specified 'Content-Length'")
+          Left(incorrectContentLengthError)
         }
+      }
+    }
+  }
+
+  def create(to: File)(implicit ex: ExecutionContext): BodyParser[DigestedFile] =
+    BodyParser("digested file, to=" + to) { request => {
+      slurp(to).map { case (md, os) =>
+        os.close()
+        validate(request, to, md)
       }
     }
   }
