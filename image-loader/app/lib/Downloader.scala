582e61276ee36699ecb22adf0d70abfe48772058
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-06-11 15:24:08 +0100

    Add import endpoint to image-loader to load from URI

---
+++ image-loader/app/lib/Downloader.scala
@@ -0,0 +1,42 @@
+package lib
+
+import java.io.{FileOutputStream, File}
+import java.net.URI
+import java.security.MessageDigest
+
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits._
+
+import play.api.Play.current
+import play.api.libs.iteratee.Iteratee
+import play.api.libs.ws.WS
+
+import com.gu.mediaservice.lib.play.DigestedFile
+
+
+object Downloader {
+  def createTempFile = File.createTempFile("download", "", new File(Config.tempDir))
+
+  def download(uri: URI): Future[DigestedFile] =
+    WS.url(uri.toString).getStream().flatMap { case (headers, body) =>
+      val md = MessageDigest.getInstance("SHA-1")
+      val file = createTempFile
+      val outputStream = new FileOutputStream(file)
+
+      // The iteratee that writes to the output stream
+      val iteratee = Iteratee.foreach[Array[Byte]] { bytes =>
+        outputStream.write(bytes)
+        md.update(bytes)
+      }
+
+      // Feed the body into the iteratee
+      (body |>>> iteratee).andThen {
+        case result =>
+          // Close the output stream whether there was an error or not
+          outputStream.close()
+          // Get the result or rethrow the error
+          result.get
+      }.map(_ => DigestedFile(file, md.digest))
+    }
+
+}
