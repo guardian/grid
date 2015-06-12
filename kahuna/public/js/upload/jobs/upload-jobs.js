b0f17f197d8d0c48dfe4567fc9c8575684fa9a41
James A Gorrie <james.gorrie@theguardian.com>
2015-06-12 12:37:11 +0100

    add tracking to uploading

--- kahuna/public/js/upload/jobs/upload-jobs.js
+++ kahuna/public/js/upload/jobs/upload-jobs.js
@@ -1,17 +1,21 @@
 import angular from 'angular';
 import template from './upload-jobs.html!text';
 import '../../preview/image';
+import '../../analytics/track';
 
 export var jobs = angular.module('kahuna.upload.jobs', ['kahuna.preview.image']);
 
 
 jobs.controller('UploadJobsCtrl',
-                ['poll', function(poll) {
+                ['poll', 'track', function(poll, track) {
+
+    var ctrl = this;
 
     // State machine-esque async transitions
     var pollFrequency = 500; // ms
     var pollTimeout   = 20 * 1000; // ms
-    this.jobs.forEach(jobItem => {
+
+    ctrl.jobs.forEach(jobItem => {
         jobItem.status = 'uploading';
 
         jobItem.resourcePromise.then(resource => {
@@ -21,23 +25,30 @@ jobs.controller('UploadJobsCtrl',
             // TODO: grouped polling for all resources we're interested in?
             var findImage = () => resource.get();
             var imageResource = poll(findImage, pollFrequency, pollTimeout);
+            
             imageResource.then(image => {
                 jobItem.status = 'uploaded';
                 jobItem.image = image;
                 jobItem.thumbnail = image.data.thumbnail;
+
+                track('Image upload', { state: 'successful' });
             }, error => {
                 jobItem.status = 'upload error';
                 jobItem.error = error.message;
+
+                track('Image upload', { state: 'failed-index' });
             });
         }, error => {
             var message = error.body.errorMessage;
             jobItem.status = 'upload error';
             jobItem.error = message;
+
+            track('Image upload', { state: 'failed-upload' });
         });
     });
 
     // this needs to be a function due to the stateful `jobItem`
-    this.jobImages = () => this.jobs.map(jobItem => jobItem.image);
+    ctrl.jobImages = () => this.jobs.map(jobItem => jobItem.image);
 
 }]);
 
