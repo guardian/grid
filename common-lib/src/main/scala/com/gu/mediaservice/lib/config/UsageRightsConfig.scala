a70a6bab5655a8a250efe7a3fff2aafbf0a76f7e
James A Gorrie <james.gorrie@theguardian.com>
2015-06-19 14:12:23 +0100

    make usagerights category optional

--- common-lib/src/main/scala/com/gu/mediaservice/lib/config/UsageRightsConfig.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/config/UsageRightsConfig.scala
@@ -4,13 +4,14 @@ import com.gu.mediaservice.model._
 
 object UsageRightsConfig {
 
-  val categoryCosts: Map[UsageRightsCategory, Cost] = Map(
-    Handout         -> Free,
-    Screengrab      -> Free,
-    PrImage         -> Conditional,
-    GuardianWitness -> Conditional,
-    SocialMedia     -> Conditional,
-    Obituary        -> Conditional
+  val categoryCosts: Map[Option[UsageRightsCategory], Cost] = Map(
+    None                  -> Pay,
+    Some(Handout)         -> Free,
+    Some(Screengrab)      -> Free,
+    Some(PrImage)         -> Conditional,
+    Some(GuardianWitness) -> Conditional,
+    Some(SocialMedia)     -> Conditional,
+    Some(Obituary)        -> Conditional
   )
 
 }
