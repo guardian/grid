76e39c5757c2988a25566f9d7a87cd8a414cde7f
James A Gorrie <james.gorrie@theguardian.com>
2015-06-24 14:39:12 +0100

    better route naming

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -222,7 +222,7 @@ object MediaApi extends Controller with ArgoHelpers {
   }
 
   def suggestMetadataCredit(q: String) = Authenticated.async { request =>
-    ElasticSearch.completionSuggestion(q, "suggestMetadataCredit") map(c => respond(c.results))
+    ElasticSearch.completionSuggestion("suggestMetadataCredit", q) map(c => respond(c.results))
   }
 
   // TODO: work with analysed fields
