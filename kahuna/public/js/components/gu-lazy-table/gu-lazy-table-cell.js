ec2d3cc913b99adaddebbdc663bb95bb9a423113
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-06-26 16:56:49 +0100

    Reduce DOM weight by removing content of hidden rows altogether

--- kahuna/public/js/components/gu-lazy-table/gu-lazy-table-cell.js
+++ kahuna/public/js/components/gu-lazy-table/gu-lazy-table-cell.js
@@ -10,11 +10,16 @@ lazyTableCell.directive('guLazyTableCell',
     return {
         restrict: 'A',
         require: '^guLazyTable',
+        // Limit DOM weight by only transcluding the content of this
+        // element iff it is visible.
+        transclude: true,
+        template: '<ng-transclude ng:if="guLazyTableCellVisible"></ng-transclude>',
         link: function(scope, element, attrs, ctrl) {
             const item = scope.$eval(attrs.guLazyTableCell);
             const position$ = ctrl.getCellPosition$(item);
             subscribe$(scope, position$,
                        ({top, left, width, height, display}) => {
+                scope.guLazyTableCellVisible = display === 'block';
                 element.css({
                     position: 'absolute',
                     top:    top    + 'px',
