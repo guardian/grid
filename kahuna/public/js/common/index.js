b5f67be98b093d1f1ca2237388cbacb724bb65b7
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-07-05 00:23:07 +0100

    Move gr-top-bar into components/

--- kahuna/public/js/common/index.js
+++ kahuna/public/js/common/index.js
@@ -2,10 +2,8 @@ import angular from 'angular';
 
 import './user-actions';
 import './track-image-loadtime';
-import './top-bar';
 
 export var common = angular.module('kahuna.common', [
     'kahuna.common.userActions',
-    'kahuna.common.trackImageLoadtime',
-    'kahuna.common.topBar'
+    'kahuna.common.trackImageLoadtime'
 ]);
