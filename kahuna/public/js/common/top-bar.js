1484c96eef24f1f19220a5e45cc1a2f1999fc695
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-07-04 23:40:42 +0100

    Inline trivial templates

--- kahuna/public/js/common/top-bar.js
+++ kahuna/public/js/common/top-bar.js
@@ -1,30 +1,30 @@
 import angular from 'angular';
-import template from './top-bar.html!text';
-import navTemplate from './top-bar-nav.html!text';
-import actionsTemplate from './top-bar-actions.html!text';
 
 export var topBar = angular.module('kahuna.common.topBar', []);
 
 topBar.directive('gridTopBar', [function() {
     return {
         restrict: 'E',
-        template: template,
-        transclude: 'replace'
+        transclude: 'replace',
+        template: `<div ng:transclude class="top-bar-inner"></div>`
+
     };
 }]);
 
 topBar.directive('gridTopBarNav', [function() {
     return {
         restrict: 'E',
-        template: navTemplate,
-        transclude: true
+        transclude: true,
+        template: `<ng:transclude></ng:transclude>`
     };
 }]);
 
 topBar.directive('gridTopBarActions', [function() {
     return {
         restrict: 'E',
-        template: actionsTemplate,
-        transclude: true
+        transclude: true,
+        // Always have user actions at the end of actions
+        template: `<ng:transclude></ng:transclude>
+                   <ui-user-actions></ui-user-actions>`
     };
 }]);
