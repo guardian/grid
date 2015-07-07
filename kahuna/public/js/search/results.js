15daf4c7603fc2193cdc80431e24ee17cd0fc748
Robert Kenny <kenoir@gmail.com>
2015-07-07 16:19:38 +0100

    add simple large preview

--- kahuna/public/js/search/results.js
+++ kahuna/public/js/search/results.js
@@ -40,9 +40,19 @@ results.controller('SearchResultsCtrl', [
         ctrl.getLastSeenVal = getLastSeenVal;
         ctrl.imageHasBeenSeen = imageHasBeenSeen;
 
-        ctrl.searched = search().then(function(images) {
+        ctrl.updateImages = function(images) {
             ctrl.totalResults = images.total;
             ctrl.images = images.data;
+        };
+
+        ctrl.resetImages = function() {
+            ctrl.updateImages(ctrl.lastImages);
+            ctrl.fullPreview = false;
+        }
+
+        ctrl.searched = search().then(function(images) {
+            ctrl.lastImages = images;
+            ctrl.updateImages(images);
             // yield so images render before we check if there's more space
             fillRemainingSpace();
             checkForNewImages();
@@ -133,7 +143,7 @@ results.controller('SearchResultsCtrl', [
         function addImages() {
             // TODO: stop once reached the end
             const lastImage = ctrl.images.slice(-1)[0];
-            if (lastImage) {
+            if (lastImage && !ctrl.fullPreview) {
                 const until = lastImage.data.uploadTime;
                 return search({until: until}).then(function(moreImages) {
                     // Filter out duplicates (esp. on exact same 'until' date)
@@ -209,6 +219,10 @@ results.controller('SearchResultsCtrl', [
                 else {
                     ctrl.toggleSelection(image, !ctrl.imageHasBeenSelected(image));
                 }
+            } else {
+                ctrl.updateImages({ data: [image] });
+                ctrl.toggleSelection(image, !ctrl.imageHasBeenSelected(image));
+                ctrl.fullPreview = true;
             }
         };
 
