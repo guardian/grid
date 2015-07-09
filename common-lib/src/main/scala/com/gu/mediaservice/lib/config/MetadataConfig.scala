a257af7e28540656bcd1d3cc8ca4571a88b65f04
James A Gorrie <james.gorrie@theguardian.com>
2015-07-09 14:34:38 +0100

    a more extensive note on a hack

--- common-lib/src/main/scala/com/gu/mediaservice/lib/config/MetadataConfig.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/config/MetadataConfig.scala
@@ -33,8 +33,8 @@ object MetadataConfig {
 
 
     val creditBylineMap: Map[String, List[String]] = store
-      .groupBy(_ match { case (photographer, publication) => publication })
-      .map(_ match { case (publication, photographers) => publication -> photographers.keys.toList })
+      .groupBy{ case (photographer, publication) => publication }
+      .map{ case (publication, photographers) => publication -> photographers.keys.toList }
 
     val list = store.keys.toList
 
