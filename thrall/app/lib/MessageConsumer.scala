a8adc9f87872a32d81c3d85b8321bf400e16f878
Akash A <akash1810@gmail.com>
2015-07-10 12:17:00 +0100

    Revert "Revert "Updating thrall health check end-point""
    This reverts commit 4c05d5f5f719fe84d2cd4f723c8ef8ecbf2203c7.

--- thrall/app/lib/MessageConsumer.scala
+++ thrall/app/lib/MessageConsumer.scala
@@ -1,5 +1,7 @@
 package lib
 
+import java.util.concurrent.atomic.{AtomicReference, AtomicLong}
+
 import com.amazonaws.services.cloudwatch.model.Dimension
 import org.elasticsearch.action.deletebyquery.DeleteByQueryResponse
 import org.joda.time.DateTime
@@ -25,6 +27,8 @@ import java.util.concurrent.Executors
 object MessageConsumer {
 
   val actorSystem = ActorSystem("MessageConsumer")
+
+  val lastHeartBeat = new AtomicReference[DateTime](DateTime.now)
 
   private implicit val ctx: ExecutionContext =
     ExecutionContext.fromExecutor(Executors.newCachedThreadPool)
@@ -71,7 +75,12 @@ object MessageConsumer {
       case "update-image"               => indexImage
       case "update-image-exports"       => updateImageExports
       case "update-image-user-metadata" => updateImageUserMetadata
+      case "heartbeat"                  => heartbeat
     }
+
+  def heartbeat(msg: JsValue) = Future {
+    lastHeartBeat.lazySet(DateTime.now)
+  }
 
   def deleteOnSuccess(msg: SQSMessage)(f: Future[Any]): Unit =
     f.onSuccess { case _ => deleteMessage(msg) }
