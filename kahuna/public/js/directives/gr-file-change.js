db69f96e05d0b4826df3fd2bb33511b5d40e913d
James A Gorrie <james.gorrie@theguardian.com>
2015-07-10 08:13:48 +0100

    unscope file uploader

--- kahuna/public/js/directives/gr-file-change.js
+++ kahuna/public/js/directives/gr-file-change.js
@@ -2,17 +2,14 @@ import angular from 'angular';
 
 export var fileChange = angular.module('gr.fileChange', []);
 
-fileChange.directive('grFileChange', function() {
+fileChange.directive('grFileChange', ['$parse', function($parse) {
     return {
         restrict: 'A',
-        scope: {
-            onchange: '&grFileChange'
-        },
-        link: function(scope, element) {
+        link: function(scope, element, attrs) {
             element.on('change', function() {
-                // TODO: no function reference
-                scope.onchange()(Array.from(element[0].files));
+                const onchange = $parse(attrs.grFileChange)(scope);
+                onchange(Array.from(element[0].files));
             });
         }
     };
-});
+}]);
