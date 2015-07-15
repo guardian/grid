d9e9979fc21bbfe729711ecadbbbfe62d2152873
James A Gorrie <james.gorrie@theguardian.com>
2015-07-15 12:51:18 +0100

    bad long line removal

--- kahuna/public/js/image/service.js
+++ kahuna/public/js/image/service.js
@@ -13,7 +13,10 @@ imageService.factory('imageService', ['editsService', function(editsService) {
 
     function usageRights(image) {
         // we override the data with the overrides data from the edits API.
-        const data = angular.extend({}, image.data.usageRights, image.data.userMetadata.data.usageRights.data);
+        const data = angular.extend({},
+            image.data.usageRights,
+            image.data.userMetadata.data.usageRights.data
+        );
         const resource = image.data.userMetadata.data.usageRights;
 
         const save = newData => editsService.update(resource, newData, image);
