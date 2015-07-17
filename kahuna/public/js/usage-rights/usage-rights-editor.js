0bca1223555319140c1ef712a76a8339a1a10343
James A Gorrie <james.gorrie@theguardian.com>
2015-07-17 14:11:44 +0100

    add confirmation to delete

--- kahuna/public/js/usage-rights/usage-rights-editor.js
+++ kahuna/public/js/usage-rights/usage-rights-editor.js
@@ -117,7 +117,6 @@ usageRightsEditor.controller('UsageRightsEditorCtrl',
     }
 }]);
 
-
 usageRightsEditor.directive('grUsageRightsEditor', [function() {
     return {
         restrict: 'E',
@@ -131,4 +130,31 @@ usageRightsEditor.directive('grUsageRightsEditor', [function() {
             onSave: '&?grOnSave'
         }
     };
+}]);
+
+
+usageRightsEditor.directive('grConfirmDelete', ['$timeout', function($timeout) {
+
+    return {
+        restrict: 'E',
+        template: `
+            <button class="button-ico" type="button" ng:click="showConfirm = true"
+                title="remove usage rights overrides">
+                <span class="confirm" ng:if="showConfirm">Sure?</span>
+                <gr-icon>delete</gr-icon>
+            </button>`,
+
+        link: function(scope, element, attrs) {
+            const onChange = () => scope.$eval(attrs.grOnConfirm);
+
+            element.on('click', function() {
+                element.on('click', onChange);
+                $timeout(() => {
+                    element.off('click', onChange);
+                    scope.showConfirm = false;
+                }, 5000);
+            });
+        }
+    }
+
 }]);
