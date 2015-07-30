23016fbaf7f6cbf06810cf1d35bad4ef39d93501
Robert Kenny <kenoir@gmail.com>
2015-07-30 15:31:50 +0100

    allow authenticatedservices to delete from media api

--- media-api/app/controllers/MediaApi.scala
+++ media-api/app/controllers/MediaApi.scala
@@ -88,6 +88,7 @@ object MediaApi extends Controller with ArgoHelpers {
           case _ => permissionStore.hasPermission(permission, user.email)
         }
       }
+      case service: AuthenticatedService => Future.successful(true)
       case _ => Future.successful(false)
     }
   }
