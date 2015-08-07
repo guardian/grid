21925ab8b24406f0ff4e19787e87b5326d05ed86
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-08-08 00:49:31 +0100

    Actually send Angular errors to Sentry

--- kahuna/public/js/sentry/sentry.js
+++ kahuna/public/js/sentry/sentry.js
@@ -3,20 +3,18 @@ import raven from 'raven-js';
 
 export var sentry = angular.module('sentry', []);
 
-// TODO: Add user information
-// TODO: Catch angular errors nicely
-
 sentry.factory('sentryEnabled', ['sentryDsn', function(sentryDsn) {
     return angular.isString(sentryDsn);
 }]);
 
-sentry.factory('sentry', [function() {
-
-    function trigger(errorMessage, extra) {
-        raven.captureException(new Error(errorMessage), { extra });
-    }
-
-    return { trigger };
+// TODO: Alternatively could investigate angular-raven
+sentry.config(['$provide', function ($provide) {
+    $provide.decorator('$exceptionHandler', ['$delegate', function ($delegate) {
+        return function (exception, cause) {
+            $delegate(exception, cause);
+            raven.captureException(exception, cause);
+        };
+    }]);
 }]);
 
 sentry.run(['$rootScope', 'sentryEnabled', 'sentryDsn',
