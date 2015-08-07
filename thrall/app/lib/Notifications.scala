5d6acbf303e52eeae15bb7405b5c2965100010de
Akash A <akash1810@gmail.com>
2015-08-07 11:26:39 +0100

    Updating new resources to a more accurate name.

--- thrall/app/lib/Notifications.scala
+++ thrall/app/lib/Notifications.scala
@@ -2,4 +2,4 @@ package lib
 
 import com.gu.mediaservice.lib.aws.SNS
 
-object ImageUploadNotifications extends SNS(Config.awsCredentials, Config.imageUploadTopicArn)
+object ImageUploadNotifications extends SNS(Config.awsCredentials, Config.imageIndexedTopicArn)
