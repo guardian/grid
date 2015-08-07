5eca21e38b225956df63c34324285c64e8bc9f4c
Akash A <akash1810@gmail.com>
2015-08-07 18:29:30 +0100

    Using the usage rights as a way to determine if the image should be archived.

--- metadata-editor/app/lib/MetadataMessageConsumer.scala
+++ metadata-editor/app/lib/MetadataMessageConsumer.scala
@@ -1,12 +1,11 @@
 package lib
 
-import _root_.play.api.libs.json._
+import play.api.libs.json._
 import com.gu.mediaservice.lib.aws.MessageConsumer
-import com.gu.mediaservice.lib.config.MetadataConfig.staffPhotographers
-import com.gu.mediaservice.model.Image
+import com.gu.mediaservice.model._
+import play.api.libs.concurrent.Execution.Implicits._
 
 import scala.concurrent.Future
-import play.api.libs.concurrent.Execution.Implicits._
 
 object MetadataMessageConsumer extends MessageConsumer(
   Config.queueUrl, Config.awsEndpoint, Config.awsCredentials, MetadataEditorMetrics.processingLatency) {
@@ -16,17 +15,16 @@ object MetadataMessageConsumer extends MessageConsumer(
       case "image-indexed" => processIndexedImage
     }
 
-  def isStaffPhotographer(image: Image) = {
-    image.metadata.byline match {
-      case Some(byline: String) => staffPhotographers.contains(byline)
-      case _                    => false
+  def isArchivingUsageRights[T <: UsageRights](usageRights: T) =
+    usageRights match {
+      case _:StaffPhotographer | _:ContractPhotographer | _:CommissionedPhotographer => true
+      case _ => false
     }
-  }
 
-  def processIndexedImage(jsImage: JsValue): Future[Any] = {
-    jsImage.asOpt[Image] match {
+  def processIndexedImage(message: JsValue): Future[Any] = {
+    message.asOpt[Image] match {
       case Some(image: Image) => {
-        isStaffPhotographer(image) match {
+        isArchivingUsageRights(image.usageRights) match {
           case true => DynamoEdits.setArchived(image.id, true)
           case _    => Future(image)
         }
