14b0ffb52eaebb4f8d3a6f814c73b5608d83c629
Akash A <akash1810@gmail.com>
2015-08-07 10:49:12 +0100

    Delete image component.

--- kahuna/public/js/components/gr-confirm-delete/gr-confirm-delete.js
+++ kahuna/public/js/components/gr-confirm-delete/gr-confirm-delete.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import './gr-confirm-delete.css!';
 
 export const confirmDelete = angular.module('gr.confirmDelete', []);
 
@@ -6,10 +7,14 @@ confirmDelete.directive('grConfirmDelete', ['$timeout', function($timeout) {
 
     return {
         restrict: 'E',
+        transclude: true,
         template: `
-            <button class="button-ico" type="button" ng:click="showConfirm = true">
-                <span class="confirm" ng:if="showConfirm">Sure?</span>
-                <gr-icon>undo</gr-icon>
+            <button class="gr-confirm-delete" type="button"
+                ng:click="showConfirm = true"
+                ng:class="{'gr-confirm-delete--confirm': showConfirm}">
+                <gr-icon>delete</gr-icon>
+                <ng-transclude ng:if="!showConfirm"></ng-transclude>
+                <span class="gr-confirm-delete__label" ng:if="showConfirm">Confirm delete</span>
             </button>`,
 
         link: function(scope, element, attrs) {
