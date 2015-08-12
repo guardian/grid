6c398e039c8b8de027c2c69c63f80373a58a7ec5
Akash A <akash1810@gmail.com>
2015-08-12 14:41:42 +0100

    Adding delete button to uploads page.

--- kahuna/public/js/upload/controller.js
+++ kahuna/public/js/upload/controller.js
@@ -1,18 +1,55 @@
 import angular from 'angular';
 import '../edits/image-editor';
+import '../components/gr-delete-image/gr-delete-image';
 
 var upload = angular.module('kahuna.upload.controller', ['kahuna.edits.imageEditor']);
 
-upload.controller('UploadCtrl',
-                  ['$scope', 'uploadManager', 'mediaApi',
-                   function($scope, uploadManager, mediaApi) {
+upload.controller('UploadCtrl', [
+    '$scope', '$state', '$window', 'uploadManager', 'mediaApi',
+    function($scope, $state, $window, uploadManager, mediaApi) {
+        var ctrl = this;
 
-    // TODO: Show multiple jobs?
-    this.latestJob = uploadManager.getLatestRunningJob();
+        ctrl.deletable = new Set();
 
-    // my uploads
-    mediaApi.getSession().then(session => {
-        var uploadedBy = session.user.email;
-        mediaApi.search('', { uploadedBy }).then(resource => this.myUploads = resource);
-    });
-}]);
+        // TODO: Show multiple jobs?
+        ctrl.latestJob = uploadManager.getLatestRunningJob();
+
+        // my uploads
+        mediaApi.getSession().then(session => {
+            var uploadedBy = session.user.email;
+            mediaApi.search('', { uploadedBy }).then(resource => {
+
+                resource.data.forEach(image => {
+                    mediaApi.canDelete(image).then(deletable => {
+                        if (deletable) {
+                            ctrl.deletable.add(image);
+                        }
+                    });
+                });
+
+                ctrl.myUploads = resource;
+            });
+        });
+
+        ctrl.canBeDeleted = function (image) {
+            return ctrl.deletable.has(image);
+        };
+
+        ctrl.onDeleteSuccess = function (resp, image) {
+            var index = ctrl.myUploads.data.findIndex(i => i.data.id === image.data.id);
+
+            if (index > -1) {
+                ctrl.myUploads.data.splice(index, 1);
+                ctrl.deletable.delete(image);
+            }
+        };
+
+        ctrl.onDeleteError = function (err) {
+            if (err.body.errorKey === 'image-not-found') {
+                $state.go('search');
+            } else {
+                $window.alert(err.body.errorMessage);
+            }
+        };
+    }
+]);
