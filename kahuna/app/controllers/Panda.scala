25e228d9543f86fd3bc975f8c1a6fdf0bf7fd4e3
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-08-17 22:56:56 +0100

    Validate redirectUri (/after/ auth though ;_;)

--- kahuna/app/controllers/Panda.scala
+++ kahuna/app/controllers/Panda.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import java.net.URI
+
 import lib.Config
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
@@ -20,6 +22,7 @@ object Panda extends Controller
   override lazy val authCallbackBaseUri = Config.rootUri
   override lazy val loginUriTemplate    = Config.loginUriTemplate
 
+  import Config.domainRoot
 
   // FIXME: how to use usual Authenticated action helper for this? separate Controller?
   def session = Action { implicit request =>
@@ -44,11 +47,24 @@ object Panda extends Controller
     }
   }
 
+
+  def isOwnDomainAndSecure(uri: URI): Boolean = {
+    uri.getHost.endsWith(domainRoot) && uri.getScheme == "https"
+  }
+  def isValidDomain(inputUri: String): Boolean = {
+    Try(URI.create(inputUri)).filter(isOwnDomainAndSecure).isSuccess
+  }
+
+
   // Trigger the auth cycle
   // If a redirectUri is provided, redirect the browser there once auth'd,
   // else return a dummy page (e.g. for automatically re-auth'ing in the background)
+  // FIXME: validate redirectUri before doing the auth
   def doLogin(redirectUri: Option[String] = None) = AuthAction { req =>
-    redirectUri map (Redirect(_)) getOrElse Ok("logged in")
+    redirectUri map {
+      case uri if isValidDomain(uri) => Redirect(uri)
+      case _ => Ok("logged in (not redirecting to external redirectUri)")
+    } getOrElse Ok("logged in")
   }
 
   def oauthCallback = Action.async { implicit request =>
