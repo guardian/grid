29477e27e266bdb3ae40830ceef905f23063a171
Robert Kenny <robert.kenny@guardian.co.uk>
2015-08-24 11:48:28 +0100

    add logging to elk for all services except ES and ftp-watcher

--- image-loader/app/Global.scala
+++ image-loader/app/Global.scala
@@ -3,12 +3,18 @@ import play.api.mvc.WithFilters
 import play.api.{Application, GlobalSettings}
 import play.filters.gzip.GzipFilter
 
+import lib.{LogConfig, Config}
+
 import controllers.{Application => App}
 
 import com.gu.mediaservice.lib.play.RequestLoggingFilter
 
 
 object Global extends WithFilters(CorsFilter, RequestLoggingFilter, new GzipFilter) with GlobalSettings {
+
+  override def beforeStart(app: Application) {
+    LogConfig.init(Config)
+  }
 
   override def onStart(app: Application) {
     App.keyStore.scheduleUpdates(Akka.system(app).scheduler)
