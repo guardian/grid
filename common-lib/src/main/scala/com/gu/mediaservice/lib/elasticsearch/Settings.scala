65684e711a28f79dc0e665a69f5cdf72cf812c69
Robert Kenny <robert.kenny@guardian.co.uk>
2015-08-25 11:15:37 +0100

    add stopwords, rejig structure

--- common-lib/src/main/scala/com/gu/mediaservice/lib/elasticsearch/Settings.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/elasticsearch/Settings.scala
@@ -6,34 +6,38 @@ import play.api.libs.json.Json
 object IndexSettings {
 
   val englishSStemmer = Json.obj(
+    "type" -> "custom",
     "tokenizer" -> "standard",
     "filter" -> Json.arr(
+      "gu_stopwords",
       "lowercase",
       "s_stemmer"
     )
   )
 
-  val sStemmer = Json.obj(
+  val filter = Json.obj(
     "s_stemmer" -> Json.obj(
       "type" -> "stemmer",
       "language" -> "minimal_english"
+    ),
+    "gu_stopwords" -> Json.obj(
+      "type" -> "stop",
+      "stopwords" -> "_english_"
     )
-  )
-
-  val analysis = Json.obj(
-    "filter" -> sStemmer
   )
 
   val analyzer = Json.obj(
     "english_s_stemmer" -> englishSStemmer
   )
 
+  val analysis = Json.obj(
+    "filter" -> filter,
+    "analyzer" -> analyzer
+  )
+
   val imageSettings: String =
     Json.stringify(Json.obj(
-      "settings" -> Json.obj(
-        "analysis" -> analysis,
-        "analyzer" -> analyzer
-      )
+      "analysis" -> analysis
     )
   )
 
