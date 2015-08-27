82b12242491d0652a7b619ee66a01e41778fdcf7
Robert Kenny <robert.kenny@guardian.co.uk>
2015-08-27 11:48:23 +0100

    initial import

---
+++ s3watcher/S3Watcher.js
@@ -0,0 +1,54 @@
+// dependencies
+var async = require('async');
+var AWS = require('aws-sdk');
+var util = require('util');
+var request = require('request');
+var fs = require('fs');
+
+// get reference to S3 client
+var s3 = new AWS.S3();
+
+exports.handler = function(event, context) {
+    // Read options from the event.
+    console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+
+    var srcBucket = event.Records[0].s3.bucket.name;
+
+    // Object key may have spaces or unicode non-ASCII characters.
+    var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+
+    // Download the image from S3, transform, and upload to a different S3 bucket.
+    async.waterfall([
+        function download(next) {
+            // Download the image from S3 into a buffer.
+            s3.getObject({
+                    Bucket: srcBucket,
+                    Key: srcKey
+                },
+                next);
+            },
+        function upload(response, next) {
+            // Stream the image to the image loader
+
+            // TODO: Get configurable properties from outside script (i.e. URL, api-token)
+            var options = {
+                url: 'http://www.example.com',
+                headers: { 'X-Secret': 'api-token' }
+            };
+
+            response.createReadStream().pipe(request.put(options))
+
+            }
+        ], function (err) {
+            if (err) {
+                console.error('Failed:' + err);
+                // TODO: Move failed uploads to failed bucket
+            } else {
+                console.log('Success!');
+                // TODO: Delete successful uploads
+            }
+
+            context.done();
+        }
+    );
+};
