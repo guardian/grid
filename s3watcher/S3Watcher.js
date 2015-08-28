62f8ad94606bac0b1ede63054837bd0e9ceea3ca
Robert Kenny <robert.kenny@guardian.co.uk>
2015-08-28 09:28:49 +0100

    fiddling

--- s3watcher/S3Watcher.js
+++ s3watcher/S3Watcher.js
@@ -9,36 +9,51 @@ var fs = require('fs');
 var s3 = new AWS.S3();
 
 exports.handler = function(event, context) {
-    // Read options from the event.
-    console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
-
     var srcBucket = event.Records[0].s3.bucket.name;
-
-    // Object key may have spaces or unicode non-ASCII characters.
     var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+    var splitSrcKey = srcKey.split("/");
+    var simpleKey = splitSrcKey[(splitSrcKey.length - 1)]
 
     // Download the image from S3 and upload it to image loader
     async.waterfall([
         function download(next) {
             // Download the image from S3 into a buffer.
+
+            var filename = '/tmp/' + simpleKey;
+            var file = require('fs').createWriteStream(filename);
+
             s3.getObject({
-                    Bucket: srcBucket,
-                    Key: srcKey
-                },
-                next);
-            },
-        function upload(response, next) {
+                Bucket: srcBucket,
+                Key: srcKey
+            }).createReadStream().pipe(file).on('end', function() {
+                console.log("foo");
+                next(null, filename);
+            });
+        },
+        function upload(filename, next) {
+            console.log('up');
             // Stream the image to the image loader
 
+            var fileStream = fs.createReadStream(filename);
+
+            var stats = fs.statSync(filename);
+            var fileSizeInBytes = stats["size"]
+
+            var baseUrl = "https://loader.media.test.dev-gutools.co.uk"
             // TODO: Get configurable properties from outside script (i.e. URL, api-token)
             var options = {
-                url: 'http://www.example.com',
-                headers: { 'X-Secret': 'api-token' }
+                url: baseUrl + "/images?filename=" + filename,
+                headers: {
+                    'Content-Length': fileSizeInBytes
+                }
             };
 
-            response.createReadStream().pipe(request.put(options))
-
-            }
+            fileStream.pipe(
+                request.post(options).on('response', function(response) {
+                    console.log(response.statusCode)
+                })
+            );
+        }
         ], function (err) {
             if (err) {
                 console.error('Failed:' + err);
