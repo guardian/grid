d6edc62c31b6405e4872ff6010dc851d72d883b4
Robert Kenny <robert.kenny@guardian.co.uk>
2015-08-28 16:41:16 +0100

    working code for s3watcher

--- s3watcher/S3Watcher.js
+++ s3watcher/S3Watcher.js
@@ -12,58 +12,107 @@ exports.handler = function(event, context) {
     var srcBucket = event.Records[0].s3.bucket.name;
     var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
     var splitSrcKey = srcKey.split("/");
-    var simpleKey = splitSrcKey[(splitSrcKey.length - 1)]
+    var simpleKey = splitSrcKey[(splitSrcKey.length - 1)];
+    var filename = '/tmp/' + simpleKey;
 
-    // Download the image from S3 and upload it to image loader
+    // TODO: Constants we want set from elsewhere!
+    var baseUrl = "nope";
+    var apiKey = "nope";
+    var failBucket = "nope"
+
+    var uploadUrl = baseUrl + "/images?filename=" + simpleKey;
+
+    var objectParams = {
+        Bucket: srcBucket,
+        Key: srcKey
+    }
+
+    var failedObjectParams = {
+        CopySource: srcBucket + "/" + srcKey,
+        Bucket: failBucket,
+        Key: srcKey
+    };
+
+    var deleteObject = function(obj) {
+        s3.deleteObject(objectParams, function(err) {
+            if (err) {
+                console.log("Failed to delete source: " + err);
+                context.fail(err)
+            } else {
+                console.log('Source image deleted');
+                context.done();
+            }
+        });
+    };
+
     async.waterfall([
         function download(next) {
-            // Download the image from S3 into a buffer.
+            // Download the image from S3 and upload it to image loader
+            console.log('Downloading image: ' + srcKey);
 
-            var filename = '/tmp/' + simpleKey;
-            var file = require('fs').createWriteStream(filename);
+            var fileStream = fs.createWriteStream(filename);
 
             s3.getObject({
                 Bucket: srcBucket,
                 Key: srcKey
-            }).createReadStream().pipe(file).on('end', function() {
-                console.log("foo");
-                next(null, filename);
-            });
+            }).on('error', function(err) {
+                console.log('Downloading from S3 failed');
+                next(err);
+            }).createReadStream()
+                .pipe(fileStream)
+                .on('error', function(err) {
+                    console.log("Writing to local filesystem failed");
+                    next(err);
+                })
+                .on('close', function() {
+                    console.log("Download complete");
+                    next(null, filename);
+                });
+
         },
         function upload(filename, next) {
-            console.log('up');
             // Stream the image to the image loader
+            console.log('Starting upload to ' + uploadUrl);
 
             var fileStream = fs.createReadStream(filename);
-
             var stats = fs.statSync(filename);
             var fileSizeInBytes = stats["size"]
 
-            var baseUrl = "https://loader.media.test.dev-gutools.co.uk"
-            // TODO: Get configurable properties from outside script (i.e. URL, api-token)
+            var uploadHeaders = {
+                'Content-Length': fileSizeInBytes,
+                'X-Gu-Media-Key': apiKey
+            };
+
             var options = {
-                url: baseUrl + "/images?filename=" + filename,
-                headers: {
-                    'Content-Length': fileSizeInBytes
-                }
+                url: uploadUrl,
+                headers: uploadHeaders
             };
 
             fileStream.pipe(
                 request.post(options).on('response', function(response) {
-                    console.log(response.statusCode)
+                    if(response.statusCode == 202) {
+                        next();
+                    } else {
+                        next("Failed to upload with status code:" + response.statusCode);
+                        s3.copyObject(failedObjectParams, function(err) {
+                            if (err) {
+                                console.log("Failed copying object to fail bucket");
+                                next(err);
+                            } else {
+                                deleteObject(objectParams);
+                            }
+                        });
+                    }
                 })
             );
         }
         ], function (err) {
             if (err) {
-                console.error('Failed:' + err);
-                // TODO: Move failed uploads to failed bucket
+                context.fail(err);
             } else {
-                console.log('Success!');
-                // TODO: Delete successful uploads
+                console.log('Successful upload');
+                deleteObject(objectParams);
             }
-
-            context.done();
         }
     );
 };
