49a34f178a919bf4f6c0d68297563b37812f86c1
Robert Kenny <robert.kenny@guardian.co.uk>
2015-09-03 10:13:27 +0100

    rxjsify

---
+++ s3watcher/lib/Transfer.js
@@ -0,0 +1,51 @@
+const S3Helper     = require('./S3Helper');
+const Upload       = require('./Upload');
+
+
+module.exports = {
+    init: function(s3Event, config){
+        const operation = function() {
+            console.log("Downloading from ingest bucket.");
+
+            const upload = Upload.buildUpload(config, s3Event);
+
+            return S3Helper.getS3Object({
+                Bucket: s3Event.bucket,
+                Key: s3Event.key
+            }).flatMap(function(data){
+                console.log("Uploading to image-loader.");
+
+                return Upload.postData(upload, data.Body)
+            }).retry(3);
+        };
+
+        const success = function(result) {
+            console.log("Deleting from ingest bucket.");
+            return S3Helper.deleteS3Object({
+                Bucket: s3Event.bucket,
+                Key: s3Event.key
+            });
+        };
+
+        const fail = function() {
+            console.log("Copying to fail bucket.");
+            return S3Helper.copyS3Object({
+                CopySource: s3Event.bucket + "/" + s3Event.key,
+                Bucket: config.failBucket,
+                Key: s3Event.key
+            }).flatMap(function(){
+                console.log("Deleting from ingest bucket.");
+                return S3Helper.deleteS3Object({
+                    Bucket: s3Event.bucket,
+                    Key: s3Event.key
+                });
+            });
+        };
+
+        return {
+            operation: operation,
+            success: success,
+            fail: fail
+        };
+    }
+}
