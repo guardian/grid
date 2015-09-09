6a0352a86b409c404f401a97e3f95878944e32cf
James A Gorrie <james.gorrie@theguardian.com>
2015-09-09 11:56:16 +0100

    Alamy != Allstar

--- common-lib/src/main/scala/com/gu/mediaservice/lib/cleanup/SupplierProcessors.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/cleanup/SupplierProcessors.scala
@@ -79,7 +79,7 @@ object AlamyParser extends ImageProcessor {
 object AllStarParser extends ImageProcessor {
   def apply(image: Image): Image = image.metadata.credit match {
     case Some("Allstar Picture Library") => image.copy(
-      usageRights = Agency("Alamy"),
+      usageRights = Agency("Allstar Picture Library"),
       metadata = image.metadata.copy(credit = Some("Allstar Picture Library"))
     )
     case _ => image
