13f83473a0c2b084f9eb9173876c2bc829c4adf1
Emma Milner <ekmilner@gmail.com>
2015-09-10 13:04:28 +0100

    stops copyright disappearing when field emptied on upload page

--- kahuna/public/js/upload/jobs/required-metadata-editor.js
+++ kahuna/public/js/upload/jobs/required-metadata-editor.js
@@ -22,6 +22,7 @@ jobs.controller('RequiredMetadataEditorCtrl',
     ctrl.disabled = () => Boolean(ctrl.saving || ctrl.externallyDisabled);
     ctrl.metadata = metadataFromOriginal(ctrl.originalMetadata);
     ctrl.saveOnTime = 750; // ms
+    ctrl.copyrightWasInitiallyThere = !!ctrl.metadata.copyright;
 
     ctrl.save = function() {
         ctrl.saving = true;
