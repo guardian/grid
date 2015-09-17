ffe62e0c2601de0e6da2f71b0244bdd152a1bb7d
Robert Kenny <robert.kenny@guardian.co.uk>
2015-09-17 09:58:46 +0100

    make logging more verbose

--- s3watcher/lib/Transfer.js
+++ s3watcher/lib/Transfer.js
@@ -4,45 +4,55 @@ const Upload       = require('./Upload');
 
 module.exports = {
     init: function(s3Event, config){
+        const objectKey = s3Event.bucket + "/" +  s3Event.key;
+        const s3Object = {
+            Bucket: s3Event.bucket,
+            Key: s3Event.key
+        };
+        const s3CopyObject = {
+            CopySource: s3Event.bucket + "/" + s3Event.key,
+            Bucket: config.failBucket,
+            Key: s3Event.key
+        };
+        const upload = Upload.buildUpload(config, s3Event);
+
+        const logDownload = function(){
+            console.log("Downloading from ingest bucket.", s3Object);
+        };
+        const logUpload = function(){
+            console.log("Uploading to image-loader.", upload);
+        };
+        const logDelete = function(){
+            console.log("Deleting from ingest bucket.", s3Object);
+        };
+        const logCopy = function(){
+            console.log("Copying to fail bucket.", s3CopyObject);
+        };
+
         // TODO: Pipe download stream into upload stream
         // Currently the whole file is loaded into memory before
         // being uploaded to the image loader.
-        //
         const operation = function() {
-            console.log("Downloading from ingest bucket.");
+            logDownload();
 
-            const upload = Upload.buildUpload(config, s3Event);
-
-            return S3Helper.getS3Object({
-                Bucket: s3Event.bucket,
-                Key: s3Event.key
-            }).flatMap(function(data){
-                console.log("Uploading to image-loader.");
+            return S3Helper.getS3Object(s3Object).flatMap(function(data){
+                logUpload();
 
                 return Upload.postData(upload, data.Body)
             }).retry(3);
         };
 
         const success = function(result) {
-            console.log("Deleting from ingest bucket.");
-            return S3Helper.deleteS3Object({
-                Bucket: s3Event.bucket,
-                Key: s3Event.key
-            });
+            logDelete();
+
+            return S3Helper.deleteS3Object(s3Object);
         };
 
         const fail = function() {
-            console.log("Copying to fail bucket.");
-            return S3Helper.copyS3Object({
-                CopySource: s3Event.bucket + "/" + s3Event.key,
-                Bucket: config.failBucket,
-                Key: s3Event.key
-            }).flatMap(function(){
-                console.log("Deleting from ingest bucket.");
-                return S3Helper.deleteS3Object({
-                    Bucket: s3Event.bucket,
-                    Key: s3Event.key
-                });
+            logCopy();
+            return S3Helper.copyS3Object(s3CopyObject).flatMap(function(){
+                logDelete();
+                return S3Helper.deleteS3Object(s3Object);
             });
         };
 
