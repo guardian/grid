e2c25189cb7ebf6872f0bd870f24496facb04eeb
James A Gorrie <james.gorrie@theguardian.com>
2015-09-18 12:09:23 +0100

    add in the guardian credits to cost calculator

--- media-api/app/lib/usagerights/CostCalculator.scala
+++ media-api/app/lib/usagerights/CostCalculator.scala
@@ -6,15 +6,20 @@ import com.gu.mediaservice.model._
 
 object CostCalculator {
   import UsageRightsConfig.{freeSuppliers, suppliersCollectionExcl}
+  import DeprecatedConfig.guardianCredits
 
   val defaultCost = Pay
+
+  // HACK: This is until we decide what to do with Guardian credits
+  def getCost(credit: Option[String]): Option[Cost] =
+    if (credit.exists(guardianCredits.contains)) Some(Free) else None
 
   def getCost(supplier: String, collection: Option[String]): Option[Cost] = {
       val free = isFreeSupplier(supplier) && ! collection.exists(isExcludedColl(supplier, _))
       if (free) Some(Free) else None
   }
 
-  def getCost(usageRights: UsageRights): Cost = {
+  def getCost(usageRights: UsageRights): Option[Cost] = {
       val restricted  : Option[Cost] = usageRights.restrictions.map(r => Conditional)
       val categoryCost: Option[Cost] = usageRights.defaultCost
       val supplierCost: Option[Cost] = usageRights match {
@@ -25,7 +30,12 @@ object CostCalculator {
       restricted
         .orElse(categoryCost)
         .orElse(supplierCost)
-        .getOrElse(defaultCost)
+  }
+
+  def getCost(usageRights: UsageRights, credit: Option[String]): Cost = {
+    getCost(credit)
+      .orElse(getCost(usageRights))
+      .getOrElse(defaultCost)
   }
 
   private def isFreeSupplier(supplier: String) = freeSuppliers.contains(supplier)
