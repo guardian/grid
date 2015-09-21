d28706e3de46f90b7e9be11304268ba6143d73e5
James A Gorrie <james.gorrie@theguardian.com>
2015-09-21 09:39:49 +0100

    remove old code from usage rights editpr

--- kahuna/public/js/usage-rights/usage-rights-editor.js
+++ kahuna/public/js/usage-rights/usage-rights-editor.js
@@ -72,6 +72,7 @@ usageRightsEditor.controller(
     inject$($scope, savingDisabled$, ctrl, 'savingDisabled');
 
     // TODO: Some of these, especially `isRestricted` could be streams
+    ctrl.saving = false;
     ctrl.getOptionsFor = property => {
         const options = property.options.map(option => ({ key: option, value: option }));
         if (property.required) {
@@ -82,10 +83,14 @@ usageRightsEditor.controller(
     };
 
     ctrl.save = () => {
+        ctrl.saving = true;
         // we save as `{}` if category isn't defined.
         const data = ctrl.category.value ?
             angular.extend({}, ctrl.model, { category: ctrl.category.value }) : {};
-        save(data)
+
+        save(data).
+        catch(uiError).
+        finally(() => ctrl.saving = false);
     };
 
     ctrl.reset = () => {
@@ -96,13 +101,12 @@ usageRightsEditor.controller(
         ctrl.showRestrictions || ctrl.category.defaultRestrictions || prop.required;
 
     function save(data) {
-        $q.all(ctrl.usageRights.map((usageRights) => {
+        return $q.all(ctrl.usageRights.map(usageRights => {
             const image = usageRights.image;
             const resource = image.data.userMetadata.data.usageRights;
             return editsService.update(resource, data, image).
                 then(resource => resource.data);
-        })).catch(uiError).
-            finally(() => updateSuccess(data));
+        }));
     }
 
     function getUniqueCats(usageRights) {
@@ -132,171 +136,6 @@ usageRightsEditor.controller(
         }, {});
 
         return objOfArrays;
-    }
-
-
-
-    return;
-
-    var ctrl = this;
-
-    ctrl.saving = false;
-    ctrl.saved = false;
-    ctrl.categories = [];
-    ctrl.originalCats = [];
-    ctrl.model = angular.extend({}, ctrl.usageRights.data);
-
-    const multiRights = { name: 'Multiple categories', value: '', description: '' };
-    const catsWithMultiRights = () => [multiRights].concat(ctrl.originalCats);
-    const setStandardCats = () => ctrl.categories = ctrl.originalCats;
-    const setMultiRightsCats = () => {
-        ctrl.categories = catsWithMultiRights();
-        ctrl.category = multiRights;
-    };
-
-
-    const getGroupCategory =
-        (usageRights, cats) => cats.find(cat => cat.value === usageRights.reduce(
-            (m, o) => (m == o.data.category) ? o.data.category : {},
-            usageRights[0].data.category
-        ));
-
-    const getGroupModel = usageRights =>
-        ctrl.multipleUsageRights() ? {} : angular.extend({}, usageRights[0].data);
-
-    ctrl.reset = () => {
-        ctrl.showRestrictions = false;
-        ctrl.model = {};
-    };
-    ctrl.multipleUsageRights = () => ctrl.usageRights.length > 1;
-
-    ctrl.update = function() {
-        ctrl.category = getGroupCategory(ctrl.usageRights, ctrl.categories);
-
-        // If we have multi or no rights we need to add the option to
-        // the drop down and select it to give the user feedback as to
-        // what's going on.
-        if (!ctrl.category && ctrl.usageRights.length > 1) {
-            setMultiRightsCats();
-        } else {
-            setStandardCats();
-        }
-
-        ctrl.model = getGroupModel(ctrl.usageRights);
-        ctrl.showRestrictions = angular.isDefined(ctrl.model.restrictions);
-    };
-
-    // setting our initial values
-    editsApi.getUsageRightsCategories().then(cats => {
-        const categoriesCopy = angular.copy(cats);
-
-        categoriesCopy.forEach(cat => {
-            cat.properties.forEach((property, i) => {
-                let propertyOptions = property.required ? [] : [{key: 'None', value: null}];
-
-                if (property.options) {
-                    property.options.forEach(option => {
-                        propertyOptions.push({key: option, value: option});
-                    });
-
-                    cat.properties[i].options = propertyOptions;
-                }
-            });
-        });
-
-        ctrl.categories = categoriesCopy;
-        ctrl.originalCats = categoriesCopy;
-        ctrl.update();
-    });
-
-    $scope.$watchCollection(() => ctrl.usageRights, onValChange(() => {
-        ctrl.update();
-    }));
-
-    // TODO: What error would we like to show here?
-    // TODO: How do we make this more synchronous? You can only resolve on the
-    // routeProvider, which is actually bound to the UploadCtrl in this instance
-    // SEE: https://github.com/angular/angular.js/issues/2095
-    ctrl.save = () => {
-        const data = modelToData(ctrl.model);
-        save(data);
-    };
-
-    ctrl.cancel = () => ctrl.onCancel();
-
-    // stop saving on no/multi rights
-    ctrl.savingDisabled = () => {
-        return ctrl.saving || angular.equals(ctrl.category, multiRights);
-    };
-
-    ctrl.getOptionsFor = property => {
-        const key = ctrl.category
-                        .properties
-                        .find(prop => prop.name === property.optionsMapKey)
-                        .name;
-
-        const val = ctrl.model[key];
-        return property.optionsMap[val] || [];
-    };
-
-    ctrl.isOtherValue = property => {
-        if (!ctrl.model[property.name]) {
-            // if we haven't set a value, it won't be in the list of available values,
-            // but this isn't considered "other", it's "not set".
-            return false;
-        } else {
-            const missingVal =
-                !ctrl.getOptionsFor(property)
-                    .find(option => option === ctrl.model[property.name]);
-
-            return missingVal;
-        }
-    };
-
-    ctrl.isRestricted = prop =>
-        ctrl.showRestrictions || ctrl.category.defaultRestrictions || prop.required;
-
-    $scope.$watch(() => ctrl.showRestrictions, onValChange(isRestricted => {
-        if (!isRestricted) {
-            delete ctrl.model.restrictions;
-        }
-    }));
-
-    function setCategory(val) {
-        ctrl.category = ctrl.categories.find(cat => cat.value === val);
-    }
-
-    function modelToData(model) {
-        if (ctrl.category.value === '') {
-            return {};
-        } else {
-            return angular.extend({}, model, { category: ctrl.category && ctrl.category.value });
-        }
-    }
-
-    //function save(data) {
-    //    ctrl.error = null;
-    //    ctrl.saving = true;
-    //    $q.all(ctrl.usageRights.map((usageRights) => {
-    //        const image = usageRights.image;
-    //        const resource = image.data.userMetadata.data.usageRights;
-    //        return editsService.update(resource, data, image).
-    //            then(resource => resource.data);
-    //    })).catch(uiError).
-    //        finally(() => updateSuccess(data));
-    //}
-
-    function updateSuccess(data) {
-        ctrl.model = data;
-        ctrl.onSave();
-        setCategory(data.category);
-        uiSaved();
-        ctrl.saving = false;
-    }
-
-    function uiSaved() {
-        ctrl.saved = true;
-        $timeout(() => ctrl.saved = false, 1500);
     }
 
     function uiError(error) {
