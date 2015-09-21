4c4580a89ddd3afc7912484dc1d9792e126b3916
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-09-21 12:11:42 +0100

    Correct colour profile if ICC/image colour model don't match

--- common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/imaging/ImageOperations.scala
@@ -2,6 +2,8 @@ package com.gu.mediaservice.lib.imaging
 
 import java.io._
 
+import org.im4java.core.IMOperation
+
 import com.gu.mediaservice.lib.Files._
 import com.gu.mediaservice.lib.imaging.im4jwrapper.{ExifTool, ImageMagick}
 import com.gu.mediaservice.model.{Asset, Bounds, Dimensions, ImageMetadata}
@@ -16,7 +18,14 @@ object ImageOperations {
   import ExifTool._
   import ImageMagick._
 
-  lazy val imageProfileLocation = s"${play.api.Play.current.path}/srgb.icc"
+  private def profilePath(fileName: String): String = s"${play.api.Play.current.path}/$fileName"
+
+  private def profileLocation(colourModel: String): String = colourModel match {
+    case "RGB"       => profilePath("srgb.icc")
+    case "CMYK"      => profilePath("cmyk.icc")
+    case "GRAYSCALE" => profilePath("grayscale.icc")
+    case model       => throw new Exception(s"Profile for invalid colour model requested: $model")
+  }
 
   private def tagFilter(metadata: ImageMetadata) = {
     Map[String, Option[String]](
@@ -27,23 +36,42 @@ object ImageOperations {
     ).collect { case (key, Some(value)) => (key, value) }
   }
 
+  private def applyOutputProfile(base: IMOperation) = profile(base)(profileLocation("RGB"))
+
   def identifyColourModel(sourceFile: File, mimeType: String): Future[Option[String]] = {
+    val source    = addImage(sourceFile)
+    // TODO: use mimeType to lookup other properties once we support other formats
+    val formatter = format(source)("%[JPEG-Colorspace-Name]")
     for {
-      _           <- Future.successful()
-      source       = addImage(sourceFile)
-      formatter    = format(source)("%[JPEG-Colorspace-Name]")
-      colourModel <- runIdentifyCmd(formatter).map(_.headOption)
+      output      <- runIdentifyCmd(formatter)
+      colourModel  = output.headOption
     } yield colourModel
   }
 
-  def cropImage(sourceFile: File, bounds: Bounds, qual: Double = 100d): Future[File] = {
+  // Optionally apply transforms to the base operation if the colour space
+  // in the ICC profile doesn't match the colour model of the image data
+  private def correctColour(base: IMOperation)(iccColourSpace: Option[String], colourModel: Option[String]) = {
+    (iccColourSpace, colourModel) match {
+      // If matching, all is well, just pass through
+      case (icc, model) if icc == model => base
+      // If no colour model detected, we can't do anything anyway so just hope all is well
+      case (_,   None) => base
+      // If mismatching, strip any (incorrect) ICC profile and inject a profile matching the model
+      // Note: ICC identified as "icm" here
+      case (_,   Some(model)) => profile(stripProfile(base)("icm"))(profileLocation(model))
+    }
+  }
+
+  def cropImage(sourceFile: File, bounds: Bounds, qual: Double = 100d, tempDir: File,
+                iccColourSpace: Option[String], colourModel: Option[String]): Future[File] = {
     for {
-      outputFile <- createTempFile(s"crop-", ".jpg", Config.tempDir)
+      outputFile <- createTempFile(s"crop-", ".jpg", tempDir)
       cropSource  = addImage(sourceFile)
       qualified   = quality(cropSource)(qual)
-      converted   = profile(qualified)(imageProfileLocation)
+      corrected   = correctColour(qualified)(iccColourSpace, colourModel)
+      converted   = applyOutputProfile(corrected)
       stripped    = stripMeta(converted)
-      profiled    = profile(stripped)(imageProfileLocation)
+      profiled    = applyOutputProfile(stripped)
       cropped     = crop(profiled)(bounds)
       addOutput   = addDestImage(cropped)(outputFile)
       _          <- runConvertCmd(addOutput)
@@ -58,9 +86,9 @@ object ImageOperations {
       ).map(_ => sourceFile)
   }
 
-  def resizeImage(sourceFile: File, dimensions: Dimensions, qual: Double = 100d): Future[File] = {
+  def resizeImage(sourceFile: File, dimensions: Dimensions, qual: Double = 100d, tempDir: File): Future[File] = {
     for {
-      outputFile  <- createTempFile(s"resize-", ".jpg", Config.tempDir)
+      outputFile  <- createTempFile(s"resize-", ".jpg", tempDir)
       resizeSource = addImage(sourceFile)
       qualified    = quality(resizeSource)(qual)
       resized      = scale(qualified)(dimensions)
