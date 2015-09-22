3a07d646866a6d92b121798114ae95d6a1949819
James A Gorrie <james.gorrie@theguardian.com>
2015-09-22 12:00:14 +0100

    use immutable.js as it's much nicer

--- kahuna/public/js/usage-rights/usage-rights-editor.js
+++ kahuna/public/js/usage-rights/usage-rights-editor.js
@@ -1,25 +1,29 @@
 import angular from 'angular';
 import 'angular-elastic';
 
+import Rx from 'rx';
+import '../util/rx';
+
+import {List} from 'immutable';
+
+import '../services/image-list';
+
 import template from './usage-rights-editor.html!text';
 import './usage-rights-editor.css!';
 
 import '../components/gr-confirm-delete/gr-confirm-delete.js';
 
-
-import Rx from 'rx';
-import '../util/rx';
-
 export var usageRightsEditor = angular.module('kahuna.edits.usageRightsEditor', [
     'monospaced.elastic',
     'gr.confirmDelete',
-    'util.rx'
+    'util.rx',
+    'kahuna.services.image-list'
 ]);
 
 usageRightsEditor.controller(
     'UsageRightsEditorCtrl',
-    ['$q', '$scope', 'inject$', 'observe$', 'editsService', 'editsApi',
-    function($q, $scope, inject$, observe$, editsService, editsApi) {
+    ['$q', '$scope', 'inject$', 'observe$', 'editsService', 'editsApi', 'imageList',
+    function($q, $scope, inject$, observe$, editsService, editsApi, imageList) {
 
     var ctrl = this;
     const multiCat = { name: 'Multiple categories', value: 'multi-cat', properties: [] };
@@ -58,16 +62,19 @@ usageRightsEditor.controller(
     const category$ = categoryFromUsageRights$.merge(categoryChange$).distinctUntilChanged();
 
     const model$ = usageRights$.map(urs => {
-        const multiModel = reduceObjectsToArrays(urs.map(ur => ur.data));
-        return Object.keys(multiModel).reduce((model, key) => {
-            if (unique(multiModel[key]).length === 1) {
-                model[key] = multiModel[key][0];
-            }
-            return model;
-        }, {});
+        const usageRightsData = (urs.map(ur => ur.data));
+
+        // Get a Map(property, Set(values));
+        const objs = imageList.getSetOfProperties(List(usageRightsData));
+
+        // Return an object with the value, iif there is 1 value
+        return objs.filter(obj => obj.size === 1).map(obj => obj.first()).toJS();
     });
 
+    // Stream.<Boolean>
     const savingDisabled$ = category$.map(cat => cat === multiCat);
+
+    // Stream.<Boolean>
     const forceRestrictions$ = model$.combineLatest(category$, (model, cat) => {
         const defaultRestrictions =
             cat.properties.find(prop => prop.name === 'defaultRestrictions');
@@ -77,10 +84,16 @@ usageRightsEditor.controller(
         return defaultRestrictions || (restrictedProp && restrictedProp.required);
     });
 
+    // Stream.<Boolean>
     const userSetShowRestrictions$ = observe$($scope, () => ctrl.showRestrictions);
+
+    // Stream.<Boolean>
     const modelHasRestrictions$ = model$.map(model => angular.isDefined(model.restrictions));
+
+    // Stream.<Boolean>
     const shouldShowRestrictions$ = userSetShowRestrictions$.merge(modelHasRestrictions$);
 
+    // Stream.<Boolean>
     const showRestrictions$ = forceRestrictions$.combineLatest(shouldShowRestrictions$,
         (forceRestrictions, showRestrictions) => {
 
@@ -157,27 +170,6 @@ usageRightsEditor.controller(
     function unique(arr) {
         return arr.reduce((prev, curr) =>
             prev.indexOf(curr) !== -1 ? prev : prev.concat(curr), []);
-    }
-
-    // takes an array of objects and turns it into an object with an array of unique values
-    // e.g. [{ a: 1, b: 2 }, { a: 2, b: 2, c: 3 }] => { a: [1,2], b: [2], c: [3] }
-    // TODO: Use the nicer, immutable `imageList.getSetOfProperties`
-    function reduceObjectsToArrays(objects) {
-        // find a list of available keys
-        const keys = unique(objects.reduce((keys, obj) => {
-            return Object.keys(obj).concat(keys);
-        }, []));
-
-        const objOfArrays = objects.reduce((objOfArrays, obj) => {
-            keys.forEach(key => {
-                const val = [obj[key]];
-                objOfArrays[key] = objOfArrays[key] ? objOfArrays[key].concat(val) : val;
-            });
-
-            return objOfArrays;
-        }, {});
-
-        return objOfArrays;
     }
 
     function uiError(error) {
