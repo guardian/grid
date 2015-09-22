447e5aff312b59355fd8a67907a37ab4d7141bd0
James A Gorrie <james.gorrie@theguardian.com>
2015-09-22 17:28:47 +0100

    move usage rights converter out and give  it some tests

---
+++ metadata-editor/test/UsageRightsMetadataMapperTest.scala
@@ -0,0 +1,48 @@
+package lib
+
+import com.gu.mediaservice.model._
+import org.scalatest.{FunSpec, Matchers}
+
+class UsageRightsMetadataMapperTest extends FunSpec with Matchers {
+
+  import UsageRightsMetadataMapper.usageRightsToMetadata
+
+  describe("UsageRights => ImageMetadata") {
+
+    it ("should convert StaffPhotographers") {
+      val ur = StaffPhotographer("Alicia Canter", "The Guardian")
+      usageRightsToMetadata(ur) should be
+        Some(ImageMetadata(credit = Some("The Guardian"), byline = Some("Alicia Canter")))
+    }
+
+    it ("should convert ContractPhotographers") {
+      val ur = StaffPhotographer("Andy Hall", "The Observer")
+      usageRightsToMetadata(ur) should be
+        Some(ImageMetadata(credit = Some("The Observer"), byline = Some("Andy Hall")))
+    }
+
+    it ("should convert CommissionedPhotographers") {
+      val ur = CommissionedPhotographer("Mr. Photo", Some("Weekend Magazine"))
+      usageRightsToMetadata(ur) should be
+        Some(ImageMetadata(credit = Some("Weekend Magazine"), byline = Some("Mr. Photo")))
+    }
+
+    it ("should convert ContractIllustrators") {
+      val ur = ContractIllustrator("First Dog on the Moon Institute")
+      usageRightsToMetadata(ur) should be
+        Some(ImageMetadata(credit = Some("First Dog on the Moon Institute")))
+    }
+
+    it ("should convert CommissionedIllustrators") {
+      val ur = CommissionedIllustrator("Roger Rabbit")
+      usageRightsToMetadata(ur) should be
+        Some(ImageMetadata(credit = Some("Roger Rabit")))
+    }
+
+    it ("should not convert Agencies") {
+      val ur = Agency("Rex Features")
+      usageRightsToMetadata(ur) should be(None)
+    }
+
+  }
+}
