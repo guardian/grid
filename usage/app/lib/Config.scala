cc88bd852e897ed2bfe7d334ebdb6068e33493d3
Robert Kenny <robert.kenny@guardian.co.uk>
2015-09-24 16:34:38 +0100

    move tablename into property

--- usage/app/lib/Config.scala
+++ usage/app/lib/Config.scala
@@ -20,6 +20,6 @@ object Config extends CommonPlayAppProperties with CommonPlayAppConfig {
   val kahunaUri = services.kahunaBaseUri
   val loginUriTemplate = services.loginUriTemplate
 
-  val pollerTable = "media-service-DEV-ContentPoller"
+  val pollerTable = properties("dynamo.tablename.pollTable")
   val dynamoRegion: Region = Region.getRegion(Regions.EU_WEST_1)
 }
