357f422ed7b9ed9b8f686dc4fec3b5a572a4816d
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-09-24 11:29:16 +0100

    Add tests for colour model detection

--- common-lib/src/test/scala/com/gu/mediaservice/lib/imaging/ImageOperationsTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/imaging/ImageOperationsTest.scala
@@ -1,8 +1,61 @@
 package com.gu.mediaservice.lib.imaging
 
-/**
- * Created by seb on 24/09/15.
- */
-class ImageOperationsTest {
+import java.io.File
+
+import org.scalatest.time.{Millis, Span}
+import org.scalatest.{Matchers, FunSpec}
+import org.scalatest.concurrent.ScalaFutures
+
+class ImageOperationsTest extends FunSpec with Matchers with ScalaFutures {
+
+  implicit override val patienceConfig = PatienceConfig(timeout = Span(1000, Millis), interval = Span(25, Millis))
+
+  describe("identifyColourModel") {
+    it("should return RGB for a JPG image with RGB image data and no embedded profile") {
+      val image = fileAt("rgb-wo-profile.jpg")
+      val colourModelFuture = ImageOperations.identifyColourModel(image, "image/jpeg")
+      whenReady(colourModelFuture) { colourModel =>
+        colourModel should be (Some("RGB"))
+      }
+    }
+
+    it("should return RGB for a JPG image with RGB image data and an RGB embedded profile") {
+      val image = fileAt("rgb-with-rgb-profile.jpg")
+      val colourModelFuture = ImageOperations.identifyColourModel(image, "image/jpeg")
+      whenReady(colourModelFuture) { colourModel =>
+        colourModel should be (Some("RGB"))
+      }
+    }
+
+    it("should return RGB for a JPG image with RGB image data and an incorrect CMYK embedded profile") {
+      val image = fileAt("rgb-with-cmyk-profile.jpg")
+      val colourModelFuture = ImageOperations.identifyColourModel(image, "image/jpeg")
+      whenReady(colourModelFuture) { colourModel =>
+        colourModel should be (Some("RGB"))
+      }
+    }
+
+    it("should return CMYK for a JPG image with CMYK image data") {
+      val image = fileAt("cmyk.jpg")
+      val colourModelFuture = ImageOperations.identifyColourModel(image, "image/jpeg")
+      whenReady(colourModelFuture) { colourModel =>
+        colourModel should be (Some("CMYK"))
+      }
+    }
+
+    it("should return GRAYSCALE for a JPG image with GRAYSCALE image data and no embedded profile") {
+      val image = fileAt("grayscale-wo-profile.jpg")
+      val colourModelFuture = ImageOperations.identifyColourModel(image, "image/jpeg")
+      whenReady(colourModelFuture) { colourModel =>
+        colourModel should be (Some("GRAYSCALE"))
+      }
+    }
+  }
+
+  // TODO: test cropImage and its conversions
+
+  def fileAt(resourcePath: String): File = {
+    new File(getClass.getResource(s"/$resourcePath").toURI)
+  }
 
 }
