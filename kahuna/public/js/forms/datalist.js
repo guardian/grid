74fd9b954c9b0cd102683fc038d2c70aa184189c
Emma Milner <ekmilner@gmail.com>
2015-09-25 10:05:43 +0100

    fixes lint length

--- kahuna/public/js/forms/datalist.js
+++ kahuna/public/js/forms/datalist.js
@@ -95,7 +95,8 @@ datalist.directive('grDatalistInput',
                     scope.$apply(parentCtrl.setValueFromSelectedIndex);
                 }
                 //to prevent the caret moving to the start/end of the input box
-                if (parentCtrl.active && ( keys[event.which] === 'up' || keys[event.which] === 'down' ) ) {
+                if (parentCtrl.active &&
+                    ( keys[event.which] === 'up' || keys[event.which] === 'down' ) ) {
                     event.preventDefault();
                 }
             });
