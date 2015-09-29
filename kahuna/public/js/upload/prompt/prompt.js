4e69c1b32751b87c1caf57c65e9ee4fb0a2d52d2
Emma Milner <ekmilner@gmail.com>
2015-09-29 15:02:40 +0100

    saves and displays preset labels

--- kahuna/public/js/upload/prompt/prompt.js
+++ kahuna/public/js/upload/prompt/prompt.js
@@ -2,9 +2,12 @@ import angular from 'angular';
 
 import './prompt.css!';
 import template from './prompt.html!text';
+import '../../components/gr-add-preset-label/gr-add-preset-label';
+import './preset-labeller';
 
 export let prompt = angular.module('kahuna.upload.prompt', [
-
+    'gr.addPresetLabel',
+    'kahuna.upload.prompt.presetLabeller'
 ]);
 
 prompt.directive('filePrompt', [function () {
