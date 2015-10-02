441641c43effee4addacb6a8737aec6e50cfea47
James A Gorrie <james.gorrie@theguardian.com>
2015-10-02 15:18:35 +0100

    basic modelling of usage API

--- usage/app/controllers/UsageApi.scala
+++ usage/app/controllers/UsageApi.scala
@@ -11,6 +11,8 @@ import com.gu.mediaservice.model._
 
 import lib.Config
 
+import scala.util.Random
+
 
 object UsageApi extends Controller with ArgoHelpers {
 
@@ -28,4 +30,39 @@ object UsageApi extends Controller with ArgoHelpers {
   }
 
   def index = Authenticated { indexResponse }
+
+  def forContent(contentId: String) = Authenticated {
+    respondCollection(Examples.getMedia)
+  }
+
+  def forMedia(mediaId: String) = Authenticated {
+    respondCollection(Examples.getContent)
+  }
 }
+
+
+trait Platform { val id: String }
+case object Print extends Platform { val id = "print" }
+case object Web extends Platform { val id = "web" }
+object Platform {
+  implicit val jsonWrites: Writes[Platform] = Writes[Platform](c => JsString(c.id))
+}
+
+case class ForContent(id: String, media: List[Media])
+case class ForMedia(id: String, foundIn: List[Content])
+
+case class Content(contentId: String, platform: Platform)
+object Content { implicit val jsonWrites: Writes[Content] = Json.writes[Content] }
+
+case class Media(mediaId: String)
+object Media { implicit val jsonWrites: Writes[Media] = Json.writes[Media] }
+
+
+object Examples {
+  def getMedia = List.fill(Random.nextInt(10))(Media(Random.alphanumeric.take(32).mkString))
+  def getContent = List.fill(Random.nextInt(10))(Content(Random.alphanumeric.take(32).mkString, Random.nextInt(2) match {
+    case 0 => Print
+    case _ => Web
+  }))
+}
+
