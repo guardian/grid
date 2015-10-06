be87db90f072e3f472947f7ae9bdcc5c213ec7f1
Robert Kenny <robert.kenny@guardian.co.uk>
2015-10-06 12:10:18 +0100

    add md5 hashing lib

---
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/MD5.scala
@@ -0,0 +1,11 @@
+package lib
+
+
+object MD5 {
+  def hash(s: String) = {
+    val m = java.security.MessageDigest.getInstance("MD5")
+    val b = s.getBytes("UTF-8")
+    m.update(b, 0, b.length)
+    new java.math.BigInteger(1, m.digest()).toString(16)
+  }
+}
