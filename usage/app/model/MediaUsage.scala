3976f2b6d7a05989521cd1dbbc922cfca0518143
Robert Kenny <robert.kenny@guardian.co.uk>
2015-10-07 10:11:03 +0100

    remove usage details, split lastmodified out of status

--- usage/app/model/MediaUsage.scala
+++ usage/app/model/MediaUsage.scala
@@ -10,24 +10,18 @@ import lib.MD5
 case class MediaUsage(
   usageId: String,
   grouping: String,
-  usageDetails: UsageDetails
+  mediaId: String,
+  usageType: String,
+  mediaType: String,
+  status: UsageStatus,
+  lastModified: DateTime
 ) {
-  // Used when we want to check if an existing object has been modified
-  def ===(obj: Any): Boolean = obj match {
-    case mediaUsage: MediaUsage => {
-      usageId == mediaUsage.usageId &&
-      grouping == mediaUsage.grouping &&
-      usageDetails == mediaUsage.usageDetails
-    }
-    case _ => false
-  }
-
   // Used in set comparison of UsageGroups
   override def equals(obj: Any): Boolean = obj match {
     case mediaUsage: MediaUsage => {
       usageId == mediaUsage.usageId &&
       grouping == mediaUsage.grouping &&
-      usageDetails.id == mediaUsage.usageDetails.id
+      mediaId == mediaUsage.mediaId
     } // TODO: This will work for checking if new items have been added and
       // removed but NOT if existing records have been updated (underlying image data)
       // can we override === to check?
@@ -40,37 +34,29 @@ object MediaUsage {
     MediaUsage(
       item.getString("usage_id"),
       item.getString("grouping"),
-      UsageDetails(
-        item.getString("media_id"),
-        item.getString("usage_type"),
-        item.getString("media_type"),
-        item.getString("usage_status") match {
-          case "pending" => PendingUsageStatus(new DateTime())
-          case "published" => PubishedUsageStatus(new DateTime())
-        }
-      )
+      item.getString("media_id"),
+      item.getString("usage_type"),
+      item.getString("media_type"),
+      item.getString("usage_status") match {
+        case "pending" => PendingUsageStatus()
+        case "published" => PubishedUsageStatus()
+      },
+      new DateTime()
     )
 
-  def build(media: Element, status: UsageStatus, index: Int, grouping: String) =
+  def build(media: Element, status: UsageStatus, index: Int, grouping: String, lastModified: DateTime) =
     MediaUsage(
       createUsageId(media.id, status, index),
       grouping,
-      UsageDetails.build(media,status)
+      media.id,
+      "web",
+      "image",
+      status,
+      lastModified
     )
 
   // The purpose of this hash is to obfuscate the usage_id
   def createUsageId(mediaId: String, status: UsageStatus, index: Int) =
     MD5.hash(s"${mediaId}_${index}_${status}")
-}
 
-case class UsageDetails(
-  id: String,
-  usageType: String,
-  mediaType: String,
-  status: UsageStatus
-)
-
-object UsageDetails {
-  def build(element: Element, status: UsageStatus) =
-    UsageDetails(element.id, "web", "image", status)
 }
