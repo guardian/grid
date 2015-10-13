b21425ad77bd45a5b3efec73b07ee71bd100bd27
Robert Kenny <robert.kenny@guardian.co.uk>
2015-10-13 16:03:00 +0100

    Thrall -> Usage in UsageMetrics

--- usage/app/lib/UsageMetrics.scala
+++ usage/app/lib/UsageMetrics.scala
@@ -3,7 +3,7 @@ package lib
 import com.gu.mediaservice.lib.metrics.CloudWatchMetrics
 import Config.{awsCredentials, stage}
 
-object UsageMetrics extends CloudWatchMetrics(s"$stage/Thrall", awsCredentials) {
+object UsageMetrics extends CloudWatchMetrics(s"$stage/Usage", awsCredentials) {
   val usageUpdates = new CountMetric("UsageUpdates")
   val usageUpdateErrors = new CountMetric("UsageUpdateErrors")
 }
