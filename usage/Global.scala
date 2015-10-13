925792496f61ee9443f0c8bd25bdf65dec6800db
Robert Kenny <robert.kenny@guardian.co.uk>
2015-10-13 13:31:51 +0100

    make recorder stream startup simpler, add cloudwatch stats and logs

--- usage/Global.scala
+++ usage/Global.scala
@@ -3,17 +3,12 @@ import play.api.{Application, GlobalSettings}
 import play.api.mvc.WithFilters
 import play.filters.gzip.GzipFilter
 
-import lib._
-
-import controllers.UsageApi
-
 import com.gu.mediaservice.lib.play.RequestLoggingFilter
 
+import controllers.UsageApi
+import lib._
 
 object Global extends WithFilters(RequestLoggingFilter, new GzipFilter) with GlobalSettings {
-
-  // TODO: We shouldn't need to lazy val this ideally
-  lazy val usageRecorder = UsageRecorder.observable.subscribe(_ => {})
 
   override def beforeStart(app: Application): Unit = {
     LogConfig.init(Config)
@@ -21,12 +16,10 @@ object Global extends WithFilters(RequestLoggingFilter, new GzipFilter) with Glo
 
   override def onStart(app: Application) {
     UsageApi.keyStore.scheduleUpdates(Akka.system(app).scheduler)
-
-    // This evaluates above lazy val causing the subscribe to occur (keeping the scope on the Global object)
-    usageRecorder
+    UsageRecorder.subscribe
   }
 
   override def onStop(app: Application) {
-    usageRecorder.unsubscribe()
+    UsageRecorder.unsubscribe
   }
 }
--- usage/app/lib/UsageRecorder.scala
+++ usage/app/lib/UsageRecorder.scala
@@ -1,8 +1,12 @@
 package lib
 
+import play.api.Logger
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
+
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 
 import play.api.libs.json._
 
@@ -13,17 +17,32 @@ import model._
 
 object UsageRecorder {
   val usageStream = UsageStream.observable
+  val observable = usageStream.flatMap(recordUpdates)
+
+  // Subscription should not be evaluated until required
+  lazy val subscription = UsageRecorder.observable.subscribe(
+    (usage: JsObject) => {
+      Logger.info(s"UsageRecorder processed update: ${usage}")
+      UsageMetrics.usageUpdates.increment()
+    },
+    (error: Throwable) => {
+      Logger.error("UsageRecorder stream encountered an error", error)
+      UsageMetrics.usageUpdateErrors.increment()
+    },
+    () => Logger.info("Shutting down usage recorder stream.")
+  )
+
+  def subscribe = subscription // Helper method to eval subscription
+  def unsubscribe = subscription.unsubscribe
 
   def recordUpdates(usageGroup: UsageGroup) = {
-    UsageTable.matchUsageGroup(usageGroup).map(dbUsageGroup => {
+    UsageTable.matchUsageGroup(usageGroup).flatMap(dbUsageGroup => {
 
       val deletes = (dbUsageGroup.usages -- usageGroup.usages).map(UsageTable.delete(_))
       val creates = (usageGroup.usages -- dbUsageGroup.usages).map(UsageTable.create(_))
       val updates = (usageGroup.usages & dbUsageGroup.usages).map(UsageTable.update(_))
 
-      Observable.from(deletes ++ updates ++ creates).flatten
+      Observable.from(deletes ++ updates ++ creates).flatten[JsObject]
     })
   }
-
-  val observable = usageStream.flatMap(recordUpdates(_))
 }
