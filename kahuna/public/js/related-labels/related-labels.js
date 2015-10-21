c69843e77bd6b9790fbe0ebede416a2662b23bb7
James A Gorrie <james.gorrie@theguardian.com>
2015-10-21 15:52:55 +0100

    use aggregations for 'related' labels

--- kahuna/public/js/related-labels/related-labels.js
+++ kahuna/public/js/related-labels/related-labels.js
@@ -1,17 +1,30 @@
 import angular from 'angular';
-import '../services/api/media-api';
+import Rx from 'rx';
 
 import template from './related-labels.html!text';
+import './related-labels.css!';
+import '../services/api/media-api';
 
 export const relatedLabels = angular.module('gr.relatedLabels', []);
 
 relatedLabels.controller('GrRelatedLabelsCtrl', ['mediaApi', function(mediaApi) {
     let ctrl = this;
+    ctrl.relatedLabels = [];
 
     ctrl.suggestedLabelSearch = q =>
-        mediaApi.labelsSuggest({q}).then(labels => labels.data);
+        mediaApi.
+            labelsSuggest({q}).
+            then(labels => {
+                labels.follow('related-labels').get().then(related => {
+                    ctrl.relatedLabels = related.data.map(label => {
+                        return label.key.replace(`${ctrl.collection}/`, '');
+                    });
+                });
+                return labels;
+            }).
+            then(labels => labels.data);
 
-    
+
 }]);
 
 relatedLabels.directive('grRelatedLabels', [function() {
