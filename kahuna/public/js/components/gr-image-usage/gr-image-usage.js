8b9c0200d96aa29e83dbb913f88f9c08c4a7311c
Akash A <akash1810@gmail.com>
2015-10-30 15:33:22 +0000

    remove open external icon.

--- kahuna/public/js/components/gr-image-usage/gr-image-usage.js
+++ kahuna/public/js/components/gr-image-usage/gr-image-usage.js
@@ -11,7 +11,37 @@ module.controller('grImageUsageCtrl', ['mediaUsage', function (mediaUsage) {
     let ctrl = this;
 
     mediaUsage.getUsage(ctrl.image).then(data => {
-        ctrl.usage = data;
+        ctrl.usage = {
+            "published": {
+                "length": 2,
+                "data": [
+                    {
+                        "mediaId": "eba0837091206f0e433e251d72d0bbab350c54e7",
+                        "source": {
+                            "uri": "http://www.theguardian.com/books/2015/oct/30/martin-amis-jeremy-corbyn-humour-jonathan-coe",
+                            "name": "Is Martin Amis right? Or will Jeremy Corbyn have the last laugh?"
+                        },
+                        "usageType": "web",
+                        "mediaType": "Image",
+                        "status": "published",
+                        "dateAdded": "2015-10-30T14:30:55Z",
+                        "lastModified": "2015-10-30T14:31:48Z"
+                    },
+                    {
+                        "mediaId": "eba0837091206f0e433e251d72d0bbab350c54e7",
+                        "source": {
+                            "uri": "http://www.theguardian.com/books/2015/oct/30/martin-amis-jeremy-corbyn-humour-jonathan-coe",
+                            "name": "Is Martin Amis right? Or will Jeremy Corbyn have the last laugh?"
+                        },
+                        "usageType": "web",
+                        "mediaType": "Image",
+                        "status": "published",
+                        "dateAdded": "2015-10-30T14:30:55Z",
+                        "lastModified": "2015-10-30T14:31:48Z"
+                    }
+                ]
+            }
+        };
     });
 
     ctrl.usageTypeToName = (usageType) => {
