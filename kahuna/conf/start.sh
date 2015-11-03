774d39ea52a4828ca5ab260ec122c6ee28f48aff
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-11-03 14:38:54 +0000

    Apply APP_OPTIONS to kahuna

--- kahuna/conf/start.sh
+++ kahuna/conf/start.sh
@@ -6,6 +6,8 @@ APP_SECRET=`cat /etc/gu/play-application-secret`
 # session.domain and application.secret are read by Play
 APP_OPTIONS="-Dsession.domain=$SESSION_DOMAIN -Dapplication.secret=$APP_SECRET"
 
+export JAVA_OPTS="$JAVA_OPTS $APP_OPTIONS"
+
 CMD="$USER_HOME/bin/$APP"
 echo "$CMD" > $USER_HOME/logs/cmd.txt
 $CMD > $LOGFILE 2>&1
