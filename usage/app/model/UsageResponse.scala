9bdff7ed78e0a215c96ee001ee119bdd9264e0e4
Akash A <akash1810@gmail.com>
2015-11-04 17:24:36 +0000

    print response

--- usage/app/model/UsageResponse.scala
+++ usage/app/model/UsageResponse.scala
@@ -6,27 +6,25 @@ import com.gu.mediaservice.model.DateFormat
 import com.gu.mediaservice.lib.argo.ArgoHelpers
 
 
-case class UsageResponse(
+case class UsageResponse (
   mediaId: String,
   title: String,
-  source: Map[String,MediaSource],
+  source: Map[String, MediaSource],
   usageType: String,
   mediaType: String,
   status: String,
   dateAdded: Option[DateTime],
   dateRemoved: Option[DateTime],
-  lastModified: DateTime
-)
+  lastModified: DateTime)
+
 
 object UsageResponse extends ArgoHelpers {
   def respondUsageCollection(usages: Set[MediaUsage]) = {
     val flatUsages = usages.groupBy(_.grouping).flatMap { case (grouping, groupedUsages) => {
-      val publishedUsage = groupedUsages
-        .filter(_.status match {
-          case _: PublishedUsageStatus => true
-          case _ => false
-        })
-        .headOption
+      val publishedUsage = groupedUsages.find(_.status match {
+        case _: PublishedUsageStatus => true
+        case _ => false
+      })
 
       val mergedUsages = if (publishedUsage.isEmpty) {
           groupedUsages.headOption
@@ -52,20 +50,51 @@ object UsageResponse extends ArgoHelpers {
   }
 
   def build (usage: MediaUsage): UsageResponse = {
-    UsageResponse(
-      usage.mediaId,
-      usage.data.get("webTitle").getOrElse("No title specified."),
-      MediaSource.build(usage),
-      usage.usageType,
-      usage.mediaType,
-      usage.status.toString,
-      usage.dateAdded,
-      usage.dateRemoved,
-      usage.lastModified
-    )
+    usage.usageType match {
+      case "web" => UsageResponse(
+        usage.mediaId,
+        usage.data.getOrElse("webTitle", "No title specified."),
+        MediaSource.build(usage),
+        usage.usageType,
+        usage.mediaType,
+        usage.status.toString,
+        usage.dateAdded,
+        usage.dateRemoved,
+        usage.lastModified
+      )
+      case "print" => {
+        val title = List(
+          usage.data.get("issueDate").map(date => new DateTime(date).toString("YYYY-MM-dd")),
+          usage.data.get("pageNumber").map(page => s"Page $page"),
+          usage.data.get("edition").map(edition => s"${intToOrdinalString(edition.toInt)} edition")
+        )
+
+        UsageResponse(
+          usage.mediaId,
+          title.flatten.mkString(", "),
+          MediaSource.build(usage),
+          usage.usageType,
+          usage.mediaType,
+          usage.status.toString,
+          usage.dateAdded,
+          usage.dateRemoved,
+          usage.lastModified
+        )
+      }
+    }
   }
   implicit val dateTimeFormat = DateFormat
   implicit val writes: Writes[UsageResponse] = Json.writes[UsageResponse]
+
+  def intToOrdinalString (int: Int): String = {
+    val ordinal = int % 10 match {
+      case 1 => "st"
+      case 2 => "nd"
+      case 3 => "rd"
+      case _ => "th"
+    }
+    s"$int$ordinal"
+  }
 }
 
 case class MediaSource (
