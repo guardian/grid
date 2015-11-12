539e8693ce620d896ea1a69b3a79a8a8522b015d
James A Gorrie <james.gorrie@theguardian.com>
2015-11-12 09:05:13 +0000

    add data to collection tree

--- media-api/app/model/Collection.scala
+++ media-api/app/model/Collection.scala
@@ -19,22 +19,27 @@ object Paradata {
 }
 
 case class Node[T](name: String, children: List[Node[T]], data: Option[T] = None)
+
 object Node {
   type Path = List[String]
 
   def buildTree[T](rootName: String, input: List[T], getPath: T => Path): Node[T] = {
-    val paths = input.map(getPath)
+    val pathsWithData = input.map(getPath).zip(input)
 
-    def loop(input: List[Path]): List[Node[T]] = {
+    def loop(input: List[(Path, T)]): List[Node[T]] = {
       input
-        .filter(_.nonEmpty).groupBy(_.head)
-        .map { case (parentName, children) =>
-          Node[T](parentName, loop(children.map(_.tail)))
+        .filter(_._1.nonEmpty).groupBy(_._1.head)
+        .map { case (parentName, grouped) =>
+          val nextGroup = grouped.map{ case (path, t) => (path.tail, t) }
+          // TODO: There is a bug here that if we don't have data (it's an assumed path)
+          // we inherit the child's data.
+          val dataOpt = grouped.headOption.map{case (path, data) => data }
+          Node[T](parentName, loop(nextGroup), dataOpt)
         }
         .toList
     }
 
-    Node[T](rootName, loop(paths))
+    Node[T](rootName, loop(pathsWithData))
   }
 
   implicit def nodeFormat[T: Format]: Format[Node[T]] = (
