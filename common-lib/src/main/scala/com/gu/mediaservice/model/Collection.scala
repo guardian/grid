c44fd9e62bb63f31490e76c107d1c620b7d36361
James A Gorrie <james.gorrie@theguardian.com>
2015-11-16 11:52:48 +0000

    add the id to the collection model

--- common-lib/src/main/scala/com/gu/mediaservice/model/Collection.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/model/Collection.scala
@@ -1,11 +1,33 @@
 package com.gu.mediaservice.model
 
+import java.net.URLDecoder.decode
+import java.net.URLEncoder.encode
 import org.joda.time.DateTime
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
-case class Collection(path: List[String], actionData: ActionData)
+case class Collection(path: List[String], actionData: ActionData) {
+  val pathId = CollectionManager.pathToString(path)
+}
 object Collection {
-  implicit def formats: Format[Collection] = Json.format[Collection]
+  val reads: Reads[Collection] = Json.reads[Collection]
+  val writes: Writes[Collection] = (
+    (__ \ "pathId").write[String] ~
+    (__ \ "path").write[List[String]] ~
+    (__ \ "actionData").write[ActionData]
+  ){ col: Collection => (col.pathId, col.path, col.actionData) }
+
+  implicit val formats: Format[Collection] = Format(reads, writes)
+}
+
+object CollectionManager {
+  val delimiter = "/"
+  val enc = "UTF-8"
+  def decodePathBit(s: String) = decode(s, enc)
+  def encodePathBit(s: String) = encode(s, enc)
+
+  def stringToPath(s: String) = s.split(delimiter).map(decodePathBit)
+  def pathToString(path: List[String]) = path.map(encodePathBit).mkString(delimiter)
 }
 
 // Following the crop structure
