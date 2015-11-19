9528461ecfc16347eb71572fc3e07344bf7a9bfc
Robert Kenny <robert.kenny@guardian.co.uk>
2015-11-19 11:46:49 +0000

    ' -> "

--- kahuna/public/js/image/service.js
+++ kahuna/public/js/image/service.js
@@ -24,25 +24,25 @@ imageService.factory('imageService', [function() {
 
     function usages(image) {
         function usageTitle(usage) {
-            const referenceType = usage.get("usageType") == "print" ? "indesign" : "frontend";
+            const referenceType = usage.get('usageType') == 'print' ? 'indesign' : 'frontend';
 
             const build = (usage, referenceType) => {
-                const reference = usage.get("references").find(u => u.get("referenceType") == referenceType)
-                return reference ? reference.get("name") : "No title found."
+                const reference = usage.get('references').find(u => u.get('referenceType') == referenceType)
+                return reference ? reference.get('name') : 'No title found.'
             }
 
             return build(usage, referenceType);
         }
 
-        const usagesList = Immutable.fromJS(image.data.usages).map(u => u.set("title", usageTitle(u)));
-        const groupedByState = usagesList.groupBy(u => u.get("status"));
-        const filterByUsageType = (usageType) => usagesList.filter(u => u.get("usageType") == usageType)
+        const usagesList = Immutable.fromJS(image.data.usages).map(u => u.set('title', usageTitle(u)));
+        const groupedByState = usagesList.groupBy(u => u.get('status'));
+        const filterByUsageType = (usageType) => usagesList.filter(u => u.get('usageType') == usageType)
 
         return {
             usages: usagesList.toJS(),
             groupedByState: groupedByState.toJS(),
-            hasPrintUsages: !filterByUsageType("print").isEmpty(),
-            hasWebUsages: !filterByUsageType("web").isEmpty()
+            hasPrintUsages: !filterByUsageType('print').isEmpty(),
+            hasWebUsages: !filterByUsageType('web').isEmpty()
         };
     }
 
