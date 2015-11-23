e677293d381a328dce685df80f55563bbbaf94b8
Akash A <akash1810@gmail.com>
2015-11-23 10:14:12 +0000

    switch on ENV environment variable.

--- cloud-formation/scripts/stack-name.sh
+++ cloud-formation/scripts/stack-name.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-export STACK_NAME="media-service-DEV-`aws iam get-user | jq '.User.UserName' | tr -d '"'`"
+if [ -z "$ENV" ];
+then
+    export STACK_NAME="media-service-DEV-`aws iam get-user | jq '.User.UserName' | tr -d '"'`"
+else
+    export STACK_NAME="media-service-$ENV"
+fi
