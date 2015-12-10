adc951e35554b4e9ac8d7aaaa53e0d7e04d52390
Akash A <akash1810@gmail.com>
2015-12-10 11:53:50 +0000

    remove cf template for usage lambda.
    we're not using a lambda anymore!

--- cloud-formation/scripts/stack-name.sh
+++ cloud-formation/scripts/stack-name.sh
@@ -2,12 +2,7 @@
 
 if [ -z "$ENV" ];
 then
-    if [ $# -eq 1 ];
-    then
-        export STACK_NAME="media-service-$1-DEV-`aws iam get-user | jq '.User.UserName' | tr -d '"' | tr [A-Z] [a-z]`"
-    else
-        export STACK_NAME="media-service-DEV-`aws iam get-user | jq '.User.UserName' | tr -d '"' | tr [A-Z] [a-z]`"
-    fi
+    export STACK_NAME="media-service-DEV-`aws iam get-user | jq '.User.UserName' | tr -d '"' | tr [A-Z] [a-z]`"
 else
     export STACK_NAME="media-service-$ENV"
 fi
