e913f1fd9f576990bfa52ee537d81701f8030663
npapacostas <nick.papacostas@guardian.co.uk>
2015-12-16 12:02:16 +0000

    moved known ratios out of method to only get called once, removed unnecessary distinct call

--- cropper/app/lib/AspectRatio.scala
+++ cropper/app/lib/AspectRatio.scala
@@ -5,15 +5,16 @@ import scala.annotation.tailrec
 object AspectRatio {
   case class Ratio(friendly: String, width: Int, height: Int)
 
+  val knownRatios = List(
+    Ratio("5:3", 5, 3),
+    Ratio("2:3", 2, 3),
+    Ratio("16:9", 16, 9)
+  )
+
   @tailrec
   def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 
   def calculate(width: Int, height: Int, tolerance: Int = 3) : Option[Ratio] = {
-    val knownRatios = List(
-      Ratio("5:3", 5, 3),
-      Ratio("2:3", 2, 3),
-      Ratio("16:9", 16, 9)
-    )
     val matchingRatios = for {
       w <- width - tolerance until width + tolerance
       h <- height - tolerance until height + tolerance
@@ -22,6 +23,6 @@ object AspectRatio {
       simplifiedHeight = h / g
       ratio <- knownRatios if ratio.width == simplifiedWidth &&  ratio.height == simplifiedHeight
     } yield ratio
-    matchingRatios.distinct.headOption
+    matchingRatios.headOption
   }
 }
