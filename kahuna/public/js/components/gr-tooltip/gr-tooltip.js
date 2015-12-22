4331ec5b790ce08ec5c7b36e8b17891346bc6ada
Akash A <akash1810@gmail.com>
2015-12-22 21:26:33 +0000

    allow watching of tooltip text

--- kahuna/public/js/components/gr-tooltip/gr-tooltip.js
+++ kahuna/public/js/components/gr-tooltip/gr-tooltip.js
@@ -3,15 +3,23 @@ import 'titip';
 
 export const module = angular.module('grTooltip', []);
 
-module.directive('grTooltip', [function () {
+module.directive('grTooltip', ['onValChange', function (onValChange) {
     return {
         restrict: 'A',
-        link: function (scope, element, attrs) {
+        link: function ($scope, element, attrs) {
             const position = attrs.grTooltipPosition || 'bottom';
 
             element.attr('data-title', attrs.grTooltip)
                 .addClass(`titip-default`)
                 .addClass(`titip-${position}`);
+
+            const autoUpdates = angular.isDefined(attrs.grTooltipUpdates) || false;
+
+            if (autoUpdates) {
+                $scope.$watch(() => attrs.grTooltip, onValChange(newTooltip => {
+                    element.attr('data-title', newTooltip);
+                }));
+            }
         }
     };
 }]);
