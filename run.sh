12d54b88823e908411fd09f91b9b0f9fae36d583
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-12-22 12:24:15 +0000

    Add auth service

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ x-terminal-emulator -T image-loader  -e "sbt 'project image-loader' 'run 9003'" 
 x-terminal-emulator -T ftp-loader    -e "sbt 'project ftp-watcher'  'run 9004'" &
 x-terminal-emulator -T kahuna        -e "sbt 'project kahuna'       'run 9005'" &
 x-terminal-emulator -T cropper       -e "sbt 'project cropper'      'run 9006'" &
+x-terminal-emulator -T metadata-editor -e "sbt 'project metadata-editor' 'run 9007'" &
 x-terminal-emulator -T usage         -e "sbt 'project usage'        'run 9009'" &
 x-terminal-emulator -T collections   -e "sbt 'project collections'  'run 9010'" &
-x-terminal-emulator -T metadata-editor -e "sbt 'project metadata-editor' 'run 9007'" &
-x-terminal-emulator -T imgops        -e '/bin/bash -c "cd imgops; ./dev-start.sh"' &
 x-terminal-emulator -T auth          -e "sbt 'project auth'         'run 9011'" &
+x-terminal-emulator -T imgops        -e '/bin/bash -c "cd imgops; ./dev-start.sh"' &
