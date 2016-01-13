eedbf1da6de3dac33b5407ec6dbb646dfd5adee8
Akash A <akash1810@gmail.com>
2016-01-13 21:58:03 +0000

    refactoring - remove unused function

--- docker/configs/generators/generators/osx_hosts_file.py
+++ docker/configs/generators/generators/osx_hosts_file.py
@@ -4,24 +4,19 @@ from . import *
 DOCKER_HOST = 'DOCKER_HOST'
 
 
-class DaemonConnectError(Exception):
-    def __init__(self):
-        self.message = "Cannot connect to the Docker daemon. Is the docker daemon running on this host?"
-
-
 def get_docker_host_ip():
-    if not os.environ.has_key(DOCKER_HOST):
-        raise DaemonConnectError()
-
-    return os.environ.get(DOCKER_HOST).replace('tcp://', '').split(':')[0]
-
-
-def mapping_to_domain(mapping):
-    return [mapping.get('prefix'), get_domain()].join('.')
+    if os.environ.has_key(DOCKER_HOST):
+        return os.environ.get(DOCKER_HOST).replace('tcp://', '').split(':')[0]
+    else:
+        print 'Cannot connect to the Docker daemon. Is the docker daemon running on this host?'
 
 
 def generate():
     ip = get_docker_host_ip()
+
+    if not ip:
+        return
+
     domain = get_domain()
 
     for mapping in get_mappings():
@@ -29,7 +24,4 @@ def generate():
 
 
 if __name__ == '__main__':
-    try:
-        generate()
-    except Exception, e:
-        print e.message
+    generate()
