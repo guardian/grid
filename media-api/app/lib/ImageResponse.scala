b777df6472602c8a8038c963ce5ce7a39a891843
Robert Kenny <robert.kenny@guardian.co.uk>
2016-01-13 14:29:46 +0000

    Add add-print-usage to image actions

--- media-api/app/lib/ImageResponse.scala
+++ media-api/app/lib/ImageResponse.scala
@@ -148,16 +148,19 @@ object ImageResponse extends EditsResponse {
     val imageUri = URI.create(s"${Config.rootUri}/images/$id")
     val reindexUri = URI.create(s"${Config.rootUri}/images/$id/reindex")
     val addCollectionUri = URI.create(s"${Config.collectionsUri}/images/$id")
+    val addPrintUsageUri = URI.create(s"${Config.usageUri}/usages/print")
 
     val deleteAction = Action("delete", imageUri, "DELETE")
     val reindexAction = Action("reindex", reindexUri, "POST")
 
     val addCollectionAction = Action("add-collection", addCollectionUri, "POST")
+    val addPrintUsageAction = Action("add-print-usage", addPrintUsageUri, "POST")
 
     List(
       deleteAction        -> isDeletable,
       reindexAction       -> withWritePermission,
-      addCollectionAction -> true
+      addCollectionAction -> true,
+      addPrintUsageAction -> true
     )
     .filter{ case (action, active) => active }
     .map   { case (action, active) => action }
