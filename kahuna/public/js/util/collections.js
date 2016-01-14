e9e03a37f612a500ef774818c8793ee40eb1b85d
Emma Milner <ekmilner@gmail.com>
2016-01-14 14:45:23 +0000

    grrr lint

--- kahuna/public/js/util/collections.js
+++ kahuna/public/js/util/collections.js
@@ -5,4 +5,4 @@ export const collectionsUtil = angular.module('util.collections', []);
 collectionsUtil.factory('collectionsEnabled', ['$window', function($window){
     // TODO: Remove this once we're happy with the collections panel
     return $window.localStorage.getItem('showCollectionsPanel') === 'true';
-}])
+}]);
