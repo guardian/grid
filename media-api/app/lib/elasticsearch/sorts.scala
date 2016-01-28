267d0380042c1c68f0acd711a8db419bb510c917
Emma Milner <ekmilner@gmail.com>
2016-01-28 19:03:44 +0000

    [WIP] adds collection sort to es query

--- media-api/app/lib/elasticsearch/sorts.scala
+++ media-api/app/lib/elasticsearch/sorts.scala
@@ -1,14 +1,27 @@
 package lib.elasticsearch
 
+import lib.querysyntax.Condition
 import org.elasticsearch.action.search.SearchRequestBuilder
-import org.elasticsearch.search.sort.SortOrder
+import org.elasticsearch.index.query.{FilterBuilders}
+import org.elasticsearch.search.sort.{SortBuilders, SortOrder}
 
 
 object sorts {
 
+  def createSort(sortBy: Option[String], query: List[Condition])(builder: SearchRequestBuilder) = {
+    parseFromRequest(sortBy)(builder)
+    collectionSort(query)(builder)
+  }
+
   def parseFromRequest(sortBy: Option[String])(builder: SearchRequestBuilder): SearchRequestBuilder = {
     val sorts = sortBy.fold(Seq("uploadTime" -> SortOrder.DESC))(parseSorts)
     for ((field, order) <- sorts) builder.addSort(field, order)
+    builder
+  }
+
+  def collectionSort(query: List[Condition])(builder: SearchRequestBuilder): SearchRequestBuilder = {
+    //TODO get relevant values from query
+    builder.addSort(SortBuilders.fieldSort("collections.actionData.date").order(SortOrder.DESC).setNestedFilter(FilterBuilders.termFilter("collections.pathHierarchy", query)))
     builder
   }
 
