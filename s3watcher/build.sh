3a208c691d9b7355600ccab23678f8ffdd1e4212
Robert Kenny <robert.kenny@guardian.co.uk>
2016-02-10 15:56:41 +0000

    interpolate deploy.json

---
+++ s3watcher/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+if [ -z $TEST_FUNC_NAME ]; then
+    echo 'TEST_FUNC_NAME is unset';
+    exit 1
+fi
+
+if [ -z $PROD_FUNC_NAME ]; then
+    echo 'PROD_FUNC_NAME is unset';
+    exit 1
+fi
+
+cat deploy.json \
+    | jq ".packages.lambda.data.functions.TEST.name |= \"$TEST_FUNC_NAME\" | .packages.lambda.data.functions.PROD.name |= \"$PROD_FUNC_NAME\"" \
+    > lambda/deploy.json
+
+export VERBOSE=true
+
+pushd s3watcher/lambda
+
+npm install
+
+ARTEFACT_PATH="$WORKSPACE/s3watcher/lambda" npm run build
