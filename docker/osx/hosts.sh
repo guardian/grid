f5b203746e8596e66530a49f9fb6d7d8a44e1bba
Akash A <akash1810@gmail.com>
2016-02-11 13:16:45 +0000

    use local virtualenv

--- docker/osx/hosts.sh
+++ docker/osx/hosts.sh
@@ -2,7 +2,13 @@
 
 cd ../configs/generators
 
-pip install -r requirements.txt > /dev/null 2>&1
+if [ ! -f ".venv/bin/python" ]; then
+    echo 'creating a virtualenv'
+    virtualenv --no-site-packages --quiet .venv
+fi
+
+source .venv/bin/activate
+pip install --quiet -r requirements.txt
 
 echo '-------------------------------'
 echo 'add these entries to /etc/hosts'
