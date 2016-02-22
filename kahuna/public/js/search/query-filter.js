a5b8787b096955dbe521184b997fe08cb2b7f150
Sébastien Cevey <seb.cevey@guardian.co.uk>
2016-02-22 16:06:28 +0000

    Expose useful function to create query filter

--- kahuna/public/js/search/query-filter.js
+++ kahuna/public/js/search/query-filter.js
@@ -7,15 +7,17 @@ export var queryFilters = angular.module('kahuna.search.filters.query', []);
 var containsSpace = s => / /.test(s);
 var stripDoubleQuotes = s => s.replace(/"/g, '');
 
+export function fieldFilter(field, value) {
+    const cleanValue = stripDoubleQuotes(value);
+    if (containsSpace(cleanValue)) {
+        return `${field}:"${cleanValue}"`;
+    } else {
+        return `${field}:${cleanValue}`;
+    }
+};
+
 queryFilters.filter('queryFilter', function() {
-    return (value, field) => {
-        const cleanValue = stripDoubleQuotes(value);
-        if (containsSpace(cleanValue)) {
-            return `${field}:"${cleanValue}"`;
-        } else {
-            return `${field}:${cleanValue}`;
-        }
-    };
+    return (value, field) => fieldFilter(field, value);
 });
 
 queryFilters.filter('queryLabelFilter', function() {
