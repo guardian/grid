0e31c0f704a7a122c38a38a55023ecccd6c339c0
Sébastien Cevey <seb.cevey@guardian.co.uk>
2016-02-24 15:36:07 +0000

    Fix erroneous double quoting

--- kahuna/public/js/search/query-filter.js
+++ kahuna/public/js/search/query-filter.js
@@ -18,7 +18,7 @@ export function maybeQuoted(value) {
 export function fieldFilter(field, value) {
     const cleanValue = stripDoubleQuotes(value);
     const valueMaybeQuoted = maybeQuoted(cleanValue);
-    return `${field}:"${valueMaybeQuoted}"`;
+    return `${field}:${valueMaybeQuoted}`;
 }
 
 queryFilters.filter('queryFilter', function() {
