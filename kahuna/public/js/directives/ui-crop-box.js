bb869dd17ac3e8224ae1fc4820166901e5e1cae2
Emma Milner <ekmilner@gmail.com>
2016-03-31 17:29:30 +0100

    stops image resizing when window resized

--- kahuna/public/js/directives/ui-crop-box.js
+++ kahuna/public/js/directives/ui-crop-box.js
@@ -44,6 +44,7 @@ cropBox.directive('uiCropBox', ['$timeout', '$parse', 'safeApply', 'nextTick', '
                     scalable: false,
                     zoomable: false,
                     background: false,
+                    responsive: false,
                     crop: update,
                     built: getRatio
                 };
