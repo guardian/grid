db6a5654d4978c9c30e5228fba7ceeea06f9f2ae
Robert Kenny <kenoir@users.noreply.github.com>
2016-04-19 11:25:46 +0100

    Leases: Add lease status (#1863)
    * add custom writes for medialease to calculate active lease
    
    * include compiled active for lease by media
    
    * put leasebymedia in common lib
    
    * make leasebymediawriter trait
    
    * make lastmodified optional in writes

---
+++ common-lib/src/main/scala/com/gu/mediaservice/model/leases/LeaseByMedia.scala
@@ -0,0 +1,44 @@
+package com.gu.mediaservice.model
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import com.gu.mediaservice.lib.argo.model._
+import org.joda.time.DateTime
+
+case class LeaseByMedia(
+  leases: List[MediaLease],
+  lastModified: Option[DateTime],
+  current: Option[MediaLease]
+)
+case object LeaseByMedia {
+  def apply(leases: List[MediaLease]): LeaseByMedia = {
+    def sortLease(a: MediaLease, b: MediaLease) =
+      a.createdAt.isAfter(b.createdAt)
+
+    val sortedLeases = leases
+      .sortWith(sortLease)
+
+    val currentLease = sortedLeases
+      .filter(_.active)
+      .headOption
+
+    val lastModified = sortedLeases
+      .headOption
+      .map(_.createdAt)
+
+    LeaseByMedia(sortedLeases, lastModified, currentLease)
+  }
+}
+
+trait LeaseByMediaWriter {
+  def wrapLease(lease: MediaLease): EntityReponse[MediaLease]
+
+  implicit val dateTimeFormat = DateFormat
+  implicit val writer = new Writes[LeaseByMedia] {
+    def writes(leaseByMedia: LeaseByMedia) = JsObject(
+      Seq("leases" -> Json.toJson(leaseByMedia.leases.map(wrapLease))) ++
+      leaseByMedia.current.map(l => "current" -> Json.toJson(wrapLease(l))) ++
+      leaseByMedia.lastModified.map(m => "lastModified" -> Json.toJson(m))
+    )
+  }
+}
