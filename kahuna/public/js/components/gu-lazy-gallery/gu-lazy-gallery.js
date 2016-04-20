bc66546fc8f28edd23c74fe9bbaca4f08af7f0f1
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-04-20 16:37:28 +0100

    fix gallery controls

--- kahuna/public/js/components/gu-lazy-gallery/gu-lazy-gallery.js
+++ kahuna/public/js/components/gu-lazy-gallery/gu-lazy-gallery.js
@@ -11,34 +11,34 @@ export var lazyGallery = angular.module('gu.lazyGallery', [
 ]);
 
 lazyGallery.controller('GuLazyGalleryCtrl', ['$scope', function($scope) {
-    let ctrl = this,
-        pos = 0;
+    $scope.pos = 0;
 
     function setTransform() {
-        $scope.gallery[0].style.transform = 'translate3d(' + (-pos * $scope.gallery[0].offsetWidth) + 'px,0,0)';
+        $scope.gallery[0].style.transform = 'translate3d(' + (-$scope.pos * $scope.gallery[0].offsetWidth) + 'px,0,0)';
     }
 
-    ctrl.previousItem = function() {
-        console.log("PREV");
-        pos = Math.max(pos - 1, 0);
+    $scope.previousItem = function() {
+        $scope.pos = Math.max($scope.pos - 1, 0);
         setTransform();
     };
 
-    ctrl.nextItem = function() {
-        console.log("NEXT");
-        pos = Math.min(pos + 1, $scope.gallery[0].children.length - 1);
+    $scope.nextItem = function() {
+        $scope.pos = Math.min($scope.pos + 1, $scope.galleryLength - 1);
         setTransform();
     };
 
 }]);
 
-lazyGallery.directive('guLazyGalleryControl', [function() {
+lazyGallery.directive('guLazyGalleryList', [function() {
     return {
         restrict: 'A',
         controller: 'GuLazyGalleryCtrl',
-        link: function(scope, element, attrs, ctrl) {
-            scope.nextItem = ctrl.nextItem;
-            scope.previousItem = ctrl.previousItem;
+        link: function(scope, element) {
+            scope.gallery = element;
+
+            scope.$watch(() => {
+                return element[0].children.length;
+            }, (val) => scope.galleryLength = val);
         }
     };
 }]);
@@ -46,9 +46,6 @@ lazyGallery.directive('guLazyGalleryControl', [function() {
 lazyGallery.directive('guLazyGallery', [function() {
     return {
         restrict: 'A',
-        controller: 'GuLazyGalleryCtrl',
-        link: function(scope, element) {
-            scope.gallery = element[0];
-        }
+        controller: 'GuLazyGalleryCtrl'
     };
 }]);
