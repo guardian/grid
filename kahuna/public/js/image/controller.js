d1770cb14d8fea61a7b32f171748bb3b0e461136
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-05-13 10:12:29 +0100

    place crops in metadata block

--- kahuna/public/js/image/controller.js
+++ kahuna/public/js/image/controller.js
@@ -97,7 +97,7 @@ image.controller('ImageCtrl', [
 
         ctrl.cropSelected = cropSelected;
 
-        ctrl.allCrops = [];
+        ctrl.image.allCrops = [];
 
         imageService(ctrl.image).states.canDelete.then(deletable => {
             ctrl.canBeDeleted = deletable;
@@ -125,7 +125,7 @@ image.controller('ImageCtrl', [
             ctrl.crop = crops.find(crop => crop.id === cropKey);
             ctrl.fullCrop = crops.find(crop => crop.specification.type === 'full');
             ctrl.crops = crops.filter(crop => crop.specification.type === 'crop');
-            ctrl.allCrops = ctrl.fullCrop ? [ctrl.fullCrop].concat(ctrl.crops) : ctrl.crops;
+            ctrl.image.allCrops = ctrl.fullCrop ? [ctrl.fullCrop].concat(ctrl.crops) : ctrl.crops;
             //boolean version for use in template
             ctrl.hasFullCrop = angular.isDefined(ctrl.fullCrop);
             ctrl.hasCrops = ctrl.crops.length > 0;
