9bbb58234bbccbcba4281f182875ef00bd65f1da
npapacostas <nick.papacostas@guardian.co.uk>
2016-05-19 10:25:21 +0100

    revert sneaky png stuff that snuck into my branch some how.

--- kahuna/public/js/services/api/media-cropper.js
+++ kahuna/public/js/services/api/media-cropper.js
@@ -5,67 +5,63 @@ export var cropperApi = angular.module('kahuna.services.api.cropper', [
     mediaApi.name
 ]);
 
-cropperApi.factory('mediaCropper', ['$q', 'mediaApi',
-    function($q, mediaApi) {
+cropperApi.factory('mediaCropper', ['mediaApi', function(mediaApi) {
 
-        var cropperRoot;
-        function getCropperRoot() {
-            if (! cropperRoot) {
-                cropperRoot = mediaApi.root.follow('cropper');
-            }
-            return cropperRoot;
+    var cropperRoot;
+
+    function getCropperRoot() {
+        if (! cropperRoot) {
+            cropperRoot = mediaApi.root.follow('cropper');
         }
+        return cropperRoot;
+    }
 
-        function createCrop(image, coords, ratio) {
-            return getCropperRoot().follow('crop').post({
-                type: 'crop',
-                source: image.uri,
-                x: coords.x,
-                y: coords.y,
-                width: coords.width,
-                height: coords.height,
-                aspectRatio: ratio
-            });
-        }
+    function createCrop(image, coords, ratio) {
+        return getCropperRoot().follow('crop').post({
+            type: 'crop',
+            source: image.uri,
+            x: coords.x,
+            y: coords.y,
+            width: coords.width,
+            height: coords.height,
+            aspectRatio: ratio
+        });
+    }
 
-        function createFullCrop(image) {
-            return getCropperRoot().follow('crop').post({
-                type: 'full',
-                source: image.uri
-            });
-        }
+    function createFullCrop(image) {
+        return getCropperRoot().follow('crop').post({
+            type: 'full',
+            source: image.uri
+        });
+    }
 
-        function canBeCropped(image) {
-            // Images can only be cropped if there is a link to the crops
-            // TODO: this should be an Action
-            if (image.data.source.mimeType === 'image/png') {
-                return $q.when(false);
-            } else {
-                return image.getLink('crops').
-                then(() => true, () => false);
-            }
-        }
+    function canBeCropped(image) {
+        // Images can only be cropped if there is a link to the crops
+        // TODO: this should be an Action
+        return image.getLink('crops').
+        then(() => true, () => false);
+    }
 
-        function canDeleteCrops(image) {
-            // return a function that performs the action
-            const actionName = 'delete-crops';
-            return image.follow('crops').get().then(crops =>
-                crops.getAction(actionName).then(action => {
-                    if (action) {
-                        return () => crops.perform(actionName);
-                    }
-                }));
-        }
+    function canDeleteCrops(image) {
+        // return a function that performs the action
+        const actionName = 'delete-crops';
+        return image.follow('crops').get().then(crops =>
+            crops.getAction(actionName).then(action => {
+                if (action) {
+                    return () => crops.perform(actionName);
+                }
+            }));
+    }
 
-        function getCropsFor(image) {
-            return image.follow('crops').getData();
-        }
+    function getCropsFor(image) {
+        return image.follow('crops').getData();
+    }
 
-        return {
-            createCrop,
-            createFullCrop,
-            canBeCropped,
-            getCropsFor,
-            canDeleteCrops
-        };
+    return {
+        createCrop,
+        createFullCrop,
+        canBeCropped,
+        getCropsFor,
+        canDeleteCrops
+    };
 }]);
