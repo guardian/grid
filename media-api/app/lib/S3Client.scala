eeeebb41ece688fe35b5ab933727548f711702ea
Robert Kenny <robert.kenny@guardian.co.uk>
2016-06-16 15:51:04 +0100

    Adds the capability to sign CloudFront URLs
    In order that authentication can take place at the CloudFront edge and
    increase cache hits.

--- media-api/app/lib/S3Client.scala
+++ media-api/app/lib/S3Client.scala
@@ -2,4 +2,33 @@ package lib
 
 import com.gu.mediaservice.lib.aws.S3
 
-object S3Client extends S3(Config.awsCredentials)
+
+import com.amazonaws.services.cloudfront.CloudFrontUrlSigner
+import com.amazonaws.services.cloudfront.CloudFrontUrlSigner.Protocol
+import java.io.File
+import java.net.URL
+import org.joda.time.DateTime
+import java.util.Date
+import scala.concurrent.duration._
+
+trait CloudFrontDistributable {
+  val privateKeyLocation: String
+  val privateKeyPairId: String
+
+  val protocol: Protocol = Protocol.https
+  val validForMinutes: Int = 30
+
+  private def expiresAt: Date = DateTime.now.plusMinutes(validForMinutes).toDate()
+  private val privateKeyFile: File = new File(privateKeyLocation)
+
+  def getSignedUrl(cloudFrontDomain: String, s3ObjectPath: String): String =
+    CloudFrontUrlSigner.getSignedURLWithCannedPolicy(
+      protocol, cloudFrontDomain, privateKeyFile, s3ObjectPath, privateKeyPairId, expiresAt)
+
+}
+
+object S3Client extends S3(Config.awsCredentials) with CloudFrontDistributable {
+  val privateKeyLocation = "foo"
+  val privateKeyPairId = "bar"
+}
+
