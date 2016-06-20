31469b4f7759057b99963c6fc0a3035543a4fbf5
Robert Kenny <robert.kenny@guardian.co.uk>
2016-06-20 13:38:54 +0100

    toggle on cloudfront by default

--- common-lib/src/main/scala/com/gu/mediaservice/lib/FeatureToggle.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/FeatureToggle.scala
@@ -2,7 +2,7 @@ package com.gu.mediaservice.lib
 
 object FeatureToggle {
   val defaultSwitchMap: Map[String, Boolean] = Map(
-    "cloudfront-signing" -> false
+    "cloudfront-signing" -> true
   )
 
   def get(id: String): Boolean =
