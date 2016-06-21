f4e4ed1bd7514ca34606e9a75bbbcea1a658509d
Robert Kenny <kenoir@users.noreply.github.com>
2016-06-21 10:08:50 +0100

    Adds the capability to sign CloudFront URLs (#1933)
    * Adds the capability to sign CloudFront URLs
    
    In order that authentication can take place at the CloudFront edge and
    increase cache hits.
    
    * apply signed urls to output based on feature toggle
    
    * Adds simple feature toggle object
    
    In order that features be released toggled off
    
    * hard code location of cloudfront key
    
    * lazy val key location to prevent null
    
    * optionally generate thumb and image cloudfront urls
    
    * toggle on cloudfront by default
    
    * remove imagebucket cloudfront

--- media-api/app/lib/S3Client.scala
+++ media-api/app/lib/S3Client.scala
@@ -2,4 +2,37 @@ package lib
 
 import com.gu.mediaservice.lib.aws.S3
 
-object S3Client extends S3(Config.awsCredentials)
+
+import com.amazonaws.services.cloudfront.CloudFrontUrlSigner
+import com.amazonaws.services.cloudfront.CloudFrontUrlSigner.Protocol
+import java.io.File
+import java.net.URL
+import org.joda.time.DateTime
+import java.util.Date
+import scala.concurrent.duration._
+import scala.util.Try
+
+
+trait CloudFrontDistributable {
+  val privateKeyLocation: String
+  val keyPairId: Option[String]
+
+  val protocol: Protocol = Protocol.https
+  val validForMinutes: Int = 30
+
+  private def expiresAt: Date = DateTime.now.plusMinutes(validForMinutes).toDate()
+  private lazy val privateKeyFile: File = {
+    new File(privateKeyLocation)
+  }
+
+  def signedCloudFrontUrl(cloudFrontDomain: String, s3ObjectPath: String): Option[String] = Try {
+    CloudFrontUrlSigner.getSignedURLWithCannedPolicy(
+      protocol, cloudFrontDomain, privateKeyFile, s3ObjectPath, keyPairId.get, expiresAt)
+  }.toOption
+}
+
+object S3Client extends S3(Config.awsCredentials) with CloudFrontDistributable {
+  lazy val privateKeyLocation = Config.cloudFrontPrivateKeyLocation
+  lazy val keyPairId          = Config.cloudFrontKeyPairId
+}
+
