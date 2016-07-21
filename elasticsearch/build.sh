c102e6b71023c1dba8ed28e27e963c8d61832768
Robert Kenny <robert.kenny@guardian.co.uk>
2016-07-21 14:06:00 +0100

    use supplier weight plugin

--- elasticsearch/build.sh
+++ elasticsearch/build.sh
@@ -11,6 +11,10 @@ cd $TARGET
 mkdir downloads
 mkdir -p packages/elasticsearch
 
+SORT_PLUGIN_URI="https://github.com/guardian/grid-supplier-weight-sort/releases/download/v1.0/grid-supplier-weight-sort-0.1.0.zip"
+
+wget $SORT_PLUGIN_URI -O /var/tmp/grid-supplier-weight-sort-0.1.0.zip
+
 if wget -nv -O downloads/elasticsearch.tar.gz https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ELASTICSEARCH_VERSION.tar.gz
 then
     tar xfv downloads/elasticsearch.tar.gz -C downloads
@@ -20,6 +24,7 @@ then
     ./downloads/elasticsearch/bin/plugin -install mobz/elasticsearch-head
     ./downloads/elasticsearch/bin/plugin -install com.gu/elasticsearch-cloudwatch/1.1
     ./downloads/elasticsearch/bin/plugin -install karmi/elasticsearch-paramedic
+    ./downloads/elasticsearch/bin/plugin -url file:///var/tmp/grid-supplier-weight-sort-0.1.0.zip -install grid-supplier-weight-sort
 
     cp ../elasticsearch.yml downloads/elasticsearch/config
     cp ../logging.yml downloads/elasticsearch/config
@@ -33,5 +38,3 @@ fi
 tar czfv packages/elasticsearch/elasticsearch.tar.gz -C downloads elasticsearch scripts elasticsearch.conf
 cp ../deploy.json .
 zip -rv artifacts.zip packages/ deploy.json
-
-echo "##teamcity[publishArtifacts '$(pwd)/artifacts.zip => .']"
