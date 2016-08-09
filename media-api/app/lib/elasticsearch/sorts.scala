9ade48674f2162b718a652da7348543dc4c8b887
Robert Kenny <robert.kenny@guardian.co.uk>
2016-08-09 15:03:03 +0100

    Revert "Adds weighted sort by supplier (Native Script) (#1961)"
    This reverts commit c35ee18481f65df66dadfca35ef322eee8e25725.

--- media-api/app/lib/elasticsearch/sorts.scala
+++ media-api/app/lib/elasticsearch/sorts.scala
@@ -1,39 +1,24 @@
 package lib.elasticsearch
 
-import scala.collection.JavaConversions._
-import collection.JavaConverters._
-
 import lib.querysyntax._
-import lib.Config
 import org.elasticsearch.action.search.SearchRequestBuilder
 import org.elasticsearch.index.query.{FilterBuilders}
-import org.elasticsearch.search.sort.{SortBuilders, SortOrder, ScriptSortBuilder}
+import org.elasticsearch.search.sort.{SortBuilders, SortOrder}
 
-import com.gu.mediaservice.model.{Agency, Agencies}
 
 object sorts {
+
   def createSort(sortBy: Option[String], query: List[Condition])(builder: SearchRequestBuilder) = {
     sortBy match {
       case Some("dateAddedToCollection") => addedToCollectionTimeSort(query)(builder)
-      case _ => weightedSort(sortBy)(builder)
+      //for any other sortBy term
+      case _ => uploadTimeSort(sortBy)(builder)
     }
   }
 
-  def weightedSort(sortBy: Option[String], active: Boolean = true)(builder: SearchRequestBuilder) = {
-    val SortParams(dateFieldName, sortOrder) = parseSort(sortBy)
-
-    val supplierWeights = Config.supplierWeights
-      .map { case(k,v) => (Agencies.all.get(k), v) }
-      .collect { case((Some(Agency(name,_,_)),v)) => name -> v }
-
-    val sort = new ScriptSortBuilder("grid-supplier-weight-sort", "number")
-    val weights = if (active) supplierWeights.asJava else Map()
-
-    sort.param("supplier_weights", supplierWeights.asJava)
-    sort.lang("native")
-    sort.order(sortOrder)
-
-    builder.addSort(sort)
+  def uploadTimeSort(sortBy: Option[String])(builder: SearchRequestBuilder): SearchRequestBuilder = {
+    val sorts = sortBy.fold(Seq("uploadTime" -> SortOrder.DESC))(parseSorts)
+    for ((field, order) <- sorts) builder.addSort(field, order)
     builder
   }
 
@@ -53,17 +38,14 @@ object sorts {
     builder
   }
 
-  val DescField = "-(.+)".r
   type Field = String
-  case class SortParams(field: Field, order: SortOrder)
 
-  def parseSort(sortBy: Option[String]): SortParams = {
-    val sorts = sortBy.map {
-      case DescField(field) => SortParams(field, SortOrder.DESC)
-      case field            => SortParams(field, SortOrder.ASC)
-    }.getOrElse(SortParams("uploadTime", SortOrder.DESC))
+  val DescField = "-(.+)".r
 
-    sorts
-  }
+  def parseSorts(sortBy: String): Seq[(Field, SortOrder)] =
+    sortBy.split(',').toList.map {
+      case DescField(field) => (field, SortOrder.DESC)
+      case field            => (field, SortOrder.ASC)
+    }
 
 }
