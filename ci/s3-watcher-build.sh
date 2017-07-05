5bc838d6b950b07bda71122364a26f10a82d213d
Akash A <akash1810@gmail.com>
2017-07-05 10:47:34 +0100

    nvm

--- ci/s3-watcher-build.sh
+++ ci/s3-watcher-build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+NODE_VERSION="6.2.1"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm use ${NODE_VERSION}
+
 pushd ../s3watcher/lambda
 
 npm install
