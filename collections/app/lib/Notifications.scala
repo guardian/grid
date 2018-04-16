7defdb2e0b60cd141aa2a5eb9c1066fd77d46fc2
Michael Barton <michael.barton@guardian.co.uk>
2018-04-16 14:49:43 +0100

    Fix compile error

--- collections/app/lib/Notifications.scala
+++ collections/app/lib/Notifications.scala
@@ -2,4 +2,4 @@ package lib
 
 import com.gu.mediaservice.lib.aws.SNS
 
-class Notifications(config: CollectionsConfig) extends SNS(config.awsCredentials, config.topicArn)
+class Notifications(config: CollectionsConfig) extends SNS(config, config.topicArn)
