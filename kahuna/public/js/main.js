3da7c6992b950bd4d02cc5dad88894a2578403d6
shaundillon <shaun.dillon@guardian.co.uk>
2018-04-20 16:17:18 +0100

    fix controller by allowing them to pre-bind to scopr values

--- kahuna/public/js/main.js
+++ kahuna/public/js/main.js
@@ -116,6 +116,7 @@ kahuna.config(['$urlRouterProvider',
 // https://code.angularjs.org/1.5.5/docs/guide/production
 kahuna.config(['$compileProvider', function ($compileProvider) {
   $compileProvider.debugInfoEnabled(false);
+  $compileProvider.preAssignBindingsEnabled(true);
 }]);
 
 kahuna.run(['$log', '$rootScope', 'mediaApi', function($log, $rootScope, mediaApi) {
