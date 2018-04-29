6b0cfd065ebdcdb37a5d48291ec11c3fe11586a0
Michael Barton <michael.barton@guardian.co.uk>
2018-04-29 20:17:26 +0100

    Cloudformation skeleton for an image CDN

---
+++ cdn/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
