23bb5e5dbdf9144671b3b620e37d542ecebb898b
Michael Barton <michael.barton@guardian.co.uk>
2018-04-29 21:07:42 +0100

    Skeleton panda auth

--- cdn/src/panda.ts
+++ cdn/src/panda.ts
@@ -1,6 +1,20 @@
-import { CloudFrontRequestEvent, CloudFrontRequestResult, Callback, Context, Handler } from 'aws-lambda';
+import { CloudFrontRequestEvent, CloudFrontRequestResult, Callback, Context, Handler, CloudFrontRequest } from 'aws-lambda';
+
+import { parse } from 'cookie';
+import { getPEM } from 'pan-domain-public-keys';
+import validateUser from 'pan-domain-validate-user';
+
+const { STAGE } = process.env;
+
+function handle(request: CloudFrontRequest): Promise<CloudFrontRequestResult> {
+  const cookie = parse(request.headers['cookie'][0].value)['gutoolsAuth-assym'];
+
+  return getPEM(STAGE).then(key => {
+    return validateUser(key, cookie).then((_: void) => request);
+  });
+}
 
 export const handler: Handler = (event: CloudFrontRequestEvent, context: Context, cb: Callback<CloudFrontRequestResult>) => {
   const request = event.Records[0].cf.request;
-  cb(null, request);
+  handle(request).then(resp => cb(null, resp)).catch(err => cb(err, null));
 }
