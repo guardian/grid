429eebf33eee4efa1079ed8b74a10145d7f24a04
Akash A <akash1810@gmail.com>
2018-05-23 22:43:21 +0100

    remove the wat config
    its being added to the DOM, but not used

--- kahuna/app/lib/KahunaConfig.scala
+++ kahuna/app/lib/KahunaConfig.scala
@@ -12,7 +12,6 @@ class KahunaConfig(override val configuration: Configuration) extends CommonConf
   val authUri: String = services.authBaseUri
 
   val sentryDsn: Option[String] = properties.get("sentry.dsn").filterNot(_.isEmpty)
-  val watUri: Option[String] = properties.get("wat.uri").filterNot(_.isEmpty)
 
   val thumbOrigin: String = properties("origin.thumb")
   val fullOrigin: String = properties("origin.full")
