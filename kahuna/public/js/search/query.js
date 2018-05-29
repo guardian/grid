c9509953cf6ca8a7345a222402ac58847b1f240c
shaundillon <shaun.dillon@guardian.co.uk>
2018-05-29 12:12:59 +0100

    show paids false value is undefined

--- kahuna/public/js/search/query.js
+++ kahuna/public/js/search/query.js
@@ -162,7 +162,7 @@ query.controller('SearchQueryCtrl',
     mediaApi.getSession().then(session => {
         ctrl.user = session.user;
         ctrl.filter.uploadedByMe = ctrl.uploadedBy === ctrl.user.email;
-        ctrl.filter.nonFree = session.user.permissions.showPaid;
+        ctrl.filter.nonFree = session.user.permissions.showPaid ? session.user.permissions.showPaid : undefined;
     });
 
     function resetQuery() {
