2642689bd360af258d3f2f3df05df91fc238f368
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2018-06-12 14:29:46 +0100

    Encode + and space correctly

--- common-lib/src/main/scala/com/gu/mediaservice/lib/net/URI.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/net/URI.scala
@@ -3,6 +3,8 @@ package com.gu.mediaservice.lib.net
 import java.net.{URLDecoder, URLEncoder}
 
 object URI {
-  def encode(uri: String) = URLEncoder.encode(uri, "UTF-8")
-  def decode(uri: String) = URLDecoder.decode(uri, "UTF-8")
+  def encode(uri: String): String = URLEncoder.encode(uri, "UTF-8").replace("+", "%20")
+  def decode(uri: String): String = URLDecoder.decode(uri, "UTF-8")
+
+  def encodePlus(uri: String): String = uri.replace("+", "%2B")
 }
