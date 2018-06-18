d8a69113f855112230087e5b8aeed9e81c26f611
shaundillon <shaun.dillon@guardian.co.uk>
2018-06-18 16:19:13 +0100

    create batch cropping directive and add to results bar

---
+++ kahuna/public/js/components/gr-batch-export-original-images/gr-batch-export-original-images.js
@@ -0,0 +1,51 @@
+import angular from 'angular';
+
+import template from './gr-batch-export-original-images.html';
+
+export const batchExportOriginalImages = angular.module('gr.batchExportOriginalImages', []);
+
+batchExportOriginalImages.controller('grBatchExportOriginalImagesCtrl', [
+    '$scope', '$rootScope', '$state', 'mediaCropper',
+    function($scope, $rootScope, $state, mediaCropper) {
+        let ctrl = this;
+
+        ctrl.callBatchCrop = function() {
+            //prevents return key on the crop button posting crop twice
+            if (!ctrl.cropping) {
+              cropImages();
+            }
+        };
+
+        function cropImages() {
+            ctrl.cropping = true;
+
+            const cropImages = ctrl.images.map(image => {
+              mediaCropper.createFullCrop(image).then(crop => {
+                  //Global notification of action
+                  $rootScope.$emit('events:crop-created', {
+                      image: ctrl.image,
+                      crop: crop
+                  });
+              });
+            });
+
+            Promise.all(cropImages).finally(() => {
+              ctrl.cropping = false;
+            });
+        }
+    }
+]);
+
+batchExportOriginalImages.directive('grBatchExportOriginalImages', [function() {
+    return {
+        restrict: 'E',
+        controller: 'grBatchExportOriginalImagesCtrl',
+        controllerAs: 'ctrl',
+        bindToController: true,
+        scope: {
+            images: '=',
+            cropping: '='
+        },
+        template: template
+    };
+}]);
