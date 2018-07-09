66a54a18d59df5ae78aee91a3a2e17c7f40cb787
Michael Barton <michael.barton@guardian.co.uk>
2018-07-09 09:27:18 +0100

    Schedule updates for quota and usage stores

--- media-api/app/lib/UsageQuota.scala
+++ media-api/app/lib/UsageQuota.scala
@@ -1,20 +1,35 @@
 package lib
 
+import akka.actor.Scheduler
 import com.gu.mediaservice.lib.FeatureToggle
 import com.gu.mediaservice.model.{Image, UsageRights}
-import controllers.Quotas
 import lib.elasticsearch.ElasticSearch
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.util.Try
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
 case class ImageNotFound() extends Exception("Image not found")
 case class NoUsageQuota() extends Exception("No usage found for this image")
 
-class UsageQuota(config: MediaApiConfig, elasticSearch: ElasticSearch) extends Quotas(config) {
+class UsageQuota(config: MediaApiConfig, elasticSearch: ElasticSearch, scheduler: Scheduler) {
+  val quotaStore = new QuotaStore(
+    config.quotaStoreConfig.storeKey,
+    config.quotaStoreConfig.storeBucket,
+    config
+  )
+
+  val usageStore = new UsageStore(
+    config.usageMailBucket,
+    config,
+    quotaStore
+  )
+
+  def scheduleUpdates(): Unit = {
+    quotaStore.scheduleUpdates(scheduler)
+    usageStore.scheduleUpdates(scheduler)
+  }
 
   def isOverQuota(rights: UsageRights, waitMillis: Int = 100) = Try {
     Await.result(
