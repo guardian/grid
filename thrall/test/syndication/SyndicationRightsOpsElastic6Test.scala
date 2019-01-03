ffe40579fcee26e026b85ce9513e4e832b47e497
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2019-01-03 14:31:21 +0000

    Test config; no replicas in test.

--- thrall/test/syndication/SyndicationRightsOpsElastic6Test.scala
+++ thrall/test/syndication/SyndicationRightsOpsElastic6Test.scala
@@ -5,7 +5,7 @@ import play.api.Configuration
 
 class SyndicationRightsOpsElastic6Test extends SyndicationRightsOpsTestsBase {
 
-  val elasticSearchConfig = ElasticSearch6Config("writeAlias", "localhost", 9206, "media-service-test", 1, 1)
+  val elasticSearchConfig = ElasticSearch6Config("writeAlias", "localhost", 9206, "media-service-test", 1, 0)
 
   val ES = new ElasticSearch6(elasticSearchConfig, new ThrallMetrics(new ThrallConfig(Configuration.empty)))
 
