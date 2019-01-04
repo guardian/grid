168c326021237be86474b4af56a7290dba013124
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2019-01-04 17:11:10 +0000

    WIP Navive porting of SearchFilters; filters DSL continues to in insulate us the the underlying elastic version.

--- media-api/app/lib/elasticsearch/impls/elasticsearch6/ElasticSearch.scala
+++ media-api/app/lib/elasticsearch/impls/elasticsearch6/ElasticSearch.scala
@@ -1,17 +1,21 @@
 package lib.elasticsearch.impls.elasticsearch6
 
+import com.gu.mediaservice.lib.elasticsearch.ImageFields
 import com.gu.mediaservice.lib.elasticsearch6.ElasticSearchClient
 import com.gu.mediaservice.model.Image
 import com.sksamuel.elastic4s.http.ElasticDsl
 import com.sksamuel.elastic4s.http.ElasticDsl._
+import com.sksamuel.elastic4s.searches.queries.Query
 import lib.elasticsearch._
 import lib.{MediaApiConfig, MediaApiMetrics, SupplierUsageSummary}
 import play.api.libs.json.Json
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
+import scalaz.NonEmptyList
+import scalaz.syntax.std.list._
 
-class ElasticSearch(config: MediaApiConfig, mediaApiMetrics: MediaApiMetrics) extends ElasticSearchVersion with ElasticSearchClient {
+class ElasticSearch(config: MediaApiConfig, mediaApiMetrics: MediaApiMetrics) extends ElasticSearchVersion with ElasticSearchClient with ImageFields {
 
   lazy val imagesAlias = config.imagesAlias
   lazy val host = "localhost"
@@ -32,19 +36,72 @@ class ElasticSearch(config: MediaApiConfig, mediaApiMetrics: MediaApiMetrics) ex
     def matchAllQuery = ElasticDsl.search(imagesAlias)
 
     def queryFor(params: SearchParams) = {
-      val filters = Seq(
-        params.syndicationStatus.map(status => syndicationFilter.statusFilter(status)),
-        searchFilters.tierFilter(params.tier))
-        .flatten
+      val uploadTimeFilter  = filters.date("uploadTime", params.since, params.until)
+      val lastModTimeFilter = filters.date("lastModified", params.modifiedSince, params.modifiedUntil)
+      val takenTimeFilter   = filters.date("metadata.dateTaken", params.takenSince, params.takenUntil)
+      // we only inject filters if there are actual date parameters
+      val dateFilterList    = List(uploadTimeFilter, lastModTimeFilter, takenTimeFilter).flatten.toNel
+      val dateFilter        = dateFilterList.map(dateFilters => filters.and(dateFilters.list: _*))
 
-      if (filters.nonEmpty) {
-        matchAllQuery bool must(filters)
-      } else {
-        matchAllQuery
+      val idsFilter         = params.ids.map(filters.ids)
+      val labelFilter       = params.labels.toNel.map(filters.terms("labels", _))
+      val metadataFilter    = params.hasMetadata.map(metadataField).toNel.map(filters.exists)
+      val archivedFilter    = params.archived.map(filters.existsOrMissing(editsField("archived"), _))
+      val hasExports        = params.hasExports.map(filters.existsOrMissing("exports", _))
+      val hasIdentifier     = params.hasIdentifier.map(idName => filters.exists(NonEmptyList(identifierField(idName))))
+      val missingIdentifier = params.missingIdentifier.map(idName => filters.missing(NonEmptyList(identifierField(idName))))
+      val uploadedByFilter  = params.uploadedBy.map(uploadedBy => filters.terms("uploadedBy", NonEmptyList(uploadedBy)))
+      val simpleCostFilter  = params.free.flatMap(free => if (free) searchFilters.freeFilter else searchFilters.nonFreeFilter)
+      val costFilter        = params.payType match {
+        case Some(PayType.Free) => searchFilters.freeFilter
+        case Some(PayType.MaybeFree) => searchFilters.maybeFreeFilter
+        case Some(PayType.Pay) => searchFilters.nonFreeFilter
+        case _ => None
       }
+
+      val hasRightsCategory = params.hasRightsCategory.filter(_ == true).map(_ => searchFilters.hasRightsCategoryFilter)
+
+      val validityFilter = params.valid.flatMap(valid => if(valid) searchFilters.validFilter else searchFilters.invalidFilter)
+
+      val persistFilter = params.persisted map {
+        case true   => searchFilters.persistedFilter
+        case false  => searchFilters.nonPersistedFilter
+      }
+
+      val usageFilter: Iterable[Query] =
+       params.usageStatus.toNel.map(status => filters.terms(usagesField("status"), status.map(_.toString))) ++
+         params.usagePlatform.toNel.map(filters.terms(usagesField("platform"), _))
+
+      val syndicationStatusFilter: Option[Query] = params.syndicationStatus.map(status => syndicationFilter.statusFilter(status))
+
+      val filterOpt = (
+        metadataFilter.toList
+          ++ persistFilter
+          ++ labelFilter
+          ++ archivedFilter
+          ++ uploadedByFilter
+          ++ idsFilter
+          ++ validityFilter
+          ++ simpleCostFilter
+          ++ costFilter
+          ++ hasExports
+          ++ hasIdentifier
+          ++ missingIdentifier
+          ++ dateFilter
+          ++ usageFilter
+          ++ hasRightsCategory
+          ++ searchFilters.tierFilter(params.tier)
+          ++ syndicationStatusFilter
+        ).toNel.map(filter => filter.list.reduceLeft(filters.and(_, _)))
+
+      filterOpt.map { f =>
+        matchAllQuery bool must(f)
+      }.getOrElse(matchAllQuery)
     }
 
-    client.execute(queryFor(params)).map { r =>
+    val query = queryFor(params)
+
+    client.execute(query).map { r =>
       val hits = r.result.hits.hits.map { h =>
         Try {
           val image = Json.toJson(h.sourceAsString).as[Image]
--- media-api/app/lib/elasticsearch/impls/elasticsearch6/SearchFilters.scala
+++ media-api/app/lib/elasticsearch/impls/elasticsearch6/SearchFilters.scala
@@ -1,17 +1,91 @@
 package lib.elasticsearch.impls.elasticsearch6
 
 import com.gu.mediaservice.lib.auth.{Syndication, Tier}
-import com.gu.mediaservice.model.QueuedForSyndication
+import com.gu.mediaservice.lib.config.UsageRightsConfig
+import com.gu.mediaservice.lib.elasticsearch.ImageFields
+import com.gu.mediaservice.model._
 import com.sksamuel.elastic4s.searches.queries.Query
 import lib.MediaApiConfig
+import scalaz.NonEmptyList
+import scalaz.syntax.std.list._
 
-class SearchFilters(config: MediaApiConfig) {
+class SearchFilters(config: MediaApiConfig)  extends ImageFields {
 
   val syndicationFilter = new SyndicationFilter(config)
+
+  import UsageRightsConfig.{freeSuppliers, suppliersCollectionExcl}
+
+  // Warning: The current media-api definition of invalid includes other requirements
+  // so does not match this filter exactly!
+  val validFilter: Option[Query] = config.requiredMetadata.map(metadataField).toNel.map(filters.exists)
+  val invalidFilter: Option[Query] = config.requiredMetadata.map(metadataField).toNel.map(filters.anyMissing)
+
+  val (suppliersWithExclusions, suppliersNoExclusions) = freeSuppliers.partition(suppliersCollectionExcl.contains)
+  val suppliersWithExclusionsFilters: List[Query] = for {
+    supplier            <- suppliersWithExclusions
+    excludedCollections <- suppliersCollectionExcl.get(supplier).flatMap(_.toNel)
+  } yield {
+    filters.bool.must(
+      filters.term(usageRightsField("supplier"), supplier)
+    )// TODO .mustNot(
+     // filters.terms(usageRightsField("suppliersCollection"), excludedCollections)
+   // )
+  }
+
+  val suppliersWithExclusionsFilter: Option[Query] = suppliersWithExclusionsFilters.toNel.map(filters.or)
+  val suppliersNoExclusionsFilter: Option[Query] = suppliersNoExclusions.toNel.map(filters.terms(usageRightsField("supplier"), _))
+  val freeSupplierFilter: Option[Query] = filterOrFilter(suppliersWithExclusionsFilter, suppliersNoExclusionsFilter)
+
+  // We're showing `Conditional` here too as we're considering them potentially
+  // free. We could look into sending over the search query as a cost filter
+  // that could take a comma separated list e.g. `cost=free,conditional`.
+  val freeUsageRightsFilter: Option[Query] = freeToUseCategories.toNel.map(filters.terms(usageRightsField("category"), _))
+
+  val hasRightsCategoryFilter: Query = filters.existsOrMissing(usageRightsField("category"), exists = true)
+
+  val freeFilter: Option[Query] = filterOrFilter(freeSupplierFilter, freeUsageRightsFilter)
+  val nonFreeFilter: Option[Query] = freeFilter.map(filters.not2)
+
+  val maybeFreeFilter: Option[Query] = filterOrFilter(freeFilter, Some(filters.not2(hasRightsCategoryFilter)))
+
+  lazy val freeToUseCategories: List[String] =
+    UsageRights.all.filter(ur => ur.defaultCost.exists(cost => cost == Free || cost == Conditional)).map(ur => ur.category)
+
+  val persistedCategories = NonEmptyList(
+    StaffPhotographer.category,
+    ContractPhotographer.category,
+    CommissionedPhotographer.category,
+    ContractIllustrator.category,
+    StaffIllustrator.category,
+    CommissionedIllustrator.category,
+    CommissionedAgency.category
+  )
+
+  val persistedFilter: Query = filters.or(
+    filters.bool.must(filters.existsOrMissing("exports", exists = true)),
+    filters.bool.must(filters.existsOrMissing("usages", exists = true)),
+    filters.exists(NonEmptyList(identifierField(config.persistenceIdentifier))),
+    filters.bool.must(filters.boolTerm(editsField("archived"), value = true)),
+    filters.bool.must(filters.terms(usageRightsField("category"), persistedCategories)),
+    filters.bool.must(filters.terms(collectionsField("path"), config.persistedRootCollections.toNel.get)),
+    filters.exists(NonEmptyList(editsField("photoshoot")))
+  )
+
+  val nonPersistedFilter: Query = filters.not2(persistedFilter)
 
   def tierFilter(tier: Tier): Option[Query] = tier match {
     case Syndication => Some(syndicationFilter.statusFilter(QueuedForSyndication))
     case _ => None
+  }
+
+  def filterOrFilter(filter: Option[Query], orFilter: Option[Query]): Option[Query] = (filter, orFilter) match {
+    case (Some(someFilter), Some(orSomeFilter)) => Some(filters.or(someFilter, orSomeFilter))
+    case (filterOpt,    orFilterOpt)    => filterOpt orElse orFilterOpt
+  }
+
+  def filterAndFilter(filter: Option[Query], andFilter: Option[Query]): Option[Query] = (filter, andFilter) match {
+    case (Some(someFilter), Some(andSomeFilter)) => Some(filters.and(someFilter, andSomeFilter))
+    case (filterOpt,    andFilterOpt)    => filterOpt orElse andFilterOpt
   }
 
 }
