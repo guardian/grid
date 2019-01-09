8e307def74132bbaec0ce8b8510051e4184a7af3
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2019-01-09 11:35:47 +0000

    Clean ups; visually more aligned with the ES1 implementation.

--- media-api/app/lib/elasticsearch/impls/elasticsearch6/QueryBuilder.scala
+++ media-api/app/lib/elasticsearch/impls/elasticsearch6/QueryBuilder.scala
@@ -16,42 +16,40 @@ class QueryBuilder(matchFields: Seq[String]) extends ImageFields {
     ElasticDsl.multiMatchQuery(value).fields(fields).matchType(MultiMatchQueryBuilderType.PHRASE)
 
   private def makeMultiQuery(value: Value, fields: Seq[String]): MultiMatchQuery = value match {
-    case Words(value) => ElasticDsl.multiMatchQuery(value).
-      fields(fields).
+    case Words(value) => ElasticDsl.multiMatchQuery(value).fields(fields).
       operator(Operator.AND).
-      analyzer(IndexSettings.enslishSStemmerAnalyzerName).
-      matchType(MultiMatchQueryBuilderType.CROSS_FIELDS)
+      matchType(MultiMatchQueryBuilderType.CROSS_FIELDS).
+      analyzer(IndexSettings.enslishSStemmerAnalyzerName)
     case Phrase(string) => multiMatchPhraseQuery(string, fields)
     // That's OK, we only do date queries on a single field at a time
     case e => throw InvalidQuery(s"Cannot do multiQuery on $e")
   }
 
-  private def makeQueryBit(condition: Match): Query = {
-    condition.field match {
-      case AnyField => makeMultiQuery(condition.value, matchFields)
-      case MultipleField(fields) => makeMultiQuery(condition.value, fields)
-      case SingleField(field) => condition.value match {
-        // Force AND operator else it will only require *any* of the words, not *all*
-        case Words(value) => matchQuery(field, value).operator(Operator.AND)
-        case Phrase(value) => matchPhraseQuery(field, value)
-        case DateRange(start, end) => rangeQuery(field).gte(printDateTime(start)).lte(printDateTime(end))
-        case e => throw InvalidQuery(s"Cannot do single field query on $e")
-      }
-      case HierarchyField => condition.value match {
-        case Phrase(value) => termQuery(getFieldPath("pathHierarchy"), value)
-        case _ => throw InvalidQuery("Cannot accept non-Phrase value for HierarchyField Match")
-      }
-      case HasField => condition.value match {
-        case HasValue(value) => boolQuery().filter(existsQuery(getFieldPath(value)))
-        case _ => throw InvalidQuery(s"Cannot perform has field on ${condition.value}")
-      }
+  private def makeQueryBit(condition: Match): Query = condition.field match {
+    case AnyField => makeMultiQuery(condition.value, matchFields)
+    case MultipleField(fields) => makeMultiQuery(condition.value, fields)
+    case SingleField(field) => condition.value match {
+      // Force AND operator else it will only require *any* of the words, not *all*
+      case Words(value) => matchQuery(field, value).operator(Operator.AND)
+      case Phrase(value) => matchPhraseQuery(field, value)
+      case DateRange(start, end) => rangeQuery(field).gte(printDateTime(start)).lte(printDateTime(end))
+      case e => throw InvalidQuery(s"Cannot do single field query on $e")
+    }
+    case HierarchyField => condition.value match {
+      case Phrase(value) => termQuery(getFieldPath("pathHierarchy"), value)
+      case _ => throw InvalidQuery("Cannot accept non-Phrase value for HierarchyField Match")
+    }
+    case HasField => condition.value match {
+      case HasValue(value) => boolQuery().filter(existsQuery(getFieldPath(value)))
+      case _ => throw InvalidQuery(s"Cannot perform has field on ${condition.value}")
     }
   }
 
   def makeQuery(conditions: List[Condition]) = conditions match {
     case Nil => matchAllQuery
     case condList => {
-      val (nested, normal: List[Condition]) = (
+
+      val (nested: List[Nested], normal: List[Condition]) = (
         condList collect { case n: Nested => n },
         condList collect { case c: Condition => c }
       )
@@ -62,7 +60,7 @@ class QueryBuilder(matchFields: Seq[String]) extends ImageFields {
         case (query, _) => query
       }
 
-      val nestedQueries: List[Query] = nested
+      val nestedQueries = nested
         .groupBy(_.parentField)
         .map {
           case (parent: SingleField, n: List[Nested]) => {
@@ -82,5 +80,4 @@ class QueryBuilder(matchFields: Seq[String]) extends ImageFields {
       nestedQueries.foldLeft(query) { case (q, nestedQ) => q.withMust(nestedQ) }
     }
   }
-
 }
