c46fe66df09a0bbe89c327671582b670d6506e46
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2019-01-09 12:18:09 +0000

    elastic4s can take a list of sorts which removes our responsibility for composing the subsorts ourselves.

--- media-api/app/lib/elasticsearch/impls/elasticsearch6/sorts.scala
+++ media-api/app/lib/elasticsearch/impls/elasticsearch6/sorts.scala
@@ -1,12 +1,22 @@
 package lib.elasticsearch.impls.elasticsearch6
 
 import com.sksamuel.elastic4s.http.ElasticDsl._
-import com.sksamuel.elastic4s.searches.sort.{Sort, SortOrder}
+import com.sksamuel.elastic4s.searches.sort.{FieldSort, Sort, SortOrder}
 
 object sorts {
 
-  def createSort(sortBy: Option[String]): Sort = {
-    fieldSort("uploadTime").order(SortOrder.DESC)
+  private val UploadTimeDescending: Sort = fieldSort("uploadTime").order(SortOrder.DESC)
+  private val HasDescFieldPrefix = "-(.+)".r
+
+  def createSort(sortBy: Option[String]): Seq[Sort] = {
+    sortBy.fold(Seq(UploadTimeDescending))(parseSortBy)
+  }
+
+  private def parseSortBy(sortBy: String): Seq[FieldSort] = {
+    sortBy.split(',').toList.map {
+      case HasDescFieldPrefix(field) => fieldSort(field).order(SortOrder.DESC)
+      case field => fieldSort(field).order(SortOrder.ASC)
+    }
   }
 
 }
