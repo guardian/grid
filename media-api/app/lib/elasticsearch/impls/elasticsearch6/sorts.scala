e7e56c9c7afa2be063625dbc9955e5d73f42c17e
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2019-01-09 12:29:33 +0000

    Added to collection date sort special case. Regression - the Elastic1 sort code mutates the query definition at this point. We'll need to pull this special case up.

--- media-api/app/lib/elasticsearch/impls/elasticsearch6/sorts.scala
+++ media-api/app/lib/elasticsearch/impls/elasticsearch6/sorts.scala
@@ -1,21 +1,26 @@
 package lib.elasticsearch.impls.elasticsearch6
 
 import com.sksamuel.elastic4s.http.ElasticDsl._
-import com.sksamuel.elastic4s.searches.sort.{FieldSort, Sort, SortOrder}
+import com.sksamuel.elastic4s.searches.sort.{Sort, SortOrder}
 
 object sorts {
 
   private val UploadTimeDescending: Sort = fieldSort("uploadTime").order(SortOrder.DESC)
+  private val DateAddedToCollectionDescending: Sort = fieldSort("collections.actionData.date").order(SortOrder.DESC)
+
   private val HasDescFieldPrefix = "-(.+)".r
 
   def createSort(sortBy: Option[String]): Seq[Sort] = {
     sortBy.fold(Seq(UploadTimeDescending))(parseSortBy)
   }
 
-  private def parseSortBy(sortBy: String): Seq[FieldSort] = {
-    sortBy.split(',').toList.map {
-      case HasDescFieldPrefix(field) => fieldSort(field).order(SortOrder.DESC)
-      case field => fieldSort(field).order(SortOrder.ASC)
+  private def parseSortBy(sortBy: String): Seq[Sort] = {
+    sortBy match {
+      case "dateAddedToCollection" => Seq(DateAddedToCollectionDescending)  // TODO Elastic1 code also mutates the query at this point =(
+      case _ => sortBy.split(',').toList.map {
+        case HasDescFieldPrefix(field) => fieldSort(field).order(SortOrder.DESC)
+        case field => fieldSort(field).order(SortOrder.ASC)
+      }
     }
   }
 
