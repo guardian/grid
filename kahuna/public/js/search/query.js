b413321582bda29d9049b30662f63f279c530ef3
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2019-01-24 15:48:42 +0000

    Force refresh with toggling index.

--- kahuna/public/js/search/query.js
+++ kahuna/public/js/search/query.js
@@ -188,6 +188,17 @@ query.controller('SearchQueryCtrl',
         } else {
             cookies.remove(toggleCookieName, {"domain": domain});
         }
+
+        function revealNewImages() {
+            // FIXME: should ideally be able to just call $state.reload(),
+            // but there seems to be a bug (alluded to in the docs) when
+            // notify is false, so forcing to true explicitly instead:
+            $state.transitionTo($state.current, $stateParams, {
+                reload: true, inherit: false, notify: true
+            });
+        }
+
+        revealNewImages();
     }
 
     function resetQuery() {
