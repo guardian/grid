ddef4876898096da623280fa2f4d4cc839c8960e
Akash A <akash1810@gmail.com>
2019-02-28 23:02:52 +0000

    indent on 2 spaces not 4

--- kahuna/public/js/components/gr-info-panel/gr-info-panel.js
+++ kahuna/public/js/components/gr-info-panel/gr-info-panel.js
@@ -13,182 +13,182 @@ import '../../forms/gr-xeditable/gr-xeditable';
 import '../../util/rx';
 import { descriptionEditOptions } from '../../util/constants/descriptionEditOptions';
 export const grInfoPanel = angular.module('grInfoPanel', [
-    'kahuna.services.image-accessor',
-    'kahuna.services.image-list',
-    'kahuna.services.label',
-    'kahuna.services.panel',
-    'kahuna.edits.service',
-    'grXeditable',
-    'ui.bootstrap',
-    'util.rx'
+  'kahuna.services.image-accessor',
+  'kahuna.services.image-list',
+  'kahuna.services.label',
+  'kahuna.services.panel',
+  'kahuna.edits.service',
+  'grXeditable',
+  'ui.bootstrap',
+  'util.rx'
 ]);
 
 grInfoPanel.controller('GrInfoPanelCtrl', [
-    '$rootScope',
-    '$scope',
-    '$window',
-    '$q',
-    'inject$',
-    'subscribe$',
-    'mediaApi',
-    'imageAccessor',
-    'imageList',
-    'selectedImagesList$',
-    'labelService',
-    'editsService',
-    'editsApi',
-    function (
-        $rootScope,
-        $scope,
-        $window,
-        $q,
-        inject$,
-        subscribe$,
-        mediaApi,
-        imageAccessor,
-        imageList,
-        selectedImagesList$,
-        labelService,
-        editsService,
-        editsApi) {
+  '$rootScope',
+  '$scope',
+  '$window',
+  '$q',
+  'inject$',
+  'subscribe$',
+  'mediaApi',
+  'imageAccessor',
+  'imageList',
+  'selectedImagesList$',
+  'labelService',
+  'editsService',
+  'editsApi',
+  function (
+    $rootScope,
+    $scope,
+    $window,
+    $q,
+    inject$,
+    subscribe$,
+    mediaApi,
+    imageAccessor,
+    imageList,
+    selectedImagesList$,
+    labelService,
+    editsService,
+    editsApi) {
 
-        const ctrl = this;
+    const ctrl = this;
 
-        ctrl.showUsageRights = false;
+    ctrl.showUsageRights = false;
 
-        inject$($scope, selectedImagesList$, ctrl, 'selectedImages');
+    inject$($scope, selectedImagesList$, ctrl, 'selectedImages');
 
-        const selectedCosts$ = selectedImagesList$.
-              map(imageList.getCost).
-              map(imageList.getOccurrences);
-        inject$($scope, selectedCosts$, ctrl, 'selectedCosts');
-
-
-        const selectedLabels$ = selectedImagesList$.
-              map(imageList.getLabels).
-              map(imageList.getOccurrences);
-        inject$($scope, selectedLabels$, ctrl, 'selectedLabels');
-
-        const selectedUsageRights$ = selectedImagesList$.map(selectedImagesList => {
-            // FIXME: wrap into slightly weird shape expected by usage
-            // rights editor component
-            return selectedImagesList.map(image => {
-                return {
-                    image: image,
-                    data: imageAccessor.readUsageRights(image)
-                };
-            });
-        });
-        const selectedUsageRightsArray$ = selectedUsageRights$.map(selectedUsageRights => {
-            return selectedUsageRights.toArray();
-        });
-
-        const categoriesPromise = editsApi.getUsageRightsCategories();
-        const usageRightsCategories$ = Rx.Observable.fromPromise(categoriesPromise);
-        const selectedUsageCategory$ = Rx.Observable.combineLatest(
-            selectedUsageRights$,
-            usageRightsCategories$,
-            (usageRights, categories) => {
-                const categoryCode = usageRights.reduce((m, o) => {
-                    return (m == o.data.category) ? o.data.category : 'multiple categories';
-                }, usageRights.first() && usageRights.first().data.category);
-
-                const usageCategory = categories.find(cat => cat.value === categoryCode);
-                return usageCategory ? usageCategory.name : categoryCode;
-            }
-        );
-
-        inject$($scope, selectedUsageRightsArray$, ctrl, 'usageRights');
-        inject$($scope, selectedUsageCategory$,    ctrl, 'usageCategory');
+    const selectedCosts$ = selectedImagesList$.
+    map(imageList.getCost).
+    map(imageList.getOccurrences);
+    inject$($scope, selectedCosts$, ctrl, 'selectedCosts');
 
 
-        // FIXME: distinct?
-        const selectedMetadata$ = selectedImagesList$.
-              map(imageList.getMetadata).
-              map(imageList.getSetOfProperties);
-        const rawMetadata$ = selectedMetadata$.map(selectedMetadata => {
-            return selectedMetadata.map((values) => {
-                switch (values.size) {
-                case 0:  return undefined;
-                case 1:  return values.first();
-                default: return Array.from(values);
-                }
-            }).toObject();
-        });
-        const displayMetadata$ = selectedMetadata$.map(selectedMetadata => {
-            return selectedMetadata.map((values) => {
-                switch (values.size) {
-                case 1:  return values.first();
-                default: return undefined;
-                }
-            }).toObject();
-        });
-        inject$($scope, rawMetadata$, ctrl, 'rawMetadata');
-        inject$($scope, displayMetadata$, ctrl, 'metadata');
+    const selectedLabels$ = selectedImagesList$.
+    map(imageList.getLabels).
+    map(imageList.getOccurrences);
+    inject$($scope, selectedLabels$, ctrl, 'selectedLabels');
 
-        const extraInfo$ = selectedImagesList$.
-              map(imageList.getExtraInfo).
-              map(imageList.getSetOfProperties).
-              map(extraInfo => extraInfo.map((values) => {
-                  switch (values.size) {
-                  case 0:  return undefined;
-                  case 1:  return values.first();
-                  default: return Array.from(values);
-                  }
-              }).toObject()
-        );
-
-        inject$($scope, extraInfo$, ctrl, 'extraInfo');
-
-        const selectionIsEditable$ = selectedImagesList$.
-              map(list => list.map(editsService.canUserEdit).toArray()).
-              map($q.all).
-              flatMap(Rx.Observable.fromPromise).
-              map(allEditable => allEditable.every(v => v === true));
-        inject$($scope, selectionIsEditable$, ctrl, 'userCanEdit');
-
-        ctrl.hasMultipleValues = (val) => Array.isArray(val) && val.length > 1;
-
-        ctrl.credits = function(searchText) {
-            return ctrl.metadataSearch('credit', searchText);
+    const selectedUsageRights$ = selectedImagesList$.map(selectedImagesList => {
+      // FIXME: wrap into slightly weird shape expected by usage
+      // rights editor component
+      return selectedImagesList.map(image => {
+        return {
+          image: image,
+          data: imageAccessor.readUsageRights(image)
         };
+      });
+    });
+    const selectedUsageRightsArray$ = selectedUsageRights$.map(selectedUsageRights => {
+      return selectedUsageRights.toArray();
+    });
 
-        ctrl.metadataSearch = (field, q) => {
-            return mediaApi.metadataSearch(field,  { q }).then(resource => {
-                return resource.data.map(d => d.key);
-            });
-        };
+    const categoriesPromise = editsApi.getUsageRightsCategories();
+    const usageRightsCategories$ = Rx.Observable.fromPromise(categoriesPromise);
+    const selectedUsageCategory$ = Rx.Observable.combineLatest(
+      selectedUsageRights$,
+      usageRightsCategories$,
+      (usageRights, categories) => {
+        const categoryCode = usageRights.reduce((m, o) => {
+          return (m == o.data.category) ? o.data.category : 'multiple categories';
+        }, usageRights.first() && usageRights.first().data.category);
 
-        ctrl.descriptionOption = descriptionEditOptions.overwrite.value;
+        const usageCategory = categories.find(cat => cat.value === categoryCode);
+        return usageCategory ? usageCategory.name : categoryCode;
+      }
+    );
 
-        ctrl.descriptionOptions = [
-          descriptionEditOptions.overwrite,
-          descriptionEditOptions.prepend,
-          descriptionEditOptions.append
-        ];
+    inject$($scope, selectedUsageRightsArray$, ctrl, 'usageRights');
+    inject$($scope, selectedUsageCategory$,    ctrl, 'usageCategory');
 
-        ctrl.updateDescriptionField = function() {
-          ctrl.updateMetadataField('description', ctrl.metadata.description);
-        };
 
-        ctrl.updateMetadataField = function (field, value) {
-            var imageArray = Array.from(ctrl.selectedImages);
-            return editsService.batchUpdateMetadataField(
-              imageArray,
-              field,
-              value,
-              ctrl.descriptionOption
-            );
-        };
+    // FIXME: distinct?
+    const selectedMetadata$ = selectedImagesList$.
+    map(imageList.getMetadata).
+    map(imageList.getSetOfProperties);
+    const rawMetadata$ = selectedMetadata$.map(selectedMetadata => {
+      return selectedMetadata.map((values) => {
+        switch (values.size) {
+          case 0:  return undefined;
+          case 1:  return values.first();
+          default: return Array.from(values);
+        }
+      }).toObject();
+    });
+    const displayMetadata$ = selectedMetadata$.map(selectedMetadata => {
+      return selectedMetadata.map((values) => {
+        switch (values.size) {
+          case 1:  return values.first();
+          default: return undefined;
+        }
+      }).toObject();
+    });
+    inject$($scope, rawMetadata$, ctrl, 'rawMetadata');
+    inject$($scope, displayMetadata$, ctrl, 'metadata');
 
-        ctrl.addLabel = function (label) {
-            var imageArray = Array.from(ctrl.selectedImages);
-            labelService.batchAdd(imageArray, [label]);
-        };
+    const extraInfo$ = selectedImagesList$.
+    map(imageList.getExtraInfo).
+    map(imageList.getSetOfProperties).
+    map(extraInfo => extraInfo.map((values) => {
+        switch (values.size) {
+          case 0:  return undefined;
+          case 1:  return values.first();
+          default: return Array.from(values);
+        }
+      }).toObject()
+    );
 
-        ctrl.removeLabel = function (label) {
-            var imageArray = Array.from(ctrl.selectedImages);
-            labelService.batchRemove(imageArray, label);
-        };
-    }
+    inject$($scope, extraInfo$, ctrl, 'extraInfo');
+
+    const selectionIsEditable$ = selectedImagesList$.
+    map(list => list.map(editsService.canUserEdit).toArray()).
+    map($q.all).
+    flatMap(Rx.Observable.fromPromise).
+    map(allEditable => allEditable.every(v => v === true));
+    inject$($scope, selectionIsEditable$, ctrl, 'userCanEdit');
+
+    ctrl.hasMultipleValues = (val) => Array.isArray(val) && val.length > 1;
+
+    ctrl.credits = function(searchText) {
+      return ctrl.metadataSearch('credit', searchText);
+    };
+
+    ctrl.metadataSearch = (field, q) => {
+      return mediaApi.metadataSearch(field,  { q }).then(resource => {
+        return resource.data.map(d => d.key);
+      });
+    };
+
+    ctrl.descriptionOption = descriptionEditOptions.overwrite.value;
+
+    ctrl.descriptionOptions = [
+      descriptionEditOptions.overwrite,
+      descriptionEditOptions.prepend,
+      descriptionEditOptions.append
+    ];
+
+    ctrl.updateDescriptionField = function() {
+      ctrl.updateMetadataField('description', ctrl.metadata.description);
+    };
+
+    ctrl.updateMetadataField = function (field, value) {
+      var imageArray = Array.from(ctrl.selectedImages);
+      return editsService.batchUpdateMetadataField(
+        imageArray,
+        field,
+        value,
+        ctrl.descriptionOption
+      );
+    };
+
+    ctrl.addLabel = function (label) {
+      var imageArray = Array.from(ctrl.selectedImages);
+      labelService.batchAdd(imageArray, [label]);
+    };
+
+    ctrl.removeLabel = function (label) {
+      var imageArray = Array.from(ctrl.selectedImages);
+      labelService.batchRemove(imageArray, label);
+    };
+  }
 ]);
