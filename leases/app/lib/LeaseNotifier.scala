51a91c8c8f0574abcb13ee6fca099b969809e326
Jonathon Herbert <jonathon_herbert@34841.gnm.int>
2019-03-05 14:48:42 +0000

    Revert "Added LeaseNotice to common"
    This reverts commit b892d33e6f6f8226bab966d4db4b8ec241d1eb8f.

--- leases/app/lib/LeaseNotifier.scala
+++ leases/app/lib/LeaseNotifier.scala
@@ -7,6 +7,31 @@ import com.gu.mediaservice.model.{LeaseNotice, LeasesByMedia, MediaLease}
 import org.joda.time.DateTime
 import play.api.libs.json._
 
+case class LeaseNotice(mediaId: String, leaseByMedia: JsValue) {
+  def toJson = Json.obj(
+    "id" -> mediaId,
+    "data" -> leaseByMedia,
+    "lastModified" -> printDateTime(DateTime.now())
+  )
+}
+
+object LeaseNotice {
+  import JodaWrites._
+
+  implicit val writer = new Writes[LeasesByMedia] {
+    def writes(leaseByMedia: LeasesByMedia) = {
+      LeasesByMedia.toJson(
+        Json.toJson(leaseByMedia.leases),
+        Json.toJson(leaseByMedia.lastModified)
+      )
+    }
+  }
+  def apply(mediaLease: MediaLease): LeaseNotice = LeaseNotice(
+    mediaLease.mediaId,
+    Json.toJson(LeasesByMedia(List(mediaLease), Some(mediaLease.createdAt)))
+  )
+}
+
 class LeaseNotifier(config: LeasesConfig, store: LeaseStore) extends MessageSender(config, config.topicArn) {
   private def build(mediaId: String, leases: List[MediaLease] ): LeaseNotice = {
     LeaseNotice(mediaId, Json.toJson(LeasesByMedia.build(leases)))
