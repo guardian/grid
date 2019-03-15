9f9f0e5fdedfefeb697ce73f80d59292fa4d9167
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-03-15 15:19:26 +0000

    The metric been past to message consumer bu metadata editor is been used as a message counter not a latency timer; rename it as such.
    Also move to a CountMetric; this was compiling because TimeMetric is also a Long CloudWatch type.

--- metadata-editor/app/lib/MetadataEditorMetrics.scala
+++ metadata-editor/app/lib/MetadataEditorMetrics.scala
@@ -4,6 +4,6 @@ import com.gu.mediaservice.lib.metrics.CloudWatchMetrics
 
 class MetadataEditorMetrics(config: EditsConfig) extends CloudWatchMetrics(s"${config.stage}/MetadataEditor", config) {
 
-  val processingLatency = new TimeMetric("ProcessingLatency")
+  val snsMessage = new CountMetric("SNSMessage")
 
 }
