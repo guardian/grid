2a74ea459bd7b40eb522c2854143e72b9694acb6
David Blishen <david.blishen@guardian.co.uk>
2019-03-20 18:03:15 +0000

    Update SupplierProcessors.scala
    Exclude Replay Images from being identified as Getty due to presence of GettyGIFT XMP

--- common-lib/src/main/scala/com/gu/mediaservice/lib/cleanup/SupplierProcessors.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/cleanup/SupplierProcessors.scala
@@ -174,7 +174,7 @@ trait GettyProcessor {
 object GettyXmpParser extends ImageProcessor with GettyProcessor {
   def apply(image: Image): Image = {
     val excludedCredit = List(
-      "newspix international", "i-images", "photoshot", "Ian Jones", "Panoramic/Avalon", "Avalon", "INS News Agency Ltd", "Discovery.", "EPA"
+      "Replay Images", "newspix international", "i-images", "photoshot", "Ian Jones", "Panoramic/Avalon", "Avalon", "INS News Agency Ltd", "Discovery.", "EPA"
     )
 
     val excludedSource = List(
