1bde01bc80b7d707dc8f4d1436d8dbb029d7c4c4
Michael Barton <michael.barton@guardian.co.uk>
2019-04-05 14:36:23 +0100

    WIP config refactor - kinesis logging

--- common-lib/src/main/scala/com/gu/mediaservice/lib/logging/LogConfig.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/logging/LogConfig.scala
@@ -6,35 +6,32 @@ import com.gu.logback.appender.kinesis.KinesisAppender
 import net.logstash.logback.layout.LogstashLayout
 import org.slf4j.{LoggerFactory, Logger => SLFLogger}
 import play.api.ApplicationLoader.Context
-import play.api.LoggerConfigurator
 import play.api.libs.json._
+import play.api.{Configuration, LoggerConfigurator}
 import scalaz.syntax.id._
-import scala.util.Try
 
+case class LogConfig(stream: String, region: String, roleArn: String, stack: String, stage: String, app: String)
 
 object LogConfig {
 
   val rootLogger: LogbackLogger = LoggerFactory.getLogger(SLFLogger.ROOT_LOGGER_NAME).asInstanceOf[LogbackLogger]
 
-  case class KinesisAppenderConfig(stream: String, region: String, roleArn: String, bufferSize: Int)
-
-  def makeCustomFields(config: CommonConfig): String = {
+  def makeCustomFields(config: LogConfig): String = {
     Json.toJson(Map(
-      "stack" -> config.stackName,
+      "stack" -> config.stack,
       "stage" -> config.stage.toUpperCase,
-      "app"   -> config.appName,
-      "sessionId" -> config.sessionId
+      "app"   -> config.app
     )).toString()
   }
 
   def makeLayout(customFields: String) = new LogstashLayout() <| (_.setCustomFields(customFields))
 
-  def makeKinesisAppender(layout: LogstashLayout, context: LoggerContext, appenderConfig: KinesisAppenderConfig) =
+  def makeKinesisAppender(layout: LogstashLayout, context: LoggerContext, appenderConfig: LogConfig) =
     new KinesisAppender[ILoggingEvent]() <| { a =>
       a.setStreamName(appenderConfig.stream)
       a.setRegion(appenderConfig.region)
       a.setRoleToAssumeArn(appenderConfig.roleArn)
-      a.setBufferSize(appenderConfig.bufferSize)
+      a.setBufferSize(100)
 
       a.setContext(context)
       a.setLayout(layout)
@@ -43,33 +40,33 @@ object LogConfig {
       a.start()
   }
 
-  def initKinesisLogging(config: CommonConfig): Unit = {
-    if (config.isDev) {
-      rootLogger.info("Kinesis logging disabled in DEV")
-    } else {
-      Try {
-        rootLogger.info("LogConfig initializing")
-        rootLogger.info("Configuring Logback")
+  def parseConfig(config: Configuration): Option[LogConfig] = for {
+    stream <- config.getOptional[String]("logger.kinesis.stream")
+    region <- config.getOptional[String]("logger.kinesis.region")
+    roleArn <- config.getOptional[String]("logger.kinesis.roleArn")
+    stack <- config.getOptional[String]("logger.kinesis.stack")
+    stage <- config.getOptional[String]("logger.kinesis.stage")
+    app <- config.getOptional[String]("logger.kinesis.app")
+  } yield {
+    LogConfig(stream, region, roleArn, stack, stage, app)
+  }
+
+  def initKinesisLogging(rawConfig: Configuration): Unit = {
+    parseConfig(rawConfig) match {
+      case Some(config) =>
+        rootLogger.info("Configuring Kinesis logging")
 
         val customFields = makeCustomFields(config)
         val context      = rootLogger.getLoggerContext
         val layout       = makeLayout(customFields)
-        val bufferSize   = 1000
 
-        val appender     = makeKinesisAppender(layout, context,
-          KinesisAppenderConfig(
-            config.properties("logger.kinesis.stream"),
-            config.properties("logger.kinesis.region"),
-            config.properties("logger.kinesis.roleArn"),
-            bufferSize
-          )
-        )
+        val appender     = makeKinesisAppender(layout, context, config)
 
         rootLogger.addAppender(appender)
-        rootLogger.info("Configured Logback")
-      } recover {
-        case e => rootLogger.error("LogConfig Failed!", e)
-      }
+        rootLogger.info("Kinesis logging enabled")
+
+      case None =>
+        rootLogger.info("Kinesis logging disabled")
     }
   }
 
