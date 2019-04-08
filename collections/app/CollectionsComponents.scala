396867a0e369ccc88bf6e9a7ee31d730970d5ec6
Michael Barton <michael.barton@guardian.co.uk>
2019-04-08 09:56:51 +0100

    WIP config refactor - put back collections metrics

--- collections/app/CollectionsComponents.scala
+++ collections/app/CollectionsComponents.scala
@@ -1,9 +1,11 @@
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder
 import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder
 import com.amazonaws.services.sns.AmazonSNSClientBuilder
 import com.gu.mediaservice.lib.aws.{DynamoDB, Kinesis, MessageSender, SNS}
 import com.gu.mediaservice.lib.play.{GridAuthentication, GridComponents}
 import controllers.{CollectionsController, ImageCollectionsController}
+import lib.CollectionsMetrics
 import play.api.ApplicationLoader.Context
 import router.Routes
 import store.CollectionsStore
@@ -12,18 +14,20 @@ class CollectionsComponents(context: Context) extends GridComponents(context) wi
   val dynamoClient = AmazonDynamoDBAsyncClientBuilder.standard().withRegion(region).withCredentials(awsCredentials).build()
   val snsClient = AmazonSNSClientBuilder.standard().withRegion(region).withCredentials(awsCredentials).build()
   val kinesisClient = AmazonKinesisClientBuilder.standard().withRegion(region).withCredentials(awsCredentials).build()
+  val cloudwatchClient = AmazonCloudWatchClientBuilder.standard().withRegion(region).withCredentials(awsCredentials).build()
 
   val collectionsTable = config.get[String]("dynamo.table.collections")
   val imageCollectionsTable = config.get[String]("dynamo.table.imageCollections")
   val snsTopicArn = config.get[String]("sns.topic.arn")
   val thrallKinesisStream = config.get[String]("thrall.kinesis.stream")
+  val cloudwatchMetricsNamespace = config.get[String]("cloudwatch.metrics.namespace")
 
   val sns = new SNS(snsClient, snsTopicArn)
   val kinesis = new Kinesis(kinesisClient, thrallKinesisStream)
 
   val collectionsStore = new CollectionsStore(new DynamoDB(dynamoClient, collectionsTable))
   val imageCollectionsStore = new DynamoDB(dynamoClient, imageCollectionsTable)
-  // TODO MRB: are the collection metrics (deleted here) valuable?
+  val metrics = new CollectionsMetrics(cloudwatchMetricsNamespace, cloudwatchClient)
   val notifications = new MessageSender(sns, kinesis)
 
   val collections = new CollectionsController(auth, services, collectionsStore, controllerComponents)
