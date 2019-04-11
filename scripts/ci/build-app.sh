ee8ed373c84e84618b305ff7202b7da7a4332512
Simon Hildrew <simon@hildrew.net>
2019-04-11 11:42:24 +0100

    Add docker build again

--- scripts/ci/build-app.sh
+++ scripts/ci/build-app.sh
@@ -1,12 +1,24 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ -z "${TEAMCITY_BUILDCONF_NAME}" ]; then
     echo "No TEAMCITY_BUILDCONF_NAME env var set"
     exit 1
 fi
 
+
+docker login -u ${DOCKER_REPO_USER} --password-stdin <<EOF
+${DOCKER_REPO_PASSWORD}
+EOF
+
+function cleanup {
+  docker logout
+}
+
+trap cleanup EXIT
+
 sbt common-lib/clean \
     ${TEAMCITY_BUILDCONF_NAME}/clean \
     common-lib/test \
     ${TEAMCITY_BUILDCONF_NAME}/test \
+    ${TEAMCITY_BUILDCONF_NAME}/docker:publish \
     ${TEAMCITY_BUILDCONF_NAME}/riffRaffUpload
