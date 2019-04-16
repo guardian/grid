7bda22994276e96470a8439a4e54aa34371d828b
Akash A <akash1810@gmail.com>
2019-04-16 12:59:23 +0100

    switch node version in dev-start.sh
    to remove the pain of having to remember to do this manually each time

--- dev-start.sh
+++ dev-start.sh
@@ -86,8 +86,24 @@ downloadApplicationConfig() {
     fi
 }
 
+fileExists() {
+  test -e "$1"
+}
+
+changeNodeVersion() {
+  if ! fileExists "$NVM_DIR/nvm.sh"; then
+    node_version=`cat .nvmrc`
+    echo -e "${red}nvm not found ${plain} NVM is required to run this project"
+    echo -e "Install it from https://github.com/creationix/nvm#installation"
+  else
+    source "$NVM_DIR/nvm.sh"
+    nvm install
+  fi
+}
+
 main() {
     checkRequirements
+    changeNodeVersion
     setupImgops
     downloadApplicationConfig
     startDockerContainers
