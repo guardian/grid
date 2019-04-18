fcf079b3608bc942c2025419c409dd1b33a778fa
Michael Barton <mbarton@users.noreply.github.com>
2019-04-18 12:53:09 +0100

    Revert "Revert "Run docker integration tests using DockerKit""

--- image-loader/app/lib/Downloader.scala
+++ image-loader/app/lib/Downloader.scala
@@ -1,12 +1,11 @@
 package lib
 
-import java.io.{File, FileOutputStream}
+import java.io.File
 import java.net.URI
 import java.nio.file.Files
 
-import com.google.common.hash.{Hashing, HashingOutputStream}
-import com.google.common.io.ByteStreams
 import okhttp3.{OkHttpClient, Request}
+import org.apache.commons.codec.digest.DigestUtils
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -15,7 +14,6 @@ case object TruncatedDownload extends Exception
 //TODO Revisit this logic
 class Downloader(implicit ec: ExecutionContext) {
   private val client = new OkHttpClient()
-  private val digester = Hashing.sha1()
 
   def download(uri: URI, file: File): Future[DigestedFile] = Future {
     val request = new Request.Builder().url(uri.toString).build()
@@ -24,15 +22,9 @@ class Downloader(implicit ec: ExecutionContext) {
     val expectedSize = response.header("Content-Length").toInt
     val input = response.body().byteStream()
 
-    val output = new FileOutputStream(file)
-    val hashedOutput = new HashingOutputStream(digester, output)
-
-    ByteStreams.copy(input, hashedOutput)
-
-    val hash = hashedOutput.hash().asBytes()
-
+    // SHA-1 is deprecated cryptographically but we still use it here for backwards compatibility
+    val hash = DigestUtils.sha1(input)
     input.close()
-    hashedOutput.close()
 
     val actualSize = Files.size(file.toPath)
 
--- thrall/test/syndication/SyndicationRightsOpsElastic6Test.scala
+++ thrall/test/syndication/SyndicationRightsOpsElastic6Test.scala
@@ -1,13 +1,21 @@
 package syndication
 
 import com.gu.mediaservice.lib.elasticsearch6.ElasticSearch6Config
+import com.whisk.docker.{DockerContainer, DockerReadyChecker}
 import lib._
 import play.api.Configuration
+import scala.concurrent.duration._
 
 class SyndicationRightsOpsElastic6Test extends SyndicationRightsOpsTestsBase {
 
   val elasticSearchConfig = ElasticSearch6Config("writeAlias", "localhost", 9206, "media-service-test", 1, 0)
 
   val ES = new ElasticSearch6(elasticSearchConfig, new ThrallMetrics(new ThrallConfig(Configuration.empty)))
-
+  val esContainer = Some(DockerContainer("docker.elastic.co/elasticsearch/elasticsearch:6.6.0")
+    .withPorts(9200 -> Some(9206))
+    .withEnv("cluster.name=media-service", "xpack.security.enabled=false", "discovery.type=single-node", "network.host=0.0.0.0")
+    .withReadyChecker(
+      DockerReadyChecker.HttpResponseCode(9200, "/", Some("0.0.0.0")).within(10.minutes).looped(40, 1250.millis)
+    )
+  )
 }
