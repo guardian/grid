8f1eb57948e6d05e1b643e91f9fb7fbfe7155d8d
Sam Cutler <sam.cutler@guardian.co.uk>
2019-06-13 14:30:19 +0100

    fix issues with apostrophied names in bylines

--- common-lib/src/main/scala/com/gu/mediaservice/lib/cleanup/InitialJoinerByline.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/cleanup/InitialJoinerByline.scala
@@ -6,7 +6,7 @@ object InitialJoinerByline extends MetadataCleaner {
   // Squish together pairs of dangling initials. For example: "C P Scott" -> "CP Scott"
   override def clean(metadata: ImageMetadata): ImageMetadata = {
     metadata.copy(
-      byline = metadata.byline.map(_.replaceAll("\\b(\\p{Lu})\\s(\\p{Lu})\\b", "$1$2"))
+      byline = metadata.byline.map(_.replaceAll("\\b(\\p{Lu})\\s(\\p{Lu}(\\s|$))", "$1$2"))
     )
   }
 }
