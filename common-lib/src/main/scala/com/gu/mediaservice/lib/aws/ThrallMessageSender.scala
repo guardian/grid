49a4ba5a51c9f0e7ed40f46686eb470610d92e38
Akash A <akash1810@gmail.com>
2019-09-11 20:32:54 +0100

    add markers to thrall logs
    To provide more structure for dashboarding.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/ThrallMessageSender.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/ThrallMessageSender.scala
@@ -3,7 +3,10 @@ package com.gu.mediaservice.lib.aws
 import com.gu.mediaservice.lib.config.CommonConfig
 import com.gu.mediaservice.model._
 import com.gu.mediaservice.model.usage.UsageNotice
+import net.logstash.logback.marker.{LogstashMarker, Markers}
 import org.joda.time.DateTime
+
+import scala.collection.JavaConverters._
 
 // TODO MRB: replace this with the simple Kinesis class once we migrate off SNS
 class ThrallMessageSender(config: CommonConfig) {
@@ -27,4 +30,13 @@ case class UpdateMessage(
   mediaLease: Option[MediaLease] = None,
   leases: Option[Seq[MediaLease]] = None,
   syndicationRights: Option[SyndicationRights] = None
-)
+) {
+  def toLogMarker: LogstashMarker = {
+    val markers = Map (
+      "subject" -> subject,
+      "id" -> id.getOrElse(image.map(_.id).getOrElse("none"))
+    )
+
+    Markers.appendEntries(markers.asJava)
+  }
+}
