775c4e3c79a20aa77e706c17179ac3d53f5691d2
Akash A <akash1810@gmail.com>
2019-09-20 18:21:51 +0100

    add json gzip compression utils

---
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/json/JsonByteArrayUtilTest.scala
@@ -0,0 +1,47 @@
+package com.gu.mediaservice.lib.json
+
+import org.scalatest.{FunSuite, Matchers}
+import play.api.libs.json.Json
+
+case class Shape(name: String, numberOfSides: Int)
+
+object Shape {
+  implicit val reads = Json.reads[Shape]
+  implicit val writes = Json.writes[Shape]
+}
+
+class JsonByteArrayUtilTest extends FunSuite with Matchers {
+  val circle = Shape("circle", 1)
+  val triangle = Shape("triangle", 3)
+  val square = Shape("square", 4)
+
+  test("To byte array and back again") {
+    val bytes = JsonByteArrayUtil.toByteArray(circle)
+    JsonByteArrayUtil.hasCompressionMarker(bytes) shouldBe false
+    JsonByteArrayUtil.fromByteArray[Shape](bytes) shouldBe Some(circle)
+  }
+
+  test("To compressed byte array and back again") {
+    val bytes = JsonByteArrayUtil.toByteArray(triangle, withCompression = true)
+    JsonByteArrayUtil.hasCompressionMarker(bytes) shouldBe true
+    JsonByteArrayUtil.fromByteArray[Shape](bytes) shouldBe Some(triangle)
+  }
+
+  test("From byte array into different type") {
+    val bytes = JsonByteArrayUtil.toByteArray(square)
+    JsonByteArrayUtil.fromByteArray[String](bytes) shouldBe None
+  }
+
+  test("Compressing... compresses") {
+    // gzip compression is only effective above a certain length
+    // compressing `circle` by itself results in a longer byte array 😅
+    val shapes = List(circle, triangle, square)
+
+    val uncompressedBytes = JsonByteArrayUtil.toByteArray(shapes, withCompression = false)
+    val compressedBytes = JsonByteArrayUtil.toByteArray(shapes, withCompression = true)
+    compressedBytes.length < uncompressedBytes.length shouldBe true
+
+    JsonByteArrayUtil.fromByteArray[List[Shape]](uncompressedBytes) shouldBe Some(shapes)
+    JsonByteArrayUtil.fromByteArray[List[Shape]](compressedBytes) shouldBe Some(shapes)
+  }
+}
