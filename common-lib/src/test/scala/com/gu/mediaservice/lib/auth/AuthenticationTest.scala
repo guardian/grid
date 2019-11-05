009529393c1f06f8e35bb7cc663994f3fb6a04cd
jamie-pitts <jamie.pitts@bbc.co.uk>
2019-11-05 17:16:52 +0000

    fixed broken tests

--- common-lib/src/test/scala/com/gu/mediaservice/lib/auth/AuthenticationTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/auth/AuthenticationTest.scala
@@ -11,15 +11,16 @@ class AuthenticationTest extends FunSuite with MustMatchers {
   val email = "barry.chuckle@guardian.co.uk"
   val emailList =  Some(List(email))
   val user = AuthenticatedUser(User("Barry", "Chuckle", email, None),
-    "media-service", Set("media-service"), Instant.now().plusSeconds(100).toEpochMilli, multiFactor = true)
+    "media-service", Set("media-service"), Instant.now().plusSeconds(100).toEpochMilli, multiFactor = true, Set("Grid Access"))
 
   test("user fails email domain validation") {
     validateUser(user, "chucklevision.biz", None, emailList) must be(false)
   }
-
-  test("user fails email validation") {
-    validateUser(user, "guardian.co.uk", None, Some(List("valid.email@guardian.co.uk"))) must be(false)
-  }
+  
+// replaced with PP integration
+//  test("user fails email validation") {
+//    validateUser(user, "guardian.co.uk", None, Some(List("valid.email@guardian.co.uk"))) must be(false)
+//  }
 
   test("user passes email domain validation") {
     validateUser(user, "guardian.co.uk", None, emailList) must be(true)
