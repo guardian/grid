aea6b913e4d87343b10b6dc1bf6028df5ddd6457
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2019-11-06 14:01:00 +0000

    refining scripts

--- dev-start.sh
+++ dev-start.sh
@@ -113,8 +113,8 @@ setupLocalKinesis() {
   # ignore stream already exists error
   set +e
   stream_name='media-service-DEV-ThrallMessageQueue-1N0T2UXYNUIC9'
-  aws --profile media-service --endpoint-url=http://localhost:4568 kinesis create-stream --shard-count 1 --stream-name "${stream_name}"
-  aws --profile media-service --endpoint-url=http://localhost:4568 kinesis --region=eu-west-2 list-streams
+  aws --profile media-service --region=eu-west-1 --endpoint-url=http://localhost:4568 kinesis create-stream --shard-count 1 --stream-name "${stream_name}"
+  aws --profile media-service --region=eu-west-1 --endpoint-url=http://localhost:4568 kinesis list-streams
 }
 
 main() {
