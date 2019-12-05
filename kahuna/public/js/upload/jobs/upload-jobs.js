181345c882b32bc86b861feeca8361d82058535d
Alex Ware <alex.ware@guardian.co.uk>
2019-12-05 11:26:13 +0000

    Add promise queueing to try and make batch operations faster

--- kahuna/public/js/upload/jobs/upload-jobs.js
+++ kahuna/public/js/upload/jobs/upload-jobs.js
@@ -1,178 +1,195 @@
-import angular from 'angular';
-import template from './upload-jobs.html';
-import '../../preview/image';
-import '../../components/gr-delete-image/gr-delete-image';
-import '../../image/service';
-import '../../edits/service';
-import '../../services/label';
-import '../../services/preset-label';
+import angular from "angular";
+import template from "./upload-jobs.html";
+import "../../preview/image";
+import "../../components/gr-delete-image/gr-delete-image";
+import "../../image/service";
+import "../../edits/service";
+import "../../services/label";
+import "../../services/preset-label";
 
-export var jobs = angular.module('kahuna.upload.jobs', [
-    'kahuna.preview.image',
-    'gr.image.service',
-    'kahuna.services.label',
-    'kahuna.services.presetLabel',
-    'kahuna.edits.service'
+export var jobs = angular.module("kahuna.upload.jobs", [
+  "kahuna.preview.image",
+  "gr.image.service",
+  "kahuna.services.label",
+  "kahuna.services.presetLabel",
+  "kahuna.edits.service"
 ]);
 
+jobs.controller("UploadJobsCtrl", [
+  "$rootScope",
+  "$scope",
+  "$window",
+  "apiPoll",
+  "imageService",
+  "labelService",
+  "presetLabelService",
+  "editsService",
 
-jobs.controller('UploadJobsCtrl', [
-    '$rootScope',
-    '$scope',
-    '$window',
-    'apiPoll',
-    'imageService',
-    'labelService',
-    'presetLabelService',
-    'editsService',
-
-    function($rootScope,
-            $scope,
-            $window,
-            apiPoll,
-            imageService,
-            labelService,
-            presetLabelService,
-            editsService) {
-
+  function(
+    $rootScope,
+    $scope,
+    $window,
+    apiPoll,
+    imageService,
+    labelService,
+    presetLabelService,
+    editsService
+  ) {
     var ctrl = this;
     const presetLabels = presetLabelService.getLabels();
 
     // State machine-esque async transitions
-    const eventName = 'Image upload';
+    const eventName = "Image upload";
 
-    ctrl.jobs.forEach(jobItem => {
-        jobItem.status = 'uploading';
-
-        jobItem.resourcePromise.then(resource => {
-            jobItem.status = 'indexing';
+    const uploads = ctrl.jobs.map(jobItem => {
+      jobItem.status = "uploading";
+      return () => {
+        jobItem.resourcePromise.then(
+          resource => {
+            jobItem.status = "indexing";
             jobItem.resource = resource;
 
             // TODO: grouped polling for all resources we're interested in?
             const findImage = () => resource.get();
             const imageResource = apiPoll(findImage);
 
-            imageResource.then(image => {
-                jobItem.status = 'uploaded';
+            imageResource.then(
+              image => {
+                jobItem.status = "uploaded";
                 jobItem.image = image;
                 jobItem.thumbnail = image.data.thumbnail;
 
                 imageService(image).states.canDelete.then(deletable => {
-                    jobItem.canBeDeleted = deletable;
+                  jobItem.canBeDeleted = deletable;
                 });
 
                 // TODO: we shouldn't have to do this ;_;
                 // If the image is updated (e.g. label added,
                 // archived, etc), refresh the copy we hold
-                $rootScope.$on('image-updated', (e, updatedImage) => {
-                    if (updatedImage.data.id === image.data.id) {
-                        jobItem.image = updatedImage;
-                    }
+                $rootScope.$on("image-updated", (e, updatedImage) => {
+                  if (updatedImage.data.id === image.data.id) {
+                    jobItem.image = updatedImage;
+                  }
                 });
 
                 // we use the filename of the image if the description is missing
                 if (!jobItem.image.data.metadata.description) {
-                    const newDescription = jobItem.name
-                        .substr(0, jobItem.name.lastIndexOf('.'))
-                        .replace(/_/g, ' ');
+                  const newDescription = jobItem.name
+                    .substr(0, jobItem.name.lastIndexOf("."))
+                    .replace(/_/g, " ");
 
-                    editsService.updateMetadataField(jobItem.image, 'description', newDescription);
+                  editsService.updateMetadataField(
+                    jobItem.image,
+                    "description",
+                    newDescription
+                  );
                 }
 
                 if (presetLabels.length > 0) {
-                    labelService.add(image, presetLabels);
+                  labelService.add(image, presetLabels);
                 }
 
                 $rootScope.$emit(
-                  'track:event',
+                  "track:event",
                   eventName,
                   null,
-                  'Success',
+                  "Success",
                   null,
-                  { 'Labels' : presetLabels.length}
+                  { Labels: presetLabels.length }
                 );
-            }, error => {
-                jobItem.status = 'upload error';
+              },
+              error => {
+                jobItem.status = "upload error";
                 jobItem.error = error.message;
 
                 $rootScope.$emit(
-                  'track:event',
+                  "track:event",
                   eventName,
                   null,
-                  'Failure',
+                  "Failure",
                   null,
-                  { 'Failed on': 'index'}
+                  { "Failed on": "index" }
                 );
-            });
-        }, error => {
+              }
+            );
+          },
+          error => {
             const reason = error.body && error.body.errorKey;
 
-            const message = reason === 'unsupported-type' ?
-                'The Grid only supports JPG and PNG images.' +
-                ' Please convert the image and try again.' :
-                error.body && error.body.errorMessage || 'unknown';
+            const message =
+              reason === "unsupported-type"
+                ? "The Grid only supports JPG and PNG images." +
+                  " Please convert the image and try again."
+                : (error.body && error.body.errorMessage) || "unknown";
 
-            jobItem.status = 'upload error';
+            jobItem.status = "upload error";
             jobItem.error = message;
 
-            $rootScope.$emit(
-              'track:event',
-              eventName,
-              null,
-              'Failure',
-              null,
-              { 'Failed on': 'upload'}
-            );
-        });
+            $rootScope.$emit("track:event", eventName, null, "Failure", null, {
+              "Failed on": "upload"
+            });
+          }
+        );
+      };
     });
-
+    pAll(uploads, { concurrency: 2 });
     // this needs to be a function due to the stateful `jobItem`
     ctrl.jobImages = () => ctrl.jobs.map(jobItem => jobItem.image);
 
-    ctrl.removeJob = (job) => {
-        const index = ctrl.jobs.findIndex(j => j.name === job.name);
+    ctrl.removeJob = job => {
+      const index = ctrl.jobs.findIndex(j => j.name === job.name);
 
-        if (index > -1) {
-            ctrl.jobs.splice(index, 1);
-        }
+      if (index > -1) {
+        ctrl.jobs.splice(index, 1);
+      }
     };
 
-    const freeImageDeleteListener = $rootScope.$on('images-deleted', (e, images) => {
+    const freeImageDeleteListener = $rootScope.$on(
+      "images-deleted",
+      (e, images) => {
         images.forEach(image => {
-            var index = ctrl.jobs.findIndex(i => i.image.data.id === image.data.id);
+          var index = ctrl.jobs.findIndex(
+            i => i.image.data.id === image.data.id
+          );
 
-            if (index > -1) {
-                ctrl.jobs.splice(index, 1);
-            }
+          if (index > -1) {
+            ctrl.jobs.splice(index, 1);
+          }
         });
-    });
+      }
+    );
 
-    const freeImageDeleteFailListener = $rootScope.$on('image-delete-failure', (err, image) => {
+    const freeImageDeleteFailListener = $rootScope.$on(
+      "image-delete-failure",
+      (err, image) => {
         if (err.body && err.body.errorMessage) {
-            $window.alert(err.body.errorMessage);
+          $window.alert(err.body.errorMessage);
         } else {
-            $window.alert(`Failed to delete image ${image.data.id}`);
+          $window.alert(`Failed to delete image ${image.data.id}`);
         }
+      }
+    );
+
+    $scope.$on("$destroy", function() {
+      freeImageDeleteListener();
+      freeImageDeleteFailListener();
     });
+  }
+]);
 
-    $scope.$on('$destroy', function() {
-        freeImageDeleteListener();
-        freeImageDeleteFailListener();
-    });
-}]);
-
-
-jobs.directive('uiUploadJobs', [function() {
+jobs.directive("uiUploadJobs", [
+  function() {
     return {
-        restrict: 'E',
-        scope: {
-            // Annoying that we can't make a uni-directional binding
-            // as we don't really want to modify the original
-            jobs: '='
-        },
-        controller: 'UploadJobsCtrl',
-        controllerAs: 'ctrl',
-        bindToController: true,
-        template: template
+      restrict: "E",
+      scope: {
+        // Annoying that we can't make a uni-directional binding
+        // as we don't really want to modify the original
+        jobs: "="
+      },
+      controller: "UploadJobsCtrl",
+      controllerAs: "ctrl",
+      bindToController: true,
+      template: template
     };
-}]);
+  }
+]);
--- kahuna/public/js/util/batch-tracking.js
+++ kahuna/public/js/util/batch-tracking.js
@@ -1,18 +1,25 @@
 // TODO MRB: invoke function lazily, does it improve UI jank?
+import { pAll } from "p-all";
 export function trackAll($rootScope, key, input, fn) {
-    let completed = 0;
-    $rootScope.$broadcast("events:batch-operations:start",
-        { key, completed: 0, total: input.size ? input.size : input.length });
+  let completed = 0;
+  $rootScope.$broadcast("events:batch-operations:start", {
+    key,
+    completed: 0,
+    total: input.size ? input.size : input.length
+  });
 
-    const results = input.map(item => {
-        return fn(item).then(result => {
-            completed++;
-            $rootScope.$broadcast("events:batch-operations:progress", { key, completed });
-            return result;
-        });
-    });
+  const results = input.map(item => () =>
+    fn(item).then(result => {
+      completed++;
+      $rootScope.$broadcast("events:batch-operations:progress", {
+        key,
+        completed
+      });
+      return result;
+    })
+  );
 
-    return Promise.all(results).finally(() => {
-        $rootScope.$broadcast("events:batch-operations:complete", { key });
-    });
+  return pAll(results, { concurrency: 2 }).finally(() => {
+    $rootScope.$broadcast("events:batch-operations:complete", { key });
+  });
 }
