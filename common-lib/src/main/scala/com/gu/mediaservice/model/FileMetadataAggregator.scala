40f02a96b7d2ef7915f44a11b444b3702d6ba6bd
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2019-12-27 14:05:07 +0000

    preserving original ordering in array values

--- common-lib/src/main/scala/com/gu/mediaservice/model/FileMetadataAggregator.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/model/FileMetadataAggregator.scala
@@ -19,10 +19,28 @@ object FileMetadataAggregator {
   }
 
   private def entryToAggregatedKeyAndJsValue(k: String, v: JsValue): (String, JsValue) = {
-   if (isArrayKey(k)) (normaliseArrayKey(k), JsArray(Seq(v))) else if (isDynamicObjectKey(k)) toObjectNameAndValue(k, v) else (k, v)
+    if (isArrayKey(k)) (normaliseArrayKey(k), JsArray(Seq(v))) else if (isDynamicObjectKey(k)) toObjectNameAndValue(k, v)
+    else (k, v)
   }
 
-  def aggregateCurrentMetadataLevel(nodes: Map[String, JsValue]): Map[String, JsValue] = {
+  private def sortAggregatedValuesAtLevel(nodes: Map[String, JsValue], arrayNormKeyValuePairToIdx: Map[String, Int]) = {
+    nodes.map {
+      case (k, v) =>
+        val sortedValues = if (v.isInstanceOf[JsArray]) {
+          val sorted = v.as[JsArray].value.map(item => {
+            val idx = if (item.isInstanceOf[JsString]) {
+              val entryValue = item.as[JsString].value
+              arrayNormKeyValuePairToIdx.getOrElse(s"$k-$entryValue", Int.MaxValue)
+            } else Int.MaxValue
+            (item, idx)
+          }).sortBy(_._2).map(_._1)
+          JsArray(sorted)
+        } else v
+        k -> sortedValues
+    }
+  }
+
+  private def aggregateCurrentMetadataLevel(nodes: Map[String, JsValue], arrayNormKeyValuePairToIdx: Map[String, Int]): Map[String, JsValue] = {
 
     val mutableMap = scala.collection.mutable.Map[String, JsValue]()
 
@@ -38,20 +56,25 @@ object FileMetadataAggregator {
         }
     }
 
-    mutableMap.toMap
+    sortAggregatedValuesAtLevel(mutableMap.toMap, arrayNormKeyValuePairToIdx)
   }
 
   def aggregateMetadataMap(flatProperties: Map[String, String]): Map[String, JsValue] = {
 
+    val arrayNormKeyValuePairToIdx = flatProperties.filter { case (k, _) => isArrayKey(k) }.map { case (k, v) =>
+      val idx = k.substring(k.lastIndexOf("[") + 1, k.lastIndexOf("]")).trim.toInt
+      s"${normaliseArrayKey(k)}-$v" -> idx
+    }
+
     val initialMetadataStructure = flatProperties.map { case (k, v) => k -> JsString(v) }
 
-    var aggMetadata = aggregateCurrentMetadataLevel(initialMetadataStructure)
+    var aggMetadata = aggregateCurrentMetadataLevel(initialMetadataStructure, arrayNormKeyValuePairToIdx)
 
     def anyKeyIsArrayKey(keys: Set[String]) = keys.exists(isArrayKey)
 
     def anyKeyIsDynamicObjectKey(keys: Set[String]) = keys.exists(isDynamicObjectKey)
 
-    while (anyKeyIsArrayKey(aggMetadata.keySet) || anyKeyIsDynamicObjectKey(aggMetadata.keySet)) aggMetadata = aggregateCurrentMetadataLevel(aggMetadata)
+    while (anyKeyIsArrayKey(aggMetadata.keySet) || anyKeyIsDynamicObjectKey(aggMetadata.keySet)) aggMetadata = aggregateCurrentMetadataLevel(aggMetadata, arrayNormKeyValuePairToIdx)
 
     aggMetadata
   }
