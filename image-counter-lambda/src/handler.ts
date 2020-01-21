93ad2c46df14ea58009be3ad409c1153e4e5266e
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2020-01-21 15:05:50 +0000

    Successfully sending metric to CloudWatch
    Co-authored-by: Stephen Geller <stephen.gb.geller+github@gmail.com>

--- image-counter-lambda/src/handler.ts
+++ image-counter-lambda/src/handler.ts
@@ -1,12 +1,15 @@
 import getCredentials from "./getCredentials";
 import fetch from "node-fetch";
+import AWS from "aws-sdk";
 
-interface AWSCredentials {
+interface MediaAPICredentials {
   baseUrl: string;
   "X-Gu-Media-Key": string;
 }
 
-const getImageCount = async (credentials: AWSCredentials): Promise<number> => {
+const getImageCount = async (
+  credentials: MediaAPICredentials
+): Promise<number> => {
   const response = await fetch(credentials.baseUrl + "/images", {
     headers: {
       "X-Gu-Media-Key": credentials["X-Gu-Media-Key"]
@@ -16,17 +19,37 @@ const getImageCount = async (credentials: AWSCredentials): Promise<number> => {
   return images.total;
 };
 
+const metric = (value: number) => ({
+  MetricData: [
+    {
+      MetricName: "ImageCount",
+      Unit: "Count",
+      Value: value
+    }
+  ],
+  Namespace: "PROD/MediaApi"
+});
+
 const handler = async (): Promise<{ statusCode: number; body: string }> => {
-  // get credentials
-  const credentials = await getCredentials();
+  try {
+    // get credentials
+    const credentials = await getCredentials();
 
-  // query media api with credentials
-  const images = await getImageCount(credentials);
+    // query media api with credentials
+    const images = await getImageCount(credentials);
 
-  // post it to CW as metric
+    // post it to CW as metric
+    const cloudwatch = new AWS.CloudWatch({ region: "eu-west-1" });
+    await cloudwatch
+      .putMetricData(metric(images))
+      .promise()
 
-  // return happy lambda response to caller
-  return { statusCode: 200, body: "Metric sent" };
+    // return happy lambda response to caller
+    return { statusCode: 200, body: "Metric sent" };
+  } catch (err) {
+    console.error(err);
+    return { statusCode: 500, body: `Error sending metric: ${err.message}` };
+  }
 };
 
 const fns = { getCredentials, handler, getImageCount };
