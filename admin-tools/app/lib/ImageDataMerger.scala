b698789e295469f063a08ed98e44ee918d436580
Akash Askoolum <akash1810@gmail.com>
2020-01-28 17:57:24 +0000

    get collections

--- admin-tools/app/lib/ImageDataMerger.scala
+++ admin-tools/app/lib/ImageDataMerger.scala
@@ -1,10 +1,12 @@
 package lib
 
+import java.net.URL
+
 import com.gu.mediaservice.lib.auth.Authentication
-import com.gu.mediaservice.model.Image
+import com.gu.mediaservice.model.{Collection, Image}
 import com.gu.mediaservice.model.Image._
 import okhttp3.{OkHttpClient, Request}
-import play.api.libs.json.Json
+import play.api.libs.json._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -14,13 +16,25 @@ class ImageDataMerger(config: AdminToolsConfig)(implicit ec: ExecutionContext) {
   def getMergedImageData(mediaId: String): Future[Image] = {
     for {
       image <- getImageLoaderProjection(mediaId)
-    } yield image
+      collections <- getCollectionsResponse(mediaId)
+    } yield image.copy(
+      collections = collections
+    )
+  }
+
+  private def makeRequest (url: URL): JsValue = {
+    val request = new Request.Builder().url(url).header(Authentication.apiKeyHeaderName, config.apiKey).build
+    val response = client.newCall(request).execute
+    Json.parse(response.body.string)
   }
 
   private def getImageLoaderProjection(mediaId: String): Future[Image] = Future{
-    val url = s"${config.services.loaderBaseUri}/images/project/${mediaId}"
-    val request = new Request.Builder().url(url).header(Authentication.apiKeyHeaderName, config.apiKey).build
-    val response = client.newCall(request).execute
-    Json.parse(response.body.string).as[Image]
+    val url = new URL(s"${config.services.loaderBaseUri}/images/project/${mediaId}")
+    makeRequest(url).as[Image]
+  }
+
+  private def getCollectionsResponse(mediaId: String): Future[List[Collection]] = Future{
+    val url = new URL(s"${config.services.collectionsBaseUri}/images/${mediaId}")
+    (makeRequest(url) \ "data").as[List[Collection]]
   }
 }
