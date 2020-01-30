8e86f4e526297c1e560dc476652431376325470c
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-01-30 16:43:32 +0000

    admin-tool lambda execution initial work

--- admin-tools/lambda/src/main/scala/com/gu/mediaservice/LambdaHandler.scala
+++ admin-tools/lambda/src/main/scala/com/gu/mediaservice/LambdaHandler.scala
@@ -1,27 +1,42 @@
 package com.gu.mediaservice
 
+import play.api.libs.json.Json
+
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 class LambdaHandler {
 
   def handleImageProjection(mediaId: String) = {
 
     val cfg = ImageDataMergerConfig(
-      apiKey = "TODO",
-      imgLoaderApiBaseUri = "TODO",
-      collectionsApiBaseUri = "TODO",
-      metadataApiBaseUri = "TODO",
-      cropperApiBaseUri = "TODO",
-      leasesApiBaseUri = "TODO",
-      usageBaseApiUri = "TODO"
+      apiKey = sys.env("API_KEY"),
+      imgLoaderApiBaseUri = "https://loader.media.test.dev-gutools.co.uk",
+      collectionsApiBaseUri = "https://media-collections.test.dev-gutools.co.uk",
+      metadataApiBaseUri = "https://media-metadata.test.dev-gutools.co.uk",
+      cropperApiBaseUri = "https://cropper.media.test.dev-gutools.co.uk",
+      leasesApiBaseUri = "https://media-leases.test.dev-gutools.co.uk",
+      usageBaseApiUri = "https://media-usage.test.dev-gutools.co.uk"
     )
+
+    println(s"starting handleImageProjection for mediaId=$mediaId")
+    println(s"with config: $cfg")
 
     val merger = new ImageDataMerger(cfg)
 
-    val image = merger.getMergedImageData(mediaId)
+    val maybeImageFuture = merger.getMergedImageData(mediaId)
 
-    println(s"image projected \n $image")
+    val maybeJson = maybeImageFuture.map { imgFuture =>
+      val img = Await.result(imgFuture, Duration.Inf)
+      Json.toJson(img)
+    }
 
-    image
+    maybeJson match {
+      case Some(img) =>
+        println(s"image projected \n $img")
+        img
+      case _ => Json.obj("message" -> s"image with id=$mediaId not-found")
+    }
   }
 }
