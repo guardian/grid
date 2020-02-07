738d0ae08e2cfb3fc9b4753246ea20cd51213fd9
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-02-07 16:29:09 +0000

    cleanup

--- admin-tools/dev/src/main/scala/com/gu/mediaservice/BatchIndexLocalHandler.scala
+++ admin-tools/dev/src/main/scala/com/gu/mediaservice/BatchIndexLocalHandler.scala
@@ -20,16 +20,6 @@ object BatchIndexLocalHandler extends App {
 
   private val batchIndex = BatchIndexHandler(cfg)
 
-  // ids that exists on your local grid
-  val mediaIds = List(
-    "011db3facffd4ef3d3e07eabbd3c07bb41cbf819",
-    "011a95f670b71e4dd223f4b8eda5bfb41235ca7e",
-    "066d163d11dfdc8223fb9a15fbf5265f0e47655b",
-    "d23c0faaed970d9769e67d7ed4eefbbb35b6fccc",
-    "d23c0e19cdc10a7573d4c712313a105863e998c3",
-    "non-existent"
-  )
-
   batchIndex.processImages()
 
 }
