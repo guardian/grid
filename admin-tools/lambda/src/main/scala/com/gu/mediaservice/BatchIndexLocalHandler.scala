c74db7b00e9fad28718a1138630dfe60dd06325b
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-02-10 15:45:18 +0000

    using env vars in local run

--- admin-tools/lambda/src/main/scala/com/gu/mediaservice/BatchIndexLocalHandler.scala
+++ admin-tools/lambda/src/main/scala/com/gu/mediaservice/BatchIndexLocalHandler.scala
@@ -5,11 +5,11 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object BatchIndexLocalHandler extends App {
 
   private val cfg = BatchIndexHandlerConfig(
-    apiKey = "dev-",
-    domainRoot = "local.dev-gutools.co.uk",
-    batchIndexBucket = "media-service-admin-tools-dev",
-    kinesisStreamName = "media-service-DEV-ThrallMessageQueue-1N0T2UXYNUIC9",
-    dynamoTableName = "grid-images-to-reingest-DEV",
+    apiKey = sys.env("API_KEY"),
+    domainRoot = sys.env("DOMAIN_ROOT"),
+    batchIndexBucket = sys.env("BATCH_INDEX_BUCKET"),
+    kinesisStreamName = sys.env("KINESIS_STREAM"),
+    dynamoTableName = sys.env("IMAGES_TO_INDEX_DYNAMO_TABLE"),
     batchSize = 3,
     kinesisEndpoint = Some("http://localhost:4568")
   )
