c4b0c28f7393adaf7ffbf1b98ab8e559d3480aa5
Mateusz <mateusz.karpow@guardian.co.uk>
2020-02-12 15:30:23 +0000

    Test applying StripCopyrightPrefix only to Credit and Byline fields

--- common-lib/src/test/scala/com/gu/mediaservice/lib/cleanup/StripCopyrightPrefixTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/cleanup/StripCopyrightPrefixTest.scala
@@ -10,64 +10,62 @@ class StripCopyrightPrefixTest extends FunSpec with Matchers with MetadataHelper
     cleanedMetadata.copyright should be (None)
   }
 
-  it("should leave unprefixed copyright as-is") {
-    val metadata = createImageMetadata("copyright" -> "Acme Corporation")
+  it("should leave unprefixed byline as-is") {
+    val metadata = createImageMetadata("credit" -> "Acme Corporation")
     val cleanedMetadata = StripCopyrightPrefix.clean(metadata)
-    cleanedMetadata.copyright should be (Some("Acme Corporation"))
+    cleanedMetadata.credit should be (Some("Acme Corporation"))
   }
 
   it("should strip any copyright symbol prefix") {
-    val metadata = createImageMetadata("copyright" -> "© Acme Corporation")
+    val metadata = createImageMetadata("credit" -> "© Acme Corporation")
     val cleanedMetadata = StripCopyrightPrefix.clean(metadata)
-    cleanedMetadata.copyright should be (Some("Acme Corporation"))
+    cleanedMetadata.credit should be (Some("Acme Corporation"))
   }
 
   it("should strip any Copyright text prefix") {
-    val metadata = createImageMetadata("copyright" -> "Copyright Acme Corporation")
+    val metadata = createImageMetadata("credit" -> "Copyright Acme Corporation")
     val cleanedMetadata = StripCopyrightPrefix.clean(metadata)
-    cleanedMetadata.copyright should be (Some("Acme Corporation"))
+    cleanedMetadata.credit should be (Some("Acme Corporation"))
   }
 
   it("should strip lowercase copyright prefix") {
-    val metadata = createImageMetadata("copyright" -> "copyright Acme Corporation")
+    val metadata = createImageMetadata("credit" -> "copyright Acme Corporation")
     val cleanedMetadata = StripCopyrightPrefix.clean(metadata)
-    cleanedMetadata.copyright should be (Some("Acme Corporation"))
+    cleanedMetadata.credit should be (Some("Acme Corporation"))
   }
 
   it("should strip Copyright of prefix") {
-    val metadata = createImageMetadata("copyright" -> "Copyright of Acme Corporation")
+    val metadata = createImageMetadata("credit" -> "Copyright of Acme Corporation")
     val cleanedMetadata = StripCopyrightPrefix.clean(metadata)
-    cleanedMetadata.copyright should be (Some("Acme Corporation"))
+    cleanedMetadata.credit should be (Some("Acme Corporation"))
   }
 
   it("should strip copyright followed by colon prefix") {
-    val metadata = createImageMetadata("copyright" -> "Copyright : Acme Corporation")
+    val metadata = createImageMetadata("credit" -> "Copyright : Acme Corporation")
     val cleanedMetadata = StripCopyrightPrefix.clean(metadata)
-    cleanedMetadata.copyright should be (Some("Acme Corporation"))
+    cleanedMetadata.credit should be (Some("Acme Corporation"))
   }
 
   it("should strip any (c) prefix") {
-    val metadata = createImageMetadata("copyright" -> "(c) Acme Corporation")
+    val metadata = createImageMetadata("credit" -> "(c) Acme Corporation")
     val cleanedMetadata = StripCopyrightPrefix.clean(metadata)
-    cleanedMetadata.copyright should be (Some("Acme Corporation"))
+    cleanedMetadata.credit should be (Some("Acme Corporation"))
   }
 
   it("should strip a combination of copyright prefixes") {
-    val metadata = createImageMetadata("copyright" -> "Copyright (c) Acme Corporation")
+    val metadata = createImageMetadata("credit" -> "Copyright (c) Acme Corporation")
     val cleanedMetadata = StripCopyrightPrefix.clean(metadata)
-    cleanedMetadata.copyright should be (Some("Acme Corporation"))
+    cleanedMetadata.credit should be (Some("Acme Corporation"))
   }
 
-  it("should strip these from byline, credit and copyright") {
+  it("should strip these from byline and credit") {
     val metadata = createImageMetadata(
       "byline" -> "© Acme Corporation",
       "credit" -> "© Acme Corporation",
-      "copyright" -> "© Acme Corporation"
     )
     val cleanedMetadata = StripCopyrightPrefix.clean(metadata)
     cleanedMetadata.byline should be (Some("Acme Corporation"))
     cleanedMetadata.credit should be (Some("Acme Corporation"))
-    cleanedMetadata.copyright should be (Some("Acme Corporation"))
   }
 
   it("should leave these in fields like description") {
