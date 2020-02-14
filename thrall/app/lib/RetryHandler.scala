8d836e3f0e2f8cfa63280006bdafeffc05baf919
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-02-14 16:53:44 +0000

    Update thrall/app/lib/RetryHandler.scala

--- thrall/app/lib/RetryHandler.scala
+++ thrall/app/lib/RetryHandler.scala
@@ -54,7 +54,6 @@ object RetryHandler {
       }
       retry(attempt, retries, delay)
     }
-    println("HELLO I AM DOING SOMETHING RETRY RELATED")
     handleWithRetry(handleWithTimeout(logFailures(f), timeout), retries, delay)
   }
 }
