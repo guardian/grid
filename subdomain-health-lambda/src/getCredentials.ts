660ebce9b06c8199134f1ab7e3831500a79201c5
stephengeller <stephen.geller@hotmail.com>
2020-02-18 16:20:58 +0000

    basic functioanlity working

--- subdomain-health-lambda/src/getCredentials.ts
+++ subdomain-health-lambda/src/getCredentials.ts
@@ -6,11 +6,18 @@ async function getCredentials() {
     Bucket: "grid-conf",
     Key: `${process.env.STAGE}/subdomain-health-lambda/conf.json`
   };
+  console.log("Getting credentials, params:", params);
+  try {
+    const data = await s3
+      .getObject(params)
+      .promise()
+    const body = data.Body?.toString("utf8") || "";
 
-  const data = await s3.getObject(params).promise();
-  const body = data.Body?.toString("utf8") || "";
-
-  return JSON.parse(body);
+    return JSON.parse(body);
+  } catch (err) {
+    console.error('getCredentials error', err)
+    throw err
+  }
 }
 
 export default getCredentials;
--- subdomain-health-lambda/src/handler.ts
+++ subdomain-health-lambda/src/handler.ts
@@ -28,40 +28,67 @@ const getImageCount = async (
 const metric = (key: string, value: number) => ({
   MetricData: [
     {
-      MetricName: "ImageCount-" + key,
-      Unit: "Count",
+      MetricName: `${key}-Response`,
+      Unit: "StatusCode",
       Value: value
     }
   ],
-  Namespace: `${process.env.STAGE}/MediaApi`
+  Namespace: `${process.env.STAGE}/Subdomains`
 });
+
+const getResponse = async (
+  domain: string
+): Promise<{
+  domain: string;
+  response: number;
+}> => {
+  const result = await fetch(domain);
+  return {
+    domain,
+    response: result.status
+  };
+};
 
 export const handler = async (): Promise<{
   statusCode: number;
   body: string;
 }> => {
-  // get credentials
-  const credentials = await getCredentials();
+  console.log("Starting up!");
+  // TODO: Make it so the credentials return objects of the {name: string, domain: string} type
+  // so that we can pass it as a metric name.
+  // Currently, metric name is just the domain
+  const credentials: { subdomains: string[] } = await getCredentials();
+  const { subdomains } = credentials;
+  console.log("subdomains", subdomains);
 
-  // query media api with credentials
-  const images = await getImageCount(credentials);
-  console.log("image counts", images);
-
-  // post it to CW as metric
   const client = new CloudWatch({ region: "eu-west-1" });
 
-  for (const key of Object.keys(images)) {
-    const met = metric(key.toUpperCase(), images[key]);
-    await client.putMetricData(met).promise();
-  }
+  await Promise.all(
+    subdomains
+      .map(async domain => await getResponse(domain))
+      .map(async response => {
+        const d = await response;
+        const met = metric(d.domain, d.response);
+        console.log("logging metric:", met);
+        await client.putMetricData(met).promise();
+      })
+  );
 
   // return happy lambda response to caller
   return {
     statusCode: 200,
-    body: `Metrics sent for metrics: ${JSON.stringify(images)}`
+    body: `Metrics sent for metrics: ${JSON.stringify(subdomains)}`
   };
 };
 
 const fns = { getCredentials, handler, getImageCount };
 
 export default fns;
+
+(async function() {
+  try {
+    await handler();
+  } catch (err) {
+    console.error(err);
+  }
+})();
