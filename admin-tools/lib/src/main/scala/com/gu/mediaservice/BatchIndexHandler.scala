417aaf138258e82321e75615f80234c9e8fb0a0c
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2020-02-28 15:17:41 +0000

    reingestion adjusting timeouts

--- admin-tools/lib/src/main/scala/com/gu/mediaservice/BatchIndexHandler.scala
+++ admin-tools/lib/src/main/scala/com/gu/mediaservice/BatchIndexHandler.scala
@@ -34,14 +34,13 @@ class BatchIndexHandler(cfg: BatchIndexHandlerConfig) extends LoggingWithMarkers
 
   import cfg._
 
-  private val ProjectionTimoutInMins = 11
-  private val GetIdsTimoutInMins = 1
-  private val OthersTimoutInMins = 1
-  private val MainProcessingTimoutInMins = ProjectionTimoutInMins + OthersTimoutInMins
+  private val ProjectionTimoutInSec = 740
+  private val OthersTimoutInSec = 90
+  private val MainProcessingTimoutInMins = ProjectionTimoutInSec + OthersTimoutInSec
 
-  private val GetIdsTimeout = new FiniteDuration(GetIdsTimoutInMins, TimeUnit.MINUTES)
-  private val GlobalTimeout = new FiniteDuration(MainProcessingTimoutInMins, TimeUnit.MINUTES)
-  private val ImagesProjectionTimeout = new FiniteDuration(ProjectionTimoutInMins, TimeUnit.MINUTES)
+  private val GetIdsTimeout = new FiniteDuration(20, TimeUnit.SECONDS)
+  private val GlobalTimeout = new FiniteDuration(MainProcessingTimoutInMins, TimeUnit.SECONDS)
+  private val ImagesProjectionTimeout = new FiniteDuration(ProjectionTimoutInSec, TimeUnit.MINUTES)
   private val gridClient = GridClient(maxIdleConnections, debugHttpResponse = false)
 
   private val ImagesBatchProjector = new ImagesBatchProjection(apiKey, projectionEndpoint, ImagesProjectionTimeout, gridClient)
