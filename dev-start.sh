bfdbaab2e2124e34ce4a0f79461d7ba62fbbfb6f
Alex Ware <alex.ware@guardian.co.uk>
2020-03-24 09:50:34 +0000

    Update dev-start.sh
    Co-Authored-By: Akash Askoolum <akash1810@users.noreply.github.com>

--- dev-start.sh
+++ dev-start.sh
@@ -116,7 +116,8 @@ setupLocalKinesis() {
   # ignore stream already exists error
   set +e
   stream_name='media-service-DEV-ThrallMessageQueue-1N0T2UXYNUIC9'
-  aws --profile media-service --region=eu-west-1 --endpoint-url=http://localhost:4568 kinesis create-stream --shard-count 1 --stream-name "${stream_name}" | cat
+  export AWS_PAGER=""
+  aws --profile media-service --region=eu-west-1 --endpoint-url=http://localhost:4568 kinesis create-stream --shard-count 1 --stream-name "${stream_name}"
   aws --profile media-service --region=eu-west-1 --endpoint-url=http://localhost:4568 kinesis list-streams | cat
 }
 
