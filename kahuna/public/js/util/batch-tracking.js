5d5d47fd98ffd2bc992a78e46f5d4dada49e3997
Alex Ware <alex.ware@guardian.co.uk>
2020-03-25 10:27:54 +0000

    throttle batch edits

--- kahuna/public/js/util/batch-tracking.js
+++ kahuna/public/js/util/batch-tracking.js
@@ -1,18 +1,21 @@
 // TODO MRB: invoke function lazily, does it improve UI jank?
+import pAll from "p-all";
+
 export function trackAll($rootScope, key, input, fn) {
     let completed = 0;
     $rootScope.$broadcast("events:batch-operations:start",
         { key, completed: 0, total: input.size ? input.size : input.length });
 
     const results = input.map(item => {
-        return fn(item).then(result => {
+        return () => {
+          fn(item).then(result => {
             completed++;
             $rootScope.$broadcast("events:batch-operations:progress", { key, completed });
             return result;
         });
+      }
     });
-
-    return Promise.all(results).finally(() => {
+    return pAll(results,{concurrency: 10, stopOnError: false}).finally(() => {
         $rootScope.$broadcast("events:batch-operations:complete", { key });
     });
 }
