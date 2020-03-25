e0a08f6cd061bebed4461b79518b2a6ba2531c22
Alex Ware <alex.ware@guardian.co.uk>
2020-03-25 12:14:31 +0000

    limit concurrency on batch edits

--- kahuna/public/js/util/batch-tracking.js
+++ kahuna/public/js/util/batch-tracking.js
@@ -1,18 +1,23 @@
+import throat from "throat";
+
+const limiter = throat(10)
+
 // TODO MRB: invoke function lazily, does it improve UI jank?
 export function trackAll($rootScope, key, input, fn) {
-    let completed = 0;
-    $rootScope.$broadcast("events:batch-operations:start",
-        { key, completed: 0, total: input.size ? input.size : input.length });
+  let completed = 0;
+  $rootScope.$broadcast("events:batch-operations:start",
+    {key, completed: 0, total: input.size ? input.size : input.length});
 
-    const results = input.map(item => {
-        return fn(item).then(result => {
-            completed++;
-            $rootScope.$broadcast("events:batch-operations:progress", { key, completed });
-            return result;
-        });
-    });
+  const results = input.map(item => limiter(() =>
+      fn(item).then(result => {
+        completed++;
+        $rootScope.$broadcast("events:batch-operations:progress", {key, completed});
+        return result;
+      })
+    )
+  );
 
-    return Promise.all(results).finally(() => {
-        $rootScope.$broadcast("events:batch-operations:complete", { key });
-    });
+  return Promise.all(results).finally(() => {
+    $rootScope.$broadcast("events:batch-operations:complete", {key});
+  });
 }
