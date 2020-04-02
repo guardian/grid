08fe85dd34ae1cf24fce475f0d4be45e1a1b1152
Alex Ware <alex.ware@guardian.co.uk>
2020-04-02 10:29:20 +0100

    move poll changes to new pr

--- kahuna/public/js/util/async.js
+++ kahuna/public/js/util/async.js
@@ -2,9 +2,6 @@ import angular from 'angular';
 
 export var async = angular.module('util.async', []);
 
-let concurrentPolls = 0 ;
-const maxConcurrentPolls = 10;
-const timeoutError = new Error('timeout');
 /**
  * Return a lazy function that will yield before calling the input `func`.
  */
@@ -53,37 +50,6 @@ async.factory('race',
     }]);
 
 
-async.factory('pollQ', ['$q', 'delay'],
-  ($q, delay) => {
-    let queue = [];
-    let running = false;
-    const run = () => {
-      running = true;
-      if (queue.length === 0 ){
-        running = false;
-        return;
-      }
-      const {promise, func} = queue.shift();
-      func().then(resolved => promise.resolve(resolved)).catch(()=>{
-        queue.push({promise, func});
-      }).finally(()=>{
-        delay(500).then(()=>{
-        setTimeout(()=>run(),0);
-        });
-      });
-    };
-
-    return (func) => {
-      let deferred = $q.defer();
-      queue.push({promise: deferred, func});
-      if (!running) {
-        setTimeout(()=> run(),0);
-      }
-      return deferred;
-    };
-  });
-
-
 async.factory('poll',
   ['$q', 'delay', 'race',
     function($q, delay, race) {
@@ -108,25 +74,7 @@ async.factory('poll',
         };
 
         function pollRecursive() {
-          let isRunning = true;
-          if (concurrentPolls > maxConcurrentPolls) {
-            console.warn("Too many concurrent polls.", concurrentPolls, func);
-            return withTimeout(delay(pollEvery)).then(pollRecursive);
-          }
-          concurrentPolls += 1;
-          return func().then(result => {
-            if (!isRunning){
-              console.error("THEN AFTER CANCEL!", func, result);
-              return result;
-            }
-            concurrentPolls -= 1;
-            return result;
-          }).catch((error) => {
-            isRunning = false;
-            if (error !== timeoutError){
-              console.error("Something blew up while polling",error, func);
-            }
-            concurrentPolls -= 1;
+          return func().catch(() => {
             return withTimeout(delay(withBackoff())).then(pollRecursive);
           });
         }
@@ -139,12 +87,12 @@ async.factory('poll',
 
 
 // Polling with sensible defaults for API polling
-async.factory('apiPoll', ['pollQ', function(pollQ) {
+async.factory('apiPoll', ['poll', function(poll) {
 
-  // const pollFrequency = 500; // ms
-  // const pollTimeout   = 30 * 1000; // ms
+  const pollFrequency = 500; // ms
+  const pollTimeout   = 30 * 1000; // ms
 
-  return func => pollQ(func);
+  return func => poll(func, pollFrequency, pollTimeout);
 }]);
 
 
