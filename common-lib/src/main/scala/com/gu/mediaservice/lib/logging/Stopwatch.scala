5ffcd1a28a54d0b7f73169f59287eaa61fc47625
Akash Askoolum <akash1810@gmail.com>
2020-04-03 12:56:27 +0100

    add more detail to stopwatch
    adding a `start` and `end` marker to stop watch logs for more information

--- common-lib/src/main/scala/com/gu/mediaservice/lib/logging/Stopwatch.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/logging/Stopwatch.scala
@@ -1,17 +1,30 @@
 package com.gu.mediaservice.lib.logging
 
-import net.logstash.logback.marker.LogstashMarker
+import java.time.ZonedDateTime
+
+import com.gu.mediaservice.lib.DateTimeUtils
+
 import scala.concurrent.duration._
 
-case class DurationForLogging(duration: Duration) extends LogMarker {
+case class DurationForLogging(startTime: ZonedDateTime, duration: Duration) extends LogMarker {
   def toMillis: Long = duration.toMillis
-  def markerContents = Map("duration" -> toMillis)
+
+  override def markerContents = Map(
+    "start" -> DateTimeUtils.toString(startTime),
+    "end" -> DateTimeUtils.toString(DateTimeUtils.now()),
+    "duration" -> toMillis
+  )
 }
 
 class Stopwatch {
+  // This method can only be used to measure elapsed time and is not related to any other notion of system or wall-clock time.
+  // Therefore we additionally have `startTime` to track the time.
+  // see https://docs.oracle.com/javase/9/docs/api/java/lang/System.html#nanoTime--
   private val startedAt = System.nanoTime()
 
-  def elapsed: DurationForLogging = DurationForLogging((System.nanoTime() - startedAt).nanos)
+  private val startTime = DateTimeUtils.now()
+
+  def elapsed: DurationForLogging = DurationForLogging(startTime, (System.nanoTime() - startedAt).nanos)
 }
 
 object Stopwatch {
