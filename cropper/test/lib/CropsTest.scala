7d129211255e51101ab3b76997927778de680904
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2020-04-07 15:49:32 +0100

    Add some tests for output file name

--- cropper/test/lib/CropsTest.scala
+++ cropper/test/lib/CropsTest.scala
@@ -1,9 +1,12 @@
 package lib
 
+import com.gu.mediaservice.lib.imaging.ImageOperations
 import com.gu.mediaservice.model._
+import org.scalatest.mockito.MockitoSugar
+
 import org.scalatest.{FunSpec, Matchers}
 
-class CropsTest extends FunSpec with Matchers {
+class CropsTest extends FunSpec with Matchers with MockitoSugar {
   it("should return JPEG when the input type is a JPEG") {
     Crops.cropType(Jpeg, "True Color", hasAlpha = false) shouldBe Jpeg
     Crops.cropType(Jpeg, "Monkey", hasAlpha = false) shouldBe Jpeg
@@ -35,5 +38,33 @@ class CropsTest extends FunSpec with Matchers {
 
   it("should return JPEG when the input type is TIFF and it doesn't have alpha and it is true color") {
     Crops.cropType(Tiff, "TrueColor", hasAlpha = false) shouldBe Jpeg
+  }
+
+  private val config = mock[CropperConfig]
+  private val store = mock[CropStore]
+  private val imageOperations: ImageOperations = mock[ImageOperations]
+  private val source: SourceImage = SourceImage("test", mock[Asset], valid = true, mock[ImageMetadata], mock[FileMetadata])
+  private val bounds: Bounds = Bounds(10, 20, 30, 40)
+  private val outputWidth = 1234
+
+  it("should should construct a correct address for a master jpg") {
+    val outputFilename = new Crops(config, store, imageOperations)
+      .outputFilename(source, bounds, outputWidth, Jpeg, isMaster = true)
+    outputFilename shouldBe "test/10_20_30_40/master/1234.jpg"
+  }
+  it("should should construct a correct address for a non-master jpg") {
+    val outputFilename = new Crops(config, store, imageOperations)
+      .outputFilename(source, bounds, outputWidth, Jpeg)
+    outputFilename shouldBe "test/10_20_30_40/1234.jpg"
+  }
+  it("should should construct a correct address for a non-master tiff") {
+    val outputFilename = new Crops(config, store, imageOperations)
+      .outputFilename(source, bounds, outputWidth, Tiff)
+    outputFilename shouldBe "test/10_20_30_40/1234.tiff"
+  }
+  it("should should construct a correct address for a non-master png") {
+    val outputFilename = new Crops(config, store, imageOperations)
+      .outputFilename(source, bounds, outputWidth, Png)
+    outputFilename shouldBe "test/10_20_30_40/1234.png"
   }
 }
