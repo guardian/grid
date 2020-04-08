4b913a11dbaa17532c3df8afc826a0b4e0f501ac
Alex Ware <alex.ware@guardian.co.uk>
2020-04-08 15:54:29 +0100

    Make batch-tracking use the queue

--- kahuna/public/js/util/batch-tracking.js
+++ kahuna/public/js/util/batch-tracking.js
@@ -1,16 +1,25 @@
+import { createQueue } from "./queue";
+
 // TODO MRB: invoke function lazily, does it improve UI jank?
 export function trackAll($rootScope, key, input, fn) {
     let completed = 0;
     $rootScope.$broadcast("events:batch-operations:start",
         { key, completed: 0, total: input.size ? input.size : input.length });
+  const queue = createQueue({ maxWorkers: 30 });
+  const results = input.map(item => {
+    return new Promise((resolve, reject) =>
+      queue.add({
+        func: () => fn(item).then(result => {
+          completed++;
+          $rootScope.$broadcast("events:batch-operations:progress", { key, completed });
+          return result;
+        }),
+        reject,
+        resolve
+      })
+    );
+  });
 
-    const results = input.map(item => {
-        return fn(item).then(result => {
-            completed++;
-            $rootScope.$broadcast("events:batch-operations:progress", { key, completed });
-            return result;
-        });
-    });
 
     return Promise.all(results).finally(() => {
         $rootScope.$broadcast("events:batch-operations:complete", { key });
