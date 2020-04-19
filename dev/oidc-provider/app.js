d16c13d567e8849ca663a0360398605cb44bc396
Akash Askoolum <akash1810@gmail.com>
2020-04-19 14:18:45 +0100

    make oidc container simpler
    - mount users.json at run time (means updating is faster)
    - simplify json structure
    - paramatise email domain

--- dev/oidc-provider/app.js
+++ dev/oidc-provider/app.js
@@ -1,7 +1,8 @@
 // inspired by https://github.com/panva/node-oidc-provider-example/tree/master/03-oidc-views-accounts
 
 const Provider = require("oidc-provider");
-const accounts = require("./accounts.json");
+
+const { EMAIL_DOMAIN } = process.env;
 
 const port = 9014;
 const issuer = `http://localhost:${port}`;
@@ -9,17 +10,39 @@ const clientId = "grid-local-id";
 const clientSecret = "grid-local-secret";
 const redirectURI = "https://media-auth.local.dev-gutools.co.uk/oauthCallback";
 
-async function findAccount(_, id) {
-  const account = accounts.find(a => a.email === id);
+async function findAccount(_, incomingEmail) {
+  if (!incomingEmail.endsWith(`@${EMAIL_DOMAIN}`)) {
+    console.log(`rejecting: ${incomingEmail} doesn't end with @${EMAIL_DOMAIN}`);
+    return;
+  }
 
-  if (!account) {
+  const users = require("/etc/gu/users.json").map(u => {
+    return {
+      ...u,
+      email: `${u.id}@${EMAIL_DOMAIN}`
+    }
+  });
+
+  const user = users.find(u => u.email === incomingEmail);
+
+  if (!user) {
+    console.log(`rejecting: user with email ${incomingEmail} not found`);
     return;
   }
 
   return {
-    accountId: id,
+    accountId: user.email,
     async claims() {
-      return account;
+      const { id, email, firstName, lastName } = user;
+
+      return {
+        sub: id,
+        email: email,
+        email_verified: true,
+        given_name: firstName,
+        family_name: lastName,
+        name: [firstName, lastName].join(" ")
+      };
     }
   }
 }
