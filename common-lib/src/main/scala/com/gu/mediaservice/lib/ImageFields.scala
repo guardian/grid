488b9e524537cffbd342b53ba425f4bdf8786425
Mateusz <mateusz.karpow@guardian.co.uk>
2020-06-18 16:42:31 +0100

    Update ImageFields.scala

--- common-lib/src/main/scala/com/gu/mediaservice/lib/ImageFields.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/ImageFields.scala
@@ -50,7 +50,8 @@ trait ImageFields {
     "croppedBy" -> "exports.author",
     "filename"  -> "uploadInfo.filename",
     "photoshoot"-> photoshootField("title"),
-    "leasedBy" -> "leases.leases.leasedBy"
+    "leasedBy" -> "leases.leases.leasedBy",
+    "people" -> "peopleInImage"
   )
 
   def getFieldPath(field: String) = field match {
