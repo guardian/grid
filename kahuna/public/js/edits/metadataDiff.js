33c665d77a0dfbe712a8bcb56cec52006b95dd64
Alex Ware <alex.ware@guardian.co.uk>
2020-06-18 12:17:41 +0100

    Simpler metadata diff

--- kahuna/public/js/edits/metadataDiff.js
+++ kahuna/public/js/edits/metadataDiff.js
@@ -1,30 +1,25 @@
-function isDefined(value){return typeof value !== 'undefined';}
-function isUndefined(value) { return typeof value === 'undefined'; }
-//These two functions have been copied from angular.js and will be deleted.
-
 export const getMetadataDiff = (image, metadata) => {
-  var diff = {};
-
+  const diff = {};
+  const originalMetadata = image.data.originalMetadata;
   // jscs has a maximumLineLength of 100 characters, hence the line break
-  var keys = new Set(Object.keys(metadata).concat(
-    Object.keys(image.data.originalMetadata)));
-
-  // Keywords, peopleInImage and subjects are arrays, the comparison below only works with string comparison.
-  // For simplicity, ignore them as we're not updating these fields at the moment.
-  keys.delete('keywords');
-  keys.delete('subjects');
-  keys.delete('peopleInImage');
+  var keys = new Set(
+    Object.keys(metadata).concat(Object.keys(originalMetadata))
+  );
 
   keys.forEach((key) => {
-    if (metadata[key] && isUndefined(image.data.originalMetadata[key])) {
-      diff[key] = metadata[key];
-    } else if (metadata[key] !== image.data.originalMetadata[key] &&
-      isDefined(image.data.originalMetadata[key])) {
-      // if the user has provided an override of '' (e.g. they want remove the title),
-      // angular sets the value in the object to undefined.
-      // We need to use an empty string in the PUT request to obey user input.
-      diff[key] = metadata[key] || '';
+    if (originalMetadata[key] === metadata[key]) {
+      return;
     }
+
+    //This only works with string fields and does not support arrays
+    if (Array.isArray(metadata[key]) || Array.isArray()) {
+      return;
+    }
+
+    // if the user has provided an override of '' (e.g. they want remove the title),
+    // angular sets the value in the object to undefined.
+    // We need to use an empty string in the PUT request to obey user input.
+    diff[key] = metadata[key] || "";
   });
 
   return diff;
