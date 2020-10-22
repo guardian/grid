a1a9637a556075f5545cb9103b178cc5f30e8e3b
Ochieng Olanga <ochiengolanga@gmail.com>
2020-10-23 01:45:57 +0300

    enable setup valid emails configuration in dev setup

--- dev/script/setup.sh
+++ dev/script/setup.sh
@@ -267,6 +267,7 @@ main() {
   if [[ $LOCAL_AUTH == true ]]; then
     createLocalAuthStack
     setupPermissionConfiguration
+    setupValidEmailsConfiguration
     setupPanDomainConfiguration
   fi
 
