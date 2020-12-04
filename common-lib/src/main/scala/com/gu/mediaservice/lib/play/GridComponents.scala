0c0eeafe66dc8c7e6d921ef2f5fb04da364e33b2
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2020-12-04 16:33:21 +0000

    Implement generic error handling to cope with failed uploads

--- common-lib/src/main/scala/com/gu/mediaservice/lib/play/GridComponents.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/play/GridComponents.scala
@@ -6,8 +6,8 @@ import com.gu.mediaservice.lib.logging.{GridLogging, LogConfig}
 import com.gu.mediaservice.lib.management.{BuildInfo, Management}
 import play.api.ApplicationLoader.Context
 import play.api.Logger.logger
-import play.api.http.{DefaultHttpErrorHandler, HttpErrorHandler}
-import play.api.{BuiltInComponentsFromContext, Configuration, UnexpectedException}
+import play.api.http.{DefaultHttpErrorHandler, HttpErrorConfig, HttpErrorHandler}
+import play.api.{BuiltInComponentsFromContext, Configuration, Environment, Mode, UnexpectedException}
 import play.api.libs.ws.ahc.AhcWSComponents
 import play.api.mvc.{EssentialFilter, RequestHeader, Result, Results}
 import play.filters.HttpFiltersComponents
@@ -25,8 +25,8 @@ abstract class GridComponents[Config <: CommonConfig](context: Context, val load
   LogConfig.initKinesisLogging(config)
   LogConfig.initLocalLogShipping(config)
 
-  override lazy val httpErrorHandler: HttpErrorHandler = new GridHttpErrorHandler(environment, configuration, sourceMapper,
-    Some(router))
+  lazy val hiddenHttpErrorHandler: HttpErrorHandler = new DefaultHttpErrorHandler(environment, configuration, sourceMapper, Some(router))
+  override lazy val httpErrorHandler: HttpErrorHandler = new GridHttpErrorHandler(hiddenHttpErrorHandler)
 
   def buildInfo: BuildInfo
 
@@ -44,12 +44,17 @@ abstract class GridComponents[Config <: CommonConfig](context: Context, val load
   val auth = new Authentication(config, actorSystem, defaultBodyParser, wsClient, controllerComponents, executionContext)
 }
 
-class GridHttpErrorHandler extends DefaultHttpErrorHandler with Results {
+// Ugly wrapper class.  Sorry.
+class GridHttpErrorHandler(httpErrorHandler: HttpErrorHandler) extends HttpErrorHandler with Results {
+
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = exception match {
     case e:UnexpectedException if e.cause.getClass.getCanonicalName == "akka.http.scaladsl.model.EntityStreamException" => {
       logger.info(s"Upload failed? Request = $request", e)
       Future.successful(UnprocessableEntity("The upload did not complete"))
     }
-    case x => super.onServerError(request, x)
+    case x => httpErrorHandler.onServerError(request, x)
   }
+
+  override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] =
+    httpErrorHandler.onClientError(request, statusCode, message)
 }
