c06fb46c9a7f0c1dcfdd357da05e326ef435bb2c
Mateusz <mateusz.karpow@guardian.co.uk>
2020-12-15 17:27:03 +0000

    Tradition matters

--- common-lib/src/main/scala/com/gu/mediaservice/model/ImageMetadata.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/model/ImageMetadata.scala
@@ -5,9 +5,9 @@ import org.joda.time.DateTime
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
-case class ImageMetadata(
-/* these are standard metadata fields that exist in multiple schemas,
+/* following are standard metadata fields that exist in multiple schemas,
 most canonical being https://www.iptc.org/std/photometadata/specification/IPTC-PhotoMetadata */
+case class ImageMetadata(
   dateTaken:           Option[DateTime] = None,
   description:         Option[String]   = None,
   credit:              Option[String]   = None,
