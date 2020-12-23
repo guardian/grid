c63f355a3297fe90f05ad24b6f6bd78e71cd74be
Georges Ribeiro <georges.ribeiro.ext@bbc.co.uk>
2020-12-23 10:03:14 -0300

    Bringing BBC MetadataStore to main.

---
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/bbc/MetadataConfig.scala
@@ -0,0 +1,48 @@
+package com.gu.mediaservice.lib.bbc
+
+import com.gu.mediaservice.model.{ContractPhotographer, Photographer, StaffPhotographer}
+import play.api.libs.json._
+
+case class MetadataConfig(
+                           staffIllustrators: List[String],
+                           creativeCommonsLicense: List[String],
+                           externalStaffPhotographers: List[Company],
+                           internalStaffPhotographers: List[Company],
+                           contractedPhotographers: List[Company],
+                           contractIllustrators: List[Company]) {
+  val staffPhotographers: List[Company] = MetadataConfig.flattenCompanyList(
+    internalStaffPhotographers ++ externalStaffPhotographers)
+  val allPhotographers: List[Company] = MetadataConfig.flattenCompanyList(
+    internalStaffPhotographers ++ externalStaffPhotographers ++ contractedPhotographers)
+
+  def caseInsensitiveLookup(store: List[Company], lookup: String) = {
+    store.map {
+      case Company(name, photographers) if photographers.map(_.toLowerCase) contains lookup.toLowerCase() => Some(lookup, name)
+      case _ => None
+    }.find(_.isDefined).flatten
+  }
+
+  def getPhotographer(photographer: String): Option[Photographer] = {
+    caseInsensitiveLookup(staffPhotographers, photographer).map {
+      case (name, pub) => StaffPhotographer(name, pub)
+    }.orElse(caseInsensitiveLookup(contractedPhotographers, photographer).map {
+      case (name, pub) => ContractPhotographer(name, Some(pub))
+    })
+  }
+}
+
+case class Company(name: String, photographers: List[String])
+
+object Company {
+  implicit val companyClassFormats = Json.format[Company]
+}
+
+object MetadataConfig {
+  implicit val metadataConfigClassFormats = Json.format[MetadataConfig]
+
+  def flattenCompanyList(companies: List[Company]): List[Company] = companies
+    .groupBy(_.name)
+    .map { case (group, companies) => Company(group, companies.flatMap(company => company.photographers)) }
+    .toList
+
+}
