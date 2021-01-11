4679fd7846bb32547b24cbfe99389d609916517f
Simon Hildrew <simon@hildrew.net>
2021-01-11 15:03:24 +0000

    Fixup the ImageLoaderConfig to use generic provider loader

--- common-lib/src/main/scala/com/gu/mediaservice/lib/cleanup/ImageProcessorResources.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/cleanup/ImageProcessorResources.scala
@@ -7,8 +7,4 @@ import play.api.Configuration
 /**
   * Resources that can be injected into a dynamically loaded ImageProcessor
   */
-trait ImageProcessorResources {
-  def processorConfiguration: Configuration
-  def commonConfiguration: CommonConfig
-  def actorSystem: ActorSystem
-}
+case class ImageProcessorResources(commonConfiguration: CommonConfig, actorSystem: ActorSystem)
