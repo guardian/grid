3cc44ac875ea20df3063078c4ad2fb35f3cf287f
Simon Hildrew <simon@hildrew.net>
2021-01-19 16:30:32 +0000

    Add a comment to a bbc file

--- common-lib/src/main/scala/com/gu/mediaservice/lib/bbc/BBCImageProcessorConfig.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/bbc/BBCImageProcessorConfig.scala
@@ -2,6 +2,7 @@ package com.gu.mediaservice.lib.bbc
 
 import play.api.Configuration
 
+/** a comment on a BBC file */
 class BBCImageProcessorConfig(config: Configuration) {
   val configBucket: String = config.get[String]("s3.config.bucket")
 }
