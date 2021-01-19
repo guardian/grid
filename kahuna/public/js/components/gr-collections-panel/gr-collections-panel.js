05c8c44caa5f5a2d07f70caea5ab4a7e6a8fbbcb
Sam Hession <samhession@live.co.uk>
2021-01-19 14:58:54 +0000

    Update kahuna/public/js/components/gr-collections-panel/gr-collections-panel.js
    Co-authored-by: Alex Ware <alex.ware@guardian.co.uk>

--- kahuna/public/js/components/gr-collections-panel/gr-collections-panel.js
+++ kahuna/public/js/components/gr-collections-panel/gr-collections-panel.js
@@ -103,11 +103,11 @@ grCollectionsPanel.controller('GrNodeCtrl',
     const pathId = ctrl.node.data.data.pathId;
 
     //This filter remove child nodes with missing data, preventing display errors from occurring
-    $scope.filterChildren = children => children.filter(node => !!node.data.data);
+    ctrl.filterChildren = children => children.filter(node => !!node.data.data);
 
-    ctrl.children = $scope.filterChildren(ctrl.node.data.children);
+    ctrl.children = ctrl.filterChildren(ctrl.node.data.children);
     $scope.$watch('ctrl.node.data.children', children => {
-      ctrl.children = $scope.filterChildren(children);
+      ctrl.children = ctrl.filterChildren(children);
     });
 
     ctrl.saving = false;
