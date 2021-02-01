679b67b6826efdb20e1d2f4881088c393f963309
Alex Ware <alex.ware@gmail.com>
2021-02-01 11:17:45 +0000

    set timeout instead of relying on "safeApply"

--- kahuna/public/js/util/digest.js
+++ kahuna/public/js/util/digest.js
@@ -6,6 +6,10 @@ export var digest = angular.module('util.digest', []);
 // Helper to $apply the fn on the scope iff we're not
 // already in a $digest cycle.  Necessary because of
 // the different contexts we can be called from.
+
+// Consider briefly whether you would equally well
+// be served by setting a zero timeout.
+
 digest.value('safeApply', function (scope, fn) {
     if (scope.$$phase || scope.$root.$$phase) {
         fn();
