8b901aff1fb6059754023734f10fc9973cc3263a
Georges Ribeiro <georges.ribeiro.ext@bbc.co.uk>
2021-02-25 16:34:39 -0300

    remove bbc folder

--- common-lib/src/main/scala/com/gu/mediaservice/lib/bbc/BBCImageProcessorConfig.scala
+++
@@ -1,7 +0,0 @@
-package com.gu.mediaservice.lib.bbc
-
-import play.api.Configuration
-
-class BBCImageProcessorConfig(config: Configuration) {
-  val configBucket: String = config.get[String]("s3.config.bucket")
-}
