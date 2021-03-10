2c09e0001877138fc98d980165c61be0124603ea
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2021-03-10 09:46:51 +0000

    Reorder and use new labels

--- common-lib/src/main/scala/com/gu/mediaservice/JsonDiff.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/JsonDiff.scala
@@ -3,6 +3,11 @@ package com.gu.mediaservice
 import play.api.libs.json.{JsArray, JsObject, JsValue}
 
 object JsonDiff {
+
+  private val ADD = "---"
+  private val REMOVE = "+++"
+  private val CHANGE = "≠≠≠"
+
   def diff(a: JsValue, b: JsValue): JsObject = {
     (a, b) match {
       case (ao: JsObject, bo: JsObject) =>
@@ -10,13 +15,15 @@ object JsonDiff {
         allKeys.toList.foldLeft[JsObject](JsObject.empty) {
           (acc, k) => {
             if (!bo.keys.contains(k)) {
-              acc + ((k, JsObject(Seq(("-", ao(k))))))
+              acc + ((k, JsObject(Seq((ADD, ao(k))))))
             } else if (!ao.keys.contains(k)) {
-              acc + ((k, JsObject(Seq(("+", bo(k))))))
+              acc + ((k, JsObject(Seq((REMOVE, bo(k))))))
             } else {
               val subDiff = diff(ao(k), bo(k))
               if (subDiff.keys.isEmpty) acc
-              else acc + ((k, JsObject(Seq(("~", subDiff)))))
+              else {
+                acc + ((k, JsObject(Seq((CHANGE, subDiff)))))
+              }
             }
           }
         }
@@ -25,11 +32,11 @@ object JsonDiff {
         val arrayAdded = Set(ba.value: _*) -- Set(aa.value: _*)
         val arrayRemoved = Set(aa.value: _*) -- Set(ba.value: _*)
         JsObject(Seq(
-         ("+", JsArray(arrayAdded.toSeq)),
-         ("-", JsArray(arrayRemoved.toSeq))
+         (ADD, JsArray(arrayRemoved.toSeq)),
+         (REMOVE, JsArray(arrayAdded.toSeq))
         ))
       case (av: JsValue, bv: JsValue) if av == bv => JsObject.empty
-      case (av: JsValue, bv: JsValue) => JsObject(Seq(("+", av), ("-", bv)))
+      case (av: JsValue, bv: JsValue) => JsObject(Seq((ADD, bv), (REMOVE, av)))
     }
   }
 }
