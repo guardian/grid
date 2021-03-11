a8d0463f38041480a05437b41a86f50184c56402
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2021-03-11 08:58:03 +0000

    Improve json diff

--- common-lib/src/test/scala/com/gu/mediaservice/JsonDiffTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/JsonDiffTest.scala
@@ -18,26 +18,6 @@ class JsonDiffTest extends FunSpec with Matchers {
     JsonDiff.diff(jsonA, jsonB) should be (jsonC)
   }
 
-  it ("should detect changes") {
-
-    val jsonA = load(""" {"a": 1} """)
-
-    val jsonB = load(""" {"a": 2} """)
-
-    val jsonC = load("""
-      |{
-      |  "a": {
-      |    "≠≠≠": {
-      |      "---": 1,
-      |      "+++": 2
-      |    }
-      |  }
-      |}
-      """)
-
-    JsonDiff.diff(jsonA, jsonB) should be (jsonC)
-  }
-
   it ("should detect additions") {
 
     val jsonA = load(""" {} """)
@@ -68,6 +48,26 @@ class JsonDiffTest extends FunSpec with Matchers {
       |  }
       |}
     """)
+
+    JsonDiff.diff(jsonA, jsonB) should be (jsonC)
+  }
+
+  it ("should detect changes") {
+
+    val jsonA = load(""" {"a": 1} """)
+
+    val jsonB = load(""" {"a": 2} """)
+
+    val jsonC = load("""
+      |{
+      |  "a": {
+      |    "≠≠≠": {
+      |      "---": 1,
+      |      "+++": 2
+      |    }
+      |  }
+      |}
+      """)
 
     JsonDiff.diff(jsonA, jsonB) should be (jsonC)
   }
@@ -166,6 +166,7 @@ class JsonDiffTest extends FunSpec with Matchers {
     JsonDiff.diff(jsonA, jsonB) should be (jsonC)
   }
 
+
   it ("should handle and find completely different elements in arrays") {
 
     val jsonA = load("""
@@ -184,14 +185,151 @@ class JsonDiffTest extends FunSpec with Matchers {
                        |{
                        |  "a": {
                        |    "≠≠≠": {
-                       |      "+++": ["a", 2, "c"],
-                       |      "---": [1, "b", 3]
+                       |      "---": [1, "b", 3],
+                       |      "+++": ["a", 2, "c"]
                        |    }
                        |  }
                        |}
       """)
 
     JsonDiff.diff(jsonA, jsonB) should be (jsonC)
+  }
+
+  it ("should handle disordered lists which are still equal") {
+    val jsonA = load("""
+                       |{
+                       |  "a": [1, 2]
+                       |}
+    """)
+    val jsonB = load("""
+                       |{
+                       |  "a": [2, 1]
+                       |}
+    """)
+    val jsonC = load("{}")
+    JsonDiff.diff(jsonA, jsonB) should be (jsonC)
+
+  }
+
+  it ("should handle disordered lists of lists which are still really equal") {
+    val jsonA = load("""
+                       |{
+                       |  "a": [
+                       |    {
+                       |      "b": [1, 2]
+                       |    },
+                       |    {
+                       |      "b": [3, 4]
+                       |    }
+                       |  ]
+                       |}
+    """)
+    val jsonB = load("""
+                       |{
+                       |  "a": [
+                       |    {
+                       |      "b": [4, 3]
+                       |    },
+                       |    {
+                       |      "b": [2, 1]
+                       |    }
+                       |  ]
+                       |}
+    """)
+    val jsonC = load("{}")
+    JsonDiff.diff(jsonA, jsonB) should be (jsonC)
+
+  }
+
+  it ("should handle disordered lists of lists which are not really equal") {
+    val jsonA = load("""
+                       |{
+                       |  "a": [
+                       |    {
+                       |      "b": [1, 2]
+                       |    },
+                       |    {
+                       |      "b": [3, 4, 5]
+                       |    }
+                       |  ]
+                       |}
+    """)
+    val jsonB = load("""
+                       |{
+                       |  "a": [
+                       |    {
+                       |      "b": [4, 3]
+                       |    },
+                       |    {
+                       |      "b": [2, 1]
+                       |    }
+                       |  ]
+                       |}
+    """)
+    val jsonC = load("""
+                       |{
+                       |  "a": {
+                       |    "≠≠≠": {
+                       |      "---": [
+                       |        {
+                       |          "b": [3,4,5]
+                       |        }
+                       |      ],
+                       |      "+++": [
+                       |        {
+                       |          "b": [4,3]
+                       |        }
+                       |      ]
+                       |    }
+                       |  }
+                       |}
+      """)
+
+
+    JsonDiff.diff(jsonA, jsonB) should be (jsonC)
+
+  }
+
+  it ("should handle recursive disordered lists which are still really equal") {
+    val jsonA = load("""
+                       |{
+                       |  "a": [
+                       |    {
+                       |      "b": [
+                       |        {"c": 1},
+                       |        {"c": 2}
+                       |      ]
+                       |    },
+                       |    {
+                       |      "b": [
+                       |        {"c": 3},
+                       |        {"c": 4}
+                       |      ]
+                       |    }
+                       |  ]
+                       |}
+    """)
+    val jsonB = load("""
+                       |{
+                       |  "a": [
+                       |    {
+                       |      "b": [
+                       |        {"c": 4},
+                       |        {"c": 3}
+                       |      ]
+                       |    },
+                       |    {
+                       |      "b": [
+                       |        {"c": 2},
+                       |        {"c": 1}
+                       |      ]
+                       |    }
+                       |  ]
+                       |}
+    """)
+    val jsonC = load("{}")
+    JsonDiff.diff(jsonA, jsonB) should be (jsonC)
+
   }
 
 }
