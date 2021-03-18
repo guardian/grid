a72f677da2e7b8efd6fcdefa4aac253e2fb2b1e8
Adriel Ulanovsky <adriel.ulanovsky.ext@bbc.co.uk>
2021-03-18 15:45:44 -0300

    feature/upload-images-permission: Add support email to error page

--- kahuna/public/js/upload/controller.js
+++ kahuna/public/js/upload/controller.js
@@ -11,8 +11,10 @@ var upload = angular.module('kahuna.upload.controller', [
 upload.controller('UploadCtrl', ['uploadManager', 'mediaApi', function(uploadManager, mediaApi) {
     var ctrl = this;
 
-    mediaApi.canUserUpload().then(canUpload => {
-        ctrl.canUpload = canUpload;
+    ctrl.supportEmailLink = window._clientConfig.supportEmail;
+
+    mediaApi.getSession().then(session => {
+        ctrl.canUpload = session.user.permissions.canUpload;
     });
 
     // TODO: Show multiple jobs?
