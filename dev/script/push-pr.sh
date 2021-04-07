f6b52f6a38525204777bcad2492638cc24e58a70
Alex Ware <alex.ware@gmail.com>
2021-04-07 14:19:15 +0100

    correct bash arg

--- dev/script/push-pr.sh
+++ dev/script/push-pr.sh
@@ -3,13 +3,13 @@
 set -e
 
 
-if [ $# -eq 0 ]
+if [ -z $1 ]
 then
   echo "Please run this script with a PR number as its argument."
   exit 1
 fi
 
-PR=$0
+PR=$1
 
 echo "Pushing PR $PR to origin"
 git fetch origin pull/${PR}/merge:pr${PR}
