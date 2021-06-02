e41fa49e9a4730dd3921ba501be7271cae3dc43f
Tom Richards <tom.richards@guardian.co.uk>
2021-06-02 18:35:05 +0100

    make the `/innerServiceStatusCheck` go 'via' all the other services to demonstrate the 'onBehalfOf' functionality
    Co-authored-by: Tom Richards <tom.richards@guardian.co.uk>

--- rest-lib/src/main/scala/com/gu/mediaservice/lib/management/InnerServiceStatusCheckController.scala
+++ rest-lib/src/main/scala/com/gu/mediaservice/lib/management/InnerServiceStatusCheckController.scala
@@ -2,15 +2,15 @@ package com.gu.mediaservice.lib.management
 
 import com.gu.mediaservice.lib.argo.ArgoHelpers
 import com.gu.mediaservice.lib.auth.Authentication
-import com.gu.mediaservice.lib.auth.provider.InnerServiceAuthenticationProvider
+import com.gu.mediaservice.lib.auth.Authentication.InnerServicePrincipal
 import com.gu.mediaservice.lib.config.Services
-import play.api.libs.json.{Json, Writes}
-import play.api.libs.ws.WSClient
+import play.api.libs.json.{JsValue, Json, Writes}
+import play.api.libs.ws.{WSClient, WSRequest}
 import play.api.mvc.{BaseController, ControllerComponents}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-case class WhoAmIResponse (baseUri: String, status: Int, body: String)
+case class WhoAmIResponse (baseUri: String, status: Int, body: JsValue)
 object WhoAmIResponse { implicit val writes: Writes[WhoAmIResponse] = Json.writes[WhoAmIResponse] }
 
 class InnerServiceStatusCheckController(
@@ -20,18 +20,30 @@ class InnerServiceStatusCheckController(
   ws: WSClient
 )(implicit ec: ExecutionContext)
   extends BaseController with ArgoHelpers {
-  def whoAmI = auth { request =>
-    Ok(request.user.toString)
-  }
 
-  def statusCheck = Action.async {
+  private def callAllInternalServices(authenticator: WSRequest => WSRequest) = {
     val whoAmIFutures = services.allInternalUris.map { baseUri =>
-      auth.innerServiceCall(ws.url(s"$baseUri/management/whoAmI")).get.map(resp => WhoAmIResponse(baseUri, resp.status, resp.body))
+      authenticator(ws.url(s"$baseUri/management/whoAmI")).get.map(resp => WhoAmIResponse(baseUri, resp.status, resp.body[JsValue]))
     }
 
     Future.sequence(whoAmIFutures).map { whoAmIResponses =>
       val overallStatus = whoAmIResponses.map(_.status).max
       new Status(overallStatus)(Json.toJson(whoAmIResponses.map(resp => resp.baseUri -> resp).toMap))
     }
+  }
+
+  def whoAmI = auth.async { request =>
+    request.user match {
+      case principal: InnerServicePrincipal if !principal.identity.contains(" via ") =>
+        callAllInternalServices(auth.getOnBehalfOfPrincipal(principal))
+      case _ =>
+        Future.successful(
+          Ok(Json.toJson(request.user.toString))
+        )
+    }
+  }
+
+  def statusCheck = Action.async {
+    callAllInternalServices(auth.innerServiceCall)
   }
 }
