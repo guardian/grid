655d0a329702eba337f4a3187b453cedf8a48161
Tom Richards <tom.richards@guardian.co.uk>
2021-06-09 17:37:58 +0100

    safely parse whoAmI responses to json, returning string if not parsable (e.g. they return some error HTML) and 503 if unreachable
    Co-authored-by: Tom Richards <tom.richards@guardian.co.uk>

--- rest-lib/src/main/scala/com/gu/mediaservice/lib/management/InnerServiceStatusCheckController.scala
+++ rest-lib/src/main/scala/com/gu/mediaservice/lib/management/InnerServiceStatusCheckController.scala
@@ -4,11 +4,12 @@ import com.gu.mediaservice.lib.argo.ArgoHelpers
 import com.gu.mediaservice.lib.auth.Authentication
 import com.gu.mediaservice.lib.auth.Authentication.InnerServicePrincipal
 import com.gu.mediaservice.lib.config.Services
-import play.api.libs.json.{JsValue, Json, Writes}
+import play.api.libs.json.{JsString, JsValue, Json, Writes}
 import play.api.libs.ws.{WSClient, WSRequest}
 import play.api.mvc.{BaseController, ControllerComponents}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 case class WhoAmIResponse (baseUri: String, status: Int, body: JsValue)
 object WhoAmIResponse { implicit val writes: Writes[WhoAmIResponse] = Json.writes[WhoAmIResponse] }
@@ -21,9 +22,17 @@ class InnerServiceStatusCheckController(
 )(implicit ec: ExecutionContext)
   extends BaseController with ArgoHelpers {
 
+  private def safeJsonParse(maybeJsonStr: String) = Try(Json.parse(maybeJsonStr)).getOrElse(JsString(maybeJsonStr))
+
   private def callAllInternalServices(authenticator: WSRequest => WSRequest) = {
     val whoAmIFutures = services.allInternalUris.map { baseUri =>
-      authenticator(ws.url(s"$baseUri/management/whoAmI")).get.map(resp => WhoAmIResponse(baseUri, resp.status, resp.body[JsValue]))
+        authenticator(ws.url(s"$baseUri/management/whoAmI")).get
+          .map(resp => WhoAmIResponse(baseUri, resp.status, safeJsonParse(resp.body)))
+          .recover{
+            case throwable: Throwable => WhoAmIResponse(baseUri, SERVICE_UNAVAILABLE, Json.obj(
+            "errorMessage" -> throwable.getMessage,
+            "stackTrace" -> throwable.getStackTrace.map(_.toString)
+          ))}
     }
 
     Future.sequence(whoAmIFutures).map { whoAmIResponses =>
