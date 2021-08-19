0555653cf5091c6c16667165d411cc868bb4b280
Andrew Nowak <andrew.nowak@guardian.co.uk>
2021-08-19 15:08:35 +0100

    retain previous status when in error state

--- common-lib/src/main/scala/com/gu/mediaservice/lib/elasticsearch/MigrationClient.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/elasticsearch/MigrationClient.scala
@@ -11,6 +11,7 @@ object MigrationStatus {
   import scala.language.implicitConversions
   implicit def migrationStatusToOptionOfMigrationIndexName(migrationStatus: MigrationStatus): Option[String] =
     migrationStatus match {
+      case StatusRefreshError(_, previousStatus) => migrationStatusToOptionOfMigrationIndexName(previousStatus)
       case InProgress(migrationIndexName) => Some(migrationIndexName)
       case _ => None
     }
@@ -19,7 +20,16 @@ object MigrationStatus {
 case object NotRunning extends MigrationStatus
 case class InProgress(migrationIndexName: String) extends MigrationStatus
 case object Complete extends MigrationStatus
-case object Error extends MigrationStatus
+case class StatusRefreshError(cause: Throwable, preErrorStatus: MigrationStatus) extends MigrationStatus
+object StatusRefreshError {
+  // custom constructor to unwrap when previousStatus is also Error - prevents nested Errors!
+  def apply(cause: Throwable, preErrorStatus: MigrationStatus): StatusRefreshError = {
+    preErrorStatus match {
+      case StatusRefreshError(_, olderStatus) => apply(cause, olderStatus)
+      case status => new StatusRefreshError(cause, status)
+    }
+  }
+}
 
 trait MigrationClient {
   self: ElasticSearchClient =>
@@ -39,7 +49,7 @@ trait MigrationClient {
         // Emits log messages when requesting the name fails, then swallows exception to prevent bubbling up
         // `migrationStatusRef` will remain the previous value until next scheduled execution
         logger.error("Failed to get name of index for ongoing migration", e)
-        Error
+        StatusRefreshError(cause = e, preErrorStatus = migrationStatusRef.get())
       }
 
     migrationStatusRef.set(
