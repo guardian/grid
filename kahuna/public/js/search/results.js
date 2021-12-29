2441180edec2625a9d960e8611e5f7405e1f3ff0
Ebuson <abdelrahmansd@gmail.com>
2021-12-29 10:22:29 +0200

    fix peopleInImage progress bar

--- kahuna/public/js/search/results.js
+++ kahuna/public/js/search/results.js
@@ -512,6 +512,9 @@ results.controller('SearchResultsCtrl', [
 
         $scope.$on("events:batch-operations:start", (e, entry) => {
             ctrl.batchOperations = [entry, ...ctrl.batchOperations];
+            if(entry.key === "peopleInImage" && ctrl.batchOperations.length > 1){
+              ctrl.batchOperations = [Object.assign({}, entry, { total: ctrl.batchOperations.length })]
+            }
 
             window.onbeforeunload = function() {
                 return 'Batch update in progress, are you sure you want to leave?';
