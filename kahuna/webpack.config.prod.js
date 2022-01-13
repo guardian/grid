541bc62d6d76fe45d9fd0b49f0303f4d86e920ff
Andrew Nowak <andrew.nowak@guardian.co.uk>
2022-01-13 18:53:49 +0000

    Upgrade loaders and plugins to latest

--- kahuna/webpack.config.prod.js
+++ kahuna/webpack.config.prod.js
@@ -1,15 +1,11 @@
-const TerserPlugin = require('terser-webpack-plugin');
 const shared = require('./webpack.config.shared');
 const { merge } = require('webpack-merge');
 
 module.exports = merge(shared, {
     mode: 'production',
+    devtool: 'source-map',
     optimization: {
       minimize: true,
-      minimizer: [new TerserPlugin({
-        sourceMap: true
-      })]
-
     }
   }
 );
