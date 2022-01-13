e37daa637323f4e47debab8a037b0f049b287df7
Andrew Nowak <andrew.nowak@guardian.co.uk>
2022-01-13 18:16:43 +0000

    Upgrade to latest webpack 4

--- kahuna/webpack.config.dev.js
+++ kahuna/webpack.config.dev.js
@@ -1,5 +1,5 @@
 const shared = require('./webpack.config.shared');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 module.exports = merge(shared,{
   mode: 'development',
