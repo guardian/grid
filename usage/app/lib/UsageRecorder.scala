73e26c90df45c80451358b2118e1c52ac6621db7
Andrew Nowak <andrew.nowak@guardian.co.uk>
2022-05-06 15:39:08 +0100

    Improve calculation of which usages should be updated
    Previous calculation of usages used a hacky override of .equals and
    .hashCode, which were buggy and hard to get right.  A better way is to
    extract the unique parts of the usage into its own case class, and use
    these for our set operations, and then index into maps to get the full
    usage details

--- usage/app/lib/UsageRecorder.scala
+++ usage/app/lib/UsageRecorder.scala
@@ -64,7 +64,14 @@ class UsageRecorder(
       )
 
       val dbUsages = matchedUsageGroup.dbUsages
+      val dbUsageMap = dbUsages.map(_.entry).toMap
+      val dbUsageKeys = dbUsageMap.keySet
+
+      val unremovedDbUsageKeys = dbUsageMap.filterNot(_._2.isRemoved).keySet
+
       val usageGroup   = matchedUsageGroup.usageGroup
+      val streamUsageMap = usageGroup.usages.map(_.entry).toMap
+      val streamUsageKeys = streamUsageMap.keySet
 
       dbUsages.foreach(mediaUsage => {
         logger.info(logMarker, s"Seen DB Usage for ${mediaUsage.mediaId}")
@@ -77,20 +84,23 @@ class UsageRecorder(
         val result = func(mediaUsage)
         logger.info(
           logMarker,
-          s"'$opName' DB Operation for ${mediaUsage.grouping} -  on mediaID: ${mediaUsage.mediaId} with result: ${result}"
+          s"'$opName' DB Operation for ${mediaUsage.grouping} - on mediaID: ${mediaUsage.mediaId} with result: $result"
         )
         usageMetrics.incrementUpdated
         result
       }
 
-      val markAsRemovedOps = (dbUsages diff usageGroup.usages)
+      val toMarkAsRemoved = (dbUsageKeys diff streamUsageKeys).flatMap(dbUsageMap.get)
+      val markAsRemovedOps = toMarkAsRemoved
         .map(performAndLogDBOperation(usageTable.markAsRemoved, "markAsRemoved"))
 
-      val createOps = (if(usageGroup.isReindex) usageGroup.usages else usageGroup.usages diff dbUsages)
-        .map(performAndLogDBOperation(usageTable.create, "create"))
+      val toCreate = (if(usageGroup.isReindex) streamUsageKeys else streamUsageKeys diff unremovedDbUsageKeys)
+        .flatMap(streamUsageMap.get)
+      val createOps = toCreate.map(performAndLogDBOperation(usageTable.create, "create"))
 
-      val updateOps = (if(usageGroup.isReindex) Set() else usageGroup.usages intersect dbUsages)
-        .map(performAndLogDBOperation(usageTable.update, "update"))
+      val toUpdate = (if (usageGroup.isReindex) Set() else streamUsageKeys intersect dbUsageKeys)
+        .flatMap(streamUsageMap.get)
+      val updateOps = toUpdate.map(performAndLogDBOperation(usageTable.update, "update"))
 
       val mediaIdsImplicatedInDBUpdates =
         (usageGroup.usages ++ dbUsages)
