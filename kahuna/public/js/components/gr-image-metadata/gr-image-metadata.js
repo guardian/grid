92ca2c2542734efc751e3a2e0ae130267c892f0f
Mateusz <mateusz.karpow@guardian.co.uk>
2022-10-03 16:34:24 +0100

    Use alias instead of elsticsearchPath
    this is better:
    - cannot have spaces
    - uses chips without fileMetadata. at the beginning

--- kahuna/public/js/components/gr-image-metadata/gr-image-metadata.js
+++ kahuna/public/js/components/gr-image-metadata/gr-image-metadata.js
@@ -222,7 +222,7 @@ module.controller('grImageMetadataCtrl', [
           .map(([key, value]) => {
             let fieldAlias = ctrl.fieldAliases.find(_ => _.alias === key);
             if (fieldAlias && fieldAlias.displayInAdditionalMetadata === true) {
-              return [fieldAlias.label, { value, elasticsearchPath: '"' + fieldAlias.elasticsearchPath + '"' }];
+              return [fieldAlias.label, { value, alias: fieldAlias.alias}];
             }
           })
           .filter(_ => _ !== undefined));
