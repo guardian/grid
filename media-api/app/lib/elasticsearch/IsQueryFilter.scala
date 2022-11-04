65c9f071ed99368333fd35eb3abd5668b3ae0ba1
Rhys Mills <rhystmills@gmail.com>
2022-11-04 11:18:23 +0000

    Add a new filter showing reapable images

--- media-api/app/lib/elasticsearch/IsQueryFilter.scala
+++ media-api/app/lib/elasticsearch/IsQueryFilter.scala
@@ -4,6 +4,8 @@ import com.gu.mediaservice.lib.ImageFields
 import com.gu.mediaservice.model._
 import com.sksamuel.elastic4s.ElasticDsl.matchAllQuery
 import com.sksamuel.elastic4s.requests.searches.queries.Query
+import lib.MediaApiConfig
+import org.joda.time.DateTime
 import scalaz.syntax.std.list._
 
 sealed trait IsQueryFilter extends Query with ImageFields {
@@ -15,19 +17,21 @@ sealed trait IsQueryFilter extends Query with ImageFields {
     case IsOwnedImage(staffPhotographerOrg) => s"$staffPhotographerOrg-owned"
     case _: IsDeleted => "deleted"
     case _: IsUnderQuota => "under-quota"
+    case _: IsReapable => "reapable"
   }
 }
 
 object IsQueryFilter {
   // for readability, the client capitalises gnm, so `toLowerCase` it before matching
-  def apply(value: String, overQuotaAgencies: () => List[Agency], staffPhotographerOrganisation: String): Option[IsQueryFilter] = {
-   val organisation = staffPhotographerOrganisation.toLowerCase
+  def apply(value: String, overQuotaAgencies: () => List[Agency], config: MediaApiConfig): Option[IsQueryFilter] = {
+    val organisation = config.staffPhotographerOrganisation.toLowerCase
     value.toLowerCase match {
       case s if s == s"$organisation-owned-photo" => Some(IsOwnedPhotograph(organisation))
       case s if s == s"$organisation-owned-illustration" => Some(IsOwnedIllustration(organisation))
       case s if s == s"$organisation-owned" => Some(IsOwnedImage(organisation))
       case "under-quota" => Some(IsUnderQuota(overQuotaAgencies()))
       case "deleted" => Some(IsDeleted(true))
+      case "reapable" => Some(IsReapable(config.persistedRootCollections, config.persistenceIdentifier))
       case _ => None
     }
   }
@@ -60,5 +64,28 @@ case class IsUnderQuota(overQuotaAgencies: List[Agency]) extends IsQueryFilter {
 case class IsDeleted(isDeleted: Boolean) extends IsQueryFilter {
   override def query: Query = filters.or(
     (filters.existsOrMissing("softDeletedMetadata", _))(isDeleted)
+  )
+}
+
+case class IsReapable(persistedRootCollections: List[String], persistenceIdentifier: String) extends IsQueryFilter {
+  val moreThanTwentyDaysOld = filters.date("uploadTime", Some(new DateTime(0)), Some(DateTime.now().minusDays(20))).getOrElse(matchAllQuery())
+
+  val persistedQueries = filters.or(
+    PersistedQueries.hasCrops,
+    PersistedQueries.usedInContent,
+    PersistedQueries.addedToLibrary,
+    PersistedQueries.hasUserEditsToImageMetadata,
+    PersistedQueries.hasPhotographerUsageRights,
+    PersistedQueries.hasIllustratorUsageRights,
+    PersistedQueries.hasAgencyCommissionedUsageRights,
+    PersistedQueries.addedToPhotoshoot,
+    PersistedQueries.hasLabels,
+    PersistedQueries.hasLeases,
+    PersistedQueries.existedPreGrid(persistenceIdentifier),
+    PersistedQueries.addedGNMArchiveOrPersistedCollections(persistedRootCollections)
+  )
+  override def query: Query = filters.and(
+    moreThanTwentyDaysOld,
+    filters.not(persistedQueries)
   )
 }
