8f21daab390c93e6fd6c188277d356c6d5741af4
fweddi <freddiepreece@outlook.com>
2022-11-14 15:11:14 +0000

    Add script for reindexing usages

---
+++ scripts/usage-reindex/reindex-usages.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+isFastForwarding=true
+
+touch lastQueriedId.txt
+lastQueriedId=$(cat lastQueriedId.txt)
+
+if [[ $lastQueriedId == "" ]]
+  then isFastForwarding=false
+fi
+
+while read line; do
+  id=$(echo "$line" | cut -d "," -f 1)
+  path=$(echo "$line" | cut -d "," -f 2)
+
+  if [[ $lastQueriedId == "$id" ]]
+    then isFastForwarding=false
+    continue
+  fi
+
+  if [[ $isFastForwarding == 'true' ]]
+    then continue
+  fi
+
+#  only gets to this if isFastForwarding is false
+  if curl "https://media-usage.test.dev-gutools.co.uk/usages/digital/content/$path/reindex" -H "X-Gu-Media-Key: $GRID_API_KEY" -f
+    then
+    echo "$id" > lastQueriedId.txt
+    echo "Reindexed id: $id, path: $path"
+  else
+    echo "$id" >> failedIds.txt
+    echo "Failed on id: $id, path: $path"
+  fi
+
+done < codeContentIdAndPath.csv
