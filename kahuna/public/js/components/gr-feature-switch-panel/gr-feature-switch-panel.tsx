b0961bf26af4e2e0f7f4dbda15f63e2d48f5e597
Rhys Mills <rhystmills@gmail.com>
2022-11-15 16:01:16 +0000

    Use css module

--- kahuna/public/js/components/gr-feature-switch-panel/gr-feature-switch-panel.tsx
+++ kahuna/public/js/components/gr-feature-switch-panel/gr-feature-switch-panel.tsx
@@ -4,30 +4,19 @@ import { react2angular } from "react2angular";
 import { useEffect, useRef, useState } from "react";
 
 import styles from "./gr-feature-switch-panel.module.css";
-const initialStyles = {
-  // backgroundColor: "grey",
-  // border: "1px solid black",
-  padding: "10px",
-  position: "fixed" as const,
-  top: "50px",
-  right: "0px",
-  visibility: "hidden" as "hidden" | "visible"
-};
-
-const closeIconStyles = {
-  fill: "rgb(204,204,204)"
-};
 
 type KeydownHandler = (e: KeyboardEvent) => void
 
 const CloseIcon = () =>
-  <svg style={closeIconStyles} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"height="18" width="18">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"height="18" width="18">
     <path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z"/>
   </svg>;
 
 const FeatureSwitchPanel: React.FC = () => {
     const [visible, setVisible] = useState(false);
-    const [styles, setStyles] = useState(initialStyles);
+    const [panelStyles, setPanelStyles] = useState({
+      visibility: "hidden" as "visible" | "hidden"
+    });
 
     const useEventListener = (eventName: string, handler: (e: KeyboardEvent) => void, element = window) => {
       const savedHandler = useRef<KeydownHandler>();
@@ -60,16 +49,26 @@ const FeatureSwitchPanel: React.FC = () => {
     useEventListener('keydown', handleKeyDown);
 
     useEffect(() => {
-      setStyles({
-        ...styles,
+      setPanelStyles({
         visibility: visible ? "visible" : "hidden"
       });
     }, [visible]);
 
     return (
-        <div style={styles} className="gr-panel__content">
-          Feature Switches
-          <CloseIcon/>
+        <div style={panelStyles}>
+          <div className={`gr-panel__content ${styles['panel']}`}>
+            <header>
+              <span className={`image-info__heading ${styles['panelTitle']}`}>
+                Feature Switches
+              </span>
+              <button
+                onClick={(e) => setVisible(false)}
+                className={styles['closeButton']}
+              >
+                <CloseIcon />
+              </button>
+            </header>
+          </div>
         </div>
     );
 };
