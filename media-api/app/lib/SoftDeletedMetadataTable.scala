c70c60a3d2ea77be4e9d2bb8374c2fa741424040
Tom Richards <tom.richards@guardian.co.uk>
2023-09-20 12:25:19 +0100

    stop using the global execution context for `SoftDeletedMetadataTable` operations

--- media-api/app/lib/SoftDeletedMetadataTable.scala
+++ media-api/app/lib/SoftDeletedMetadataTable.scala
@@ -2,25 +2,23 @@ package lib
 
 import com.gu.mediaservice.lib.aws.DynamoDB
 import com.gu.mediaservice.model.ImageStatusRecord
-import com.gu.scanamo.error.DynamoReadError
 import com.gu.scanamo._
 import com.gu.scanamo.syntax._
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
 
-class SoftDeletedMetadataTable(config: MediaApiConfig) extends DynamoDB(config, config.softDeletedMetadataTable) {
-  private val softDeletedMetadataTable = Table[ImageStatusRecord](config.softDeletedMetadataTable)
+class SoftDeletedMetadataTable(config: MediaApiConfig) extends DynamoDB[ImageStatusRecord](config, config.softDeletedMetadataTable) {
+  private val softDeletedMetadataTable = Table[ImageStatusRecord](table.getTableName)
 
-  def getStatus(imageId: String) = {
+  def getStatus(imageId: String)(implicit ex: ExecutionContext) = {
     ScanamoAsync.exec(client)(softDeletedMetadataTable.get('id -> imageId))
   }
 
-  def setStatus(imageStatus: ImageStatusRecord) = {
+  def setStatus(imageStatus: ImageStatusRecord)(implicit ex: ExecutionContext) = {
     ScanamoAsync.exec(client)(softDeletedMetadataTable.put(imageStatus))
   }
 
-  def updateStatus(imageId: String, isDeleted: Boolean) = {
+  def updateStatus(imageId: String, isDeleted: Boolean)(implicit ex: ExecutionContext) = {
     val updateExpression = set('isDeleted -> isDeleted)
     ScanamoAsync.exec(client)(
       softDeletedMetadataTable
