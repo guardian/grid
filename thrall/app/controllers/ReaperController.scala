753d04599442ca82aa4d65eaaf6e8f4408af511c
Tom Richards <tom.richards@guardian.co.uk>
2023-09-25 11:31:00 +0100

    [reaper] run 'reaper' every 15mins (IF reaper bucket is configured) weighted based on ingestion rate of the environment (to reduce configuration)

--- thrall/app/controllers/ReaperController.scala
+++ thrall/app/controllers/ReaperController.scala
@@ -1,7 +1,7 @@
 package controllers
 
+import akka.actor.Scheduler
 import com.gu.mediaservice.lib.ImageIngestOperations
-import com.gu.mediaservice.lib.auth.Authentication.Principal
 import com.gu.mediaservice.lib.auth.Permissions.DeleteImage
 import com.gu.mediaservice.lib.auth.{Authentication, Authorisation, BaseControllerWithLoginRedirects}
 import com.gu.mediaservice.lib.config.Services
@@ -9,28 +9,71 @@ import com.gu.mediaservice.lib.elasticsearch.ReapableEligibility
 import com.gu.mediaservice.lib.logging.{GridLogging, MarkerMap}
 import com.gu.mediaservice.lib.metadata.SoftDeletedMetadataTable
 import com.gu.mediaservice.model.{ImageStatusRecord, SoftDeletedMetadata}
-import lib.ThrallStore
+import lib.{ThrallConfig, ThrallStore}
 import lib.elasticsearch.ElasticSearch
 import org.joda.time.{DateTime, DateTimeZone}
-import play.api.libs.json.Json
-import play.api.mvc.{ControllerComponents, Result}
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
 
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters.collectionAsScalaIterableConverter
 import scala.language.postfixOps
 
 class ReaperController(
   es: ElasticSearch,
   store: ThrallStore,
   authorisation: Authorisation,
-  persistedRootCollections: List[String],
-  persistenceIdentifier: String,
+  config: ThrallConfig,
+  scheduler: Scheduler,
   softDeletedMetadataTable: SoftDeletedMetadataTable,
   override val auth: Authentication,
   override val services: Services,
   override val controllerComponents: ControllerComponents,
 )(implicit val ec: ExecutionContext) extends BaseControllerWithLoginRedirects with GridLogging {
 
-  private def batchDeleteWrapper(count: Int)(func: (Principal, ReapableEligibility) => Future[Result]) = auth.async { request =>
+  private val CONTROL_FILE_NAME = "PAUSED"
+
+  private val INTERVAL = 15 minutes // based on max of 1000 per reap, this interval will max out at 96,000 images per day
+  private val REAPS_PER_WEEK = 7.days / INTERVAL
+
+  implicit val logMarker: MarkerMap = MarkerMap()
+
+  private def getIsReapable = new ReapableEligibility {
+    override val persistedRootCollections: List[String] = config.persistedRootCollections
+    override val persistenceIdentifier: String = config.persistenceIdentifier
+  }
+
+  config.maybeReaperBucket.foreach { reaperBucket =>
+    scheduler.scheduleAtFixedRate(
+      initialDelay = 0 seconds,
+      interval = INTERVAL,
+    ){ () =>
+      if(store.client.doesObjectExist(reaperBucket, CONTROL_FILE_NAME)) {
+        logger.info("Reaper is paused")
+      } else {
+        es.countImagesIngestedInLast(7 days)(DateTime.now(DateTimeZone.UTC)).flatMap { imagesIngestedInLast7Days =>
+
+          val imagesIngestedPer15Mins = imagesIngestedInLast7Days / REAPS_PER_WEEK
+
+          val countOfImagesToReap = Math.min(imagesIngestedPer15Mins, 1000).toInt
+
+          if (countOfImagesToReap == 1000) {
+            logger.warn(s"Reaper is reaping at maximum rate of 1000 images per $INTERVAL. If this persists, the INTERVAL will need to become more frequent.")
+          }
+
+          val isReapable = getIsReapable
+
+          Future.sequence(Seq(
+            doBatchSoftReap(countOfImagesToReap, deletedBy = "reaper", isReapable),
+            doBatchHardReap(countOfImagesToReap, deletedBy = "reaper", isReapable)
+          ))
+        }
+      }
+    }
+  }
+
+  private def batchDeleteWrapper(count: Int)(func: (Int, String, ReapableEligibility) => Future[JsValue]) = auth.async { request =>
     if (!authorisation.hasPermissionTo(DeleteImage)(request.user)) {
       Future.successful(Forbidden)
     }
@@ -38,77 +81,76 @@ class ReaperController(
       Future.successful(BadRequest("Too many IDs. Maximum 1000."))
     }
     else {
-      val isReapable = new ReapableEligibility {
-        override val persistedRootCollections: List[String] = ReaperController.this.persistedRootCollections
-        override val persistenceIdentifier: String = ReaperController.this.persistenceIdentifier
-      }
       func(
-        request.user,
-        isReapable
-      )
+        count,
+        request.user.accessor.identity,
+        getIsReapable
+      ).map(Ok(_))
     }
   }
 
-  def doBatchSoftReap(count: Int) = batchDeleteWrapper(count){ (user, isReapable) => Future {
+  private def persistedBatchDeleteOperation(deleteType: String)(doBatchDelete: => Future[JsValue]) = config.maybeReaperBucket match {
+    case None => Future.failed(new Exception("Reaper bucket not configured"))
+    case Some(reaperBucket) => doBatchDelete.map { json =>
+      val now = DateTime.now(DateTimeZone.UTC)
+      val key = s"$deleteType/${now.toString("YYYY-MM-dd")}/$deleteType-${now.toString()}.json"
+      store.client.putObject(reaperBucket, key, json.toString())
+      json
+    }
+  }
 
-    implicit val logMarker: MarkerMap = MarkerMap()
+  def doBatchSoftReap(count: Int): Action[AnyContent] = batchDeleteWrapper(count)(doBatchSoftReap)
+
+  def doBatchSoftReap(count: Int, deletedBy: String, isReapable: ReapableEligibility): Future[JsValue] = persistedBatchDeleteOperation("soft"){
 
     logger.info(s"Soft deleting next $count images...")
 
     val deleteTime = DateTime.now(DateTimeZone.UTC)
-    val deletedBy = user.accessor.identity
 
-    val results = for {
+    (for {
       idsSoftDeletedInES: Set[String] <- es.softDeleteNextBatchOfImages(isReapable, count, SoftDeletedMetadata(deleteTime, deletedBy))
-      // TODO add some logging that no IDs needed deleting (might already happen inside ES function)
       if idsSoftDeletedInES.nonEmpty
       dynamoStatuses <- softDeletedMetadataTable.setStatuses(idsSoftDeletedInES.map(
         ImageStatusRecord(
           _,
-          deletedBy = user.accessor.identity,
+          deletedBy,
           deleteTime = deleteTime.toString,
           isDeleted = true
         )
       ))
-    } yield idsSoftDeletedInES //TODO do something with the dynamoStatuses and log per ID
+    } yield idsSoftDeletedInES.map { id =>
+      val detail = Map(
+        "dynamo" -> !dynamoStatuses.exists(_.getUnprocessedItems.values().asScala.exists(_.asScala.exists(_.getPutRequest.getItem.get("id").getS == id))),
+      )
+      logger.info(s"Soft deleted image $id : $detail")
+      id -> detail
+    }.toMap).map(Json.toJson(_))
+  }
 
-    val resultsJson = results.map(Json.toJson(_))
-
-    // FIXME write permanent log to S3
-
-    resultsJson.map(Ok(_))
-  }.flatten}
 
 
-  def doBatchHardReap(count: Int) = batchDeleteWrapper(count){ (_, isReapable) => Future {
+  def doBatchHardReap(count: Int): Action[AnyContent] = batchDeleteWrapper(count)(doBatchHardReap)
 
-    implicit val logMarker: MarkerMap = MarkerMap()
+  def doBatchHardReap(count: Int, deletedBy: String, isReapable: ReapableEligibility): Future[JsValue] = persistedBatchDeleteOperation("hard"){
 
     logger.info(s"Hard deleting next $count images...")
 
-    val results = for {
+    (for {
       idsHardDeletedFromES: Set[String] <- es.hardDeleteNextBatchOfImages(isReapable, count)
-      // TODO add some logging that no IDs needed deleting (might already happen inside ES function)
       if idsHardDeletedFromES.nonEmpty
       mainImagesS3Deletions <- store.deleteOriginals(idsHardDeletedFromES)
       thumbsS3Deletions <- store.deleteThumbnails(idsHardDeletedFromES)
       pngsS3Deletions <- store.deletePNGs(idsHardDeletedFromES)
     } yield idsHardDeletedFromES.map { id =>
-      // TODO log per ID
-      id -> Map(
+      val detail = Map(
         "ES" -> Some(true), // since this is list of IDs deleted from ES
         "mainImage" -> mainImagesS3Deletions.get(ImageIngestOperations.fileKeyFromId(id)),
         "thumb" -> thumbsS3Deletions.get(ImageIngestOperations.fileKeyFromId(id)),
         "optimisedPng" -> pngsS3Deletions.get(ImageIngestOperations.optimisedPngKeyFromId(id))
       )
-    }.toMap
-
-    val resultsJson = results.map(Json.toJson(_))
-
-    // FIXME write permanent log to S3
-
-    resultsJson.map(Ok(_))
-
-  }.flatten}
+      logger.info(s"Hard deleted image $id : $detail")
+      id -> detail
+    }.toMap).map(Json.toJson(_))
+  }
 
 }
