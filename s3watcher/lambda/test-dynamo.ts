fd4f1b33e9b6943ab140fffb4be6f40022a6d3ed
David Blatcher <dv.bltchr@gmail.com>
2023-10-05 11:43:40 +0100

    separate out the config logic

--- s3watcher/lambda/test-dynamo.ts
+++ s3watcher/lambda/test-dynamo.ts
@@ -9,26 +9,18 @@ const envConfig = readConfig()
 // config problem? getting error trying to reach dynamo from local stack:
 // code: 'NetworkingError',
 // Error: unable to verify the first certificate
-const USE_LOCAL_STACK_ON_DEV = false as boolean
+const USE_LOCAL_STACK_ON_DEV = true as boolean
 
-const configureAndCreateDynamo = () => {
-  if (!USE_LOCAL_STACK_ON_DEV || !envConfig.isDev) {
-    const credentials = new AWS.SharedIniFileCredentials({
-      profile: "media-service",
-    })
-    AWS.config.credentials = credentials
-  }
-
-  const awsConfig =
-    USE_LOCAL_STACK_ON_DEV && envConfig.isDev
-      ? {
-          accessKeyId: "test",
-          secretAccessKey: "test",
-          region: envConfig.region,
-          endpoint: "https://localstack.media.local.dev-gutools.co.uk",
-          s3ForcePathStyle: true,
-        }
-      : undefined
+const configureAndCreateDynamoWithLocalStackOnDev = () => {
+  const awsConfig = envConfig.isDev
+    ? {
+        accessKeyId: "test",
+        secretAccessKey: "test",
+        region: envConfig.region,
+        endpoint: "https://localstack.media.local.dev-gutools.co.uk",
+        s3ForcePathStyle: true,
+      }
+    : undefined
 
   AWS.config.update({
     region: envConfig.region,
@@ -40,16 +32,43 @@ const configureAndCreateDynamo = () => {
   })
 }
 
-const ddb = configureAndCreateDynamo()
+const configureAndCreateDynamoUsingAwsOnDev = () => {
+  if (envConfig.isDev) {
+    const credentials = new AWS.SharedIniFileCredentials({
+      profile: envConfig.profile,
+    })
+    AWS.config.credentials = credentials
+  }
+
+  AWS.config.update({
+    region: envConfig.region,
+  })
+
+  return new AWS.DynamoDB({
+    apiVersion: "2012-08-10",
+  })
+}
+
+const ddb = USE_LOCAL_STACK_ON_DEV
+  ? configureAndCreateDynamoWithLocalStackOnDev()
+  : configureAndCreateDynamoUsingAwsOnDev()
 
 // access the test table for now -  could parameterise and/or use config to set this
 const TEST_TABLE_PREFIX = "media-service-TEST-UploadStatusDynamoTable"
 
 const logResults = async () => {
-  const tableName = await getTableNameFromPrefix(ddb, TEST_TABLE_PREFIX)
-  const results = await scanTable(ddb, tableName, 10)
-  console.log(`items in ${TEST_TABLE_PREFIX}`)
-  console.log(results)
+  try {
+
+    console.log(' >>> looking up table name...')
+    const tableName = await getTableNameFromPrefix(ddb, TEST_TABLE_PREFIX)
+    console.log(` >>> scanning table ${tableName}...`)
+    const results = await scanTable(ddb, tableName, 10)
+    console.log(` >>> ${results?.length || 0} items in ${TEST_TABLE_PREFIX}:`)
+    console.log(results)
+  } catch(err) {
+    console.log(' >>> FAILED')
+    console.log(err)
+  }
 }
 
 logResults().finally(exit)
