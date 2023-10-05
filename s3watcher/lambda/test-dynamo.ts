988d36833b797b1373b27ec2046cf6e4f19c4369
David Blatcher <dv.bltchr@gmail.com>
2023-10-05 17:27:12 +0100

    always use local stack on dev

--- s3watcher/lambda/test-dynamo.ts
+++ s3watcher/lambda/test-dynamo.ts
@@ -12,60 +12,34 @@ import { exit } from "process"
 
 const envConfig = readConfig()
 
-const USE_LOCAL_STACK_ON_DEV = true as boolean
+const awsConfig = envConfig.isDev
+  ? {
+      accessKeyId: "test",
+      secretAccessKey: "test",
+      region: envConfig.region,
+      // running locally need to use localhost, not the proxy
+      // error using  https://localstack.media.local.dev-gutools.co.uk was:
+      // code: 'NetworkingError',
+      // Error: unable to verify the first certificate
+      endpoint: "http://localhost:4566",
+    }
+  : undefined
 
-const configureAndCreateDynamoWithLocalStackOnDev = () => {
-  const awsConfig = envConfig.isDev
-    ? {
-        accessKeyId: "test",
-        secretAccessKey: "test",
-        region: envConfig.region,
-        // running locally need to use localhost, not the proxy
-        // error using  https://localstack.media.local.dev-gutools.co.uk was:
-        // code: 'NetworkingError',
-        // Error: unable to verify the first certificate
-        endpoint: "http://localhost:4566",
-      }
-    : undefined
+AWS.config.update({
+  region: envConfig.region,
+})
 
-  AWS.config.update({
-    region: envConfig.region,
-  })
-
-  return new AWS.DynamoDB({
-    ...awsConfig,
-    apiVersion: "2012-08-10",
-  })
-}
-
-const configureAndCreateDynamoUsingAwsOnDev = () => {
-  if (envConfig.isDev) {
-    const credentials = new AWS.SharedIniFileCredentials({
-      profile: envConfig.profile,
-    })
-    AWS.config.credentials = credentials
-  }
-
-  AWS.config.update({
-    region: envConfig.region,
-  })
-
-  return new AWS.DynamoDB({
-    apiVersion: "2012-08-10",
-  })
-}
-
-const ddb = USE_LOCAL_STACK_ON_DEV
-  ? configureAndCreateDynamoWithLocalStackOnDev()
-  : configureAndCreateDynamoUsingAwsOnDev()
+const ddb = new AWS.DynamoDB({
+  ...awsConfig,
+  apiVersion: "2012-08-10",
+})
 
 // Table name is not the same formatin localstack as in AWS
 // Only accessing the test table for now (peace of mind - don't want to touch PROD while testing)
 // TO DO - parameterise the prefix using envConfig.stage (PROD=PROD, CODE=TEST) or get the full name from config
-const TABLE_PREFIX =
-  envConfig.isDev && USE_LOCAL_STACK_ON_DEV
-    ? "UploadStatusTable"
-    : "media-service-TEST-UploadStatusDynamoTable"
+const TABLE_PREFIX = envConfig.isDev
+  ? "UploadStatusTable"
+  : "media-service-TEST-UploadStatusDynamoTable"
 
 const logAllRecords = async () => {
   console.log(` >>> looking up table name beginning with "${TABLE_PREFIX}"...`)
@@ -91,7 +65,12 @@ const putInARecord = async (recordToPut: UploadStatusTableRecord) => {
 }
 
 const recordToPut = createQueuedUploadRecord(
-  ["path", "to", "file", "image with encoded id.tiff?random-param=foobar&otherparam=1234567"],
+  [
+    "path",
+    "to",
+    "file",
+    "image with encoded id.tiff?random-param=foobar&otherparam=1234567",
+  ],
   "image with encoded id.tiff",
   new Date("2030-01-01").valueOf()
 )
