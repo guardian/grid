2f0a1648fe16264b748eea52dfac05b70b74dac9
Tom Richards <tom.richards@guardian.co.uk>
2023-11-20 17:17:32 +0000

    'digest' files from the queue (read to temp. file, producing SHA1 hash along the way) ready to be wired into existing logic from either `loadImage` or `importImage`
    Co-authored-by: David Blatcher <30567854+dblatcher@users.noreply.github.com>

--- image-loader/app/lib/ImageLoaderStore.scala
+++ image-loader/app/lib/ImageLoaderStore.scala
@@ -4,14 +4,13 @@ import lib.ImageLoaderConfig
 import com.gu.mediaservice.lib
 import _root_.lib.DigestedFile
 import com.amazonaws.HttpMethod
+import java.io.File
 import java.time.ZonedDateTime
 import java.util.Date
 
 class ImageLoaderStore(config: ImageLoaderConfig) extends lib.ImageIngestOperations(config.imageBucket, config.thumbnailBucket, config) {
 
-  def addFileToIngestBucket(digestedFile:DigestedFile) = {
-    client.putObject(config.ingestBucket, digestedFile.digest, digestedFile.file)
-  }
+  def getIngestObject(key: String) = client.getObject(config.ingestBucket, key)
 
   def generatePreSignedUploadUrl(filename: String, expiration: ZonedDateTime, uploadedBy: String): String = {
     client.generatePresignedUrl(
