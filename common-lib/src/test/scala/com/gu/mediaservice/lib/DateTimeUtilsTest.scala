a145fbac79be77d5af38304e71c1830c900aeaf8
Tom Richards <tom.richards@guardian.co.uk>
2023-11-22 11:27:14 +0000

    ensure reaper doesn't run more frequently than the interval when grid is redeployed, i.e. behave more like cron

--- common-lib/src/test/scala/com/gu/mediaservice/lib/DateTimeUtilsTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/DateTimeUtilsTest.scala
@@ -4,6 +4,11 @@ import org.joda.time.DateTime
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
+import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit
+import scala.concurrent.duration.{DurationInt, DurationLong, FiniteDuration}
+
+
 class DateTimeUtilsTest extends AnyFunSpec with Matchers {
   it ("should convert a string to a DateTime") {
     val dateString = "2020-01-01T12:34:56.000Z"
@@ -20,5 +25,29 @@ class DateTimeUtilsTest extends AnyFunSpec with Matchers {
   it ("should return a date with no input") {
     val actual = DateTimeUtils.fromValueOrNow(None)
     actual shouldBe a[DateTime]
+  }
+
+  it ("should return the time until the next instance of the interval relative to the hour"){
+    def toZonedDateTime(timePart: String) = ZonedDateTime.parse(s"2023-11-21T${timePart}Z[Europe/London]")
+    def test(nowTime: String, expectedTime: String, interval: FiniteDuration = 15.minutes) = {
+      DateTimeUtils.timeUntilNextInterval(
+        interval,
+        toZonedDateTime(nowTime)
+      ) shouldEqual ChronoUnit.MILLIS.between(
+        toZonedDateTime(nowTime),
+        toZonedDateTime(expectedTime)
+      ).millis
+    }
+    test(nowTime = "11:11:23.887", expectedTime = "11:15:00.000")
+    test(nowTime = "11:23:23.887", expectedTime = "11:30:00.000")
+    test(nowTime = "11:33:23.887", expectedTime = "11:45:00.000")
+    test(nowTime = "11:50:23.887", expectedTime = "12:00:00.000")
+    test(nowTime = "11:00:00.000", expectedTime = "11:00:00.000")
+    test(nowTime = "11:00:00.001", expectedTime = "11:15:00.000")
+
+    test(nowTime = "11:00:00.001", expectedTime = "11:01:00.000", interval = 1.minute)
+    test(nowTime = "11:00:00.001", expectedTime = "11:02:00.000", interval = 2.minute)
+
+    test(nowTime = "11:01:00.001", expectedTime = "12:00:00.000", interval = 1.hour)
   }
 }
