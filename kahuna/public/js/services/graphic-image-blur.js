guardian/grid

commit 5b8be76fa91d506fd5ca8a1ad8410fecc46390e1

[graphic-content blurring] refine the blurring based on SMOUT (Sensitive Material Out) to when it appears capitalised in special instructions or as an entire keyword (case in-sensitive)
crucially no longer matching on title & description too (like the other search phrases) since this was causing many false positives e.g. when description includes the word `Portsmouth`

kahuna/public/js/services/graphic-image-blur.js
@@ -31,8 +31,8 @@ export const graphicImageBlurService = angular.module("kahuna.services.graphicIm
         window.location.reload();
       },
       isPotentiallyGraphic: (image) => shouldBlurGraphicImages && (
-        image.data.isPotentiallyGraphic || // server can flag images as potentially graphic by inspecting deep in the metadata at query time (not available to the client)
-        !![
+        image.data.isPotentiallyGraphic // server can flag images as potentially graphic by inspecting deep in the metadata at query time (not available to the client)
+        || !![
           'graphic content',
           'depicts death',
           'dead child',
@@ -41,14 +41,16 @@ export const graphicImageBlurService = angular.module("kahuna.services.graphicIm
           'dead body',
           'dead bodies',
           'body of',
-          'bodies of',
-          'smout' // this is short for sensitive material out, often used in specialInstructions
+          'bodies of'
         ].find( searchPhrase =>
           image.data?.metadata?.description?.toLowerCase()?.includes(searchPhrase) ||
           image.data?.metadata?.title?.toLowerCase()?.includes(searchPhrase) ||
           image.data?.metadata?.specialInstructions?.toLowerCase()?.includes(searchPhrase) ||
           image.data?.metadata?.keywords?.find(keyword => keyword?.toLowerCase()?.includes(searchPhrase))
         ))
+        // SMOUT is short for sensitive material out, often used in specialInstructions
+        || image.data?.metadata?.specialInstructions?.includes("SMOUT")
+        || !!image.data?.metadata?.keywords?.find(keyword => keyword?.toUpperCase() === "SMOUT")
     };
   }]
 );

commit ac8c937b912ac5aaedebe0f6fdddf0efc8b5574f

[graphic-content blurring] fix ORing logic broken in 5b8be76fa91d506fd5ca8a1ad8410fecc46390e1

kahuna/public/js/services/graphic-image-blur.js
@@ -47,10 +47,11 @@ export const graphicImageBlurService = angular.module("kahuna.services.graphicIm
           image.data?.metadata?.title?.toLowerCase()?.includes(searchPhrase) ||
           image.data?.metadata?.specialInstructions?.toLowerCase()?.includes(searchPhrase) ||
           image.data?.metadata?.keywords?.find(keyword => keyword?.toLowerCase()?.includes(searchPhrase))
-        ))
+        )
         // SMOUT is short for sensitive material out, often used in specialInstructions
         || image.data?.metadata?.specialInstructions?.includes("SMOUT")
         || !!image.data?.metadata?.keywords?.find(keyword => keyword?.toUpperCase() === "SMOUT")
+      )
     };
   }]
 );