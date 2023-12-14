e6acd94ff65a0756956ffb43bbeae6cf12ab6404
AndyKilmory <andrew.downing@bbc.co.uk>
2023-12-14 14:10:15 +0000

    re-basing branch

--- kahuna/public/js/components/gr-permissions-filter/gr-toggle-switch.tsx
+++ kahuna/public/js/components/gr-permissions-filter/gr-toggle-switch.tsx
@@ -4,14 +4,16 @@ import "./gr-toggle-switch.css";
 interface ToggleSwitchProps {
   label: string;
   chargeable: boolean;
+  chargeableChange: () => void;
 }
 
 const ToggleSwitch:React.FC<ToggleSwitchProps> = (props) => {
-  const {label, chargeable} = props
+  const {label, chargeable, chargeableChange} = props
   const [isToggled, setIsToggled] = React.useState(chargeable);
 
   const handleToggle = () => {
     setIsToggled(prevState => !prevState);
+    chargeableChange();
   };
 
   return (
