21e244b536d56e250159a99f0ff9e4355252d523
Tom Richards <tom.richards@guardian.co.uk>
2023-12-19 12:19:31 +0000

    use pattern matching in the extraction of S3 key from SQS message (rather than imperative guard pattern)
    Co-authored-by: David Blatcher <30567854+dblatcher@users.noreply.github.com>

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/SqsHelpers.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/SqsHelpers.scala
@@ -15,17 +15,12 @@ trait SqsHelpers {
     // we only need the key, the full message shape can be seen at
     // https://docs.aws.amazon.com/AmazonS3/latest/userguide/notification-content-structure.html
 
-    val s3Records = Json.parse(message.getBody) \ "Records" \\ "s3"
-
-    if(s3Records.size != 1) {
-      throw new Exception(s"Expected 1 record containing 's3' in message body, got ${s3Records.size}")
-    }
-
     // note that we don't bother reading the bucket name currently, but attempt the read elsewhere based on the
     // configured bucket value, this could be checked here if we're concerned (s3Records.head \ "bucket" \ "name")
 
-    uriDecode(
-      (s3Records.head \ "object" \ "key").as[String]
-    )
+    Json.parse(message.getBody) \ "Records" \\ "s3" match {
+      case head :: Nil => uriDecode((head \ "object" \ "key").as[String])
+      case s3Records => throw new Exception(s"Expected 1 record containing 's3' in message body, got ${s3Records.size}")
+    }
   }
 }
