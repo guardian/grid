6ac6d13b802fd411f1c6d53bda5a7672f5a07f3f
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-05-19 12:17:52 +0100

    Patch instance url s"" usages with toString.

--- common-lib/src/main/scala/com/gu/mediaservice/model/Instance.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/model/Instance.scala
@@ -1,3 +1,5 @@
 package com.gu.mediaservice.model
 
-case class Instance(id: String)
+case class Instance(id: String) {
+  override def toString: String = id  // TODO need to visit all the urls builders an make them use .id
+}
