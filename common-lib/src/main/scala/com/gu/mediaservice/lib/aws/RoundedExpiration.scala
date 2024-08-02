b03fdf46cd1c4087348995d2615fbea3acf1ce74
Tony McCrae <tony.mccrae@guardian.co.uk>
2024-08-02 12:10:14 +0100

    Rename to highlight caching intent.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/RoundedExpiration.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/RoundedExpiration.scala
@@ -6,7 +6,7 @@ trait RoundedExpiration {
 
   // Round expiration time to try and hit the cache as much as possible
   // TODO: do we really need these expiration tokens? they kill our ability to cache...
-  def defaultExpiration: DateTime = roundDateTime(DateTime.now, Duration.standardMinutes(10)).plusMinutes(20)
+  def cachableExpiration: DateTime = roundDateTime(DateTime.now, Duration.standardMinutes(10)).plusMinutes(20)
 
   private def roundDateTime(t: DateTime, d: Duration): DateTime = t minus (t.getMillis - (t.getMillis.toDouble / d.getMillis).round * d.getMillis)
 
