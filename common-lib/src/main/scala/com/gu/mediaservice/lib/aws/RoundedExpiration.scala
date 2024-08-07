1fa7ecae87c177efc58faa0a227c495dd48eeb10
Tony McCrae <tony.mccrae@guardian.co.uk>
2024-08-07 17:11:52 +0100

    Test exercise cachableExpiration to show that it's value are within a range of 15 to 25 minutes into the future.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/RoundedExpiration.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/RoundedExpiration.scala
@@ -8,6 +8,10 @@ trait RoundedExpiration {
   // TODO: do we really need these expiration tokens? they kill our ability to cache...
   def cachableExpiration(after: DateTime = DateTime.now): DateTime = roundDateTime(after, Duration.standardMinutes(10)).plusMinutes(20)
 
-  private def roundDateTime(t: DateTime, d: Duration): DateTime = t minus (t.getMillis - (t.getMillis.toDouble / d.getMillis).round * d.getMillis)
+  private def roundDateTime(t: DateTime, d: Duration): DateTime = {
+    // Appears to round up or down to the nearest t minute interval and then adds d.
+    // For inputs 10 and 20 the output will be a a range between 15 and 25.
+    t minus (t.getMillis - (t.getMillis.toDouble / d.getMillis).round * d.getMillis)
+  }
 
 }
--- common-lib/src/test/scala/com/gu/mediaservice/lib/aws/RoundedExpirationTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/aws/RoundedExpirationTest.scala
@@ -1,10 +1,10 @@
 package com.gu.mediaservice.lib.aws
-import org.joda.time.DateTime
+
+import org.joda.time.{DateTime, Duration}
 import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
 
-
-class RoundedExpirationTest extends AnyFunSuiteLike with  RoundedExpiration {
+class RoundedExpirationTest extends AnyFunSuiteLike with RoundedExpiration {
 
   test("should provide an expiration date in the near future") {
     val expires = cachableExpiration()
@@ -18,6 +18,15 @@ class RoundedExpirationTest extends AnyFunSuiteLike with  RoundedExpiration {
     val soon = now.plusSeconds(7);
 
     cachableExpiration(now) mustEqual cachableExpiration(soon)
+  }
+
+  test("should always give an expiration time at least 10 minutes into the future") {
+    val now = new DateTime(2024, 8, 7, 12, 20, 0, 0)
+    val durations = Range.inclusive(0, 20).map { i =>
+      val time = now.plusMinutes(i)
+      new Duration(time, cachableExpiration(time))
+    }
+    durations.forall(_.getStandardMinutes >= 10) mustBe true
   }
 
 }
