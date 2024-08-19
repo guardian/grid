dfd43a3d72dfb8dd24615f6603b8acb83551527f
AndyKilmory <andrew.downing@bbc.co.uk>
2024-08-19 17:25:49 +0100

    adding handler for set payable images event

--- kahuna/public/js/components/gr-permissions-filter/gr-permissions-filter.tsx
+++ kahuna/public/js/components/gr-permissions-filter/gr-permissions-filter.tsx
@@ -63,6 +63,10 @@ const hasClassInSelfOrParent = (node: Element | null, className: string): boolea
   return false;
 };
 
+//-- payable images event (optional param to avoid CI check issues) --
+interface PayableImagesEventDetail { showPaid: boolean }
+interface PayableImagesEvent extends CustomEvent<PayableImagesEventDetail> {optional?: string}
+
 //-- query change event - adding optional param to avoid CI/CD check issue!--
 interface QueryChangeEventDetail { query: string, showPaid: boolean }
 interface QueryChangeEvent extends CustomEvent<QueryChangeEventDetail> {optional?: string}
@@ -98,6 +102,10 @@ const PermissionsFilter: React.FC<PermissionsWrapperProps> = ({ props }) => {
     setIsChargeable(event.detail.showPaid);
   };
 
+  const handleSetPayableImages = (event: PayableImagesEvent) => {
+    setIsChargeable(event.detail.showPaid);
+  };
+
   const handleQueryChange = (event: QueryChangeEvent ) => {
     const newQuery = event.detail.query ? (" " + event.detail.query) : "";
 
@@ -122,6 +130,7 @@ const PermissionsFilter: React.FC<PermissionsWrapperProps> = ({ props }) => {
   useEffect(() => {
     window.addEventListener('queryChangeEvent', handleQueryChange);
     window.addEventListener('logoClick', handleLogoClick);
+    window.addEventListener('setPayableImages', handleSetPayableImages);
     window.addEventListener('mouseup', autoHideListener);
     window.addEventListener('scroll', autoHideListener);
     window.addEventListener('keydown', autoHideListener);
@@ -132,9 +141,10 @@ const PermissionsFilter: React.FC<PermissionsWrapperProps> = ({ props }) => {
       setCurrentIndex(-1);
       window.removeEventListener('queryChangeEvent', handleQueryChange);
       window.removeEventListener('logoClick', handleLogoClick);
-      window.removeEventListener("mouseup", autoHideListener);
-      window.removeEventListener("scroll", autoHideListener);
-      window.removeEventListener("keydown", autoHideListener);
+      window.removeEventListener('setPayableImages', handleSetPayableImages);
+      window.removeEventListener('mouseup', autoHideListener);
+      window.removeEventListener('scroll', autoHideListener);
+      window.removeEventListener('keydown', autoHideListener);
     };
   }, []);
 
