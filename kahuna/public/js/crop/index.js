2ac4d875ee236407cd8b3efce49445c13562ad09
Tom Richards <tom.richards@guardian.co.uk>
2024-10-01 23:35:41 +0100

    add a `shouldShowCropGuttersIfApplicable` query param and only show crop gutters if `true` (and other conditions for showing the gutters)

--- kahuna/public/js/crop/index.js
+++ kahuna/public/js/crop/index.js
@@ -18,7 +18,7 @@ crop.config(['$stateProvider',
              function($stateProvider) {
 
     $stateProvider.state('crop', {
-        url: '/images/:imageId/crop?cropType&customRatio',
+        url: '/images/:imageId/crop?cropType&customRatio&shouldShowCropGuttersIfApplicable',
         template: cropTemplate,
         controller: 'ImageCropCtrl',
         controllerAs: 'ctrl',
