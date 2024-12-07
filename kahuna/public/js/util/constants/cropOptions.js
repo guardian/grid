f8488604b942642f0f72d5dab774fa27423af282
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-12-07 11:13:08 +0000

    Register 3:2 as a crop option.

--- kahuna/public/js/util/constants/cropOptions.js
+++ kahuna/public/js/util/constants/cropOptions.js
@@ -1,9 +1,9 @@
 // `ratioString` is sent to the server, being `undefined` for `freeform` is expected 🙈
-export const landscapeOld = {key: 'landscape OLD', ratio: 5 / 3, ratioString: '5:3', isHidden: true};
-export const landscape = {key: 'landscape', ratio: 5 / 4, ratioString: '5:4'};
+export const landscape32 = {key: 'landscape32', ratio: 3 / 2, ratioString: '3:2'};
+export const landscape = {key: 'landscape', ratio: 5 / 3, ratioString: '5:3'};
 export const portrait = {key: 'portrait', ratio: 4 / 5, ratioString: '4:5'};
 export const video = {key: 'video', ratio: 16 / 9, ratioString: '16:9'};
 export const square = {key: 'square', ratio: 1, ratioString: '1:1'};
 export const freeform = {key: 'freeform', ratio: null, isDefault: true};
 
-export const cropOptions = [landscapeOld, landscape, portrait, video, square, freeform];
+export const cropOptions = [landscape32, landscape, portrait, video, square, freeform];
