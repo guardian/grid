63099f9e876fd22404b1af9c1e26640654d9dfe7
AndyKilmory <andrew.downing@bbc.co.uk>
2025-01-08 10:21:52 +0000

    removing commented out previous version of parserRe

--- kahuna/public/js/search/structured-query/syntax.js
+++ kahuna/public/js/search/structured-query/syntax.js
@@ -3,7 +3,6 @@ import {getLabel, getCollection} from '../../search-query/query-syntax';
 
 // Line too long for eslint, but can't break it down..
 /*eslint-disable max-len */
-//const parserRe = /(-?)(?:(?:([a-zA-Z@><]+):|(#)|(~))(?:([^ "']+)|"([^"]+)"|'([^']+)')|([a-zA-Z0-9]+)|"([^"]*)"|'([^']*)')/g;
 const parserRe = /(-?)(?:(?:([\p{L}@><]+):|(#)|(~))(?:([^ "']+)|"([^"]+)"|'([^']+)')|([\p{L}0-9]+)|"([^"]*)"|'([^']*)')/gu;
 /*eslint-enable max-len */
 const falsyValuesToEmptyString = (value) => {
