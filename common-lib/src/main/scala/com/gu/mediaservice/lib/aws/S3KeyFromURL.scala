12b446411628b7d431fcedef16a0f79e07fb6959
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-01-24 09:36:49 +0000

    Local S£ urls to keys.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/S3KeyFromURL.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/S3KeyFromURL.scala
@@ -8,9 +8,9 @@ trait S3KeyFromURL extends GridLogging {
 
   def keyFromS3URL(bucket: S3Bucket, url: URI): String = {
     val key = if (bucket.endpoint == "10.0.45.121:32090") {
-      url.getPath.drop(bucket.bucket.length + 1)
+      url.getPath.drop(bucket.bucket.length + 2)
     } else {
-      url.getPath.drop(2)
+      url.getPath.drop(1)
     }
     logger.info("Key from bucket " + bucket + " URL " + url + ": " + key)
     key
