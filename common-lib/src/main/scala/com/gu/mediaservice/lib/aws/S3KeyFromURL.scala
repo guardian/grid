b06193c957a59a264c3c4c44e583e53cba2d4a09
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-02-05 19:14:10 +0000

    Log levels.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/aws/S3KeyFromURL.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/aws/S3KeyFromURL.scala
@@ -7,13 +7,11 @@ import java.net.URI
 trait S3KeyFromURL extends GridLogging {
 
   def keyFromS3URL(bucket: S3Bucket, url: URI): String = {
-    val key = if (bucket.endpoint == "minio.griddev.eelpieconsulting.co.uk") {
+    if (bucket.endpoint == "minio.griddev.eelpieconsulting.co.uk") {
       url.getPath.drop(bucket.bucket.length + 2)
     } else {
       url.getPath.drop(1)
     }
-    logger.info("Key from bucket " + bucket + " URL " + url + ": " + key)
-    key
   }
 
 }
