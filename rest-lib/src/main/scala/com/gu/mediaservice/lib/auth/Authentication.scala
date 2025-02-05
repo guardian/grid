f06f24abaae4d6246b88e6a36cc9bb619e95d32b
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-02-05 18:43:39 +0000

    Log levels.

--- rest-lib/src/main/scala/com/gu/mediaservice/lib/auth/Authentication.scala
+++ rest-lib/src/main/scala/com/gu/mediaservice/lib/auth/Authentication.scala
@@ -88,7 +88,6 @@ class Authentication(config: CommonConfig,
             block(new AuthenticatedRequest(innerServicePrincipal, request))
 
           case m: MachinePrincipal =>
-            logger.info("Authing machine principal: " + m)
             val instance = instanceOf(request)
             val maybeApiKeyInstance = m.attributes.get(ApiKeyAuthenticationProvider.ApiKeyInstance)
             logger.info("Api key instance: " + maybeApiKeyInstance)
@@ -111,7 +110,6 @@ class Authentication(config: CommonConfig,
             }
 
           case _ =>
-            logger.info("Authing user principal")
             val instance = instanceOf(request)
             principal.attributes.get(ApiKeyAuthenticationProvider.KindeIdKey).map { owner =>
               getMyInstances(owner).flatMap { principalsInstances =>
