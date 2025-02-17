f2a7cdef7e94afa0dbea8d92dda08222ef86b24d
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-02-17 12:32:27 +0000

    Expand definition of window._clientConfig to include every property in

--- kahuna/public/js/window.ts
+++ kahuna/public/js/window.ts
@@ -3,16 +3,44 @@ import { PermissionOption } from "./components/gr-permissions-filter/gr-permissi
 import { Notification } from "./components/gr-notifications-banner/gr-notifications-banner";
 
 declare global {
-    interface Window {
-      _clientConfig: {
-        rootUri: string;
-        telemetryUri: string;
-        featureSwitches: Array<FeatureSwitchData>;
-        interimFilterOptions: Array<PermissionOption>;
-        maybeOrgOwnedValue: string | undefined;
-        announcements: Array<Notification>;
-        usePermissionsFilter: boolean;
-        permissionsDefault?: string | undefined;
-      }
-    }
+  interface Window {
+    _clientConfig: {
+      rootUri: string;
+      telemetryUri: string;
+      featureSwitches: Array<FeatureSwitchData>;
+      interimFilterOptions: Array<PermissionOption>;
+      maybeOrgOwnedValue: string | undefined;
+      announcements: Array<Notification>;
+      usePermissionsFilter: boolean;
+      permissionsDefault?: string | undefined;
+      sessionId: string
+      feedbackFormLink: string
+      fieldAliases: string[];
+      homeLinkHtml: string
+      canDownloadCrop: boolean;
+      showDenySyndicationWarning: boolean;
+      showSendToPhotoSales: boolean;
+      domainMetadataSpecs: string[];
+      recordDownloadAsUsage: boolean;
+      metadataTemplates: string[];
+      additionalNavigationLinks: string[];
+      costFilterLabel: string
+      costFilterChargeable: boolean;
+      restrictDownload: boolean;
+      warningTextHeader: string
+      warningTextHeaderNoRights: string
+      unusableTextHeader: string
+      denySyndicationTextHeader: string
+      enableWarningFlags: boolean;
+      imagePreviewFlagAlertCopy: string
+      imagePreviewFlagWarningCopy: string
+      imagePreviewFlagLeaseAttachedCopy: string
+      useReaper: boolean;
+      usageRightsSummary: true;
+      defaultShouldBlurGraphicImages: true;
+      shouldUploadStraightToBucket: true;
+      maybeUploadLimitInBytes: number;
+      imageTypes: string[];
+    };
   }
+}
