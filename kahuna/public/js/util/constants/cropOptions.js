c916fdc9dda66990b4d6bf7a9e9085082dc5d612
Tom Richards <tom.richards@guardian.co.uk>
2025-03-17 10:44:33 +0000

    add 5:4 as `Landscape NEW`

--- kahuna/public/js/util/constants/cropOptions.js
+++ kahuna/public/js/util/constants/cropOptions.js
@@ -1,8 +1,9 @@
 // `ratioString` is sent to the server, being `undefined` for `freeform` is expected 🙈
 export const landscape = {key: 'landscape', ratio: 5 / 3, ratioString: '5:3'};
+export const landscapeNew = {key: 'landscape NEW', ratio: 5 / 4, ratioString: '5:4'};
 export const portrait = {key: 'portrait', ratio: 4 / 5, ratioString: '4:5'};
 export const video = {key: 'video', ratio: 16 / 9, ratioString: '16:9'};
 export const square = {key: 'square', ratio: 1, ratioString: '1:1'};
 export const freeform = {key: 'freeform', ratio: null};
 
-export const cropOptions = [landscape, portrait, video, square, freeform];
+export const cropOptions = [landscape, landscapeNew, portrait, video, square, freeform];
