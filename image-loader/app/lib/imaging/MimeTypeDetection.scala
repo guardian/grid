48f8a4edc0c3340f575e59438c2e2c1ff370093a
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-05-03 20:38:53 +0100

    Logging.

--- image-loader/app/lib/imaging/MimeTypeDetection.scala
+++ image-loader/app/lib/imaging/MimeTypeDetection.scala
@@ -11,23 +11,28 @@ import org.apache.tika.Tika
 import scala.util.{Failure, Success, Try}
 
 object MimeTypeDetection extends GridLogging {
-  def guessMimeType(file: File)(implicit logMarker: LogMarker): Either[UnsupportedMimeTypeException, MimeType] = Try(usingTika(file)) match {
-    case Success(Tiff) if isDng(file) => Left(new UnsupportedMimeTypeException("image/dng"))
-    case Success(mimeType) => Right(mimeType)
-    case Failure(tikaAttempt: UnsupportedMimeTypeException) => {
-      Try(usingMetadataExtractor(file)) match {
-        case Success(mimeType) => {
-          logger.info(logMarker, s"Using mime type from metadata extractor as tika mime type is unsupported (${tikaAttempt.mimeType})")
-          Right(mimeType)
+  def guessMimeType(file: File)(implicit logMarker: LogMarker): Either[UnsupportedMimeTypeException, MimeType] = {
+    logger.info("Starting guessMimeType")
+    val x = Try(usingTika(file)) match {
+      case Success(Tiff) if isDng(file) => Left(new UnsupportedMimeTypeException("image/dng"))
+      case Success(mimeType) => Right(mimeType)
+      case Failure(tikaAttempt: UnsupportedMimeTypeException) => {
+        Try(usingMetadataExtractor(file)) match {
+          case Success(mimeType) => {
+            logger.info(logMarker, s"Using mime type from metadata extractor as tika mime type is unsupported (${tikaAttempt.mimeType})")
+            Right(mimeType)
+          }
+          case Failure(metadataExtractorAttempt: UnsupportedMimeTypeException) => {
+            logger.warn(logMarker, s"Unsupported mime type: tika was ${tikaAttempt.mimeType}, metadata extractor was ${metadataExtractorAttempt.mimeType}", metadataExtractorAttempt)
+            Left(metadataExtractorAttempt)
+          }
+          case Failure(_: Throwable) => Left(new UnsupportedMimeTypeException(FALLBACK))
         }
-        case Failure(metadataExtractorAttempt: UnsupportedMimeTypeException) => {
-          logger.warn(logMarker, s"Unsupported mime type: tika was ${tikaAttempt.mimeType}, metadata extractor was ${metadataExtractorAttempt.mimeType}", metadataExtractorAttempt)
-          Left(metadataExtractorAttempt)
-        }
-        case Failure(_: Throwable) => Left(new UnsupportedMimeTypeException(FALLBACK))
       }
+      case Failure(_: Throwable) => Left(new UnsupportedMimeTypeException(FALLBACK))
     }
-    case Failure(_: Throwable) => Left(new UnsupportedMimeTypeException(FALLBACK))
+    logger.info("End guessMimeType")
+    x
   }
 
   // DNG files look like Tiff files, but include a DNGVersion tag (0xC612)
