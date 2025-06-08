c185515eb7549263c80f6c150e09c289e1ca0aaf
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-06-08 12:48:01 +0100

    Add types to query-suggestions' dependencies, import, and refactor to remove immutableJS
    which is no longer needed for flatMap etc.

--- kahuna/public/js/search/structured-query/query-suggestions.ts
+++ kahuna/public/js/search/structured-query/query-suggestions.ts
@@ -1,16 +1,19 @@
 import * as angular from "angular";
-import { List, Map } from "immutable";
 
-import { mediaApi } from "../../services/api/media-api";
+import { MediaApi, mediaApi } from "../../services/api/media-api";
+import { EditsApi } from "../../services/api/edits-api";
 
 export const querySuggestions = angular.module("querySuggestions", [
   mediaApi.name
 ]);
 
 export type FieldAlias = {
+  elasticsearchPath: string;
+  label: string;
+  displayInAdditionalMetadata: boolean;
   displaySearchHint: boolean;
   alias: string;
-  searchHintOptions: any;
+  searchHintOptions: string[];
 };
 
 export type Chip = {
@@ -59,28 +62,21 @@ if (window._clientConfig.useReaper === true) {
   isSearch.push("reapable");
 }
 
+type TypeaheadField = {
+  fieldName: string;
+  resolver?: ((value: string) => string[]) | string[];
+  type?: "DATE" | "TEXT";
+};
+
 export type QuerySuggestionsService = {
-  getChipSuggestions: (chip: Chip) => string[],
-  typeaheadFields: (
-    | {
-        fieldName: string;
-        resolver: (value: string) => string[];
-      }
-    | {
-        fieldName: string;
-        resolver?: undefined;
-      }
-    | {
-        fieldName: string;
-        resolver: string[];
-      }
-  )[];
+  getChipSuggestions: (chip: Chip) => string[];
+  typeaheadFields: TypeaheadField[];
 };
 
 querySuggestions.factory("querySuggestions", [
   "mediaApi",
   "editsApi",
-  function (mediaApi: any, editsApi: any): QuerySuggestionsService {
+  function (mediaApi: MediaApi, editsApi: EditsApi): QuerySuggestionsService {
     const fieldAliases = window._clientConfig.fieldAliases
       .filter((fieldAlias: FieldAlias) => fieldAlias.displaySearchHint === true)
       .reduce(
@@ -160,6 +156,8 @@ querySuggestions.factory("querySuggestions", [
       },
       { fieldName: "leasedBy" },
       { fieldName: "is", resolver: isSearch },
+      { fieldName: "dateTaken" } as const,
+      { fieldName: "date" },
       ...Object.keys(fieldAliases).map((fieldName) => ({
         fieldName,
         resolver: fieldAliases.hasOwnProperty(fieldName)
@@ -167,7 +165,14 @@ querySuggestions.factory("querySuggestions", [
               prefixFilter(value)(suggestFieldAliasOptions(fieldName))
           : undefined
       }))
-    ].sort();
+    ].sort((a, b) => {
+      if (a.fieldName > b.fieldName) {
+        return 1;
+      } else if (a.fieldName === b.fieldName) {
+        return 0;
+      }
+      return -1;
+    }) as TypeaheadField[];
 
     const fieldNames = typeaheadFields.map((field) => field.fieldName);
 
@@ -178,88 +183,71 @@ querySuggestions.factory("querySuggestions", [
     }
 
     function listSuppliers() {
-      return editsApi
-        .getUsageRightsCategories()
-        .then(
-          (results: {
-            value: string;
-            properties: { name: string; options: any }[];
-          }) => {
-            return List<typeof results>(results)
-              .filter((res: any) => res.value === "agency")
-              .flatMap((res: any) => res.properties)
-              .filter(
-                (prop: { name: string; options: string }) =>
-                  prop.name === "supplier",
-              )
-              .flatMap((prop: { options: any }) => prop.options)
-              .toJS();
-          },
-        );
+      return editsApi.getUsageRightsCategories().then((results) => {
+        return results
+          .filter((res) => res.value === "agency")
+          .flatMap((res) => res.properties)
+          .filter((prop) => prop.name === "supplier")
+          .flatMap((prop) => prop.options);
+      });
     }
 
     function listCategories() {
       // TODO: would be nice to use user friendly labels and map
       // them to the key internally
-      return editsApi.getUsageRightsCategories().then((results: any) => {
-        return results
-          .map((res: any) => res.value)
-          .filter((key: string) => key !== ""); // no empty category
+      return editsApi.getUsageRightsCategories().then((results) => {
+        return results.map((res) => res.value).filter((key) => key !== ""); // no empty category
       });
     }
 
-    const photographerCategories = List.of(
+    const photographerCategories = [
       "staff-photographer",
-      "contract-photographer",
-    );
+      "contract-photographer"
+    ];
     function listPhotographers() {
-      return editsApi.getUsageRightsCategories().then((results: any) => {
-        return List(results)
-          .filter((res: any) => photographerCategories.includes(res.value))
-          .flatMap((res: any) => res.properties)
-          .filter((prop: { name: string }) => prop.name === "photographer")
-          .flatMap((prop: { optionsMap: any }) =>
-            Map(prop.optionsMap).valueSeq(),
-          )
-          .flatMap((list) => list)
-          .sort()
-          .toJS();
+      return editsApi.getUsageRightsCategories().then((results) => {
+        return results
+          .filter((res) => photographerCategories.includes(res.value))
+          .flatMap((res) => res.properties)
+          .filter((prop) => prop.name === "photographer")
+          .flatMap((prop) => Object.values(prop.optionsMap))
+          .flat()
+          .sort();
       });
     }
 
     function listIllustrators() {
-      return editsApi.getUsageRightsCategories().then((results: any) => {
-        return List(results)
-          .filter((res: any) => res.value === "contract-illustrator")
-          .flatMap((res: any) => res.properties)
-          .filter((prop: { name: string }) => prop.name === "creator")
-          .flatMap((prop: { options: any }) => prop.options)
-          .toJS();
+      return editsApi.getUsageRightsCategories().then((results) => {
+        return results
+          .filter((res) => res.value === "contract-illustrator")
+          .flatMap((res) => res.properties)
+          .filter((prop) => prop.name === "creator")
+          .flatMap((prop) => prop.options);
       });
     }
 
     function suggestCredit(prefix: string) {
       return mediaApi
         .metadataSearch("credit", { q: prefix })
-        .then((results: any) => results.data.map((res: any) => res.key));
+        .then((results) => results.data.map((res) => res.key));
     }
 
     function suggestSource(prefix: string) {
       return mediaApi
         .metadataSearch("source", { q: prefix })
-        .then((results: any) => results.data.map((res: any) => res.key));
+        .then((results) => results.data.map((res) => res.key));
     }
 
     function suggestLabels(prefix: string) {
       return mediaApi
         .labelsSuggest({ q: prefix })
-        .then((labels: any) => labels.data);
+        .then((labels) => labels.data);
     }
 
     function suggestPhotoshoot(prefix: string) {
       return mediaApi
         .metadataSearch("photoshoot", { q: prefix })
-        .then((results: any) => results.data.map((res: any) => res.key));
+        .then((results) => results.data.map((res) => res.key));
     }
 
     function listUsagePlatforms() {
