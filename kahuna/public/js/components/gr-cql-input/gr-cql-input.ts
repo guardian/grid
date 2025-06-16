d124f2b5412771ba0c9723f5d82066044252e588
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-06-16 16:09:18 +0100

    Move query translation into own folder and add tests

--- kahuna/public/js/components/gr-cql-input/gr-cql-input.ts
+++ kahuna/public/js/components/gr-cql-input/gr-cql-input.ts
@@ -3,15 +3,13 @@ import {
   createCqlInput,
   Typeahead,
   TypeaheadField,
-  QueryChangeEventDetail,
-  CqlQuery,
-  CqlBinary,
-  CqlExpr
+  QueryChangeEventDetail
 } from "@guardian/cql";
 import {
   querySuggestions,
   QuerySuggestionsService
 } from "../../search/structured-query/query-suggestions";
+import { cqlQueryToGridQuery, gridQueryToCqlQuery } from "./query-translation";
 
 export const grCqlInput = angular.module("gr.cqlInput", [
   querySuggestions.name
@@ -77,7 +75,10 @@ grCqlInput.directive<
       lang: { operators: false, groups: false }
     });
 
-    customElements.define("cql-input", CqlInput as any);
+    customElements.define(
+      "cql-input",
+      CqlInput as unknown as CustomElementConstructor,
+    );
 
     return {
       restrict: "E",
@@ -88,15 +89,14 @@ grCqlInput.directive<
       template: `<cql-input value="{{fromGridQuery(value)}}"></cql-input>`,
       link: function (scope, element) {
         const cqlInput = element.find("cql-input")[0];
+        scope.fromGridQuery = gridQueryToCqlQuery;
 
-        const addPlusesToGridChips = /([^\s-]+\:)(?<!-[^\s]+\:)/;
-        scope.fromGridQuery = (str: string): string =>
-          str.replace(addPlusesToGridChips, "\+$1");
         cqlInput.addEventListener(
           "queryChange",
           (event: QueryChangeEventDetail) => {
             if (event.detail?.queryAst) {
-              scope.onChange()(gridQueryStrFromQuery(event.detail?.queryAst));
+              const toGrid = cqlQueryToGridQuery(event.detail?.queryAst);
+              scope.onChange()(toGrid);
             }
           },
         );
@@ -104,65 +104,3 @@ grCqlInput.directive<
     };
   }
 ]);
-
-const dateFields = ["from-date", "to-date"];
-const relativeDateRegex = /(?<polarity>[-+])(?<quantity>\d+)(?<unit>[dmyw])/;
-
-const add = (a: number, b: number) => a + b;
-const substract = (a: number, b: number) => a - b;
-
-const parseDateValue = (value: string): string => {
-  const result = relativeDateRegex.exec(value);
-  if (!result) {
-    return value;
-  }
-  const now = new Date();
-  const { polarity, quantity, unit } = result.groups as {
-    polarity: string;
-    quantity: string;
-    unit: string;
-  };
-
-  const op = polarity === "+" ? add : substract;
-
-  const year = op(now.getFullYear(), unit === "y" ? parseInt(quantity) : 0);
-  // Months are zero indexed in Javascript, ha ha ha
-  const month = op(now.getMonth(), unit === "m" ? parseInt(quantity) : 0);
-  const day = op(
-    now.getDate(),
-    unit === "d"
-      ? parseInt(quantity)
-      : unit === "w"
-        ? parseInt(quantity) * 7
-        : 0,
-  );
-  const date = new Date(year, month, day);
-  return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
-};
-
-export const gridQueryStrFromQuery = ({content }: CqlQuery): string =>
-  content ? strFromBinary(content) : "";
-
-const strFromBinary = (binary: CqlBinary): string => {
-  return (
-    strFromExpr(binary.left) +
-    (binary.right
-      ? // Ignore the operator, which can only be OR
-        ` ${strFromBinary(binary.right.binary)}`
-      : "")
-  );
-};
-
-const strFromExpr = (expr: CqlExpr) => {
-  switch (expr.content.type) {
-    case "CqlBinary":
-      return strFromBinary(expr.content);
-    case "CqlStr":
-      return expr.content.searchExpr;
-    case "CqlGroup":
-      return `(${strFromBinary(expr.content.content)})`;
-    case "CqlField":
-      const polarity = expr.content.key.tokenType === "CHIP_KEY_NEGATIVE" ? "-" : "";
-      return expr.content.key.literal ? `${polarity}${expr.content.key.literal}:"${expr.content.value?.literal ?? ""}"` : "";
-  }
-};
