5594ef27048cc12b58c511432938843b2b7aa141
Andrew Nowak <andrew.nowak@guardian.co.uk>
2025-06-18 15:26:58 +0100

    remove preview view

--- kahuna/public/js/search/results.js
+++ kahuna/public/js/search/results.js
@@ -9,9 +9,7 @@ import '../util/rx';
 import '../util/seq';
 import '../util/constants/sendToCapture-config';
 import '../components/gu-lazy-table/gu-lazy-table';
-import '../components/gu-lazy-preview/gu-lazy-preview';
 import '../components/gu-lazy-table-shortcuts/gu-lazy-table-shortcuts';
-import '../components/gu-lazy-preview-shortcuts/gu-lazy-preview-shortcuts';
 import '../components/gr-archiver/gr-archiver';
 import '../components/gr-delete-image/gr-delete-image';
 import '../components/gr-undelete-image/gr-un-delete-image';
@@ -37,8 +35,6 @@ export var results = angular.module('kahuna.search.results', [
     'util.seq',
     'gu.lazyTable',
     'gu.lazyTableShortcuts',
-    'gu.lazyPreview',
-    'gu.lazyPreviewShortcuts',
     'gr.archiver',
     'gr.downloader',
     'gr.batchExportOriginalImages',
@@ -118,9 +114,6 @@ results.controller('SearchResultsCtrl', [
         if (ctrl.image && ctrl.image.data.softDeletedMetadata !== undefined) { ctrl.isDeleted = true; }
         ctrl.newImagesCount = 0;
         ctrl.newImagesLastCheckedMoment = moment();
-
-        // Preview control
-        ctrl.previewView = false;
 
         // Map to track image->position and help remove duplicates
         let imagesPositions;
