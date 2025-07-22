70c1eca05bc454c4bdc6e7955c89da5f27df7500
AndyKilmory <andrew.downing@bbc.co.uk>
2025-07-22 16:37:16 +0100

    Changing behaviour of linked image links. Adding in messaging to Usages tab

---
+++ kahuna/public/js/util/linkedImages.js
@@ -0,0 +1,64 @@
+/* ******************************************
+   Code to indicate if messaging relating to
+   linked images should be displayed
+   ****************************************** */
+
+const DEFAULT_LINKED_MESSAGE = "This image is linked to one or more equivalent images. To see usages for these linked images please use the links on the Metadata tab.";
+const BBC_REPLACES_MESSAGE = "This image replaces a previous version. Super users can view the previous version’s usage by clicking the image ID under ‘Replaces’ in the metadata.";
+const BBC_REPLACED_MESSAGE = "This image has been replaced by a newer version. Super users can view the latest version by clicking the image ID under ‘Replaced by’ in the metadata.";
+
+const default_link_fields = [];
+const bbc_link_fields = [
+  ['data', 'metadata', 'domainMetadata', 'archives', 'supersedes'],
+  ['data', 'metadata', 'domainMetadata', 'archives', 'superseded']
+];
+
+const checkValueAtPath = (obj, path) => {
+    for (let i = 0; i < path.length; i++) {
+        if (obj && obj.hasOwnProperty(path[i])) {
+            obj = obj[path[i]];
+        } else {
+            return false;
+        }
+    }
+    return obj !== undefined && obj !== null;
+};
+
+const checkFields = (image, fields) => {
+  let hasField = false;
+  for(let i = 0; i < fields.length; i++) {
+    if (checkValueAtPath(image, fields[i])) hasField = true;
+  }
+  return hasField;
+};
+
+const message = (image, org_name) => {
+  switch (org_name.toLowerCase()) {
+    case 'bbc':
+      let bbc_msg = "";
+      if (checkValueAtPath(image, bbc_link_fields[0])) {
+        bbc_msg = BBC_REPLACES_MESSAGE;
+      }
+      if (checkValueAtPath(image, bbc_link_fields[1])) {
+        if (bbc_msg !== "") bbc_msg = bbc_msg + "<br/>&nbsp;<br/>"
+        bbc_msg = bbc_msg + BBC_REPLACED_MESSAGE;
+      }
+      return bbc_msg;
+    default:
+      return DEFAULT_LINKED_MESSAGE;
+  }
+};
+
+export const displayLinkedImagesMessage = (image, org_name) => {
+  switch (org_name.toLowerCase()) {
+    case 'bbc':
+      return checkFields(image, bbc_link_fields);
+    default:
+      return checkFields(image, default_link_fields);
+  }
+};
+
+export const linkedImagesMessage = (image, org_name) => {
+  return message(image, org_name);
+};
+
