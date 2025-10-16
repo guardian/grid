17ffac3d1740b95eb3a7151c1f7352e23e62101f
Ellen Muller <ellen.muller@guardian.co.uk>
2025-10-16 13:45:15 +0100

    update script

--- vectorcommand.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-aws s3vectors list-vectors \
-  --vector-bucket-name image-embeddings-dev \
-  --index-name cohere-embed-english-v3 \
-  --profile media-service \
-  --region eu-central-1
-
