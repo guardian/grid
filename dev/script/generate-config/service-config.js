c09a72fe901242ccc5c6e978fef18e808621e84d
Ellen Muller <ellen.muller@guardian.co.uk>
2025-10-22 11:27:11 +0100

    add shouldembed to image loader config script

--- dev/script/generate-config/service-config.js
+++ dev/script/generate-config/service-config.js
@@ -74,6 +74,7 @@ function getImageLoaderConfig(config) {
         |metrics.request.enabled=false
         |transcoded.mime.types="image/tiff"
         |upload.quarantine.enabled=false
+        |s3.vectors.shouldEmbed=false
         |`;
 }
 
