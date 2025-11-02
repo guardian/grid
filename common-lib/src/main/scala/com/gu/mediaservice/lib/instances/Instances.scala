62d05200d728c4a4f0fc4b457de5008b09976f88
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-11-02 11:00:52 +0000

    No not quietly recover from instances call failures.

--- common-lib/src/main/scala/com/gu/mediaservice/lib/instances/Instances.scala
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/instances/Instances.scala
@@ -4,7 +4,7 @@ import com.gu.mediaservice.lib.config.CommonConfig
 import com.gu.mediaservice.model.Instance
 import com.typesafe.scalalogging.StrictLogging
 import play.api.libs.json.{Json, Reads}
-import play.api.libs.ws.WSClient
+import play.api.libs.ws.{WSClient, WSResponse}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -15,27 +15,27 @@ trait Instances extends StrictLogging {
 
   def getInstances()(implicit ec: ExecutionContext): Future[Seq[Instance]] = {
     wsClient.url(config.instancesEndpoint).get().map { r =>
-      r.status match {
-        case 200 =>
-          implicit val ir: Reads[Instance] = Json.reads[Instance]
-          Json.parse(r.body).as[Seq[Instance]]
-        case _ =>
-          logger.warn("Got non 200 status for instances call: " + r.status)
-          Seq.empty
-      }
+      handleInstancesResponse(r)
     }
   }
 
   def getMyInstances(owner: String)(implicit ec: ExecutionContext): Future[Seq[Instance]] = {
     wsClient.url(config.myInstancesEndpoint).withQueryStringParameters("owner" -> owner).get().map { r =>
-      r.status match {
-        case 200 =>
-          implicit val ir: Reads[Instance] = Json.reads[Instance]
-          Json.parse(r.body).as[Seq[Instance]]
-        case _ =>
-          logger.warn("Got non 200 status for instances call: " + r.status)
-          Seq.empty
-      }
+      handleInstancesResponse(r)
+    }
+  }
+
+  private def handleInstancesResponse(r: WSResponse): Seq[Instance] = {
+    r.status match {
+      case 200 =>
+        implicit val ir: Reads[Instance] = Json.reads[Instance]
+        Json.parse(r.body).as[Seq[Instance]]
+      case 404 =>
+        logger.warn("Got 404 status for instances call; returning no permissions")
+        Seq.empty
+      case _ =>
+        logger.error("Got non 200 status for instances call: " + r.status)
+        throw new RuntimeException("Could not load instances")
     }
   }
 
