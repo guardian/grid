19a7fdd45ff9d8d56d0b579c79495e2b7d31677d
Ellen Muller <ellen.muller@guardian.co.uk>
2025-12-03 13:51:29 +0000

    add cdk lambda definition

--- cdk/lib/image-embedder-lambda.ts
+++ cdk/lib/image-embedder-lambda.ts
@@ -1,9 +1,28 @@
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
+import { GuLambdaFunction } from "@guardian/cdk/lib/constructs/lambda";
 import type { App } from "aws-cdk-lib";
+import { aws_lambda as lambda } from "aws-cdk-lib";
+import { Architecture } from "aws-cdk-lib/aws-lambda";
 
 export class ImageEmbedderLambda extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
+
+    const LAMBDA_NODE_VERSION = lambda.Runtime.NODEJS_24_X;
+
+    new GuLambdaFunction(
+      this,
+      'ImageEmbedderHandler',
+      {
+        fileName: 'image-embedder.zip',
+        functionName: `image-embedder-${props.stage}`,
+        runtime: LAMBDA_NODE_VERSION,
+        architecture: Architecture.ARM_64,
+        handler: 'dist.handler',
+        app: 'image-embedder-handler',
+      },
+    );
+
   }
 }
