9f6cd76f05f6c56208c12be0ff0808b027b0a6a0
Ellen Muller <ellen.muller@guardian.co.uk>
2025-12-11 16:30:44 +0000

    add secret for api key

--- cdk/lib/image-embedder-lambda.ts
+++ cdk/lib/image-embedder-lambda.ts
@@ -2,8 +2,11 @@ import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import { GuLambdaFunction } from "@guardian/cdk/lib/constructs/lambda";
 import type { App } from "aws-cdk-lib";
-import { aws_lambda as lambda } from "aws-cdk-lib";
+import { Duration, aws_lambda as lambda } from "aws-cdk-lib";
 import { Architecture } from "aws-cdk-lib/aws-lambda";
+// import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { Queue } from "aws-cdk-lib/aws-sqs";
 
 export class MediaService extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
@@ -11,6 +14,13 @@ export class MediaService extends GuStack {
 
     const LAMBDA_NODE_VERSION = lambda.Runtime.NODEJS_24_X;
 
+    const imageEmbedderApiKey = Secret.fromSecretNameV2(
+			this,
+			'imageEmbedderApiKey',
+			`media-service/${props.stage}/image-embedder-lambda/api-key`,
+		);
+
+    // const imageEmbedderLambda = 
     new GuLambdaFunction(
       this,
       'ImageEmbedderHandler',
@@ -21,8 +31,32 @@ export class MediaService extends GuStack {
         architecture: Architecture.ARM_64,
         handler: 'index.handler',
         app: 'image-embedder-lambda',
+        environment: {
+					"GRID_API_KEY": imageEmbedderApiKey.secretValue.toString()
+				}
       },
     );
+
+    const imageEmbedderDLQ = new Queue(this, 'imageEmbedderDLQ', {
+			queueName: `image-embedder-DLQ-${this.stage}`,
+		});
+
+		// const imageEmbedderQueue = 
+    new Queue(
+			this,
+			'imageEmbedder',
+			{
+				queueName: `image-embedder-${this.stage}`,
+				visibilityTimeout: Duration.seconds(60),
+				deadLetterQueue: {
+					queue: imageEmbedderDLQ,
+					maxReceiveCount: 3,
+				},
+			},
+		);
+		// imageEmbedderLambda.addEventSource(
+		// 	new SqsEventSource(imageEmbedderQueue),
+		// );
 
   }
 }
--- image-embedder-lambda/src/index.ts
+++ image-embedder-lambda/src/index.ts
@@ -6,10 +6,79 @@
 // Whether it's worth it to do that 
 // We can also reuse the lambda for the backfill
 
-import { Context, S3Event } from 'aws-lambda';
+import { Context, SQSEvent, SQSRecord } from 'aws-lambda';
+// import { BedrockRuntimeClient, BedrockRuntimeClientConfig, InvokeModelCommand, InvokeModelCommandOutput } from "@aws-sdk/client-bedrock-runtime";
+// import fetch from "node-fetch";
+// import { GetObjectAclCommand, GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
-export const handler = async (event: S3Event, context: Context) => {
-    console.log('Hello world!');
+// const model: string = "cohere.embed-english-v3";
+
+// const bedrockClient = new BedrockRuntimeClient({
+//     region: "eu-west-1"
+// });
+
+const GRID_API_KEY = process.env.GRID_API_KEY || "";
+
+// async function getImageFromGrid(imageId: string, apiKey: string) {
+
+//     const imageUri = `https://api.media.gutools.co.uk/images/${imageId}`;
+//     console.log(`Fetching metadata for image from ${imageUri}`);
     
-    // Function code
+//     const response = await fetch(imageUri, {
+//         headers: { "X-Gu-Media-Key": apiKey }
+//     });
+//     const imageMetadata = await response.json();
+    
+//     const secureUrl = imageMetadata.data.source.secureUrl;
+//     console.log("Fetching image from secureUrl");
+    
+//     const imageResponse = await fetch(secureUrl);
+//     const imageBytes = await imageResponse.arrayBuffer();
+    
+//     console.log(`The bytes string are: ${imageBytes}`);
+//     return imageBytes;
+// }
+
+// async function getCredentials(s3Client: S3Client) {
+    
+//     const params = {
+//         Bucket: "grid-conf",
+//         Key: `TEST/image-embedder-lambda/conf.json`
+//     };
+
+//     const command = new GetObjectCommand(params)
+    
+//     const data = await s3Client.send(command)
+//     const body = data.Body?.toString("utf8") || "";
+    
+//     return JSON.parse(body);  
+// }
+
+// async function embedImage(inputData: Array<String>): Promise<InvokeModelCommandOutput> {
+//     const body = {
+//         inputType: "image",
+//         embeddingTypes: ["float"],
+//         images: inputData
+//     }
+ 
+//     const input = {
+//         modelId: model,
+//         body: body,
+//         accept: "*/*",
+//         contentType: "application/json",
+//     }
+    
+//     const command = new InvokeModelCommand(input);
+    
+//     const chatCompletion = await bedrockClient.send(command);
+//     return chatCompletion
+// }
+
+export const handler = async (event: SQSEvent, context: Context) => {
+
+    console.log(`Starting handler embedding pipeline`);
+    const records: SQSRecord[] = event.Records;
+    const recordBody = records[0].body
+    console.log(`recordBody: ${recordBody}`);
+    console.log(`secret (DUMMY VALUE ATM OBVS BECAUSE PRINTING): ${GRID_API_KEY}`);
 };
