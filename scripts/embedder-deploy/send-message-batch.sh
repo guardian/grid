3872af94021ad524337bcd1491b02693f2140dd5
Ellen Muller <ellen.muller@guardian.co.uk>
2026-01-28 13:59:27 +0000

    add comment

--- scripts/embedder-deploy/send-message-batch.sh
+++ scripts/embedder-deploy/send-message-batch.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# This script send a batch of duplicate messages and the handler will loop through them. 
+# BUT the write to the S3 Vector Store will fail because we're trying to write duplicate keys! 
+# Right now, this script is just useful for testing the loop behaviour 
+# and perhaps may be useful in the future to test skipping images already embedded (when we add that)
+
 # Check for required arguments
 if [ $# -ne 4 ]; then
   echo "Usage: $0 <imageId> <fileType> <s3Bucket> <s3Key>"
