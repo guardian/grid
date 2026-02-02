ddf1e7ce95416b14fc0827f2f18308c947eba0f3
Joseph Smith <joseph.smith@theguardian.com>
2026-02-02 13:12:16 +0000

    Manually put sharp and its deps into the deploy zip

--- scripts/embedder-deploy/build.sh
+++ scripts/embedder-deploy/build.sh
@@ -6,8 +6,19 @@ set -e
     cd image-embedder-lambda
     npm ci
     # Install sharp's native bindings for the lambda target architecture (linux-arm64)
-    npm install --os=linux --cpu=arm64 sharp
+    npm install --cpu=arm64 --os=linux --libc=glibc sharp
     npm run build
+
+    # Copy sharp and all its dependencies into dist for packaging
+    mkdir -p dist/node_modules/@img
+    cp -r node_modules/sharp dist/node_modules/
+    cp -r node_modules/@img/sharp-linux-arm64 dist/node_modules/@img/
+    cp -r node_modules/@img/sharp-libvips-linux-arm64 dist/node_modules/@img/
+    cp -r node_modules/@img/colour dist/node_modules/@img/
+    # Sharp's JS dependencies
+    cp -r node_modules/detect-libc dist/node_modules/
+    cp -r node_modules/semver dist/node_modules/
+
     cd dist
-    zip -r ../../image-embedder.zip index.js
+    zip -r ../../image-embedder.zip .
 )
