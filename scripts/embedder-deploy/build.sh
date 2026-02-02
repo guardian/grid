ed92fc20db66af65e9415bcdb112d8ba7a3aa3cb
Joseph Smith <joseph.smith@theguardian.com>
2026-02-02 10:20:04 +0000

    Install sharp's native bindings for the lambda target architecture (linux-arm64)

--- scripts/embedder-deploy/build.sh
+++ scripts/embedder-deploy/build.sh
@@ -5,6 +5,8 @@ set -e
 (
     cd image-embedder-lambda
     npm ci
+    # Install sharp's native bindings for the lambda target architecture (linux-arm64)
+    npm install --os=linux --cpu=arm64 sharp
     npm run build
     cd dist
     zip -r ../../image-embedder.zip index.js
