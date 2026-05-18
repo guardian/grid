dd174a4fe5bdf261b2deb34f7916de1748ade55b
lindseydew <lindsey.dew@guardian.co.uk>
2026-05-18 17:55:39 +0100

    Define API endpoint for crop variations

--- media-api/app/controllers/ConfigurationController.scala
+++ media-api/app/controllers/ConfigurationController.scala
@@ -1,9 +1,10 @@
 package controllers
 
 import com.gu.mediaservice.lib.auth.Authentication
+import lib.crops.CropOption
 import play.api.libs.json.Json
 import play.api.mvc.{BaseController, ControllerComponents}
 
 class ConfigurationController(auth: Authentication, override val controllerComponents: ControllerComponents) extends BaseController {
-    def index = auth { _ => Ok(Json.obj("hello" -> "world")) }
+    def cropVariations = auth { _ => Ok(Json.toJson(CropOption.supported)) }
 }
