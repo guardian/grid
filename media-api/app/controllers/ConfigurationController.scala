5bcac47ff35f195d77be29405817cea21b8a1134
lindseydew <lindsey.dew@guardian.co.uk>
2026-05-27 10:50:49 +0100

    Add a comment for the non-authed endpoint

--- media-api/app/controllers/ConfigurationController.scala
+++ media-api/app/controllers/ConfigurationController.scala
@@ -1,10 +1,11 @@
 package controllers
 
-import com.gu.mediaservice.lib.auth.Authentication
 import lib.crops.CropOption
 import play.api.libs.json.Json
 import play.api.mvc.{BaseController, ControllerComponents}
 
 class ConfigurationController(override val controllerComponents: ControllerComponents) extends BaseController {
+    // The contents of this endpoint is not sensitive, so to avoid unnecessary complexity we have not
+    // put authentication on this
     def cropVariations = Action { _ => Ok(Json.toJson(CropOption.supported)) }
 }
