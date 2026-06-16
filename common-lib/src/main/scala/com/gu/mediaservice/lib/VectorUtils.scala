fa7ccd0ba11e6f17372b035034958f76bcf1024a
Joseph Smith <joseph.smith@theguardian.com>
2026-06-16 12:14:08 +0100

    Add VectorUtils

---
+++ common-lib/src/main/scala/com/gu/mediaservice/lib/VectorUtils.scala
@@ -0,0 +1,15 @@
+package com.gu.mediaservice.lib
+
+object VectorUtils {
+  def dotProduct(vectorOne: List[Double], vectorTwo: List[Double]): Double = {
+    vectorOne.zip(vectorTwo).map(component => component._1 * component._2).sum
+  }
+
+  def magnitude(vector: List[Double]): Double = {
+    math.sqrt(vector.map(math.pow(_, 2)).sum)
+  }
+
+  def cosineSimilarity(vectorOne: List[Double], vectorTwo: List[Double]): Double = {
+    dotProduct(vectorOne, vectorTwo) / (magnitude(vectorOne) * magnitude(vectorTwo))
+  }
+}
---
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/VectorUtilsTest.scala
@@ -0,0 +1,43 @@
+package com.gu.mediaservice.lib
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class VectorUtilsTest extends AnyFunSpec with Matchers {
+  it ("should compute the dot product of two vectors") {
+    VectorUtils.dotProduct(List(1.0, 2.0, 3.0), List(4.0, 5.0, 6.0)) shouldBe 32.0
+  }
+
+  it ("should compute a dot product of zero for orthogonal vectors") {
+    VectorUtils.dotProduct(List(1.0, 0.0), List(0.0, 1.0)) shouldBe 0.0
+  }
+
+  it ("should return zero for the dot product of empty vectors") {
+    VectorUtils.dotProduct(List.empty, List.empty) shouldBe 0.0
+  }
+
+  it ("should compute the magnitude of a vector") {
+    VectorUtils.magnitude(List(3.0, 4.0)) shouldBe 5.0
+  }
+
+  it ("should return a magnitude of zero for a zero vector") {
+    VectorUtils.magnitude(List(0.0, 0.0, 0.0)) shouldBe 0.0
+  }
+
+  it ("should return a cosine similarity of 1 for identical vectors") {
+    VectorUtils.cosineSimilarity(List(1.0, 2.0, 3.0), List(1.0, 2.0, 3.0)) shouldBe 1.0 +- 1e-9
+  }
+
+  it ("should return a cosine similarity of 1 for parallel vectors") {
+    VectorUtils.cosineSimilarity(List(1.0, 2.0, 3.0), List(2.0, 4.0, 6.0)) shouldBe 1.0 +- 1e-9
+  }
+
+  it ("should return a cosine similarity of 0 for orthogonal vectors") {
+    VectorUtils.cosineSimilarity(List(1.0, 0.0), List(0.0, 1.0)) shouldBe 0.0 +- 1e-9
+  }
+
+  it ("should return a cosine similarity of -1 for opposite vectors") {
+    VectorUtils.cosineSimilarity(List(1.0, 2.0, 3.0), List(-1.0, -2.0, -3.0)) shouldBe -1.0 +- 1e-9
+  }
+}
