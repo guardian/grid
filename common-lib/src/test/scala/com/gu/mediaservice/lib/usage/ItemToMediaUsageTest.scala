1cc3c8e2461c5b52ccb36ade73033264be3dfdac
lindseydew <lindsey.dew@guardian.co.uk>
2026-07-20 12:45:53 +0100

    Add a test for the v2 version with simple fields

--- common-lib/src/test/scala/com/gu/mediaservice/lib/usage/ItemToMediaUsageTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/usage/ItemToMediaUsageTest.scala
@@ -5,6 +5,8 @@ import com.gu.mediaservice.model.usage.{ChildUsageMetadata, DigitalUsageMetadata
 import org.joda.time.DateTime
 import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import software.amazon.awssdk.enhanced.dynamodb.DefaultAttributeConverterProvider
+import software.amazon.awssdk.enhanced.dynamodb.document.EnhancedDocument
 
 import scala.jdk.CollectionConverters.MapHasAsJava
 import java.math.BigDecimal
@@ -12,56 +14,56 @@ import java.net.URI
 
 class ItemToMediaUsageTest extends AnyFunSuiteLike {
 
+  val lastModifiedMillis = 1700000000000L
+  val dateAddedMillis    = 1700000100000L
+  val dateRemovedMillis  = 1700000200000L
+  val printSizeMap = new java.util.LinkedHashMap[String, java.math.BigDecimal]()
+  printSizeMap.put("x", new BigDecimal(100))
+  printSizeMap.put("y", new BigDecimal(200))
+
+  val printMetadata = Map[String, Any](
+    "sectionName"     -> "News",
+    "issueDate"       -> "2026-07-20T10:00:00.000Z",
+    "pageNumber"      -> new BigDecimal(5),
+    "storyName"       -> "Lead Story",
+    "publicationCode" -> "GND",
+    "publicationName" -> "The Guardian",
+    "layoutId"        -> new BigDecimal(12),
+    "edition"         -> new BigDecimal(1),
+    "size"            -> printSizeMap,
+    "orderedBy"       -> "editor1",
+    "sectionCode"     -> "GND-NEWS",
+    "notes"           -> "Top priority",
+    "source"          -> "Desk"
+  ).asJava
+
+  val digitalMetadata = Map[String, Any](
+    "webUrl"      -> "https://www.theguardian.com/world/2026/jul/20/article",
+    "webTitle"    -> "Breaking News",
+    "sectionId"   -> "world",
+    "composerUrl" -> "https://composer.gutools.co.uk/content/123"
+  ).asJava
+
+  val syndicationMetadata = Map[String, Any](
+    "partnerName"  -> "Reuters",
+    "syndicatedBy" -> "syndication_user"
+  ).asJava
+
+  val frontMetadata = Map[String, Any](
+    "addedBy" -> "front_editor",
+    "front"   -> "uk-news"
+  ).asJava
+
+  val downloadMetadata = Map[String, Any](
+    "downloadedBy" -> "designer123"
+  ).asJava
+
+  val childMetadata = Map[String, Any](
+    "addedBy"      -> "parent_editor",
+    "childMediaId" -> "child-media-999"
+  ).asJava
+
   test("testTransform dynamo v1") {
-
-    val lastModifiedMillis = 1700000000000L
-    val dateAddedMillis    = 1700000100000L
-    val dateRemovedMillis  = 1700000200000L
-    val printSizeMap = new java.util.LinkedHashMap[String, java.math.BigDecimal]()
-    printSizeMap.put("x", new BigDecimal(100))
-    printSizeMap.put("y", new BigDecimal(200))
-
-    val printMetadata = Map[String, Any](
-      "sectionName"     -> "News",
-      "issueDate"       -> "2026-07-20T10:00:00.000Z",
-      "pageNumber"      -> new BigDecimal(5),
-      "storyName"       -> "Lead Story",
-      "publicationCode" -> "GND",
-      "publicationName" -> "The Guardian",
-      "layoutId"        -> new BigDecimal(12),
-      "edition"         -> new BigDecimal(1),
-      "size"            -> printSizeMap,
-      "orderedBy"       -> "editor1",
-      "sectionCode"     -> "GND-NEWS",
-      "notes"           -> "Top priority",
-      "source"          -> "Desk"
-    ).asJava
-
-    val digitalMetadata = Map[String, Any](
-      "webUrl"      -> "https://www.theguardian.com/world/2026/jul/20/article",
-      "webTitle"    -> "Breaking News",
-      "sectionId"   -> "world",
-      "composerUrl" -> "https://composer.gutools.co.uk/content/123"
-    ).asJava
-
-    val syndicationMetadata = Map[String, Any](
-      "partnerName"  -> "Reuters",
-      "syndicatedBy" -> "syndication_user"
-    ).asJava
-
-    val frontMetadata = Map[String, Any](
-      "addedBy" -> "front_editor",
-      "front"   -> "uk-news"
-    ).asJava
-
-    val downloadMetadata = Map[String, Any](
-      "downloadedBy" -> "designer123"
-    ).asJava
-
-    val childMetadata = Map[String, Any](
-      "addedBy"      -> "parent_editor",
-      "childMediaId" -> "child-media-999"
-    ).asJava
 
     val item = new Item()
       .withString("usage_id", "usage-123")
@@ -125,8 +127,41 @@ class ItemToMediaUsageTest extends AnyFunSuiteLike {
     )
   }
 
-  test("testTransform dynamo v2") {
+  test("testTransform dynamo v2 with simple fields") {
+    val enchancedDoc = EnhancedDocument.builder()
+      .attributeConverterProviders(DefaultAttributeConverterProvider.create())
+      .putString("usage_id", "usage-123")
+      .putString("grouping","group-abc")
+      .putString("media_id", "media-789")
+      .putString("usage_type", "print")
+      .putString("media_type", "image")
+      .putString("usage_status", "pending")
+      .putNumber("last_modified", lastModifiedMillis)
+      .putNumber("date_added", dateAddedMillis)
+      .putNumber("date_removed", dateRemovedMillis)
+      .build()
 
+    val mediaUsage = ItemToMediaUsage.transform(enchancedDoc)
+
+
+    mediaUsage shouldEqual MediaUsage(
+      UsageId("usage-123"),
+      "group-abc",
+      "media-789",
+      UsageType("print"),
+      "image",
+      UsageStatus("pending"),
+      None,
+      None,
+      None,
+      None,
+      None,
+      None,
+      new DateTime(lastModifiedMillis),
+      Some(new DateTime(dateAddedMillis)),
+      Some(new DateTime(dateRemovedMillis))
+    )
+    
   }
 
 }
