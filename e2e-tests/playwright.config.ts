dd89d73f32a6a25688dcf7d6d5f682bb78430775
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2026-08-03 14:05:40 +0100

    Remove media-api fixture and docs

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -10,14 +10,6 @@ const testDir = defineBddConfig({
 });
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
