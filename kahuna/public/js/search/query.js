3631c75d7e29c244c598033231c11217ce78aae4
AndyKilmory <andrew.downing@bbc.co.uk>
2026-08-05 12:11:31 +0100

    Further amendments to ensure the checkbox filters are responsive immediately after logo click, but avoid unnecessary resets which were occurring - done through 'disarm function'.

--- kahuna/public/js/search/query.js
+++ kahuna/public/js/search/query.js
@@ -169,6 +169,21 @@ query.controller('SearchQueryCtrl', [
     // see manageDefaultNonFree() below for why this is needed.
     let clearDefaultNonFreeFilterTimeout;
 
+    function disarmDefaultNonFreeFilter() {
+      if (clearDefaultNonFreeFilterTimeout) {
+        $timeout.cancel(clearDefaultNonFreeFilterTimeout);
+        clearDefaultNonFreeFilterTimeout = undefined;
+      }
+      const defaultNonFreeFilter = storage.getJs("defaultNonFreeFilter", true);
+      if (defaultNonFreeFilter && defaultNonFreeFilter.isDefault === true) {
+        storage.setJs(
+          "defaultNonFreeFilter",
+          {isDefault: false, isNonFree: defaultNonFreeFilter.isNonFree},
+          true
+        );
+      }
+    }
+
     function manageDefaultNonFree() {
       const defaultNonFreeFilter = storage.getJs("defaultNonFreeFilter", true);
       if (defaultNonFreeFilter && defaultNonFreeFilter.isDefault === true){
@@ -401,6 +416,7 @@ query.controller('SearchQueryCtrl', [
     }
 
     function selectMyUploads(myUploadsChecked) {
+      disarmDefaultNonFreeFilter();
       ctrl.filterMyUploads = myUploadsChecked;
       syncMyUploadsProps();
       watchSearchChange(ctrl.filter, "selectMyUploads");
@@ -444,6 +460,7 @@ query.controller('SearchQueryCtrl', [
 
     //-permissions filter-
     function updatePermissionsChips (permissionsSel, showChargeable) {
+      disarmDefaultNonFreeFilter();
       ctrl.permissionsProps.selectedOption = permissionsSel;
       ctrl.filter.query = updateFilterChips(permissionsSel, ctrl.filter.query);
       ctrl.filter.nonFree = toNonFreeString(showChargeable);
@@ -451,9 +468,22 @@ query.controller('SearchQueryCtrl', [
     }
 
     function chargeableChange (showChargeable) {
+      disarmDefaultNonFreeFilter();
       ctrl.filter.nonFree = toNonFreeString(showChargeable);
       watchSearchChange(ctrl.filter, "chargeableChange");
     }
+
+    ctrl.onNonFreeCheckboxChange = function() {
+      disarmDefaultNonFreeFilter();
+    };
+
+    ctrl.onUploadedByMeCheckboxChange = function() {
+      disarmDefaultNonFreeFilter();
+    };
+
+    ctrl.onOrgOwnedCheckboxChange = function() {
+      disarmDefaultNonFreeFilter();
+    };
 
     let pfOpts = PermissionsConf.permissionsOptions();
     let defOptVal = PermissionsConf.permissionsDefaultOpt();
