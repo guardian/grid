582f3e653a255b8943128f2d6ff98829666bc5c2
lindseydew <lindsey.dew@guardian.co.uk>
2026-08-06 15:44:24 +0100

    Consolidate log statements

--- scripts/src/main/scala/com/gu/mediaservice/scripts/AlamyCleanUp.scala
+++ scripts/src/main/scala/com/gu/mediaservice/scripts/AlamyCleanUp.scala
@@ -11,18 +11,15 @@ import scala.io.Source
 object AlamyCleanUp extends App {
   val GRIDKEY = sys.env.getOrElse("GRIDKEY", throw new RuntimeException("Must set a GRIDKEY env variable"))
   val STAGE = sys.env.getOrElse("STAGE", throw new RuntimeException("Must set a STAGE env variable"))
-  println(s"Running for stage ${STAGE}")
   val GRIDDOMAIN = if(STAGE == "PROD") "gutools.co.uk" else "test.dev-gutools.co.uk"
-  println(s"Using DOMAIN $GRIDDOMAIN")
+
   val ids = if(STAGE == "PROD") {
-    println("Running with prod data")
     val resource = Source.fromResource("alamy-grid-ids.csv")
     resource.getLines().toList
   } else {
-    println("Running with test data")
     List("3b2a8f1845359b84c805634fada85f008e6be297", "278c8c9801a5c8da10f07297289cc3eb26d16ff7", "fd41ca6b9b4ba27cef603bfd127841f6bf537f90")
   }
-  println(s"Running for ${ids.size} ids")
+  println(s"Running for stage $STAGE with domain $GRIDDOMAIN with ${ids.size} ids")
   val requestBody = BodyPublishers.ofString("""{"data":{"restrictions":"No longer available from Alamy","category":"chargeable"}}""")
 
   val outcomes = ids.zipWithIndex.map({case (id, index) =>
