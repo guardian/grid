c0c3034a7804af770941828413e06cb34ed4e75e
lindseydew <lindsey.dew@guardian.co.uk>
2026-09-02 16:29:17 +0100

    Add form for passing in image id

--- thrall/app/controllers/ImageTakedownController.scala
+++ thrall/app/controllers/ImageTakedownController.scala
@@ -7,9 +7,14 @@ import play.api.mvc.ControllerComponents
 class ImageTakedownController(override val auth: Authentication,
                               override val services: Services, override val controllerComponents: ControllerComponents) extends BaseControllerWithLoginRedirects {
 
-    def index = withLoginRedirect {
-        Ok(views.html.imageTakedown())
+    def index(imageId: Option[String]) = withLoginRedirect { implicit request =>
+        Ok(views.html.imageTakedown(imageId, List("1", "2")))
     }
+
+  def takedownImage = withLoginRedirect { implicit request =>
+    val imageId = request.body.asFormUrlEncoded.flatMap(_.get("imageId").flatMap(_.headOption))
+    Redirect(controllers.routes.ImageTakedownController.index(imageId)).flashing("success" -> "Image takedown request submitted successfully.")
+  }
 
 
 }
