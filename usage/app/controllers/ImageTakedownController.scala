ba8a8fb360d1e0c2f4b611791ff005ce1d15e971
lindseydew <lindsey.dew@guardian.co.uk>
2026-09-02 16:51:35 +0100

    Migrate the component to usages

--- usage/app/controllers/ImageTakedownController.scala
+++ usage/app/controllers/ImageTakedownController.scala
@@ -1,11 +1,14 @@
 package controllers
 
-import com.gu.mediaservice.lib.auth.{Authentication, Authorisation, BaseControllerWithLoginRedirects}
+import com.gu.mediaservice.lib.auth.{Authentication, BaseControllerWithLoginRedirects}
 import com.gu.mediaservice.lib.config.Services
+import lib.LiveContentApi
 import play.api.mvc.ControllerComponents
 
 class ImageTakedownController(override val auth: Authentication,
-                              override val services: Services, override val controllerComponents: ControllerComponents) extends BaseControllerWithLoginRedirects {
+                              override val services: Services,
+                              override val controllerComponents: ControllerComponents
+                             ) extends BaseControllerWithLoginRedirects {
 
     def index(imageId: Option[String]) = withLoginRedirect { implicit request =>
         Ok(views.html.imageTakedown(imageId, List("1", "2")))
@@ -15,6 +18,4 @@ class ImageTakedownController(override val auth: Authentication,
     val imageId = request.body.asFormUrlEncoded.flatMap(_.get("imageId").flatMap(_.headOption))
     Redirect(controllers.routes.ImageTakedownController.index(imageId)).flashing("success" -> "Image takedown request submitted successfully.")
   }
-
-
 }
