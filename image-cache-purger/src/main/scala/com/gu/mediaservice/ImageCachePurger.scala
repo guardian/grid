fdf40aa22d296f1bcad096eb76115d60160d4d01
lindseydew <lindsey.dew@guardian.co.uk>
2026-09-21 14:48:24 +0100

    Print out messages

--- image-cache-purger/src/main/scala/com/gu/mediaservice/ImageCachePurger.scala
+++ image-cache-purger/src/main/scala/com/gu/mediaservice/ImageCachePurger.scala
@@ -3,9 +3,13 @@ package com.gu.mediaservice
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
+import scala.jdk.CollectionConverters._
+
 class ImageCachePurger extends RequestHandler[SQSEvent, String] {
   override def handleRequest(input: SQSEvent, context: Context): String = {
-    input.getRecords.foreach(record => context.getLogger.log(s"Received SQS message: ${record.getBody}").mkString("\n"))
+    input.getRecords.asScala.foreach { record =>
+      context.getLogger.log(s"Received SQS message: ${record.getBody}")
+    }
 
     "Image cache purge requested"
   }
--- image-cache-purger/src/test/scala/com/gu/mediaservice/ImageCachePurgerTest.scala
+++ image-cache-purger/src/test/scala/com/gu/mediaservice/ImageCachePurgerTest.scala
@@ -7,17 +7,27 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.mockito.MockitoSugar
 
+import scala.jdk.CollectionConverters._
+
 class ImageCachePurgerTest extends AnyFunSpec with Matchers with MockitoSugar {
   describe("ImageCachePurger") {
-	it("acknowledges and logs an invocation") {
+	it("logs each SQS record") {
 	  val logger = mock[LambdaLogger]
 	  val context = mock[Context]
 	  when(context.getLogger).thenReturn(logger)
 
-	  val result = new ImageCachePurger().handleRequest(new SQSEvent(), context)
+	  val firstRecord = new SQSEvent.SQSMessage()
+	  firstRecord.setBody("first message")
+	  val secondRecord = new SQSEvent.SQSMessage()
+	  secondRecord.setBody("second message")
+	  val event = new SQSEvent()
+	  event.setRecords(List(firstRecord, secondRecord).asJava)
+
+	  val result = new ImageCachePurger().handleRequest(event, context)
 
 	  result shouldBe "Image cache purge requested"
-	  verify(logger).log("Image cache purge requested")
+	  verify(logger).log("Received SQS message: first message")
+	  verify(logger).log("Received SQS message: second message")
 	}
   }
 }
