f12a5a1ffebee96aaeb5cabf9cd293639b4fd5a3
Lindsey Dew <lindsey.dew@guardian.co.uk>
2026-09-21 15:28:09 +0100

    Use a date time parser for checking dates (#4930)
    * Use a date time parser for checking dates
    
    * Add unit tests

--- common-lib/src/test/scala/com/gu/mediaservice/lib/DateTimeUtilsTest.scala
+++ common-lib/src/test/scala/com/gu/mediaservice/lib/DateTimeUtilsTest.scala
@@ -50,4 +50,74 @@ class DateTimeUtilsTest extends AnyFunSpec with Matchers {
 
     test(nowTime = "11:01:00.001", expectedTime = "12:00:00.000", interval = 1.hour)
   }
+
+  describe("PartialDate.parse") {
+    it("should parse supported dates containing a year") {
+      val inputs = Seq(
+        "13 September, 2020",
+        "13 Sep, 2020",
+        "13 September 2020",
+        "13 Sep 2020"
+      )
+
+      inputs.foreach { input =>
+        PartialDate.parse(input) shouldBe Some(PartialDate(13, 9, Some(2020)))
+      }
+    }
+
+    it("should parse supported dates without a year") {
+      Seq("13 September", "13 Sep").foreach { input =>
+        PartialDate.parse(input) shouldBe Some(PartialDate(13, 9, None))
+      }
+    }
+
+    it("should parse single-digit and leap-year dates and ignore surrounding whitespace") {
+      PartialDate.parse("  1 February 2024  ") shouldBe Some(PartialDate(1, 2, Some(2024)))
+      PartialDate.parse("29 Feb 2024") shouldBe Some(PartialDate(29, 2, Some(2024)))
+    }
+
+    it("should reject invalid or unsupported dates") {
+      Seq(
+        "",
+        "not a date",
+        "September 13 2020",
+        "31 February 2020",
+        "29 February 2023"
+      ).foreach { input =>
+        PartialDate.parse(input) shouldBe None
+      }
+    }
+  }
+
+  describe("PartialDate.matches") {
+    val timestamp = new DateTime(2020, 9, 13, 12, 0)
+
+    it("should match the same date") {
+      PartialDate(13, 9, Some(2020)).matches(timestamp) shouldBe true
+    }
+
+    it("should match dates one day either side within the same month") {
+      PartialDate(12, 9, Some(2020)).matches(timestamp) shouldBe true
+      PartialDate(14, 9, Some(2020)).matches(timestamp) shouldBe true
+    }
+
+    it("should not match dates more than one day apart") {
+      PartialDate(11, 9, Some(2020)).matches(timestamp) shouldBe false
+      PartialDate(15, 9, Some(2020)).matches(timestamp) shouldBe false
+    }
+
+    it("should require the month to match, including across a month boundary") {
+      PartialDate(13, 10, Some(2020)).matches(timestamp) shouldBe false
+      PartialDate(30, 9, Some(2020)).matches(new DateTime(2020, 10, 1, 0, 0)) shouldBe false
+    }
+
+    it("should require the year to match when one is present") {
+      PartialDate(13, 9, Some(2019)).matches(timestamp) shouldBe false
+    }
+
+    it("should ignore the timestamp year when the partial date has no year") {
+      PartialDate(13, 9, None).matches(timestamp) shouldBe true
+      PartialDate(13, 9, None).matches(new DateTime(2026, 9, 13, 12, 0)) shouldBe true
+    }
+  }
 }
