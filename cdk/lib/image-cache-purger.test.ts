c35636665d2643ce08006ff4652fc3fd01800552
lindseydew <lindsey.dew@guardian.co.uk>
2026-09-21 15:21:47 +0100

    Update the arn

--- cdk/lib/image-cache-purger.test.ts
+++ cdk/lib/image-cache-purger.test.ts
@@ -8,7 +8,7 @@ describe('The ImageCachePurger stack', () => {
 		const stack = new ImageCachePurger(app, 'ImageCachePurgerLambda', {
 			stack: 'media-service',
 			stage: 'TEST',
-      queueArn: 'test'
+      queueArn: 'arn:aws:sqs:eu-west-1:563563610310:test'
 		});
 		const template = Template.fromStack(stack);
 		expect(template.toJSON()).toMatchSnapshot();
