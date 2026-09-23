1579d453a7d8318a11d5a5f8245b83ed0829cb70
lindseydew <lindsey.dew@guardian.co.uk>
2026-09-23 21:19:15 +0100

    Log out secret

--- image-cache-purger/src/main/scala/com/gu/mediaservice/FastlyApiKeyProvider.scala
+++ image-cache-purger/src/main/scala/com/gu/mediaservice/FastlyApiKeyProvider.scala
@@ -3,6 +3,8 @@ package com.gu.mediaservice
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest
 
+import java.util.logging.Logger
+
 private[mediaservice] trait FastlyApiKeyProvider {
   def apiKey: String
 }
@@ -13,12 +15,17 @@ private[mediaservice] class SecretsManagerFastlyApiKeyProvider(
   client: SecretsManagerClient,
   secretId: String
 ) extends FastlyApiKeyProvider {
+  private val logger = Logger.getLogger(classOf[SecretsManagerFastlyApiKeyProvider].getName)
   override lazy val apiKey: String = {
+
+    logger.info(s"Loading Fastly API key from Secrets Manager secret $secretId...")
     val request = GetSecretValueRequest.builder().secretId(secretId).build()
+
     val secret = Option(client.getSecretValue(request).secretString())
       .filter(_.trim.nonEmpty)
       .getOrElse(throw new IllegalStateException(s"Secret $secretId has no SecretString value"))
 
+    logger.info(secret)
     secret
   }
 }
@@ -35,6 +42,7 @@ private[mediaservice] object FastlyApiKeyProvider {
     val secretId = environment.get(SecretIdEnvironmentVariable)
       .filter(_.trim.nonEmpty)
       .getOrElse(throw new IllegalStateException(s"$SecretIdEnvironmentVariable is not set"))
+
 
     new SecretsManagerFastlyApiKeyProvider(client, secretId)
   }
