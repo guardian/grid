d3fc8cf2c33df390f33047ee43a3f8e5b5fbf534
lindseydew <lindsey.dew@guardian.co.uk>
2026-09-23 10:11:10 +0100

    Remove tests

--- cdk/lib/image-cache-purger.test.ts
+++ cdk/lib/image-cache-purger.test.ts
@@ -1,5 +1,5 @@
 import { App } from 'aws-cdk-lib';
-import { Match, Template } from 'aws-cdk-lib/assertions';
+import { Template } from 'aws-cdk-lib/assertions';
 import { ImageCachePurger } from './image-cache-purger';
 
 describe('The ImageCachePurger stack', () => {
@@ -13,30 +13,6 @@ describe('The ImageCachePurger stack', () => {
 		});
 		return Template.fromStack(stack);
 	};
-
-	it('passes the secret ARN to the Lambda and grants permission to read it', () => {
-		const template = createTemplate();
-
-		template.hasResourceProperties('AWS::Lambda::Function', {
-			Environment: {
-				Variables: {
-					FASTLY_API_KEY_SECRET_ID: Match.anyValue(),
-					FASTLY_IMAGE_BASE_URL: 'https://media.test.dev-guim.co.uk',
-				},
-			},
-		});
-		template.hasResourceProperties('AWS::IAM::Policy', {
-			PolicyDocument: {
-				Statement: Match.arrayWith([
-					Match.objectLike({
-						Action: 'secretsmanager:GetSecretValue',
-						Effect: 'Allow',
-					}),
-				]),
-			},
-		});
-	});
-
 	it('matches the snapshot', () => {
 		expect(createTemplate().toJSON()).toMatchSnapshot();
 	});
