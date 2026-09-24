6eff8b313b8cd604fd87e5c69dc865ea55436c48
lindseydew <lindsey.dew@guardian.co.uk>
2026-09-24 11:19:41 +0100

    Update the logging

--- image-cache-purger/src/main/scala/com/gu/mediaservice/ImageCachePurger.scala
+++ image-cache-purger/src/main/scala/com/gu/mediaservice/ImageCachePurger.scala
@@ -16,9 +16,6 @@ class ImageCachePurgerHandler extends RequestHandler[SQSEvent, String] {
   val imageCachePurger = new ImageCachePurger(new FastlyPurger(FastlyApiKeyProvider.default, sys.env.getOrElse("STAGE", "DEV")))
 
   private val logger = Logger.getLogger(classOf[ImageCachePurger].getName)
-  logger.info("ImageCachePurgerHandler initialized")
-  logger.info(FastlyApiKeyProvider.default.apiKey)
-  logger.info("ImageCachePurgerHandler initialized")
   override def handleRequest(input: SQSEvent, context: Context): String = {
     imageCachePurger.handleRecord(input).fold(
       exception => {
